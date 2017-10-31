b5d670ca40d2d23a53f0b86e4a277755cd20a425
Nicolas DUBIEN <github@dubien.org>
2017-10-31 03:46:58 +0100

    fix typo in test name

--- test/random/generator/LinearCongruencial.spec.ts
+++ test/random/generator/LinearCongruencial.spec.ts
@@ -7,7 +7,7 @@ function rng_for(seed: number): RandomGenerator {
     return new LinearCongruential(seed);
 }
 
-describe("MersenneTwister", () => {
+describe("LinearCongruential", () => {
     it("Should return the same sequence given same seeds", () => jsc.assert(p.sameSeedSameSequences(rng_for)));
     it("Should return the same sequence if called twice", () => jsc.assert(p.sameSequencesIfCallTwice(rng_for)));
     it("Should generate values between -2**31 and 2**31 -1", () => jsc.assert(p.valuesInRange(rng_for)));
