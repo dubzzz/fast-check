9010ce996f92c9cb440fc4d39bcd37acecdbd408
Nicolas DUBIEN <github@dubien.org>
2017-11-05 22:17:44 +0100

    add tests for tuple arbitrary

--- test/check/arbitrary/TupleArbitrary.spec.ts
+++ test/check/arbitrary/TupleArbitrary.spec.ts
@@ -17,30 +17,64 @@ function dummy(id: number) {
     return new DummyArbitrary(id);
 }
 
-describe("TupleArbitrary", () => {
+function propertySameTupleForSameSeed(...arbs: DummyArbitrary[]) {
+    if (arbs.length === 0 || arbs.length > 9) {
+        throw "Unexpected length should be between 1 and 9";
+    }
+    const arb = tuple(arbs[0], ...arbs.slice(1));
+    return jsc.forall(jsc.integer, (seed) => {
+        const mrng1 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
+        const mrng2 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
+        const g1 = arb.generate(mrng1);
+        assert.ok(g1.every((v, idx) => v.startsWith(`key${arbs[idx].id}_`)));
+        assert.deepEqual(arb.generate(mrng2), g1);
+        return true;
+    });
+}
+
+describe('TupleArbitrary', () => {
     describe('tuple', () => {
         it('Should generate the same tuple1 with the same random', () => jsc.assert(
-            jsc.forall(jsc.integer, (seed) => {
-                const mrng1 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
-                const mrng2 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
-                const arb = tuple(dummy(1));
-                const g1 = arb.generate(mrng1);
-                assert.ok(g1[0].indexOf("key1_") === 0);
-                assert.deepEqual(arb.generate(mrng2), g1);
-                return true;
-            })
+            propertySameTupleForSameSeed(dummy(1))
         ));
         it('Should generate the same tuple2 with the same random', () => jsc.assert(
-            jsc.forall(jsc.integer, (seed) => {
-                const mrng1 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
-                const mrng2 = new MutableRandomGenerator(new DummyRandomGenerator(seed));
-                const arb = tuple(dummy(1), dummy(2));
-                const g1 = arb.generate(mrng1);
-                assert.ok(g1[0].indexOf("key1_") === 0);
-                assert.ok(g1[1].indexOf("key2_") === 0);
-                assert.deepEqual(arb.generate(mrng2), g1);
-                return true;
-            })
+            propertySameTupleForSameSeed(dummy(1), dummy(2))
+        ));
+        it('Should generate the same tuple3 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(dummy(1), dummy(2), dummy(3))
+        ));
+        it('Should generate the same tuple4 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12))
+        ));
+        it('Should generate the same tuple5 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12))
+        ));
+        it('Should generate the same tuple6 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12), dummy(13))
+        ));
+        it('Should generate the same tuple7 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12), dummy(13),
+                dummy(21))
+        ));
+        it('Should generate the same tuple8 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12), dummy(13),
+                dummy(21), dummy(22))
+        ));
+        it('Should generate the same tuple9 with the same random', () => jsc.assert(
+            propertySameTupleForSameSeed(
+                dummy(1), dummy(2), dummy(3),
+                dummy(11), dummy(12), dummy(13),
+                dummy(21), dummy(22), dummy(23))
         ));
     });
 });
