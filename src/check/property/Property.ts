e587fbba82ea432a8e6f5f983af10f45de94c9c2
Nicolas DUBIEN <github@dubien.org>
2017-11-12 20:44:20 +0100

    add tests for Property

--- src/check/property/Property.ts
+++ src/check/property/Property.ts
@@ -3,28 +3,34 @@ import { tuple } from '../arbitrary/TupleArbitrary'
 import MutableRandomGenerator from '../../random/generator/MutableRandomGenerator'
 
 class Property<Ts> {
-    constructor(readonly arb: Arbitrary<Ts>, readonly predicate: (t:Ts) => (boolean | undefined)) {}
+    constructor(readonly arb: Arbitrary<Ts>, readonly predicate: (t:Ts) => (boolean|void)) {}
     run(mrng: MutableRandomGenerator): boolean {
-        return !!this.predicate(this.arb.generate(mrng));
+        try {
+            const output = this.predicate(this.arb.generate(mrng));
+            return output == null ? true : !!output;
+        }
+        catch (err) {
+            return false;
+        }
     }
 }
 
 function property<T1>(
     arb1: Arbitrary<T1>,
-    predicate: (t1:T1) => (boolean|undefined)
+    predicate: (t1:T1) => (boolean|void)
 ): Property<[T1]>;
 
 function property<T1,T2>(
     arb1: Arbitrary<T1>,
     arb2: Arbitrary<T2>,
-    predicate: (t1:T1,t2:T2) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2) => (boolean|void)
 ): Property<[T1,T2]>;
 
 function property<T1,T2,T3>(
     arb1: Arbitrary<T1>,
     arb2: Arbitrary<T2>,
     arb3: Arbitrary<T3>,
-    predicate: (t1:T1,t2:T2,t3:T3) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3) => (boolean|void)
 ): Property<[T1,T2,T3]>;
 
 function property<T1,T2,T3,T4>(
@@ -32,7 +38,7 @@ function property<T1,T2,T3,T4>(
     arb2: Arbitrary<T2>,
     arb3: Arbitrary<T3>,
     arb4: Arbitrary<T4>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|void)
 ): Property<[T1,T2,T3,T4]>;
 
 function property<T1,T2,T3,T4,T5>(
@@ -41,7 +47,7 @@ function property<T1,T2,T3,T4,T5>(
     arb3: Arbitrary<T3>,
     arb4: Arbitrary<T4>,
     arb5: Arbitrary<T5>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|void)
 ): Property<[T1,T2,T3,T4,T5]>;
 
 function property<T1,T2,T3,T4,T5,T6>(
@@ -51,7 +57,7 @@ function property<T1,T2,T3,T4,T5,T6>(
     arb4: Arbitrary<T4>,
     arb5: Arbitrary<T5>,
     arb6: Arbitrary<T6>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|void)
 ): Property<[T1,T2,T3,T4,T5,T6]>;
 
 function property<T1,T2,T3,T4,T5,T6,T7>(
@@ -62,7 +68,7 @@ function property<T1,T2,T3,T4,T5,T6,T7>(
     arb5: Arbitrary<T5>,
     arb6: Arbitrary<T6>,
     arb7: Arbitrary<T7>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|void)
 ): Property<[T1,T2,T3,T4,T5,T6,T7]>;
 
 function property<T1,T2,T3,T4,T5,T6,T7,T8>(
@@ -74,7 +80,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8>(
     arb6: Arbitrary<T6>,
     arb7: Arbitrary<T7>,
     arb8: Arbitrary<T8>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|void)
 ): Property<[T1,T2,T3,T4,T5,T6,T7,T8]>;
 
 function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
@@ -87,23 +93,23 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
     arb7: Arbitrary<T7>,
     arb8: Arbitrary<T8>,
     arb9: Arbitrary<T9>,
-    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|undefined)
+    predicate: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|void)
 ): Property<[T1,T2,T3,T4,T5,T6,T7,T8,T9]>;
 
 function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
     arb1: Arbitrary<T1>,
-    arb2: Arbitrary<T2> | ((t1:T1) => (boolean|undefined)),
-    arb3?: Arbitrary<T3> | ((t1:T1,t2:T2) => (boolean|undefined)),
-    arb4?: Arbitrary<T4> | ((t1:T1,t2:T2,t3:T3) => (boolean|undefined)),
-    arb5?: Arbitrary<T5> | ((t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|undefined)),
-    arb6?: Arbitrary<T6> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|undefined)),
-    arb7?: Arbitrary<T7> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|undefined)),
-    arb8?: Arbitrary<T8> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|undefined | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|undefined)))),
-    arb9?: Arbitrary<T9> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|undefined)),
-    predicate?: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|undefined)
+    arb2: Arbitrary<T2> | ((t1:T1) => (boolean|void)),
+    arb3?: Arbitrary<T3> | ((t1:T1,t2:T2) => (boolean|void)),
+    arb4?: Arbitrary<T4> | ((t1:T1,t2:T2,t3:T3) => (boolean|void)),
+    arb5?: Arbitrary<T5> | ((t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|void)),
+    arb6?: Arbitrary<T6> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|void)),
+    arb7?: Arbitrary<T7> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|void)),
+    arb8?: Arbitrary<T8> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|void)),
+    arb9?: Arbitrary<T9> | ((t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|void)),
+    predicate?: (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|void)
 ) {
     if (predicate) {
-        const p = predicate as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|undefined);
+        const p = predicate as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8,t9:T9) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -117,7 +123,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8]));
     }
     if (arb9) {
-        const p = arb9 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|undefined);
+        const p = arb9 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7,t8:T8) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -130,7 +136,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7]));
     }
     if (arb8) {
-        const p = arb8 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|undefined);
+        const p = arb8 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6,t7:T7) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -142,7 +148,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3],t[4],t[5],t[6]));
     }
     if (arb7) {
-        const p = arb7 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|undefined);
+        const p = arb7 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5,t6:T6) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -153,7 +159,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3],t[4],t[5]));
     }
     if (arb6) {
-        const p = arb6 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|undefined);
+        const p = arb6 as (t1:T1,t2:T2,t3:T3,t4:T4,t5:T5) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -163,7 +169,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3],t[4]));
     }
     if (arb5) {
-        const p = arb5 as (t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|undefined);
+        const p = arb5 as (t1:T1,t2:T2,t3:T3,t4:T4) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -172,7 +178,7 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2],t[3]));
     }
     if (arb4) {
-        const p = arb4 as (t1:T1,t2:T2,t3:T3) => (boolean|undefined);
+        const p = arb4 as (t1:T1,t2:T2,t3:T3) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>,
@@ -180,13 +186,13 @@ function property<T1,T2,T3,T4,T5,T6,T7,T8,T9>(
         ), t => p(t[0],t[1],t[2]));
     }
     if (arb3) {
-        const p = arb3 as (t1:T1,t2:T2) => (boolean|undefined);
+        const p = arb3 as (t1:T1,t2:T2) => (boolean|void);
         return new Property(tuple(
             arb1 as Arbitrary<T1>,
             arb2 as Arbitrary<T2>
         ), t => p(t[0],t[1]));
     }
-    const p = arb2 as (t1:T1) => (boolean|undefined);
+    const p = arb2 as (t1:T1) => (boolean|void);
     return new Property(tuple(arb1), t => p(t[0]));
 }
 
---
+++ test/check/property/Property.spec.ts
@@ -0,0 +1,88 @@
+import * as assert from 'power-assert';
+import RandomGenerator from '../../../src/random/generator/RandomGenerator';
+import MutableRandomGenerator from '../../../src/random/generator/MutableRandomGenerator';
+import Arbitrary from '../../../src/check/arbitrary/Arbitrary';
+import { property } from '../../../src/check/property/Property';
+import * as jsc from 'jsverify';
+
+class NoCallGenerator implements RandomGenerator {
+    next(): [number, RandomGenerator] {
+        throw new Error("Method not implemented.");
+    }
+    min(): number {
+        throw new Error("Method not implemented.");
+    }
+    max(): number {
+        throw new Error("Method not implemented.");
+    }
+}
+function generator(): MutableRandomGenerator {
+    return new MutableRandomGenerator(new NoCallGenerator());
+}
+
+class SingleUseArbitrary<T> implements Arbitrary<T> {
+    called_once: boolean = false;
+    constructor(public id: T) {
+    }
+    generate(mrng: MutableRandomGenerator) {
+        if (this.called_once) {
+            throw "Arbitrary has already been called once";
+        }
+        this.called_once = true;
+        return this.id;
+    }
+}
+function single<T>(id: T) {
+    return new SingleUseArbitrary<T>(id);
+}
+
+describe('Property', () => {
+    it('Should fail if predicate fails', () => {
+        const p = property(single(8), (arg: number) => {
+            return false;
+        });
+        assert.equal(p.run(generator()), false, 'Property should fail');
+    });
+    it('Should fail if predicate throws', () => {
+        const p = property(single(8), (arg: number) => {
+            throw 'predicate throws';
+        });
+        assert.equal(p.run(generator()), false, 'Property should fail');
+    });
+    it('Should fail if predicate fails on asserts', () => {
+        const p = property(single(8), (arg: number) => {
+            assert.ok(false);
+        });
+        assert.equal(p.run(generator()), false, 'Property should fail');
+    });
+    it('Should succeed if predicate is true', () => {
+        const p = property(single(8), (arg: number) => {
+            return true;
+        });
+        assert.ok(p.run(generator()), 'Property should succeed');
+    });
+    it('Should succeeds if predicate does not return anything', () => {
+        const p = property(single(8), (arg: number) => {});
+        assert.ok(p.run(generator()), 'Property should succeed');
+    });
+    it('Should call and forward arbitraries one time', () => {
+        let one_call_to_predicate = false;
+        const arbs: [SingleUseArbitrary<number>, SingleUseArbitrary<string>, SingleUseArbitrary<string>] = [single(3), single("hello"), single("world")];
+        const p = property(arbs[0], arbs[1], arbs[2], (arg1: number, arb2: string, arg3: string) => {
+            if (one_call_to_predicate) {
+                throw 'Predicate has already been evaluated once';
+            }
+            one_call_to_predicate = true;
+            return arg1 === arbs[0].id;
+        });
+        assert.equal(one_call_to_predicate, false, 'The creation of a property should not trigger call to predicate');
+        for (let idx = 0 ; idx !== arbs.length ; ++idx) {
+            assert.equal(arbs[idx].called_once, false, `The creation of a property should not trigger call to generator #${idx+1}`);
+        }
+        assert.ok(p.run(generator()), 'Predicate should receive the right arguments');
+        assert.ok(one_call_to_predicate, 'Predicate should have been called by run');
+        for (let idx = 0 ; idx !== arbs.length ; ++idx) {
+            assert.ok(arbs[idx].called_once, `Generator #${idx+1} should have been called by run`);
+        }
+    });
+});
