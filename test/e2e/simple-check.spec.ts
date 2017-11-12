28cd7fe133aa823afc498dd66d9e8ae85475d9f5
Nicolas DUBIEN <github@dubien.org>
2017-11-13 00:57:38 +0100

    add e2e tests for integers and tuples

--- test/e2e/simple-check.spec.ts
+++ test/e2e/simple-check.spec.ts
@@ -4,10 +4,22 @@ import * as sc from '../../src/check/simple-check'
 const seed = Date.now();
 describe(`simple-check (seed: ${seed})`, () => {
     describe('IntegerArbitrary', () => {
-        it('Should fail and shrink to value = 100', () => {
+        it('Should shrink natural number', () => {
             const out = sc.check(sc.property(sc.nat(), (v: number) => v < 100), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
             assert.deepEqual(out.counterexample, [100], 'Should shrink to counterexample 100');
+        });
+        it('Should shrink integer with strictly negative range', () => {
+            const out = sc.check(sc.property(sc.integer(-1000, -10), (v: number) => v > -100), {seed: seed});
+            assert.ok(out.failed, 'Should have failed');
+            assert.deepEqual(out.counterexample, [-100], 'Should shrink to counterexample -100');
+        });
+    });
+    describe('TupleArbitrary', () => {
+        it('Should shrink on tuple2', () => {
+            const out = sc.check(sc.property(sc.nat(), sc.nat(), (v1: number, v2: number) => v1 < 100 || v2 < 50), {seed: seed});
+            assert.ok(out.failed, 'Should have failed');
+            assert.deepEqual(out.counterexample, [100,50], 'Should shrink to counterexample [100,50]');
         });
     });
 });
