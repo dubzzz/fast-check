cadc7bc4e665987edce9c47e0dacfba517ba709e
Nicolas DUBIEN <github@dubien.org>
2017-12-23 00:45:47 +0100

    Add end-to-end tests on base64 strings

--- src/check/simple-check.ts
+++ src/check/simple-check.ts
@@ -3,11 +3,16 @@ import { property } from './property/Property'
 
 import Arbitrary from './arbitrary/Arbitrary'
 import { array } from './arbitrary/ArrayArbitrary'
+import { char, ascii, unicode, hexa, base64 } from './arbitrary/CharacterArbitrary'
 import { integer, nat } from './arbitrary/IntegerArbitrary'
+import { lorem } from './arbitrary/LoremArbitrary'
+import { string, asciiString, unicodeString, hexaString, base64String } from './Arbitrary/StringArbitrary'
 import { tuple } from './arbitrary/TupleArbitrary'
 
 export {
     check, assert,
     property,
-    Arbitrary, array, integer, nat, tuple
+    Arbitrary, array, integer, nat, tuple,
+    char, ascii, unicode, hexa, base64,
+    string, asciiString, unicodeString, hexaString, base64String, lorem
 };
--- test/e2e/simple-check.spec.ts
+++ test/e2e/simple-check.spec.ts
@@ -44,4 +44,21 @@ describe(`simple-check (seed: ${seed})`, () => {
             assert.deepEqual(out.counterexample, [[100,50]], 'Should shrink to counterexample [100,50]');
         });
     });
+    describe('CharacterArbitrary', () => {
+        it('Should shrink on base64 containing no equal signs', () => {
+            const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w*$/.exec(s) == null), {seed: seed});
+            assert.ok(out.failed, 'Should have failed');
+            assert.equal(out.counterexample, "", 'Should shrink to counterexample ""');
+        });
+        it('Should shrink on base64 containing one equal signs', () => {
+            const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w+=$/.exec(s) == null), {seed: seed});
+            assert.ok(out.failed, 'Should have failed');
+            assert.equal(out.counterexample, "AAA=", 'Should shrink to counterexample "AAA="');
+        });
+        it('Should shrink on base64 containing two equal signs', () => {
+            const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w+==$/.exec(s) == null), {seed: seed});
+            assert.ok(out.failed, 'Should have failed');
+            assert.equal(out.counterexample, "AA==", 'Should shrink to counterexample "AA=="');
+        });
+    });
 });
