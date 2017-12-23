6dd16766e1fa2224c9354ac4ad7d096a564ab916
Nicolas DUBIEN <github@dubien.org>
2017-12-23 02:01:56 +0100

    Add e2e tests

--- test/e2e/arbitraries/IntegerArbitrary.spec.ts
+++ test/e2e/arbitraries/IntegerArbitrary.spec.ts
@@ -4,6 +4,10 @@ import * as sc from '../../../src/check/simple-check'
 const seed = Date.now();
 describe(`IntegerArbitrary (seed: ${seed})`, () => {
     describe('integer', () => {
+        it('Should generate integer within the range', () => {
+            const out = sc.check(sc.property(sc.integer(-42, -10), (v: number) => -42 <= v && v <= -10), {seed: seed});
+            assert.ok(!out.failed, 'Should have succeeded');
+        });
         it('Should shrink integer with strictly negative range', () => {
             const out = sc.check(sc.property(sc.integer(-1000, -10), (v: number) => v > -100), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
@@ -11,6 +15,10 @@ describe(`IntegerArbitrary (seed: ${seed})`, () => {
         });
     });
     describe('nat', () => {
+        it('Should generate natural numbers', () => {
+            const out = sc.check(sc.property(sc.nat(), (v: number) => v >= 0), {seed: seed});
+            assert.ok(!out.failed, 'Should have succeeded');
+        });
         it('Should shrink natural number', () => {
             const out = sc.check(sc.property(sc.nat(), (v: number) => v < 100), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
--- test/e2e/arbitraries/StringArbitrary.spec.ts
+++ test/e2e/arbitraries/StringArbitrary.spec.ts
@@ -7,17 +7,17 @@ describe(`StringArbitrary (seed: ${seed})`, () => {
         it('Should shrink on base64 containing no equal signs', () => {
             const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w*$/.exec(s) == null), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
-            assert.equal(out.counterexample, "", 'Should shrink to counterexample ""');
+            assert.deepEqual(out.counterexample, [""], 'Should shrink to counterexample ""');
         });
         it('Should shrink on base64 containing one equal signs', () => {
             const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w+=$/.exec(s) == null), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
-            assert.equal(out.counterexample, "AAA=", 'Should shrink to counterexample "AAA="');
+            assert.deepEqual(out.counterexample, ["AAA="], 'Should shrink to counterexample "AAA="');
         });
         it('Should shrink on base64 containing two equal signs', () => {
             const out = sc.check(sc.property(sc.base64String(), (s:string) => /^\w+==$/.exec(s) == null), {seed: seed});
             assert.ok(out.failed, 'Should have failed');
-            assert.equal(out.counterexample, "AA==", 'Should shrink to counterexample "AA=="');
+            assert.deepEqual(out.counterexample, ["AA=="], 'Should shrink to counterexample "AA=="');
         });
     });
 });
