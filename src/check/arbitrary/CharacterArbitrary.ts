8b8d40a443e0e3c71e179497c5fb45d18be562e1
Nicolas DUBIEN <github@dubien.org>
2017-12-24 03:40:43 +0100

    Define CharacterArbitrary as map on integer

--- src/check/arbitrary/CharacterArbitrary.ts
+++ src/check/arbitrary/CharacterArbitrary.ts
@@ -4,24 +4,13 @@ import { integer } from './IntegerArbitrary'
 import MutableRandomGenerator from '../../random/generator/MutableRandomGenerator'
 import { Stream, stream } from '../../stream/Stream'
 
-class CharacterArbitrary extends Arbitrary<string> {
-    readonly arb: Arbitrary<number>;
-    constructor(min: number, max: number, readonly mapToCode: (v: number) => number = v => v) {
-        super();
-        this.arb = integer(min, max);
-    }
-    private mapper(n: number): string {
-        return String.fromCharCode(this.mapToCode(n));
-    }
-    generate(mrng: MutableRandomGenerator): Shrinkable<string> {
-        return this.arb.generate(mrng)
-                .map(n => this.mapper(n));
-    }
+function CharacterArbitrary(min: number, max: number, mapToCode: (v: number) => number = v => v) {
+    return integer(min, max).map(n => String.fromCharCode(mapToCode(n)));
 }
 
 function char(): Arbitrary<string> {
     // Only printable characters: https://www.ascii-code.com/
-    return new CharacterArbitrary(0x20, 0x7e);
+    return CharacterArbitrary(0x20, 0x7e);
 }
 function hexa(): Arbitrary<string> {
     function mapper(v: number) {
@@ -29,7 +18,7 @@ function hexa(): Arbitrary<string> {
             ? v + 48      // 0-9
             : v + 97 -10; // a-f
     }
-    return new CharacterArbitrary(0, 15, mapper);
+    return CharacterArbitrary(0, 15, mapper);
 }
 function base64(): Arbitrary<string> {
     function mapper(v: number) {
@@ -38,14 +27,14 @@ function base64(): Arbitrary<string> {
         if (v < 62) return v + 48 -52; // 0-9
         return v === 62 ? 43 : 47;     // +/
     }
-    return new CharacterArbitrary(0, 63, mapper);
+    return CharacterArbitrary(0, 63, mapper);
 }
 
 function ascii(): Arbitrary<string> {
-    return new CharacterArbitrary(0x00, 0x7f);
+    return CharacterArbitrary(0x00, 0x7f);
 }
 function unicode(): Arbitrary<string> {
-    return new CharacterArbitrary(0x0000, 0xffff);
+    return CharacterArbitrary(0x0000, 0xffff);
 }
 
 export { char, ascii, unicode, hexa, base64 };
