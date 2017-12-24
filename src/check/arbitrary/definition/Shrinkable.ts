d92b009371e72730b5acbea064078e87645ed205
Nicolas DUBIEN <github@dubien.org>
2017-12-24 03:16:26 +0100

    Remove unnecessary comment

--- src/check/arbitrary/definition/Shrinkable.ts
+++ src/check/arbitrary/definition/Shrinkable.ts
@@ -2,7 +2,7 @@ import Stream from '../../../stream/Stream'
 
 export default class Shrinkable<T> {
     constructor(readonly value: T, readonly shrink: () => Stream<Shrinkable<T>> = () => Stream.nil<Shrinkable<T>>()) {
-    }//need to save the stream content to replay it
+    }
     map<U>(mapper: (t: T) => U): Shrinkable<U> {
         return new Shrinkable<U>(
             mapper(this.value),
