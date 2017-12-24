2c59771bb2d5c3ed3b8e8914be634e85c080c5af
Nicolas DUBIEN <github@dubien.org>
2017-12-24 02:34:35 +0100

    Add e2e test on a CodinGame example

---
+++ test/e2e/Shadows.spec.ts
@@ -0,0 +1,120 @@
+import * as assert from 'power-assert';
+import * as sc from '../../src/check/simple-check'
+
+// Code under tests
+// Based on the CodinGame https://www.codingame.com/training/medium/shadows-of-the-knight-episode-1
+
+class Space {
+    hint: string = "";
+    constructor(
+            readonly dim_x: number, readonly dim_y: number,
+            private readonly solution_x: number, private readonly solution_y: number,
+            public current_x: number, public current_y: number) {
+        this.update_hint();
+    }
+    update_hint() {
+        this.hint = "";
+        
+        if (this.solution_y < this.current_y) this.hint += "U";
+        else if (this.solution_y > this.current_y) this.hint += "D";
+        
+        if (this.solution_x < this.current_x) this.hint += "L";
+        else if (this.solution_x > this.current_x) this.hint += "R";
+    }
+    move(x: number, y: number) {
+        this.current_x = x;
+        this.current_y = y;
+        this.update_hint();
+    }
+    solved() {
+        return this.current_x === this.solution_x
+            && this.current_y === this.solution_y;
+    }
+}
+class SpaceBuilder {
+    dim_x = 0;
+    dim_y = 0;
+    solution_x = 0;
+    solution_y = 0;
+    current_x = 0;
+    current_y = 0;
+    withDimension(x: number, y: number) {
+        this.dim_x = x;
+        this.dim_y = y;
+        return this;
+    }
+    withSolution(x: number, y: number) {
+        this.solution_x = x;
+        this.solution_y = y;
+        return this;
+    }
+    withCurrent(x: number, y: number) {
+        this.current_x = x;
+        this.current_y = y;
+        return this;
+    }
+    build() {
+        return new Space(
+            this.dim_x, this.dim_y,
+            this.solution_x, this.solution_y,
+            this.current_x, this.current_y);
+    }
+}
+
+function locate_in_space(space: Space, rounds: number) {
+    let x_min = 0;
+    let x_max = space.dim_x;
+    let y_min = 0;
+    let y_max = space.dim_y;
+    
+    for (let n = 0 ; n !== rounds && ! space.solved() ; ++n) {
+        if (x_min >= x_max || y_min >= y_max) { return; }
+        
+        let x0 = space.current_x;
+        let y0 = space.current_y;
+        let hint = space.hint;
+        
+        if (hint[0] == 'U') {
+            y_max = y0 -1;
+            y0 = (y_max + y_min) /2;
+        }
+        else if (hint[0] == 'D') {
+            y_min = y0 +1;
+            y0 = (y_max + y_min) /2;
+        }
+        
+        if (hint.slice(-1) == 'L') {
+            x_max = x0 -1;
+            x0 = (x_max + x_min) /2;
+        }
+        else if (hint.slice(-1) == 'R') {
+            x_min = x0 +1;
+            x0 = (x_max + x_min) /2;
+        }
+        space.move(Math.floor(x0), Math.floor(y0));
+    }
+}
+
+const seed = Date.now();
+describe(`Shadows (seed: ${seed})`, () => {
+    it('Should detect an implementation issue', () => {
+        const out = sc.check(sc.property(
+                sc.integer(1, 1000), sc.integer(1, 1000),
+                sc.integer(1, 1000), sc.integer(1, 1000),
+                sc.integer(1, 1000), sc.integer(1, 1000),
+            (w: number, h: number, cx: number, cy: number, sx: number, sy: number) => {
+                if (cx >= w || sx >= w || cy >= h || sy >= h) {
+                    return true; //invalid case
+                }
+                const space = new SpaceBuilder()
+                        .withDimension(w, h)
+                        .withSolution(cx, cy)
+                        .withCurrent(sx, sy)
+                        .build();
+                const max_guesses = Math.ceil(Math.log(Math.max(h, w))/Math.log(2));
+                locate_in_space(space, max_guesses);
+                return space.solved();
+            }), {seed: seed});
+        assert.ok(out.failed, 'Should have failed');
+    });
+});
