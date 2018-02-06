c3eeb1cc48818f97bed6fd8a9b7a1728fa8d9dcc
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2018-02-06 09:17:15 +0100

    Create local-fast-check.sh

---
+++ example/local-fast-check.sh
@@ -0,0 +1,2 @@
+cat contains/test.js | sed s#'fast-check'#'../../lib/fast-check'#g > contains/test.js
+cat shadows-of-the-knight-codingame/test.js | sed s#'fast-check'#'../../lib/fast-check'#g > shadows-of-the-knight-codingame/test.js
