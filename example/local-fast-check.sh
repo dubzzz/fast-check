664f4228c47f2a0e48a0ad4fd442d28f492b4f88
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2018-02-06 09:18:05 +0100

    Update local-fast-check.sh

--- example/local-fast-check.sh
+++ example/local-fast-check.sh
@@ -1,2 +1,3 @@
 cat contains/test.js | sed s#'fast-check'#'../../lib/fast-check'#g > contains/test.js
 cat shadows-of-the-knight-codingame/test.js | sed s#'fast-check'#'../../lib/fast-check'#g > shadows-of-the-knight-codingame/test.js
+cat package.json | grep -v fast-check > package.json
