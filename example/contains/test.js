70d2925e091698aed6f02143b8510af3540ce3ba
Nicolas DUBIEN <github@dubien.org>
2018-02-09 19:25:49 +0100

    Put units of example contains into units.js

--- example/contains/test.js
+++ example/contains/test.js
@@ -1,25 +1,10 @@
 const assert = require('assert');
 const fc = require('fast-check');
+const { unit_tests } = require('./units');
 const { contains } = require('./contains');
 
-const successCases = [
-    {label: 'should be true when text starts with', text: 'hello world', pattern: 'he'},
-    {label: 'should be true when pattern is somewhere inside', text: 'omg!!!', pattern: 'g'},
-    {label: 'should be true when pattern is empty', text: 'whatever', pattern: ''},
-];
-const failureCases = [
-    {label: 'should be false when there is no match', text: 'other', pattern: 'no'},
-    {label: 'should be false when pattern is larger', text: '', pattern: 'no'},
-];
-
 describe('contains', () => {
-    // various unit-tests
-    for (const t of successCases)
-        it(t.label, () => assert.ok(contains(t.pattern, t.text)));
-    for (const t of failureCases)
-        it(t.label, () => assert.ok(! contains(t.pattern, t.text)));
-    
-    // properties
+    unit_tests(contains);
     it('should always contain b in a+b+c', () => fc.assert(
         fc.property(
             fc.string(), fc.string(), fc.string(),
---
+++ example/contains/units.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const successCases = [
+    {label: 'should be true when text starts with', text: 'hello world', pattern: 'he'},
+    {label: 'should be true when pattern is somewhere inside', text: 'omg!!!', pattern: 'g'},
+    {label: 'should be true when pattern is empty', text: 'whatever', pattern: ''},
+];
+const failureCases = [
+    {label: 'should be false when there is no match', text: 'other', pattern: 'no'},
+    {label: 'should be false when pattern is larger', text: '', pattern: 'no'},
+];
+
+const unit_tests = function(solver) {
+    for (const t of successCases)
+        it(t.label, () => assert.ok(solver(t.pattern, t.text)));
+    for (const t of failureCases)
+        it(t.label, () => assert.ok(! solver(t.pattern, t.text)));
+};
+
+module.exports = { unit_tests };
