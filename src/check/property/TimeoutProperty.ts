5b8fc74e397b17559737b836fc8d15184353327a
Nicolas DUBIEN <github@dubien.org>
2018-02-09 21:20:43 +0100

    Add TimeoutProperty helper
    Will be used inside Runner to timeout on a property when taking too long

---
+++ src/check/property/TimeoutProperty.ts
@@ -0,0 +1,23 @@
+import Arbitrary from '../arbitrary/definition/Arbitrary';
+import Shrinkable from '../arbitrary/definition/Shrinkable';
+import MutableRandomGenerator from '../../random/generator/MutableRandomGenerator';
+import IProperty from './IProperty';
+
+const timeoutAfter = function(timeMs: number) {
+    return new Promise<string>(
+        (resolve, reject) => setTimeout(() => resolve('Property timeout'), timeMs));
+};
+
+export class TimeoutProperty<Ts> implements IProperty<Ts> {
+    constructor(readonly property: IProperty<Ts>, readonly timeMs: number) { }
+    isAsync = () => true;
+    generate(mrng: MutableRandomGenerator): Shrinkable<Ts> {
+        return this.property.generate(mrng);
+    }
+    async run(v: Ts): Promise<string|null> {
+        return await Promise.race([
+                this.property.run(v),
+                timeoutAfter(this.timeMs)
+            ]) as string | null;
+    }
+}
---
+++ test/unit/check/property/TimeoutProperty.spec.ts
@@ -0,0 +1,38 @@
+import * as assert from 'power-assert';
+
+import Arbitrary from '../../../../src/check/arbitrary/definition/Arbitrary';
+import { asyncProperty } from '../../../../src/check/property/AsyncProperty';
+import { TimeoutProperty } from '../../../../src/check/property/TimeoutProperty';
+
+import * as stubArb from '../../stubs/arbitraries';
+import * as stubRng from '../../stubs/generators';
+
+describe('TimeoutProperty', () => {
+    it('Should not timeout if it succeeds in time', async () => {
+        const p = asyncProperty(stubArb.single(0), async (arg: number) => {
+            return await new Promise<boolean>(function(resolve, reject) {
+                setTimeout(resolve, 0);
+            });
+        });
+        const timeoutProp = new TimeoutProperty(p, 100);
+        assert.equal(await timeoutProp.run(timeoutProp.generate(stubRng.mutable.nocall()).value), null);
+    });
+    it('Should not timeout if it fails in time', async () => {
+        const p = asyncProperty(stubArb.single(0), async (arg: number) => {
+            return await new Promise<boolean>(function(resolve, reject) {
+                setTimeout(() => reject("plop"), 0);
+            });
+        });
+        const timeoutProp = new TimeoutProperty(p, 100);
+        assert.equal(await timeoutProp.run(timeoutProp.generate(stubRng.mutable.nocall()).value), 'plop');
+    });
+    it('Should timeout if it takes to long', async () => {
+        const p = asyncProperty(stubArb.single(0), async (arg: number) => {
+            return await new Promise<boolean>(function(resolve, reject) {
+                setTimeout(resolve, 100);
+            });
+        });
+        const timeoutProp = new TimeoutProperty(p, 0);
+        assert.equal(await timeoutProp.run(timeoutProp.generate(stubRng.mutable.nocall()).value), 'Property timeout');
+    });
+});
