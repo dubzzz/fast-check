3d71bf2cb62b240f117235381d193aa8725af367
Nicolas DUBIEN <github@dubien.org>
2018-02-11 05:20:36 +0100

    UniformDistribution should avoid while true loop

--- src/random/distribution/UniformDistribution.ts
+++ src/random/distribution/UniformDistribution.ts
@@ -4,18 +4,18 @@ export default class UniformDistribution {
     static inRange(from: number, to: number): (rng: RandomGenerator) => [number, RandomGenerator] {
         const diff = to - from +1;
         function helper(rng: RandomGenerator): [number, RandomGenerator] {
+            const MIN_RNG = rng.min();
             const NUM_VALUES = rng.max() - rng.min() +1;
             const MAX_ALLOWED = NUM_VALUES - (NUM_VALUES % diff);
-            
-            let prng = rng;
-            while (true) {
-                const [v, nrng] = prng.next();
-                const deltaV = v - prng.min();
-                if (deltaV < MAX_ALLOWED) {
-                    return [deltaV % diff + from, nrng];
-                }
-                prng = nrng;
+
+            let nrng = rng;
+            let deltaV = MAX_ALLOWED;
+            while (deltaV >= MAX_ALLOWED) {
+                const [v, tmpRng] = nrng.next();
+                nrng = tmpRng;
+                deltaV = v - MIN_RNG;
             }
+            return [deltaV % diff + from, nrng];
         }
         return helper;
     }
