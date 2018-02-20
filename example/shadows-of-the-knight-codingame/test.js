d447668b086d28143e04df398cb1798a5ba3c385
Nicolas DUBIEN <github@dubien.org>
2018-02-20 20:43:23 +0100

    Revamp knight example using fc.record

--- example/shadows-of-the-knight-codingame/test.js
+++ example/shadows-of-the-knight-codingame/test.js
@@ -1,24 +1,25 @@
 const assert = require('assert');
-const fc = require('fast-check');
+const fc = require('../../lib/fast-check');
 const { unit_tests } = require('./units');
 const { SpaceBuilder, Space } = require('./space');
 const { buggyKnight, knight } = require('./knight');
 
-const SpaceArbitrary = fc.tuple(
-    fc.integer(1, 1000), fc.integer(1, 1000),
-    fc.integer(1, 1000), fc.integer(1, 1000),
-    fc.integer(1, 1000), fc.integer(1, 1000)
-).filter(raw => {
-    const [w,h,cx,cy,sx,sy] = raw;
-    return cx < w && sx < w && cy < h && sy < h
-}).map(raw => {
-    const [w,h,cx,cy,sx,sy] = raw;
-    return new SpaceBuilder()
+const SpaceArbitrary = fc.record({
+    w: fc.integer(1, 1000),
+    h: fc.integer(1, 1000),
+    cx: fc.integer(1, 1000),
+    cy: fc.integer(1, 1000),
+    sx: fc.integer(1, 1000),
+    sy: fc.integer(1, 1000)
+})
+.filter(({w,h,cx,cy,sx,sy}) => cx < w && sx < w && cy < h && sy < h)
+.map(({w,h,cx,cy,sx,sy}) =>
+    new SpaceBuilder()
         .withDimension(w, h)
         .withSolution(cx, cy)
         .withCurrent(sx, sy)
-        .build();
-}).map(space =>
+        .build()
+).map(space =>
     [space, Math.ceil(Math.log(Math.max(space.dim_x, space.dim_y))/Math.log(2))]
 );
 
