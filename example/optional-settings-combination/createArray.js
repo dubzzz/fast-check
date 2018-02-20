18f7546ffbf1e7f76bf867e033a9e6740c77bb85
Nicolas DUBIEN <github@dubien.org>
2018-02-20 21:44:42 +0100

    Add settings combination example

---
+++ example/optional-settings-combination/createArray.js
@@ -0,0 +1,13 @@
+const createArray = function(gen, settings) {
+    /**
+     * Produces a array filled by calling gen
+     * settings can provide additonal parameters:
+     * - minimum_size: number
+     * - maximum_size: number
+     */
+    const minSize = settings != null && settings.minimum_size != null ? settings.minimum_size : 0;
+    const maxSize = settings != null && settings.minimum_size != null ? settings.maximum_size : minSize + 10;
+    return [...Array(minSize + Math.floor((maxSize - minSize +1) * Math.random()))].map(gen);
+};
+
+module.exports = { createArray };
