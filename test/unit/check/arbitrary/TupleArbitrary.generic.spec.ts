8d56cec7579a32125b75c91776eb899ad9187a7e
Nicolas DUBIEN <github@dubien.org>
2018-04-01 23:11:47 +0200

    Test impact of mutations on the shrinker of generic_tuple
    Related to #55

--- test/unit/check/arbitrary/TupleArbitrary.generic.spec.ts
+++ test/unit/check/arbitrary/TupleArbitrary.generic.spec.ts
@@ -6,6 +6,11 @@ import {
   propertyShrinkInRange
 } from './TupleArbitrary.properties';
 
+import { char } from '../../../../src/check/arbitrary/CharacterArbitrary';
+import { generic_tuple } from '../../../../src/check/arbitrary/TupleArbitrary';
+
+import * as genericHelper from './generic/GenericArbitraryHelper';
+
 describe('TupleArbitrary', () => {
   describe('generic_tuple', () => {
     it('Should generate the same tuple with the same random', () =>
@@ -14,5 +19,8 @@ describe('TupleArbitrary', () => {
       fc.assert(propertyShrinkInRange([dummy(42), dummy(8)], true)));
     it('Should not suggest input in tuple shrinked values', () =>
       fc.assert(propertyNotSuggestInputInShrink([dummy(42), dummy(8)], true)));
+    genericHelper.testNoImpactOfMutation(generic_tuple([char(), char()]), tab => {
+      for (let idx = 0; idx !== tab.length; ++idx) tab[idx] = '.';
+    });
   });
 });
