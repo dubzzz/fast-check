770529718e02a78bbd92795f0d592fc2ba2cddb7
Nicolas DUBIEN <github@dubien.org>
2018-04-15 15:16:27 +0200

    Lint SetArbitrary.ts

--- src/check/arbitrary/SetArbitrary.ts
+++ src/check/arbitrary/SetArbitrary.ts
@@ -1,3 +1,4 @@
+// tslint:disable:no-reserved-keywords
 import { ArrayArbitrary } from './ArrayArbitrary';
 import { Arbitrary } from './definition/Arbitrary';
 import Shrinkable from './definition/Shrinkable';
@@ -18,7 +19,7 @@ function swap<T>(tab: T[], idx1: number, idx2: number): void {
 function buildCompareFilter<T>(compare: (a: T, b: T) => boolean): ((tab: Shrinkable<T>[]) => Shrinkable<T>[]) {
   return (tab: Shrinkable<T>[]): Shrinkable<T>[] => {
     let finalLength = tab.length;
-    for (let idx = tab.length - 1; idx != -1; --idx) {
+    for (let idx = tab.length - 1; idx !== -1; --idx) {
       if (subArrayContains(tab, idx, t => compare(t.value, tab[idx].value))) {
         --finalLength;
         swap(tab, idx, finalLength);
@@ -45,16 +46,16 @@ function set<T>(
   bLength?: number | ((a: T, b: T) => boolean),
   compareFn?: (a: T, b: T) => boolean
 ): Arbitrary<T[]> {
-  const minLength: number = bLength == null || typeof bLength !== 'number' ? 0 : (aLength as number);
+  const minLength: number = bLength == null || typeof bLength !== 'number' ? 0 : <number>aLength;
   const maxLength: number =
     aLength == null || typeof aLength !== 'number' ? 10 : typeof bLength === 'number' ? bLength : aLength;
   const compare =
     compareFn != null
       ? compareFn
       : typeof bLength === 'function'
-        ? (bLength as (a: T, b: T) => boolean)
+        ? <(a: T, b: T) => boolean>bLength
         : typeof aLength === 'function'
-          ? (aLength as (a: T, b: T) => boolean)
+          ? <(a: T, b: T) => boolean>aLength
           : (a: T, b: T) => a === b;
 
   const arrayArb = new ArrayArbitrary<T>(arb, minLength, maxLength, buildCompareFilter(compare));
