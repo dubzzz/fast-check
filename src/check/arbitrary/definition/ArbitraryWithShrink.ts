2d0de9e6c56a601eefe89be8b69ede219115d638
Nicolas DUBIEN <github@dubien.org>
2018-04-15 19:02:00 +0200

    Add tests on ArbitraryWithShrink

--- src/check/arbitrary/definition/ArbitraryWithShrink.ts
+++ src/check/arbitrary/definition/ArbitraryWithShrink.ts
@@ -7,9 +7,7 @@ abstract class ArbitraryWithShrink<T> extends Arbitrary<T> {
   abstract generate(mrng: Random): Shrinkable<T>;
   abstract shrink(value: T, shrunkOnce?: boolean): Stream<T>;
   shrinkableFor(value: T, shrunkOnce?: boolean): Shrinkable<T> {
-    return new Shrinkable(value, () =>
-      this.shrink(value, shrunkOnce === true).map(v => this.shrinkableFor(v, shrunkOnce === true))
-    );
+    return new Shrinkable(value, () => this.shrink(value, shrunkOnce === true).map(v => this.shrinkableFor(v, true)));
   }
 }
 
