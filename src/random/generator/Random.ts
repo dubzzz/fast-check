92c182278f55d7c55858f5066db986b945ac3174
Nicolas DUBIEN <github@dubien.org>
2018-05-07 00:16:33 +0200

    Documentation of Random

--- src/random/generator/Random.ts
+++ src/random/generator/Random.ts
@@ -1,12 +1,20 @@
 import * as prand from 'pure-rand';
 
 export default class Random {
-  static MIN_INT: number = 0x80000000 | 0;
-  static MAX_INT: number = 0x7fffffff | 0;
-  static DBL_FACTOR: number = Math.pow(2, 27);
-  static DBL_DIVISOR: number = Math.pow(2, -53);
+  private static MIN_INT: number = 0x80000000 | 0;
+  private static MAX_INT: number = 0x7fffffff | 0;
+  private static DBL_FACTOR: number = Math.pow(2, 27);
+  private static DBL_DIVISOR: number = Math.pow(2, -53);
 
+  /**
+   * Create a mutable random number generator
+   * @param internalRng Immutable random generator from pure-rand library
+   */
   constructor(private internalRng: prand.RandomGenerator) {}
+
+  /**
+   * Clone the random number generator
+   */
   clone(): Random {
     return new Random(this.internalRng);
   }
@@ -15,17 +23,37 @@ export default class Random {
     this.internalRng = nrng;
     return v;
   }
+
+  /**
+   * Generate an integer having `bits` random bits
+   * @param bits Number of bits to generate
+   */
   next(bits: number): number {
     return this.uniformIn(0, (1 << bits) - 1);
   }
+
+  /**
+   * Generate a random boolean
+   */
   nextBoolean(): boolean {
     return this.uniformIn(0, 1) === 1;
   }
+  /**
+   * Generate a random integer (32 bits)
+   */
   nextInt(): number;
+  /**
+   * Generate a random integer between min (included) and max (included)
+   * @param min Minimal integer value
+   * @param max Maxiaml integer value
+   */
   nextInt(min: number, max: number): number;
   nextInt(min?: number, max?: number): number {
     return this.uniformIn(min == null ? Random.MIN_INT : min, max == null ? Random.MAX_INT : max);
   }
+  /**
+   * Generate a random floating point number between 0.0 (included) and 1.0 (excluded)
+   */
   nextDouble(): number {
     const a = this.next(26);
     const b = this.next(27);
