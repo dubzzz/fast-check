8c8a5b865a9eeee0a3f1a64608191f040086cd1d
Nicolas DUBIEN <github@dubien.org>
2018-06-12 02:09:10 +0200

    Fix RunExecution.spec

--- test/unit/check/runner/reporter/RunExecution.spec.ts
+++ test/unit/check/runner/reporter/RunExecution.spec.ts
@@ -1,7 +1,6 @@
 import * as assert from 'assert';
 import * as fc from '../../../../../lib/fast-check';
 
-import { QualifiedParameters } from '../../../../../src/check/runner/configuration/QualifiedParameters';
 import { RunExecution } from '../../../../../src/check/runner/reporter/RunExecution';
 
 describe('RunExecution', () => {
@@ -13,7 +12,7 @@ describe('RunExecution', () => {
         fc.array(
           fc.record({
             value: fc.integer(),
-            failureId: fc.nat(),
+            failureId: fc.nat(1000),
             message: fc.fullUnicodeString()
           }),
           1,
@@ -22,16 +21,20 @@ describe('RunExecution', () => {
         (seed, storeFailures, failuresDesc) => {
           // Simulate the run
           const run = new RunExecution<number>(storeFailures);
+          for (let idx = 0; idx !== failuresDesc[0].failureId; ++idx) {
+            run.success();
+          }
           for (const f of failuresDesc) {
             run.fail(f.value, f.failureId, f.message);
           }
           // Assert the value
           const lastFailure = failuresDesc[failuresDesc.length - 1];
-          const details = run.toRunDetails(seed, '', 42);
+          const details = run.toRunDetails(seed, '', 42, 10000);
           assert.ok(details.failed);
           assert.ok(details.counterexamplePath != null && details.counterexamplePath.length > 0);
           assert.strictEqual(details.seed, seed);
           assert.strictEqual(details.numRuns, failuresDesc[0].failureId + 1);
+          assert.strictEqual(details.numSkips, 0);
           assert.strictEqual(details.numShrinks, failuresDesc.length - 1);
           assert.strictEqual(details.counterexample, lastFailure.value);
           assert.strictEqual(details.error, lastFailure.message);
@@ -41,29 +44,43 @@ describe('RunExecution', () => {
     ));
   it('Should generate correct counterexamplePath with no initial offset', () =>
     fc.assert(
-      fc.property(fc.integer(), fc.array(fc.nat(), 1, 10), (seed, path) => {
+      fc.property(fc.integer(), fc.array(fc.nat(1000), 1, 10), (seed, path) => {
         // Simulate the run
         const run = new RunExecution<number>(false);
+        for (let idx = 0; idx !== path[0]; ++idx) {
+          run.success();
+        }
         for (const failureId of path) {
           run.fail(42, failureId, 'Failed');
         }
         // Assert the value
-        assert.strictEqual(run.toRunDetails(seed, '', 42).counterexamplePath, path.join(':'));
+        assert.strictEqual(run.toRunDetails(seed, '', 42, 10000).counterexamplePath, path.join(':'));
       })
     ));
   it('Should generate correct counterexamplePath given initial offset', () =>
     fc.assert(
-      fc.property(fc.integer(), fc.array(fc.nat(), 1, 10), fc.array(fc.nat(), 1, 10), (seed, offsetPath, addedPath) => {
-        // Simulate the run
-        const run = new RunExecution<number>(false);
-        for (const failureId of addedPath) {
-          run.fail(42, failureId, 'Failed');
+      fc.property(
+        fc.integer(),
+        fc.array(fc.nat(1000), 1, 10),
+        fc.array(fc.nat(1000), 1, 10),
+        (seed, offsetPath, addedPath) => {
+          // Simulate the run
+          const run = new RunExecution<number>(false);
+          for (let idx = 0; idx !== addedPath[0]; ++idx) {
+            run.success();
+          }
+          for (const failureId of addedPath) {
+            run.fail(42, failureId, 'Failed');
+          }
+          // Build the expected path
+          const joinedPath = [...offsetPath, ...addedPath.slice(1)];
+          joinedPath[offsetPath.length - 1] += addedPath[0];
+          // Assert the value
+          assert.strictEqual(
+            run.toRunDetails(seed, offsetPath.join(':'), 42, 10000).counterexamplePath,
+            joinedPath.join(':')
+          );
         }
-        // Build the expected path
-        const joinedPath = [...offsetPath, ...addedPath.slice(1)];
-        joinedPath[offsetPath.length - 1] += addedPath[0];
-        // Assert the value
-        assert.strictEqual(run.toRunDetails(seed, offsetPath.join(':'), 42).counterexamplePath, joinedPath.join(':'));
-      })
+      )
     ));
 });
