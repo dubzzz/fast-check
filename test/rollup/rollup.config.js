722086473cf531a7cb9dcc97bf2694a7f9be143d
Nicolas DUBIEN <github@dubien.org>
2018-08-23 20:56:07 +0200

    Remove warning suppression from rollup build

--- test/rollup/rollup.config.js
+++ test/rollup/rollup.config.js
@@ -9,12 +9,4 @@ export default {
     name: 'RunFastCheck'
   },
   plugins: [ resolve(), cjs() ],
-
-  // (!) Unresolved dependencies -- os (imported by node_modules\lorem-ipsum\lib\generator.js,  commonjs-external:os)
-  external: [ 'os' ],
-  // (!) `this` has been rewritten to `undefined`
-  onwarn: function ({code, message}) {
-    if (code === 'THIS_IS_UNDEFINED') return;
-    console.error(message);
-  }
 };
