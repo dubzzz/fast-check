32242dd83ae9e452715db646ef97d1fe193e7272
Nicolas DUBIEN <github@dubien.org>
2018-10-14 01:17:11 +0200

    CommandsIterable must only print last failure

--- src/check/model/ModelRunner.ts
+++ src/check/model/ModelRunner.ts
@@ -1,6 +1,7 @@
 import { AsyncCommand } from './command/AsyncCommand';
 import { Command } from './command/Command';
 import { ICommand } from './command/ICommand';
+import { CommandsIterable } from './commands/CommandsIterable';
 
 type Setup<Model, Real> = () => { model: Model; real: Real };
 
@@ -23,6 +24,38 @@ const genericModelRun = <Model extends object, Real, P>(
   return state;
 };
 
+/** @hidden */
+const internalModelRun = <Model extends object, Real>(
+  s: Setup<Model, Real>,
+  cmds: Iterable<Command<Model, Real>> | CommandsIterable<Model, Real, void>
+): void => {
+  const then = (p: undefined, c: () => undefined) => c();
+  try {
+    return genericModelRun(s, cmds, undefined, then);
+  } catch (err) {
+    if ('errorDetected' in cmds && typeof cmds.errorDetected === 'function') {
+      cmds.errorDetected();
+    }
+    throw err;
+  }
+};
+
+/** @hidden */
+const internalAsyncModelRun = async <Model extends object, Real>(
+  s: Setup<Model, Real>,
+  cmds: Iterable<AsyncCommand<Model, Real>> | CommandsIterable<Model, Real, Promise<void>>
+): Promise<void> => {
+  const then = (p: Promise<void>, c: () => Promise<void> | undefined) => p.then(c);
+  try {
+    return await genericModelRun(s, cmds, Promise.resolve(), then);
+  } catch (err) {
+    if ('errorDetected' in cmds && typeof cmds.errorDetected === 'function') {
+      cmds.errorDetected();
+    }
+    throw err;
+  }
+};
+
 /**
  * Run synchronous commands over a `Model` and the `Real` system
  *
@@ -33,10 +66,9 @@ const genericModelRun = <Model extends object, Real, P>(
  */
 export const modelRun = <Model extends object, Real>(
   s: Setup<Model, Real>,
-  cmds: Iterable<Command<Model, Real>>
+  cmds: Iterable<Command<Model, Real>> | CommandsIterable<Model, Real, void>
 ): void => {
-  const then = (p: undefined, c: () => undefined) => c();
-  genericModelRun(s, cmds, undefined, then);
+  internalModelRun(s, cmds);
 };
 
 /**
@@ -47,10 +79,9 @@ export const modelRun = <Model extends object, Real>(
  * @param s Initial state provider
  * @param cmds Asynchronous commands to be executed
  */
-export const asyncModelRun = <Model extends object, Real>(
+export const asyncModelRun = async <Model extends object, Real>(
   s: Setup<Model, Real>,
-  cmds: Iterable<AsyncCommand<Model, Real>>
+  cmds: Iterable<AsyncCommand<Model, Real>> | CommandsIterable<Model, Real, Promise<void>>
 ): Promise<void> => {
-  const then = (p: Promise<void>, c: () => Promise<void> | undefined) => p.then(c);
-  return genericModelRun(s, cmds, Promise.resolve(), then);
+  await internalAsyncModelRun(s, cmds);
 };
--- src/check/model/commands/CommandsIterable.ts
+++ src/check/model/commands/CommandsIterable.ts
@@ -1,15 +1,23 @@
 import { CommandWrapper } from './CommandWrapper';
 
+/** @hidden */
 export class CommandsIterable<Model extends object, Real, RunResult>
   implements Iterable<CommandWrapper<Model, Real, RunResult>> {
+  private lastErrorDetectedStr: string = '';
   constructor(readonly commands: CommandWrapper<Model, Real, RunResult>[]) {}
   [Symbol.iterator](): Iterator<CommandWrapper<Model, Real, RunResult>> {
+    for (let idx = 0; idx !== this.commands.length; ++idx) {
+      this.commands[idx].hasRan = false;
+    }
     return this.commands[Symbol.iterator]();
   }
-  toString(): string {
-    return this.commands
+  errorDetected() {
+    this.lastErrorDetectedStr = this.commands
       .filter(c => c.hasRan)
       .map(c => c.toString())
       .join(',');
+  }
+  toString(): string {
+    return this.lastErrorDetectedStr;
   }
 }
