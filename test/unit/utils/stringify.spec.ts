cf588ee8070eec5bd90002de2407e7da09a51381
Nicolas DUBIEN <github@dubien.org>
2018-12-17 02:51:29 +0100

    Fix stringify tests for old node versions

--- test/unit/utils/stringify.spec.ts
+++ test/unit/utils/stringify.spec.ts
@@ -2,11 +2,15 @@ import * as fc from '../../../lib/fast-check';
 
 import { stringify } from '../../../src/utils/stringify';
 
+declare function BigInt(n: number | bigint | string): bigint;
+
 describe('stringify', () => {
   it('Should be able to stringify fc.anything()', () =>
     fc.assert(fc.property(fc.anything(), a => typeof stringify(a) === 'string')));
   it('Should be able to stringify fc.char16bits() (ie. possibly invalid strings)', () =>
     fc.assert(fc.property(fc.char16bits(), a => typeof stringify(a) === 'string')));
-  it('Should be able to stringify bigint in object correctly', () =>
-    fc.assert(fc.property(fc.bigInt(), b => stringify({ b }) === '{"b":"' + b + 'n"}')));
+  if (typeof BigInt !== 'undefined') {
+    it('Should be able to stringify bigint in object correctly', () =>
+      fc.assert(fc.property(fc.bigInt(), b => stringify({ b }) === '{"b":"' + b + 'n"}')));
+  }
 });
