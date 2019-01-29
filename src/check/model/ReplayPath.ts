c4ad387ce515756e7bd42acfc8a55b1daf48d85a
Nicolas DUBIEN <github@dubien.org>
2019-01-30 00:53:50 +0100

    Optimize replayPath size

--- src/check/model/ReplayPath.ts
+++ src/check/model/ReplayPath.ts
@@ -1,9 +1,46 @@
 /** @hidden */
+interface Count {
+  value: boolean;
+  count: number;
+}
+
+/** @hidden */
 export class ReplayPath {
   static parse(replayPathStr: string): boolean[] {
-    return [...replayPathStr].map(v => v === '1');
+    if (replayPathStr.length % 2 !== 0) throw new Error(`Invalid replayPath ${JSON.stringify(replayPathStr)}`);
+
+    const replayPath: boolean[] = [];
+    for (let idx = 0; idx !== replayPathStr.length; idx += 2) {
+      const count = this.b64ToInt(replayPathStr.charAt(idx)) + 1;
+      const value = replayPathStr.charAt(idx + 1) === '1';
+      for (let num = 0; num !== count; ++num) replayPath.push(value);
+    }
+    return replayPath;
   }
   static stringify(replayPath: boolean[]): string {
-    return replayPath.map(s => (s ? '1' : '0')).join('');
+    const aggregatedPath = replayPath.reduce((counts: Count[], cur: boolean) => {
+      if (counts.length === 0 || counts[counts.length - 1].count === 64 || counts[counts.length - 1].value !== cur)
+        counts.push({ value: cur, count: 1 });
+      else counts[counts.length - 1].count += 1;
+      return counts;
+    }, []);
+    return aggregatedPath
+      .map(({ value, count }) => {
+        const b64 = this.intToB64(count - 1);
+        return value ? `${b64}1` : `${b64}0`;
+      })
+      .join('');
+  }
+  static intToB64(n: number): string {
+    if (n < 26) return String.fromCharCode(n + 65); // A-Z
+    if (n < 52) return String.fromCharCode(n + 97 - 26); // a-z
+    if (n < 62) return String.fromCharCode(n + 48 - 52); // 0-9
+    return String.fromCharCode(n === 62 ? 43 : 47); // +/
+  }
+  static b64ToInt(c: string): number {
+    if ('A' <= c && c <= 'Z') return c.charCodeAt(0) - 65;
+    if ('a' <= c && c <= 'z') return c.charCodeAt(0) - 97 + 26;
+    if ('0' <= c && c <= '9') return c.charCodeAt(0) - 48 + 52;
+    return c === '+' ? 62 : 63;
   }
 }
--- test/unit/check/model/ReplayPath.spec.ts
+++ test/unit/check/model/ReplayPath.spec.ts
@@ -2,10 +2,21 @@ import * as fc from '../../../../lib/fast-check';
 
 import { ReplayPath } from '../../../../src/check/model/ReplayPath';
 
+const biasedBoolean = fc.frequency(
+  { weight: 1000, arbitrary: fc.constant(true) },
+  { weight: 1, arbitrary: fc.constant(false) }
+);
+
 describe('ReplayPath', () => {
   it('Should be able to read back itself', () =>
     fc.assert(
       fc.property(fc.array(fc.boolean(), 0, 1000), (replayPath: boolean[]) => {
+        expect(ReplayPath.parse(ReplayPath.stringify(replayPath))).toEqual(replayPath);
+      })
+    ));
+  it('Should be able to read back itself (biased boolean)', () =>
+    fc.assert(
+      fc.property(fc.array(biasedBoolean, 0, 1000), (replayPath: boolean[]) => {
         expect(ReplayPath.parse(ReplayPath.stringify(replayPath))).toEqual(replayPath);
       })
     ));
