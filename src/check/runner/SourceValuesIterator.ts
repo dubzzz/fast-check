7a1b4dad3498189585de7b51c53c8e50a51c227b
Nicolas DUBIEN <github@dubien.org>
2019-01-29 01:04:55 +0100

    Extract code responsible for skip outside of runner

---
+++ src/check/runner/SourceValuesIterator.ts
@@ -0,0 +1,27 @@
+/**
+ * @hidden
+ * Try to extract maxInitialIterations non-skipped values
+ * with a maximal number of remainingSkips skipped values
+ * from initialValues source
+ */
+export class SourceValuesIterator<Ts> implements IterableIterator<Ts> {
+  constructor(
+    readonly initialValues: IterableIterator<() => Ts>,
+    private maxInitialIterations: number,
+    private remainingSkips: number
+  ) {}
+  [Symbol.iterator](): IterableIterator<Ts> {
+    return this;
+  }
+  next(value?: any): IteratorResult<Ts> {
+    if (--this.maxInitialIterations !== -1 && this.remainingSkips >= 0) {
+      const n = this.initialValues.next();
+      if (!n.done) return { value: n.value(), done: false };
+    }
+    return { value, done: true };
+  }
+  skippedOne() {
+    --this.remainingSkips;
+    ++this.maxInitialIterations;
+  }
+}
