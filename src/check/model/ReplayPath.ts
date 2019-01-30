30f81e919caa3f8eccc376004707f6fcf7e2fce2
Nicolas DUBIEN <github@dubien.org>
2019-01-30 01:29:46 +0100

    Even shorter ReplayPath

--- src/check/model/ReplayPath.ts
+++ src/check/model/ReplayPath.ts
@@ -7,12 +7,21 @@ interface Count {
 /** @hidden */
 export class ReplayPath {
   static parse(replayPathStr: string): boolean[] {
-    if (replayPathStr.length % 2 !== 0) throw new Error(`Invalid replayPath ${JSON.stringify(replayPathStr)}`);
+    const [serializedCount, serializedChanges] = replayPathStr.split(':');
+    const counts = serializedCount.split('').map(c => this.b64ToInt(c) + 1);
+    const changesInt = serializedChanges.split('').map(c => this.b64ToInt(c));
+    const changes: boolean[] = [];
+    for (let idx = 0; idx !== changesInt.length; ++idx) {
+      let current = changesInt[idx];
+      for (let n = 0; n !== 6; ++n, current >>= 1) {
+        changes.push(current % 2 === 1);
+      }
+    }
 
     const replayPath: boolean[] = [];
-    for (let idx = 0; idx !== replayPathStr.length; idx += 2) {
-      const count = this.b64ToInt(replayPathStr.charAt(idx)) + 1;
-      const value = replayPathStr.charAt(idx + 1) === '1';
+    for (let idx = 0; idx !== counts.length; ++idx) {
+      const count = counts[idx];
+      const value = changes[idx];
       for (let num = 0; num !== count; ++num) replayPath.push(value);
     }
     return replayPath;
@@ -24,12 +33,15 @@ export class ReplayPath {
       else counts[counts.length - 1].count += 1;
       return counts;
     }, []);
-    return aggregatedPath
-      .map(({ value, count }) => {
-        const b64 = this.intToB64(count - 1);
-        return value ? `${b64}1` : `${b64}0`;
-      })
-      .join('');
+    const serializedCount = aggregatedPath.map(({ count }) => this.intToB64(count - 1)).join('');
+    let serializedChanges = '';
+    for (let idx = 0; idx < aggregatedPath.length; idx += 6) {
+      const changesInt = aggregatedPath
+        .slice(idx, idx + 6)
+        .reduceRight((prev: number, cur: Count) => prev * 2 + (cur.value ? 1 : 0), 0);
+      serializedChanges += this.intToB64(changesInt);
+    }
+    return `${serializedCount}:${serializedChanges}`;
   }
   static intToB64(n: number): string {
     if (n < 26) return String.fromCharCode(n + 65); // A-Z
