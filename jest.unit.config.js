0b7e94d40e02ca5762fa88bd451a5cd140a74923
Nicolas DUBIEN <github@dubien.org>
2019-02-22 00:39:46 +0100

    Adapt Jest configuration for Node 6

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,8 +1,7 @@
 const conf = require('./jest.config');
 
-module.exports = {
-  ...conf,
+module.exports = Object.assign(conf, {
   testMatch: ['<rootDir>/test/unit/**/*.spec.ts'],
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/test/', '<rootDir>/node_modules/']
-};
+});
