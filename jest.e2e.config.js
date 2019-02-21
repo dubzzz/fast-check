4974e5c382089b0b03c711a91219aa24dc982e8d
Nicolas DUBIEN <github@dubien.org>
2019-02-22 00:22:26 +0100

    Factorize Jest configuration

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,13 +1,6 @@
+const conf = require('./jest.config');
+
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json'
-    }
-  },
-  testMatch: ['<rootDir>/test/e2e/**/*.spec.ts'],
-  setupFiles: [],
-  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
-  //setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  preset: 'ts-jest'
+  ...conf,
+  testMatch: ['<rootDir>/test/e2e/**/*.spec.ts']
 };
