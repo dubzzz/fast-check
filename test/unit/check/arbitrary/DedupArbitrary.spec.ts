5f69873da947a6d0bb51a1a7096dbe892ecea979
Nicolas DUBIEN <github@dubien.org>
2019-02-24 23:56:04 +0100

    Add units for cloneable on dedup

--- test/unit/check/arbitrary/DedupArbitrary.spec.ts
+++ test/unit/check/arbitrary/DedupArbitrary.spec.ts
@@ -6,6 +6,11 @@ import { nat } from '../../../../src/check/arbitrary/IntegerArbitrary';
 import * as genericHelper from './generic/GenericArbitraryHelper';
 
 import * as stubRng from '../../stubs/generators';
+import { hasCloneMethod } from '../../../../src/check/symbols';
+import { context } from '../../../../src/check/arbitrary/ContextArbitrary';
+import { cloneMethod } from '../../../../lib/fast-check';
+import { Arbitrary } from '../../../../src/check/arbitrary/definition/Arbitrary';
+import { Shrinkable } from '../../../../src/check/arbitrary/definition/Shrinkable';
 
 export const isStrictlySmaller = (arr1: number[], arr2: number[]) => {
   for (let idx = 0; idx !== arr1.length; ++idx) {
@@ -17,6 +22,33 @@ export const isStrictlySmaller = (arr1: number[], arr2: number[]) => {
 
 describe('DedupArbitrary', () => {
   describe('dedup', () => {
+    it('Should produce cloneable tuple if cloneable', () => {
+      const mrng = stubRng.mutable.counter(0);
+      let g = dedup(context(), 2).generate(mrng).value;
+      expect(hasCloneMethod(g)).toBe(true);
+    });
+    it('Should not produce cloneable tuple if not cloneable', () => {
+      const mrng = stubRng.mutable.counter(0);
+      let g = dedup(nat(), 2).generate(mrng).value;
+      expect(hasCloneMethod(g)).toBe(false);
+    });
+    it('Should not clone on generate', () => {
+      let numCallsToClone = 0;
+      const withClonedAndCounter = new class extends Arbitrary<any> {
+        generate() {
+          const v = {
+            [cloneMethod]: () => {
+              ++numCallsToClone;
+              return v;
+            }
+          };
+          return new Shrinkable(v);
+        }
+      }();
+      const mrng = stubRng.mutable.counter(0);
+      dedup(withClonedAndCounter, 2).generate(mrng);
+      expect(numCallsToClone).toEqual(0);
+    });
     it('Should offset the random exactly as many times as one source arbitrary', () =>
       fc.assert(
         fc.property(fc.integer(), fc.integer(1, 20), (seed, numValues) => {
