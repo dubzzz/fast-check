248e8063d525939ad992fca27f6d38f2d0448b90
Nicolas DUBIEN <github@dubien.org>
2019-02-25 01:30:59 +0100

    Stringify have to handle boxed values and Set/Map
    Fixes #318
    Fixes #319

--- test/unit/utils/stringify.spec.ts
+++ test/unit/utils/stringify.spec.ts
@@ -4,6 +4,15 @@ import { stringify } from '../../../src/utils/stringify';
 
 declare function BigInt(n: number | bigint | string): bigint;
 
+const checkEqual = (a: any, b: any): boolean => {
+  try {
+    expect(a).toEqual(b);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
 describe('stringify', () => {
   it('Should be able to stringify fc.anything()', () =>
     fc.assert(fc.property(fc.anything(), a => typeof stringify(a) === 'string')));
@@ -11,6 +20,51 @@ describe('stringify', () => {
     fc.assert(fc.property(fc.char16bits(), a => typeof stringify(a) === 'string')));
   if (typeof BigInt !== 'undefined') {
     it('Should be able to stringify bigint in object correctly', () =>
-      fc.assert(fc.property(fc.bigInt(), b => stringify({ b }) === '{"b":"' + b + 'n"}')));
+      fc.assert(fc.property(fc.bigInt(), b => stringify({ b }) === '{"b":' + b + 'n}')));
   }
+  it('Should be equivalent to JSON.stringify for JSON compliant objects', () =>
+    fc.assert(
+      fc.property(
+        fc.anything({ values: [fc.boolean(), fc.integer(), fc.double(), fc.fullUnicodeString(), fc.constant(null)] }),
+        obj => {
+          expect(stringify(obj)).toEqual(JSON.stringify(obj));
+        }
+      )
+    ));
+  it('Should stringify differently distinct objects', () =>
+    fc.assert(
+      fc.property(fc.anything(), fc.anything(), (a, b) => {
+        fc.pre(!checkEqual(a, b));
+        expect(stringify(a)).not.toEqual(stringify(b));
+      })
+    ));
+  it('Should be able to stringify values', () => {
+    expect(stringify(null)).toEqual('null');
+    expect(stringify(undefined)).toEqual('undefined');
+    expect(stringify(false)).toEqual('false');
+    expect(stringify(42)).toEqual('42');
+    expect(stringify(-0)).toEqual('-0');
+    expect(stringify(Number.POSITIVE_INFINITY)).toEqual('Number.POSITIVE_INFINITY');
+    expect(stringify(Number.NEGATIVE_INFINITY)).toEqual('Number.NEGATIVE_INFINITY');
+    expect(stringify(Number.NaN)).toEqual('Number.NaN');
+    expect(stringify('Hello')).toEqual('"Hello"');
+    if (typeof BigInt !== 'undefined') {
+      expect(stringify(BigInt(42))).toEqual('42n');
+    }
+  });
+  it('Should be able to stringify boxed values', () => {
+    expect(stringify(new Boolean(false))).toEqual('new Boolean(false)');
+    expect(stringify(new Number(42))).toEqual('new Number(42)');
+    expect(stringify(new Number(-0))).toEqual('new Number(-0)');
+    expect(stringify(new Number(Number.POSITIVE_INFINITY))).toEqual('new Number(Number.POSITIVE_INFINITY)');
+    expect(stringify(new Number(Number.NEGATIVE_INFINITY))).toEqual('new Number(Number.NEGATIVE_INFINITY)');
+    expect(stringify(new Number(Number.NaN))).toEqual('new Number(Number.NaN)');
+    expect(stringify(new String('Hello'))).toEqual('new String("Hello")');
+  });
+  it('Should be able to stringify Set', () => {
+    expect(stringify(new Set([1, 2]))).toEqual('new Set([1,2])');
+  });
+  it('Should be able to stringify Map', () => {
+    expect(stringify(new Map([[1, 2]]))).toEqual('new Map([[1,2]])');
+  });
 });
