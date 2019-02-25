f68b484bf51b03718ecd72999e2a61a589833bc9
Nicolas DUBIEN <github@dubien.org>
2019-02-25 01:30:59 +0100

    Stringify have to handle boxed values and Set/Map
    Fixes #318
    Fixes #319

--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -1,21 +1,53 @@
 /** @hidden */
-function stringifyOne<Ts>(value: Ts): string {
-  if (typeof value === 'string') return JSON.stringify(value);
-
-  const defaultRepr: string = `${value}`;
-  if (/^\[object (Object|Null|Undefined)\]$/.exec(defaultRepr) === null) return defaultRepr;
-  try {
-    return JSON.stringify(value, (k, v) => {
-      if (typeof v === 'bigint') {
-        return v.toString() + 'n';
-      } else {
-        return v;
-      }
-    });
-  } catch (err) {
-    // ignored: object cannot be stringified using JSON.stringify
+function stringifyNumber(numValue: number) {
+  switch (numValue) {
+    case 0:
+      return 1 / numValue === Number.NEGATIVE_INFINITY ? '-0' : '0';
+    case Number.NEGATIVE_INFINITY:
+      return 'Number.NEGATIVE_INFINITY';
+    case Number.POSITIVE_INFINITY:
+      return 'Number.POSITIVE_INFINITY';
+    default:
+      return numValue === numValue ? String(numValue) : 'Number.NaN';
   }
-  return defaultRepr;
+}
+
+/** @hidden */
+function stringifyOne<Ts>(value: Ts): string {
+  switch (Object.prototype.toString.call(value)) {
+    case '[object BigInt]':
+      return `${value}n`;
+    case '[object Boolean]':
+      return typeof value === 'boolean' ? JSON.stringify(value) : `new Boolean(${JSON.stringify(value)})`;
+    case '[object Map]':
+      return `new Map(${stringify(Array.from(value as any))})`;
+    case '[object Null]':
+      return `null`;
+    case '[object Number]':
+      return typeof value === 'number' ? stringifyNumber(value) : `new Number(${stringifyNumber(Number(value))})`;
+    case '[object Object]':
+      const defaultRepr: string = `${value}`;
+      if (defaultRepr !== '[object Object]') return defaultRepr;
+      return (
+        '{' +
+        Object.keys(value)
+          .map(k => `${JSON.stringify(k)}:${stringify((value as any)[k])}`)
+          .join(',') +
+        '}'
+      );
+    case '[object Set]':
+      return `new Set(${stringify(Array.from(value as any))})`;
+    case '[object String]':
+      return typeof value === 'string' ? JSON.stringify(value) : `new String(${JSON.stringify(value)})`;
+    case '[object Undefined]':
+      return `undefined`;
+    default:
+      try {
+        return `${value}`;
+      } catch {
+        return Object.prototype.toString.call(value);
+      }
+  }
 }
 
 /** @hidden */
