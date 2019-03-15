82ceb4d1c490c2d47ee81d0829b07acf9889fe60
Nicolas DUBIEN <github@dubien.org>
2019-03-15 19:56:35 +0100

    Reduce size of the generated tuple implementation
    lib/check/arbitrary/TupleArbitrary.generated.js:
    33.4 Ko -> 344 o
    
    lib/esm/check/arbitrary/TupleArbitrary.generated.js:
    32.2 Ko -> 269 o
    
    lib/ts3.2/check/arbitrary/TupleArbitrary.generated.d.ts:
    45.8 Ko -> 20.8 Ko
    
    lib/types/check/arbitrary/TupleArbitrary.generated.d.ts:
    45.8 Ko -> 20.8 Ko
    
    lib/bundle.js:
    149 Ko -> 134 Ko

--- prebuild/tuple.ts
+++ prebuild/tuple.ts
@@ -1,22 +1,4 @@
-import { arbCommas, commas, iota, joiner, txCommas, txXor } from './helpers';
-
-const classFor = (num: number): string =>
-  `
-        /** @hidden */
-        export class Tuple${num}Arbitrary<${txCommas(num)}> extends Arbitrary<[${txCommas(num)}]> {
-            readonly tupleArb: GenericTupleArbitrary<${txXor(num)}>;
-            constructor(${commas(num, v => `readonly arb${v}: Arbitrary<T${v}>`)}) {
-                super();
-                this.tupleArb = new GenericTupleArbitrary<${txXor(num)}>([${arbCommas(num)}]);
-            }
-            generate(mrng: Random): Shrinkable<[${txCommas(num)}]> {
-                return this.tupleArb.generate(mrng) as Shrinkable<[${txCommas(num)}]>;
-            }
-            withBias(freq: number) {
-                return new Tuple${num}Arbitrary(${commas(num, v => `this.arb${v}.withBias(freq)`)});
-            }
-        };
-    `;
+import { commas, iota, joiner, txCommas } from './helpers';
 
 const signatureFor = (num: number, opt?: boolean): string =>
   `
@@ -28,33 +10,17 @@ const signatureFor = (num: number, opt?: boolean): string =>
                 ${commas(num, v => `arb${v}${opt === true ? '?' : ''}: Arbitrary<T${v}>`)}
                 )`;
 
-const ifFor = (num: number): string =>
-  `
-        if (arb${num - 1}) {
-            return new Tuple${num}Arbitrary(
-                ${commas(num, v => `arb${v} as Arbitrary<T${v}>`)}
-            );
-        }`;
-
 const generateTuple = (num: number): string => {
   const blocks = [
     // imports
     `import { Arbitrary } from './definition/Arbitrary';`,
-    `import { Shrinkable } from './definition/Shrinkable';`,
-    `import { Random } from '../../random/generator/Random';`,
     `import { GenericTupleArbitrary } from './TupleArbitrary.generic';`,
-    // declare all necessary classes
-    ...iota(num).map(id => classFor(id + 1)),
     // declare all signatures
-    ...iota(num).map(id => `${signatureFor(id + 1)}: Tuple${id + 1}Arbitrary<${txCommas(id + 1)}>;`),
-    // start declare function
-    `${signatureFor(num, true)} {`,
-    // cascade ifs
-    ...iota(num)
-      .reverse()
-      .map(id => ifFor(id + 1)),
-    // end declare function
-    `}`,
+    ...iota(num).map(id => `${signatureFor(id + 1)}: Arbitrary<[${txCommas(id + 1)}]>;`),
+    // declare function
+    `function tuple<Ts>(...arbs: Arbitrary<Ts>[]): Arbitrary<Ts[]> {
+      return new GenericTupleArbitrary(arbs);
+    }`,
     // export
     `export { tuple };`
   ];
