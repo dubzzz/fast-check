dd536306f34b0a0432d2df822c65d640ea9d1868
Nicolas DUBIEN <github@dubien.org>
2019-03-18 09:07:04 +0100

    Remove lorem-ipsum dependency
    Fixes #335
    
    The choices has been to removed the dependency towards lorem-ipsum package because:
    - Version 2 of the package is not compatible with legacy versions of node
    - Package size skyrocketed with the latest major
    - Up to version 1, the code was doing a simple random on words: relying on built-ins of fast-check to compute the lorem will provide better shrinking capabilities

--- test/unit/check/arbitrary/LoremArbitrary.spec.ts
+++ test/unit/check/arbitrary/LoremArbitrary.spec.ts
@@ -22,15 +22,37 @@ describe('LoremArbitrary', () => {
         fc.property(fc.integer(), fc.integer(0, 100), (seed, num) => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const g = lorem(num).generate(mrng).value;
-          return g.indexOf('.') === -1;
+          expect(g).not.toContain('.');
         })
       ));
-    it('Should generate sentences when asked too', () =>
+    it('Should not generate commas for words', () =>
+      fc.assert(
+        fc.property(fc.integer(), fc.integer(0, 100), (seed, num) => {
+          const mrng = new Random(prand.xorshift128plus(seed));
+          const g = lorem(num).generate(mrng).value;
+          expect(g).not.toContain(',');
+        })
+      ));
+    it('Should generate sentences ending by dot', () =>
       fc.assert(
         fc.property(fc.integer(), seed => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const g = lorem(5, true).generate(mrng).value;
-          return g.indexOf('.') !== -1;
+          expect(g).toContain('.');
+          expect(g[g.length - 1]).toEqual('.');
+
+          // we remove the trailing dot at the end of the generated string
+          // we remove the leading space for sentences with index greater than 0
+          const sentences = g
+            .substr(0, g.length - 1)
+            .split('.')
+            .map((s, i) => (i === 0 ? s : s.substring(1)));
+          for (const s of sentences) {
+            expect(s).not.toEqual('');
+            expect(s).toMatch(/^[A-Z](, | )?([a-z]+(, | )?)*$/);
+          }
+          expect(sentences.length).toBeGreaterThanOrEqual(1);
+          expect(sentences.length).toBeLessThanOrEqual(5);
         })
       ));
   });
