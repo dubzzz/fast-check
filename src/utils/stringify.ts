6e7b839ad46a9ad0095ed77f4a9474c9a8e890ee
Nicolas DUBIEN <github@dubien.org>
2019-05-24 00:42:54 +0200

    Stringify Symbol as valid JavaScript (#371)
    * Stringify Symbol as valid JavaScript
    
    * Fix Symbol representation for node <12

--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -1,4 +1,22 @@
 /** @hidden */
+const findSymbolNameRegex = /^Symbol\((.*)\)$/;
+
+/**
+ * @hidden
+ * Only called with symbol produced by Symbol(string | undefined)
+ * Not Symbol.for(string)
+ */
+function getSymbolDescription(s: symbol): string | null {
+  if (s.description !== undefined) return s.description;
+
+  // description is always undefined in node 6, 8, 10 (not 12)
+  const m = findSymbolNameRegex.exec(String(s));
+  // '' considered equivalent to undefined for node <12 (unable to distinguish undefined from '')
+  // s.description would have been equal to '' in node 12+
+  return m && m[1].length ? m[1] : null;
+}
+
+/** @hidden */
 function stringifyNumber(numValue: number) {
   switch (numValue) {
     case 0:
@@ -50,6 +68,13 @@ export function stringifyInternal<Ts>(value: Ts, previousValues: any[]): string 
       return `new Set(${stringifyInternal(Array.from(value as any), currentValues)})`;
     case '[object String]':
       return typeof value === 'string' ? JSON.stringify(value) : `new String(${JSON.stringify(value)})`;
+    case '[object Symbol]':
+      const s = (value as unknown) as symbol;
+      if (Symbol.keyFor(s) !== undefined) {
+        return `Symbol.for(${JSON.stringify(Symbol.keyFor(s))})`;
+      }
+      const desc = getSymbolDescription(s);
+      return desc !== null ? `Symbol(${JSON.stringify(desc)})` : `Symbol()`;
     case '[object Undefined]':
       return `undefined`;
     default:
