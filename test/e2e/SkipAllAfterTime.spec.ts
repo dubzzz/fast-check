25fafde3506bf24fd6d7c3d6f6505494eb8f6ed4
Nicolas DUBIEN <github@dubien.org>
2019-09-06 01:42:19 +0200

    skipAllAfterTimeLimit throws an error instead of skipping tests (#425)
    Related to #423

---
+++ test/e2e/SkipAllAfterTime.spec.ts
@@ -0,0 +1,20 @@
+import * as fc from '../../src/fast-check';
+
+const seed = Date.now();
+describe(`SkipAllAfterTime (seed: ${seed})`, () => {
+  it('should skip as soon as delay expires and mark run as failed', () => {
+    let numRuns = 0;
+    const out = fc.check(
+      fc.property(fc.integer(), x => {
+        ++numRuns;
+        return true;
+      }),
+      { skipAllAfterTimeLimit: 0 }
+    );
+    expect(out.failed).toBe(true); // Not enough tests have been executed
+    expect(out.numRuns).toBe(0);
+    expect(out.numShrinks).toBe(0);
+    expect(out.numSkips).toBe(10001); // maxSkipsPerRun(100) * numRuns(100) +1
+    expect(numRuns).toBe(0); // Expired immediately (timeout = 0)
+  });
+});
