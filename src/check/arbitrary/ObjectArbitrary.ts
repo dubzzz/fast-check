dubzzz/fast-check

commit 9013969d4dcdd8a712d57e4432c22897fc577047

any -> unknown in exported signatures for object/anything arbitraries (#436)
* any -> unknown in exported signatures for object/anything arbitraries

* add myself among contributors

src/check/arbitrary/ObjectArbitrary.ts
@@ -17,7 +17,7 @@ import { tuple } from './TupleArbitrary';
 export class ObjectConstraints {
   constructor(
     readonly key: Arbitrary<string>,
-    readonly values: Arbitrary<any>[],
+    readonly values: Arbitrary<unknown>[],
     readonly maxDepth: number,
     readonly maxKeys: number,
     readonly withSet: boolean,
@@ -28,7 +28,7 @@ export class ObjectConstraints {
   /**
    * Default value of ObjectConstraints.Settings.values field
    */
-  static defaultValues(): Arbitrary<any>[] {
+  static defaultValues(): Arbitrary<unknown>[] {
     return [
       boolean(),
       integer(),
@@ -131,7 +131,7 @@ export namespace ObjectConstraints {
      *  - `Number.POSITIVE_INFINITY`,
      *  - `Number.NEGATIVE_INFINITY`
      */
-    values?: Arbitrary<any>[];
+    values?: Arbitrary<unknown>[];
     /** Also generate boxed versions of values */
     withBoxedValues?: boolean;
     /** Also generate Set */
@@ -208,7 +208,7 @@ const objectInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
  * @example
  * ```null, undefined, 42, 6.5, 'Hello', {} or {k: [{}, 1, 2]}```
  */
-function anything(): Arbitrary<any>;
+function anything(): Arbitrary<unknown>;
 /**
  * For any type of values following the constraints defined by `settings`
  *
@@ -234,8 +234,8 @@ function anything(): Arbitrary<any>;
  *
  * @param settings Constraints to apply when building instances
  */
-function anything(settings: ObjectConstraints.Settings): Arbitrary<any>;
-function anything(settings?: ObjectConstraints.Settings): Arbitrary<any> {
+function anything(settings: ObjectConstraints.Settings): Arbitrary<unknown>;
+function anything(settings?: ObjectConstraints.Settings): Arbitrary<unknown> {
   return anythingInternal(ObjectConstraints.from(settings));
 }
 
@@ -247,7 +247,7 @@ function anything(settings?: ObjectConstraints.Settings): Arbitrary<any> {
  * @example
  * ```{} or {k: [{}, 1, 2]}```
  */
-function object(): Arbitrary<any>;
+function object(): Arbitrary<unknown>;
 /**
  * For any objects following the constraints defined by `settings`
  *
@@ -258,8 +258,8 @@ function object(): Arbitrary<any>;
  *
  * @param settings Constraints to apply when building instances
  */
-function object(settings: ObjectConstraints.Settings): Arbitrary<any>;
-function object(settings?: ObjectConstraints.Settings): Arbitrary<any> {
+function object(settings: ObjectConstraints.Settings): Arbitrary<unknown>;
+function object(settings?: ObjectConstraints.Settings): Arbitrary<unknown> {
   return objectInternal(ObjectConstraints.from(settings));
 }
 
@@ -275,7 +275,7 @@ function jsonSettings(stringArbitrary: Arbitrary<string>, maxDepth?: number) {
  *
  * Keys and string values rely on {@link string}
  */
-function jsonObject(): Arbitrary<any>;
+function jsonObject(): Arbitrary<unknown>;
 /**
  * For any JSON compliant values with a maximal depth
  *
@@ -283,8 +283,8 @@ function jsonObject(): Arbitrary<any>;
  *
  * @param maxDepth Maximal depth of the generated values
  */
-function jsonObject(maxDepth: number): Arbitrary<any>;
-function jsonObject(maxDepth?: number): Arbitrary<any> {
+function jsonObject(maxDepth: number): Arbitrary<unknown>;
+function jsonObject(maxDepth?: number): Arbitrary<unknown> {
   return anything(jsonSettings(string(), maxDepth));
 }
 
@@ -293,7 +293,7 @@ function jsonObject(maxDepth?: number): Arbitrary<any> {
  *
  * Keys and string values rely on {@link unicode}
  */
-function unicodeJsonObject(): Arbitrary<any>;
+function unicodeJsonObject(): Arbitrary<unknown>;
 /**
  * For any JSON compliant values with unicode support and a maximal depth
  *
@@ -301,8 +301,8 @@ function unicodeJsonObject(): Arbitrary<any>;
  *
  * @param maxDepth Maximal depth of the generated values
  */
-function unicodeJsonObject(maxDepth: number): Arbitrary<any>;
-function unicodeJsonObject(maxDepth?: number): Arbitrary<any> {
+function unicodeJsonObject(maxDepth: number): Arbitrary<unknown>;
+function unicodeJsonObject(maxDepth?: number): Arbitrary<unknown> {
   return anything(jsonSettings(unicodeString(), maxDepth));
 }
 

test/unit/check/arbitrary/ObjectArbitrary.spec.ts
@@ -333,7 +333,7 @@ describe('ObjectArbitrary', () => {
         fc.property(fc.integer(), seed => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const g = jsonObject().generate(mrng).value;
-          expect(JSON.parse(JSON.stringify(g))).toStrictEqual(g);
+          expect(JSON.parse(JSON.stringify(g))).toStrictEqual(g as any);
         })
       ));
   });
@@ -350,7 +350,7 @@ describe('ObjectArbitrary', () => {
         fc.property(fc.integer(), seed => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const g = unicodeJsonObject().generate(mrng).value;
-          expect(JSON.parse(JSON.stringify(g))).toStrictEqual(g);
+          expect(JSON.parse(JSON.stringify(g))).toStrictEqual(g as any);
         })
       ));
   });
@@ -401,7 +401,7 @@ describe('ObjectArbitrary', () => {
           while (shrinkable.shrink().has(v => true)[0]) {
             shrinkable = shrinkable.shrink().next().value;
           } // only check one shrink path
-          return typeof shrinkable.value === 'object' && Object.keys(shrinkable.value).length === 0;
+          return typeof shrinkable.value === 'object' && Object.keys(shrinkable.value as any).length === 0;
         })
       ));
     it('Should not suggest input in shrinked values', () =>
@@ -409,7 +409,7 @@ describe('ObjectArbitrary', () => {
         fc.property(fc.integer(), seed => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const shrinkable = object().generate(mrng);
-          for (const s of shrinkable.shrink()) expect(s.value).not.toStrictEqual(shrinkable.value);
+          for (const s of shrinkable.shrink()) expect(s.value).not.toStrictEqual(shrinkable.value as any);
         })
       ));
   });

test/unit/utils/stringify.spec.ts
@@ -47,7 +47,7 @@ describe('stringify', () => {
   it('Should be readable from eval', () =>
     fc.assert(
       fc.property(fc.anything(), obj => {
-        expect(eval(`(function() { return ${stringify(obj)}; })()`)).toStrictEqual(obj);
+        expect(eval(`(function() { return ${stringify(obj)}; })()`)).toStrictEqual(obj as any);
       })
     ));
   it('Should stringify differently distinct objects', () =>

commit 441a3ff6060c1c9d0cbe10a56911cb002c420ae1

Fix typing regression on object introduced by #436

src/check/arbitrary/ObjectArbitrary.ts
@@ -52,7 +52,7 @@ export class ObjectConstraints {
   }
 
   /** @hidden */
-  private static boxArbitraries(arbs: Arbitrary<any>[]): Arbitrary<any>[] {
+  private static boxArbitraries(arbs: Arbitrary<unknown>[]): Arbitrary<unknown>[] {
     return arbs.map(arb =>
       arb.map(v => {
         switch (typeof v) {
@@ -73,7 +73,7 @@ export class ObjectConstraints {
   }
 
   /** @hidden */
-  private static boxArbitrariesIfNeeded(arbs: Arbitrary<any>[], boxEnabled: boolean): Arbitrary<any>[] {
+  private static boxArbitrariesIfNeeded(arbs: Arbitrary<unknown>[], boxEnabled: boolean): Arbitrary<unknown>[] {
     return boxEnabled ? ObjectConstraints.boxArbitraries(arbs).concat(arbs) : arbs;
   }
 
@@ -144,7 +144,7 @@ export namespace ObjectConstraints {
 }
 
 /** @hidden */
-const anythingInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
+const anythingInternal = (constraints: ObjectConstraints): Arbitrary<unknown> => {
   const arbKeys = constraints.withObjectString
     ? memo(n =>
         frequency(
@@ -157,11 +157,11 @@ const anythingInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
   const maxDepth = constraints.maxDepth;
   const maxKeys = constraints.maxKeys;
 
-  const entriesOf = (keyArb: Arbitrary<any>, valueArb: Arbitrary<any>) =>
+  const entriesOf = <T, U>(keyArb: Arbitrary<T>, valueArb: Arbitrary<U>) =>
     set(tuple(keyArb, valueArb), 0, maxKeys, (t1, t2) => t1[0] === t2[0]);
 
-  const mapOf = (ka: Arbitrary<any>, va: Arbitrary<any>) => entriesOf(ka, va).map(v => new Map(v));
-  const dictOf = (ka: Arbitrary<string>, va: Arbitrary<any>) => entriesOf(ka, va).map(v => toObject(v));
+  const mapOf = <T, U>(ka: Arbitrary<T>, va: Arbitrary<U>) => entriesOf(ka, va).map(v => new Map(v));
+  const dictOf = <U>(ka: Arbitrary<string>, va: Arbitrary<U>) => entriesOf(ka, va).map(v => toObject(v));
 
   const baseArb = oneof(...arbitrariesForBase);
   const arrayBaseArb = oneof(...arbitrariesForBase.map(arb => array(arb, 0, maxKeys)));
@@ -180,7 +180,7 @@ const anythingInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
   // {[key:string]: base} | {[key:string]: anything}
   const objectArb = memo(n => oneof(objectBaseArb(n), dictOf(arbKeys(n), anythingArb(n))));
 
-  const anythingArb: Memo<any> = memo(n => {
+  const anythingArb: Memo<unknown> = memo(n => {
     if (n <= 0) return oneof(baseArb);
     return oneof(
       baseArb,
@@ -196,7 +196,7 @@ const anythingInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
 };
 
 /** @hidden */
-const objectInternal = (constraints: ObjectConstraints): Arbitrary<any> => {
+const objectInternal = (constraints: ObjectConstraints): Arbitrary<object> => {
   return dictionary(constraints.key, anythingInternal(constraints));
 };
 
@@ -247,7 +247,7 @@ function anything(settings?: ObjectConstraints.Settings): Arbitrary<unknown> {
  * @example
  * ```{} or {k: [{}, 1, 2]}```
  */
-function object(): Arbitrary<unknown>;
+function object(): Arbitrary<object>;
 /**
  * For any objects following the constraints defined by `settings`
  *
@@ -258,8 +258,8 @@ function object(): Arbitrary<unknown>;
  *
  * @param settings Constraints to apply when building instances
  */
-function object(settings: ObjectConstraints.Settings): Arbitrary<unknown>;
-function object(settings?: ObjectConstraints.Settings): Arbitrary<unknown> {
+function object(settings: ObjectConstraints.Settings): Arbitrary<object>;
+function object(settings?: ObjectConstraints.Settings): Arbitrary<object> {
   return objectInternal(ObjectConstraints.from(settings));
 }
 

test/unit/check/arbitrary/ObjectArbitrary.spec.ts
@@ -401,7 +401,7 @@ describe('ObjectArbitrary', () => {
           while (shrinkable.shrink().has(v => true)[0]) {
             shrinkable = shrinkable.shrink().next().value;
           } // only check one shrink path
-          return typeof shrinkable.value === 'object' && Object.keys(shrinkable.value as any).length === 0;
+          return typeof shrinkable.value === 'object' && Object.keys(shrinkable.value).length === 0;
         })
       ));
     it('Should not suggest input in shrinked values', () =>
@@ -409,7 +409,7 @@ describe('ObjectArbitrary', () => {
         fc.property(fc.integer(), seed => {
           const mrng = new Random(prand.xorshift128plus(seed));
           const shrinkable = object().generate(mrng);
-          for (const s of shrinkable.shrink()) expect(s.value).not.toStrictEqual(shrinkable.value as any);
+          for (const s of shrinkable.shrink()) expect(s.value).not.toStrictEqual(shrinkable.value);
         })
       ));
   });