e2aa008e1e2e6f16617784cb014cbb34a12bf2b2
Nicolas DUBIEN <github@dubien.org>
2020-01-26 22:05:58 +0100

    ✨ Add metadata on the generated sources (#526)

--- postbuild/main.js
+++ postbuild/main.js
@@ -1,5 +1,11 @@
 // eslint-disable-next-line
+const fs = require('fs');
+// eslint-disable-next-line
+const path = require('path');
+// eslint-disable-next-line
 const replace = require('replace-in-file');
+
+// Append *.js file extension on all local imports
 
 const options = {
   files: 'lib/**/*.js',
@@ -14,3 +20,36 @@ for (const { file, hasChanged } of results) {
     console.info(`Extensions added to: ${file}`);
   }
 }
+
+// Fill metas related to the package
+
+// eslint-disable-next-line
+fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
+  if (err) {
+    // eslint-disable-next-line
+    console.error(err.message);
+    return;
+  }
+
+  const packageVersion = JSON.parse(data.toString()).version;
+
+  const commonJsReplacement = replace.sync({
+    files: 'lib/fast-check-default.js',
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g],
+    to: ['commonjs', packageVersion]
+  });
+  if (commonJsReplacement.length === 1 && commonJsReplacement[0].hasChanged) {
+    // eslint-disable-next-line
+    console.info(`Package details added onto commonjs version`);
+  }
+
+  const moduleReplacement = replace.sync({
+    files: 'lib/esm/fast-check-default.js',
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g],
+    to: ['module', packageVersion]
+  });
+  if (moduleReplacement.length === 1 && moduleReplacement[0].hasChanged) {
+    // eslint-disable-next-line
+    console.info(`Package details added onto module version`);
+  }
+});
