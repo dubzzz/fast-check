05aa5098db6a9c8ba1486386e51f46a3a7b863cc
Nicolas DUBIEN <github@dubien.org>
2020-01-29 20:58:48 +0100

    Add an example using a function generator (#542)
    Fixes #531

--- example/001-simple/sort/main.spec.ts
+++ example/001-simple/sort/main.spec.ts
@@ -30,4 +30,22 @@ describe('sort', () => {
       })
     );
   });
+
+  it('should produce an ordered array with respect to a custom compare function', () => {
+    fc.assert(
+      fc.property(fc.array(fc.integer()), fc.compareBooleanFunc(), (data, compare) => {
+        const sorted = sort(data, compare);
+        for (let idx = 1; idx < sorted.length; ++idx) {
+          // compare(sorted[idx], sorted[idx - 1]):
+          // = true : sorted[idx - 1]  > sorted[idx]
+          // = false: sorted[idx - 1] <= sorted[idx]
+          expect(compare(sorted[idx], sorted[idx - 1])).toBe(false);
+          // Meaning of compare:
+          // a = b means in terms of ordering a and b are equivalent
+          // a < b means in terms of ordering a comes before b
+          // One important property is: a < b and b < c implies a < c
+        }
+      })
+    );
+  });
 });
