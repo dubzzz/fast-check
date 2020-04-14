740bc946d64a613f88e966f213f9fc473f8993f3
Nicolas DUBIEN <github@dubien.org>
2020-04-14 20:05:15 +0200

    📝 Adapt decompPrime to be compatible with CodeSandbox (#577)

--- example/001-simple/decompPrime/src/decompPrime.ts
+++ example/001-simple/decompPrime/src/decompPrime.ts
@@ -1,10 +1,18 @@
 export const decompPrime = (n: number): number[] => {
   // Quick implementation: the maximal number supported is 2**31-1
-  const stop = Math.sqrt(n);
-  for (let i = 2; i <= stop; ++i) {
-    if (n % i === 0) {
-      return [i, ...decompPrime(Math.floor(n / i))];
+  let done = false;
+  const factors: number[] = [];
+  while (!done) {
+    done = true;
+    const stop = Math.sqrt(n);
+    for (let i = 2; i <= stop; ++i) {
+      if (n % i === 0) {
+        factors.push(i);
+        n = Math.floor(n / i);
+        done = false;
+        break;
+      }
     }
   }
-  return [n];
+  return [...factors, n];
 };
