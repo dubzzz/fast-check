0720f49b16039bb9e3b558b5e71c5a592067769d
Nicolas DUBIEN <github@dubien.org>
2020-04-15 09:06:43 +0200

    📝 Add an example showing how to test race conditions with timers (#579)

---
+++ example/005-race/debounced-autocomplete/main.spec.tsx
@@ -0,0 +1,158 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import React from 'react';
+import DebouncedAutocomplete from './src/DebouncedAutocomplete';
+
+import { act, cleanup, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fc from 'fast-check';
+
+beforeEach(() => {
+  jest.clearAllTimers();
+  jest.resetAllMocks();
+  cleanup();
+});
+
+describe('DebouncedAutocomplete', () => {
+  it('should autocomplete queries (with mocked timers)', async () => {
+    await fc.assert(
+      fc
+        .asyncProperty(
+          fc.scheduler({ act }),
+          fc.set(fc.string()),
+          fc.string(1, 10),
+          async (s, allResults, userQuery) => {
+            // Arrange
+            jest.useFakeTimers();
+            const suggestionsFor = s.scheduleFunction(async (query: string) => {
+              return allResults.filter(r => r.includes(query)).slice(0, 10);
+            });
+            const expectedResults = allResults.filter(r => r.includes(userQuery));
+
+            // Act
+            const { getByRole, queryAllByRole } = render(<DebouncedAutocomplete suggestionsFor={suggestionsFor} />);
+            s.scheduleSequence(
+              [...userQuery].map((c, idx) => ({
+                label: `Typing "${c}"`,
+                builder: () => userEvent.type(getByRole('textbox'), userQuery.substr(0, idx + 1), { allAtOnce: true })
+              }))
+            );
+            await waitAllWithTimers(s);
+
+            // Assert
+            const displayedSuggestions = queryAllByRole('listitem');
+            expect(displayedSuggestions.map(el => el.textContent)).toEqual(expectedResults);
+          }
+        )
+        .beforeEach(() => {
+          jest.clearAllTimers();
+          jest.resetAllMocks();
+          cleanup();
+        })
+    );
+  });
+
+  it('should autocomplete queries (with mocked timers)', async () => {
+    await fc.assert(
+      fc
+        .asyncProperty(
+          fc.scheduler({ act }).map(withTimers),
+          fc.set(fc.string()),
+          fc.string(1, 10),
+          async (s, allResults, userQuery) => {
+            // Arrange
+            jest.useFakeTimers();
+            const suggestionsFor = s.scheduleFunction(async (query: string) => {
+              return allResults.filter(r => r.includes(query)).slice(0, 10);
+            });
+            const expectedResults = allResults.filter(r => r.includes(userQuery));
+
+            // Act
+            const { getByRole, queryAllByRole } = render(<DebouncedAutocomplete suggestionsFor={suggestionsFor} />);
+            s.scheduleSequence(
+              [...userQuery].map((c, idx) => ({
+                label: `Typing "${c}"`,
+                builder: () => userEvent.type(getByRole('textbox'), userQuery.substr(0, idx + 1), { allAtOnce: true })
+              }))
+            );
+            await s.waitAll();
+
+            // Assert
+            const displayedSuggestions = queryAllByRole('listitem');
+            expect(displayedSuggestions.map(el => el.textContent)).toEqual(expectedResults);
+          }
+        )
+        .beforeEach(() => {
+          jest.clearAllTimers();
+          jest.resetAllMocks();
+          cleanup();
+        })
+    );
+  });
+});
+
+// Helpers
+
+// Here is a first helper that can be used to mock timers
+// It should be used to replace calls to s.waitAll
+const waitAllWithTimers = async (s: fc.Scheduler) => {
+  let alreadyScheduledTaskToUnqueueTimers = false;
+  const countWithTimers = () => {
+    // Append a scheduled task to unqueue pending timers (if task missing and pending timers)
+    if (!alreadyScheduledTaskToUnqueueTimers && jest.getTimerCount() !== 0) {
+      alreadyScheduledTaskToUnqueueTimers = true;
+      s.schedule(Promise.resolve('advance timers if any')).then(() => {
+        alreadyScheduledTaskToUnqueueTimers = false;
+        jest.advanceTimersToNextTimer();
+      });
+    }
+    return s.count();
+  };
+  while (countWithTimers() !== 0) {
+    await s.waitOne();
+  }
+};
+
+// Here is a second helper that can be used to mock timers
+// It should to build the schdeuler: fc.scheduler({ act }).map(withTimers)
+const withTimers = (s: fc.Scheduler): fc.Scheduler => {
+  let alreadyScheduledTaskToUnqueueTimers = false;
+  const appendScheduledTaskToUnqueueTimersIfNeeded = () => {
+    // Append a scheduled task to unqueue pending timers (if task missing and pending timers)
+    if (!alreadyScheduledTaskToUnqueueTimers && jest.getTimerCount() !== 0) {
+      alreadyScheduledTaskToUnqueueTimers = true;
+      s.schedule(Promise.resolve('advance timers if any')).then(() => {
+        alreadyScheduledTaskToUnqueueTimers = false;
+        jest.advanceTimersToNextTimer();
+      });
+    }
+  };
+
+  return {
+    schedule(...args) {
+      return s.schedule(...args);
+    },
+    scheduleFunction(...args) {
+      return s.scheduleFunction(...args);
+    },
+    scheduleSequence(...args) {
+      return s.scheduleSequence(...args);
+    },
+    count() {
+      return s.count();
+    },
+    toString() {
+      return String(s);
+    },
+    async waitOne() {
+      appendScheduledTaskToUnqueueTimersIfNeeded();
+      await s.waitOne();
+    },
+    async waitAll() {
+      appendScheduledTaskToUnqueueTimersIfNeeded();
+      while (s.count()) {
+        await s.waitOne();
+        appendScheduledTaskToUnqueueTimersIfNeeded();
+      }
+    }
+  } as fc.Scheduler;
+};
---
+++ example/005-race/debounced-autocomplete/src/DebouncedAutocomplete.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+type Props = {
+  suggestionsFor: (query: string) => Promise<string[]>; // Unable to mock imports in CodeSandbox
+  bug?: boolean;
+};
+
+export default function DebouncedAutocomplete(props: Props) {
+  const { bug, suggestionsFor } = props;
+  const [query, setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([] as string[]);
+
+  useEffect(() => {
+    let canceled = false;
+    if (query === '') {
+      setSuggestions([]);
+      return;
+    }
+    const timer = setTimeout(
+      () =>
+        suggestionsFor(query).then(suggestions => {
+          if (!canceled || bug) {
+            setSuggestions(suggestions);
+          }
+        }),
+      500
+    );
+    return () => {
+      canceled = true;
+      clearTimeout(timer);
+    };
+  }, [query]);
+
+  return (
+    <div>
+      <label htmlFor="autocomplete-field">Select a package: </label>
+      <input id="autocomplete-field" role="textbox" value={query} onChange={e => setQuery(e.target.value)} />
+      <ul role="list">
+        {suggestions.map(s => (
+          <li key={s} role="listitem">
+            {s}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
