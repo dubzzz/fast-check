1ad120698621a5a592b2380c011941cf59ddd746
Nicolas DUBIEN <github@dubien.org>
2020-04-25 19:04:28 +0200

    Temporary disable node-with-import build as it is not supported with the current setup of the package

--- test/esm/run.sh
+++ test/esm/run.sh
@@ -6,19 +6,19 @@ set -x
 # Autoquit on crash
 set -e
 
-# Versions of node >=13.2.0 support es modules without any flag
-NODE_MAJOR=$(node --version | cut -d. -f 1 | cut -dv -f 2)
-NODE_MINOR=$(node --version | cut -d. -f 2)
-
-if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ]; then
-    cd node-with-import
-    yarn --frozen-lockfile --ignore-engines
-    yarn link "fast-check"
-    yarn run dry
-    yarn run start
-    diff expectedResult.txt out.txt
-    cd ..
-fi
+## Versions of node >=13.2.0 support es modules without any flag
+#NODE_MAJOR=$(node --version | cut -d. -f 1 | cut -dv -f 2)
+#NODE_MINOR=$(node --version | cut -d. -f 2)
+#
+#if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ]; then
+#    cd node-with-import
+#    yarn --frozen-lockfile --ignore-engines
+#    yarn link "fast-check"
+#    yarn run dry
+#    yarn run start
+#    diff expectedResult.txt out.txt
+#    cd ..
+#fi
 
 cd node-with-require
 yarn --frozen-lockfile --ignore-engines
