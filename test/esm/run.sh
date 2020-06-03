157287334c158a8d6dec0be0f2b36884a27c0543
Nicolas DUBIEN <github@dubien.org>
2020-06-03 09:35:07 +0200

    Check compatibility with node 12.x and >= 12.18
    Now that node 12 has been unflagged and support ES Modules out-of-the-box, we need to ensure it will work correctly with fast-check.
    
    For holder releases (<12.18) and new ones (>=12.18)

--- test/esm/run.sh
+++ test/esm/run.sh
@@ -7,10 +7,11 @@ set -x
 set -e
 
 ## Versions of node >=13.2.0 support es modules without any flag
+## Versions 12.x of node >=12.18.0 support es modules without any flag
 #NODE_MAJOR=$(node --version | cut -d. -f 1 | cut -dv -f 2)
 #NODE_MINOR=$(node --version | cut -d. -f 2)
 #
-#if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ]; then
+#if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ] || [ $NODE_MAJOR -eq 12 ] && [ $NODE_MINOR -ge 18 ]; then
 #    cd node-with-import
 #    yarn --frozen-lockfile --ignore-engines
 #    yarn link "fast-check"
