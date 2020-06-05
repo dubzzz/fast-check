8a0a8cd755db64e1c4b729ea542fbb2db4640060
Nicolas DUBIEN <github@dubien.org>
2020-06-05 22:27:54 +0200

    Better typings for fc.object and fc.dictionnary

--- src/check/arbitrary/ObjectArbitrary.ts
+++ src/check/arbitrary/ObjectArbitrary.ts
@@ -206,8 +206,7 @@ const anythingInternal = (constraints: ObjectConstraints): Arbitrary<unknown> =>
 };
 
 /** @hidden */
-// eslint-disable-next-line @typescript-eslint/ban-types
-const objectInternal = (constraints: ObjectConstraints): Arbitrary<object> => {
+const objectInternal = (constraints: ObjectConstraints): Arbitrary<Record<string, unknown>> => {
   return dictionary(constraints.key, anythingInternal(constraints));
 };
 
@@ -258,8 +257,7 @@ function anything(settings?: ObjectConstraints.Settings): Arbitrary<unknown> {
  * @example
  * ```{} or {k: [{}, 1, 2]}```
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-function object(): Arbitrary<object>;
+function object(): Arbitrary<Record<string, unknown>>;
 /**
  * For any objects following the constraints defined by `settings`
  *
@@ -270,10 +268,8 @@ function object(): Arbitrary<object>;
  *
  * @param settings Constraints to apply when building instances
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-function object(settings: ObjectConstraints.Settings): Arbitrary<object>;
-// eslint-disable-next-line @typescript-eslint/ban-types
-function object(settings?: ObjectConstraints.Settings): Arbitrary<object> {
+function object(settings: ObjectConstraints.Settings): Arbitrary<Record<string, unknown>>;
+function object(settings?: ObjectConstraints.Settings): Arbitrary<Record<string, unknown>> {
   return objectInternal(ObjectConstraints.from(settings));
 }
 
