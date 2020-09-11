262094bb77f4df4e8940b98dea4ea5fe0e32a003
Nicolas DUBIEN <github@dubien.org>
2020-09-11 20:29:29 +0200

    Retry

--- src/check/precondition/Pre.ts
+++ src/check/precondition/Pre.ts
@@ -5,7 +5,7 @@ import { PreconditionFailure } from './PreconditionFailure';
  * @param expectTruthy - cancel the run whenever this value is falsy
  * @public
  */
-export function pre(expectTruthy: boolean): asserts expectTruthy {
+export function pre(expectTruthy: boolean): asserts expectTruthy is true {
   if (!expectTruthy) {
     throw new PreconditionFailure();
   }
