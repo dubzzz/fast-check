b4bd38e099043e7eeb3cae86b25c792700e3f41f
Nicolas DUBIEN <github@dubien.org>
2020-09-17 22:37:50 +0200

    ✨ Automatically simplify min and max in codemod for arrays (#987)

--- codemods/2.4.0_explicit-min-max-length/snippet-4.ts
+++ codemods/2.4.0_explicit-min-max-length/snippet-4.ts
@@ -6,20 +6,30 @@ import fcLocal from './path';
 import { array as localArray } from './path';
 
 test('test default import', () => {
-  fc.assert(fc.property(fc.array(fc.nat(), 7, 10), () => true));
+  fc.assert(fc.property(fc.array(fc.nat(), 7, 42), () => true));
 });
 test('test star import', () => {
-  fcStar.assert(fcStar.property(fcStar.array(fcStar.nat(), 7, 10), () => true));
+  fcStar.assert(fcStar.property(fcStar.array(fcStar.nat(), 7, 42), () => true));
 });
 test('test specific', () => {
-  assert(property(array(nat(), 7, 10), () => true));
+  assert(property(array(nat(), 7, 42), () => true));
 });
 test('test specific named', () => {
-  assert(property(fcArray(nat(), 7, 10), () => true));
+  assert(property(fcArray(nat(), 7, 42), () => true));
 });
 test('test default import (local)', () => {
-  fcLocal.assert(fcLocal.property(fcLocal.array(fcLocal.nat(), 7, 10), () => true));
+  fcLocal.assert(fcLocal.property(fcLocal.array(fcLocal.nat(), 7, 42), () => true));
 });
 test('test specific named (local)', () => {
-  assert(property(localArray(nat(), 7, 10), () => true));
+  assert(property(localArray(nat(), 7, 42), () => true));
+});
+test('test simplify unneeded min', () => {
+  fc.assert(fc.property(fc.array(fc.nat(), 0, 42), () => true));
+});
+test('test simplify unneeded max', () => {
+  fc.assert(fc.property(fc.array(fc.nat(), 10), () => true));
+  fc.assert(fc.property(fc.array(fc.nat(), 1, 10), () => true));
+});
+test('test simplify unneeded min and max', () => {
+  fc.assert(fc.property(fc.array(fc.nat(), 0, 10), () => true));
 });
