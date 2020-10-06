e21b25b8738910ed90bbe87f24cc358fbd879023
Nicolas DUBIEN <github@dubien.org>
2020-10-06 23:29:09 +0200

    [Towards new float] Implement float32-related helpers

---
+++ src/check/arbitrary/FloatNextArbitrary.ts
@@ -0,0 +1,96 @@
+/**
+ * Decompose a 32-bit floating point number into a mantissa and exponent
+ * such as:
+ * - mantissa over 24 bits including sign (also referred as fraction)
+ * - exponent over 8 bits including sign
+ * - whenever there are multiple possibilities we take the one having the highest mantissa (in abs)
+ *
+ * Remark in 64-bit floating point number:
+ * - mantissa is over 53 bits including sign
+ * - exponent is over 11 bits including sign
+ * - Number.MAX_VALUE = 2**1023    * (1 + (2**52-1)/2**52)
+ * - Number.MIN_VALUE = 2**(-1022) * 2**(-52)
+ * - Number.EPSILON   = 2**(-52)
+ *
+ * @param f - 32-bit floating point number to be decomposed into (mantissa, exponent)
+ *
+ * @internal
+ */
+export function decomposeFloat(f: number): { exponent: number; mantissa: number } {
+  // 1 => mantissa 0b1   - exponent 1 (will be preferred)
+  //   => mantissa 0b0.1 - exponent 2
+  const maxMantissa = 1 + (2 ** 23 - 1) / 2 ** 23;
+  for (let exponent = -126; exponent !== 128; ++exponent) {
+    const powExponent = 2 ** exponent;
+    const maxForExponent = maxMantissa * powExponent;
+    if (Math.abs(f) <= maxForExponent) {
+      return { exponent, mantissa: f / powExponent };
+    }
+  }
+  return { exponent: Number.NaN, mantissa: Number.NaN };
+}
+
+/** @internal */
+function indexInFloatFromDecomp(exponent: number, mantissa: number) {
+  // WARNING: mantissa >= 0
+
+  // By construct of mantissa in decomposeFloat,
+  // mantissa is always max-ed.
+
+  // The float close to zero are the only one having a mantissa <1, they also have an exponent of -126.
+  // They are in range: [2**(-126) * 2**(-23), 2**(-126) * (2 - 2 ** 23)]
+  // In other words there are 2**24 elements in that range if we include zero.
+  // All other ranges (other exponents) have a length of 2**23 elements.
+  if (exponent === -126) {
+    return mantissa * 0x800000; // mantissa * 2**23
+  }
+  // Offset due to exp = -126 + Offset of previous exp (excl. -126) + Offset in current exp
+  // 2**24 + (exponent - (-126) -1) * 2**23 + (mantissa - 1) * 2**23
+  return (exponent + 127) * 0x800000 + (mantissa - 1) * 0x800000;
+}
+
+/**
+ * Compute the index of f relative to other available 32-bit floating point numbers
+ * Rq: Produces negative indexes for negative floats
+ *
+ * @param f - 32-bit floating point number
+ *
+ * @internal
+ */
+export function floatToIndex(f: number): number {
+  const decomp = decomposeFloat(f);
+  const exponent = decomp.exponent;
+  const mantissa = decomp.mantissa;
+  if (Number.isNaN(exponent) || Number.isNaN(mantissa)) {
+    return Number.NaN;
+  }
+  if (f > 0 || (f === 0 && 1 / f === Number.POSITIVE_INFINITY)) {
+    return indexInFloatFromDecomp(exponent, mantissa);
+  } else {
+    return -indexInFloatFromDecomp(exponent, -mantissa) - 1;
+  }
+}
+
+/**
+ * Compute the 32-bit floating point number corresponding to the provided indexes
+ *
+ * @param n - index of the float
+ *
+ * @internal
+ */
+export function indexToFloat(index: number): number {
+  if (index < 0) {
+    return -indexToFloat(-index - 1);
+  }
+  if (index < 0x1000000) {
+    // The first 2**24 elements correspond to values having
+    // exponent = -126 and mantissa = index * 2**(-23)
+    return index * 2 ** -149;
+  }
+  const postIndex = index - 0x1000000;
+  // Math.floor(postIndex / 0x800000) = Math.floor(postIndex / 2**23) = (postIndex >> 23)
+  const exponent = -125 + (postIndex >> 23);
+  // (postIndex % 0x800000) / 0x800000 = (postIndex & 0x7fffff) / 0x800000
+  const mantissa = 1 + (postIndex & 0x7fffff) / 0x800000;
+  return mantissa * 2 ** exponent;
+}
---
+++ test/unit/check/arbitrary/FloatNextArbitrary.spec.ts
@@ -0,0 +1,101 @@
+import * as fc from '../../../../lib/fast-check';
+
+import { decomposeFloat, floatToIndex, indexToFloat } from '../../../../src/check/arbitrary/FloatNextArbitrary';
+
+const MIN_VALUE_32 = 2 ** -126 * 2 ** -23;
+const MAX_VALUE_32 = 2 ** 127 * (1 + (2 ** 23 - 1) / 2 ** 23);
+const EPSILON_32 = 2 ** -23;
+
+describe('FloatNextArbitrary', () => {
+  describe('decomposeFloat (@internal)', () => {
+    it('Should properly decompose basic values', () => {
+      expect(decomposeFloat(0)).toEqual({ exponent: -126, mantissa: 0 });
+      expect(decomposeFloat(1)).toEqual({ exponent: 0, mantissa: 1 });
+      expect(decomposeFloat(128)).toEqual({ exponent: 7, mantissa: 1 });
+      expect(decomposeFloat(201)).toEqual({ exponent: 7, mantissa: 1.5703125 });
+    });
+    it('Should properly decompose negative values', () => {
+      expect(decomposeFloat(-0)).toEqual({ exponent: -126, mantissa: -0 });
+      expect(decomposeFloat(-1)).toEqual({ exponent: 0, mantissa: -1 });
+    });
+    it('Should properly decompose extreme values', () => {
+      expect(decomposeFloat(MAX_VALUE_32)).toEqual({ exponent: 127, mantissa: 1 + (2 ** 23 - 1) / 2 ** 23 });
+      expect(decomposeFloat(MIN_VALUE_32)).toEqual({ exponent: -126, mantissa: 2 ** -23 });
+      expect(decomposeFloat(EPSILON_32)).toEqual({ exponent: -23, mantissa: 1 });
+      expect(decomposeFloat(1 + EPSILON_32)).toEqual({ exponent: 0, mantissa: 1 + 2 ** -23 });
+    });
+    it('Should decompose a 32-bit float into its equivalent (mantissa, exponent)', () =>
+      fc.assert(
+        fc.property(
+          fc.integer().map((n32) => new Float32Array(new Int32Array([n32]).buffer)[0]),
+          (f32) => {
+            fc.pre(!Number.isNaN(f32) && Number.isFinite(f32));
+            const { exponent, mantissa } = decomposeFloat(f32);
+            expect(mantissa * 2 ** exponent).toBe(f32);
+          }
+        )
+      ));
+  });
+});
+
+describe('floatToIndex (@internal)', () => {
+  it('Should properly compute indexes', () => {
+    expect(floatToIndex(0)).toBe(0);
+    expect(floatToIndex(MIN_VALUE_32)).toBe(1);
+    expect(floatToIndex(2 * MIN_VALUE_32)).toBe(2);
+    expect(floatToIndex(3 * MIN_VALUE_32)).toBe(3);
+    // EPSILON_32 === 1. * 2**-23 --> m = 1, e = -23
+    // index(EPSILON_32) = 2**24 + (-23 - (-126) -1) * 2**23
+    expect(floatToIndex(EPSILON_32)).toBe(872415232);
+    // index(1 - EPSILON_32 / 2) = index(1) - 1
+    expect(floatToIndex(1 - EPSILON_32 / 2)).toEqual(1065353215);
+    // 1 === 1. * 2**0 --> m = 1, e = 0
+    // index(1) = 2**24 + (0 - (-126) -1) * 2**23
+    expect(floatToIndex(1)).toEqual(1065353216);
+    // index(1 + EPSILON_32) = index(1) + 1
+    expect(floatToIndex(1 + EPSILON_32)).toEqual(1065353217);
+    // index(2 - EPSILON_32) = index(2) - 1 = index(1 + (2 ** 23 - 1) * EPSILON_32)
+    expect(floatToIndex(2 - EPSILON_32)).toEqual(1073741823);
+    // 1 === 1. * 2**1 --> m = 1, e = 1
+    // index(2) = index(1) * 2**23
+    expect(floatToIndex(2)).toEqual(1073741824);
+    expect(floatToIndex(MAX_VALUE_32)).toBe(2139095039);
+  });
+  it('Should properly compute negative indexes', () => {
+    expect(floatToIndex(-0)).toEqual(-1);
+    expect(floatToIndex(-MIN_VALUE_32)).toBe(-2);
+    expect(floatToIndex(-MAX_VALUE_32)).toBe(-2139095040);
+  });
+  it('Should preserve ordering between two floats', () =>
+    fc.assert(
+      fc.property(
+        fc.integer().map((n32) => new Float32Array(new Int32Array([n32]).buffer)[0]),
+        fc.integer().map((n32) => new Float32Array(new Int32Array([n32]).buffer)[0]),
+        (fa32, fb32) => {
+          fc.pre(!Number.isNaN(fa32) && Number.isFinite(fa32) && !Number.isNaN(fb32) && Number.isFinite(fb32));
+          if (fa32 <= fb32) expect(floatToIndex(fa32)).toBeLessThanOrEqual(floatToIndex(fb32));
+          else expect(floatToIndex(fa32)).toBeGreaterThan(floatToIndex(fb32));
+        }
+      )
+    ));
+});
+
+describe('indexToFloat (@internal)', () => {
+  it('Should only produce 32-bit floating point numbers', () =>
+    fc.assert(
+      fc.property(fc.integer(-2139095040, 2139095039), (index) => {
+        const f = indexToFloat(index);
+        expect(f).toBe(new Float32Array([f])[0]);
+      })
+    ));
+  it('Should reverse floatToIndex', () =>
+    fc.assert(
+      fc.property(
+        fc.integer().map((n32) => new Float32Array(new Int32Array([n32]).buffer)[0]),
+        (f32) => {
+          fc.pre(!Number.isNaN(f32) && Number.isFinite(f32));
+          expect(indexToFloat(floatToIndex(f32))).toBe(f32);
+        }
+      )
+    ));
+});
