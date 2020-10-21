c232b530f5dabeadf6f58a7daf94c74a1076afc9
Nicolas DUBIEN <github@dubien.org>
2020-10-21 22:58:35 +0200

    🎨 Stricter limits on email arbitrary (#1034)

--- test/unit/check/arbitrary/EmailArbitrary.spec.ts
+++ test/unit/check/arbitrary/EmailArbitrary.spec.ts
@@ -8,6 +8,13 @@ const isValidEmailRfc1123 = (t: string) => {
   return rfc1123.test(t);
 };
 
+const isValidEmailRfc2821 = (t: string) => {
+  const [localPart, domain] = t.split('@');
+  // The maximum total length of a user name or other local-part is 64 characters.
+  // The maximum total length of a domain name or number is 255 characters.
+  return localPart.length <= 64 && domain.length <= 255;
+};
+
 const isValidEmailRfc5322 = (t: string) => {
   // Taken from https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression
   const rfc5322 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -19,6 +26,11 @@ describe('EmailArbitrary', () => {
     describe('RFC 1123', () => {
       genericHelper.isValidArbitrary(() => emailAddress(), {
         isValidValue: (g: string) => isValidEmailRfc1123(g),
+      });
+    });
+    describe('RFC 2821', () => {
+      genericHelper.isValidArbitrary(() => emailAddress(), {
+        isValidValue: (g: string) => isValidEmailRfc2821(g),
       });
     });
     describe('RFC 5322', () => {
