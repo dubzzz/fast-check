a3d538b3b4ad10941902c21098fcc53c896144f6
Nicolas DUBIEN <github@dubien.org>
2020-11-14 23:27:59 +0100

    FIX Wrongly set commit hash

--- .github/actions/install-deps-with-current-fc/check-hash.js
+++ .github/actions/install-deps-with-current-fc/check-hash.js
@@ -1,19 +1,15 @@
-// eslint-disable-next-line
-const { exec } = require('child_process');
-const { exit } = require('process');
+const process = require('process');
 const path = require('path');
 const { __commit_hash } = require(path.join(__dirname, '..', '..', '..', process.argv[2]));
 
-exec('git rev-parse HEAD', (err, stdout, _stderr) => {
-  if (err) {
-    console.error('Unable to get commit hash: ' + err.message);
-    exit(1);
-  }
-  const expectedCommitHash = stdout.split('\n')[0];
-  if (expectedCommitHash !== __commit_hash) {
-    console.error('Expected: ' + expectedCommitHash + ', got: ' + __commit_hash);
-    exit(2);
-  }
-  console.log('✔️ Referencing the correct version of fast-check');
-  console.log('✔️ Commit hash matches expected one: ' + expectedCommitHash);
-});
+const expectedCommitHash = process.env.COMMIT_HASH;
+if (!expectedCommitHash) {
+  console.error('No COMMIT_HASH specified');
+  process.exit(1);
+}
+if (expectedCommitHash !== __commit_hash) {
+  console.error('Expected: ' + expectedCommitHash + ', got: ' + __commit_hash);
+  process.exit(2);
+}
+console.log('✔️ Referencing the correct version of fast-check');
+console.log('✔️ Commit hash matches expected one: ' + expectedCommitHash);
--- postbuild/main.cjs
+++ postbuild/main.cjs
@@ -1,9 +1,9 @@
 // eslint-disable-next-line
-const { exec } = require('child_process');
-// eslint-disable-next-line
 const fs = require('fs');
 // eslint-disable-next-line
 const path = require('path');
+// eslint-disable-next-line
+const process = require('process');
 // eslint-disable-next-line
 const replace = require('replace-in-file');
 
@@ -25,53 +25,50 @@ for (const { file, hasChanged } of results) {
 
 // Fill metas related to the package
 
-exec('git rev-parse HEAD', (err, stdout, _stderr) => {
+const commitHash = process.env.COMMIT_HASH && process.env.COMMIT_HASH.split('\n')[0];
+if (!commitHash) {
+  // eslint-disable-next-line
+  console.error('No COMMIT_HASH specified (env), please use: `COMMIT_HASH=$(git rev-parse HEAD)` to set it');
+  process.exit(1);
+}
+
+// eslint-disable-next-line
+fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
   if (err) {
     // eslint-disable-next-line
     console.error(err.message);
-    return;
+    process.exit(2);
   }
 
-  const commitHash = stdout.split('\n')[0];
+  const packageVersion = JSON.parse(data.toString()).version;
 
-  // eslint-disable-next-line
-  fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
-    if (err) {
-      // eslint-disable-next-line
-      console.error(err.message);
-      return;
-    }
-
-    const packageVersion = JSON.parse(data.toString()).version;
-
-    const commonJsReplacement = replace.sync({
-      files: 'lib/fast-check-default.js',
-      from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
-      to: ['commonjs', packageVersion, commitHash],
-    });
-    if (commonJsReplacement.length === 1 && commonJsReplacement[0].hasChanged) {
-      // eslint-disable-next-line
-      console.info(`Package details added onto commonjs version`);
-    }
-
-    const moduleReplacement = replace.sync({
-      files: 'lib/esm/fast-check-default.js',
-      from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
-      to: ['module', packageVersion, commitHash],
-    });
-    if (moduleReplacement.length === 1 && moduleReplacement[0].hasChanged) {
-      // eslint-disable-next-line
-      console.info(`Package details added onto module version`);
-    }
-
-    const dTsReplacement = replace.sync({
-      files: 'lib/types/fast-check-default.d.ts',
-      from: [/__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
-      to: [packageVersion, commitHash],
-    });
-    if (dTsReplacement.length === 1 && dTsReplacement[0].hasChanged) {
-      // eslint-disable-next-line
-      console.info(`Package details added onto d.ts version`);
-    }
+  const commonJsReplacement = replace.sync({
+    files: 'lib/fast-check-default.js',
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
+    to: ['commonjs', packageVersion, commitHash],
   });
+  if (commonJsReplacement.length === 1 && commonJsReplacement[0].hasChanged) {
+    // eslint-disable-next-line
+    console.info(`Package details added onto commonjs version`);
+  }
+
+  const moduleReplacement = replace.sync({
+    files: 'lib/esm/fast-check-default.js',
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
+    to: ['module', packageVersion, commitHash],
+  });
+  if (moduleReplacement.length === 1 && moduleReplacement[0].hasChanged) {
+    // eslint-disable-next-line
+    console.info(`Package details added onto module version`);
+  }
+
+  const dTsReplacement = replace.sync({
+    files: 'lib/types/fast-check-default.d.ts',
+    from: [/__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
+    to: [packageVersion, commitHash],
+  });
+  if (dTsReplacement.length === 1 && dTsReplacement[0].hasChanged) {
+    // eslint-disable-next-line
+    console.info(`Package details added onto d.ts version`);
+  }
 });
