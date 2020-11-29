28a5950db6e36eb6d6ec08d599c2ce763e2aa8ca
Nicolas DUBIEN <github@dubien.org>
2020-11-29 20:31:31 +0100

    Check that it produces enough intermediate values

--- test/e2e/arbitraries/DoubleNextArbitrary.spec.ts
+++ test/e2e/arbitraries/DoubleNextArbitrary.spec.ts
@@ -5,16 +5,16 @@ const seed = Date.now();
 describe(`DoubleNextArbitrary (seed: ${seed})`, () => {
   describe('doubleNext', () => {
     const numRuns = 10000;
-    const sampleFloatNext = fc.sample(fc.float({ next: true }), { seed, numRuns });
-    const sampleFloatNextNoBias = fc.sample(fc.float({ next: true }).noBias(), { seed, numRuns });
+    const sampleDoubleNext = fc.sample(fc.double({ next: true }), { seed, numRuns });
+    const sampleDoubleNextNoBias = fc.sample(fc.double({ next: true }).noBias(), { seed, numRuns });
 
     function shouldGenerate(expectedValue: number) {
       it('Should be able to generate ' + fc.stringify(expectedValue), () => {
-        const hasValue = sampleFloatNext.findIndex((v) => Object.is(v, expectedValue)) !== -1;
+        const hasValue = sampleDoubleNext.findIndex((v) => Object.is(v, expectedValue)) !== -1;
         expect(hasValue).toBe(true);
       });
       it('Should not be able to generate ' + fc.stringify(expectedValue) + ' if not biased (very unlikely)', () => {
-        const hasValue = sampleFloatNextNoBias.findIndex((v) => Object.is(v, expectedValue)) !== -1;
+        const hasValue = sampleDoubleNextNoBias.findIndex((v) => Object.is(v, expectedValue)) !== -1;
         expect(hasValue).toBe(false);
       });
     }
@@ -28,5 +28,25 @@ describe(`DoubleNextArbitrary (seed: ${seed})`, () => {
     shouldGenerate(-Number.MIN_VALUE);
     shouldGenerate(Number.MAX_VALUE);
     shouldGenerate(-Number.MAX_VALUE);
+
+    // Remark: Number.MIN_VALUE = (2**-1022) * (2**-52)
+    // Remark: Number.MAX_VALUE = (2**1023) * (2 - 2**-52)
+    // In range: [Number.MIN_VALUE ; 2 ** -1021[ there are 2**53 distinct values
+    // In range: [2**1023 ; Number.MAX_VALUE] there are 2**52 distinct values
+    // They represent 0.07 % of all the possible values
+    // (indeed there are 18437736874454810624 distinct values for double without -infinity, +infinty and NaN)
+    const filterIntermediateValues = (sample: number[]) => {
+      return sample.filter((v) => v >= 2 ** -1021 && v < 2 ** 1023);
+    };
+
+    it('Should be able to generate intermediate values most of the time even if biased', () => {
+      const countIntermediate = filterIntermediateValues(sampleDoubleNext).length;
+      expect(countIntermediate).toBeGreaterThan(0.5 * sampleDoubleNext.length);
+    });
+
+    it('Should be able to generate intermediate values most of the time if not biased', () => {
+      const countIntermediate = filterIntermediateValues(sampleDoubleNextNoBias).length;
+      expect(countIntermediate).toBeGreaterThan(0.5 * sampleDoubleNextNoBias.length);
+    });
   });
 });
