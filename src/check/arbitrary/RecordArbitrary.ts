d8882a1b9669f677f8cd36359661291a52eed2e0
Nicolas DUBIEN <github@dubien.org>
2020-12-28 22:22:16 +0100

    ✨ Arbitrary records with optional fields (#1266)
    Related to #490

--- src/check/arbitrary/RecordArbitrary.ts
+++ src/check/arbitrary/RecordArbitrary.ts
@@ -7,10 +7,22 @@ import { genericTuple } from './TupleArbitrary';
  * Constraints to be applied on {@link record}
  * @public
  */
-export interface RecordConstraints {
-  /** Allow to remove keys from the generated record */
-  withDeletedKeys?: boolean;
-}
+export type RecordConstraints<T = unknown> =
+  | {
+      /**
+       * List keys that should never be deleted.
+       * Warning: Cannot be used in conjunction with withDeletedKeys.
+       */
+      requiredKeys?: T[];
+    }
+  | {
+      /**
+       * Allow to remove keys from the generated record.
+       * Warning: Cannot be used in conjunction with requiredKeys.
+       * Prefer: `requiredKeys: []` over `withDeletedKeys: true`
+       */
+      withDeletedKeys?: boolean;
+    };
 
 /**
  * Infer the type of the Arbitrary produced by record
@@ -18,10 +30,12 @@ export interface RecordConstraints {
  * @public
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type RecordValue<T, Constraints = {}> = Constraints extends {
-  withDeletedKeys: true;
-}
+export type RecordValue<T, TConstraints = {}> = TConstraints extends { withDeletedKeys: boolean; requiredKeys: any[] }
+  ? never
+  : TConstraints extends { withDeletedKeys: true }
   ? Partial<T>
+  : TConstraints extends { requiredKeys: (infer TKeys)[] }
+  ? Partial<T> & Pick<T, TKeys & keyof T>
   : T;
 
 /** @internal */
@@ -69,18 +83,38 @@ function record<T>(recordModel: { [K in keyof T]: Arbitrary<T[K]> }): Arbitrary<
  *
  * @public
  */
-function record<T, Constraints extends RecordConstraints>(
+function record<T, TConstraints extends RecordConstraints<keyof T>>(
   recordModel: { [K in keyof T]: Arbitrary<T[K]> },
-  constraints: Constraints
-): Arbitrary<RecordValue<{ [K in keyof T]: T[K] }, Constraints>>;
-function record<T>(recordModel: { [K in keyof T]: Arbitrary<T[K]> }, constraints?: RecordConstraints) {
-  if (constraints == null || constraints.withDeletedKeys !== true) {
+  constraints: TConstraints
+): Arbitrary<RecordValue<{ [K in keyof T]: T[K] }, TConstraints>>;
+function record<T>(recordModel: { [K in keyof T]: Arbitrary<T[K]> }, constraints?: RecordConstraints<keyof T>) {
+  if (constraints == null) {
+    return rawRecord(recordModel);
+  }
+  if ('withDeletedKeys' in constraints && 'requiredKeys' in constraints) {
+    throw new Error(`requiredKeys and withDeletedKeys cannot be used together in fc.record`);
+  }
+
+  const requireDeletedKeys =
+    ('requiredKeys' in constraints && constraints.requiredKeys !== undefined) ||
+    ('withDeletedKeys' in constraints && !!constraints.withDeletedKeys);
+  if (!requireDeletedKeys) {
     return rawRecord(recordModel);
   }
 
   const updatedRecordModel: { [key: string]: Arbitrary<{ value: T[keyof T] } | null> } = {};
+  const requiredKeys = ('requiredKeys' in constraints ? constraints.requiredKeys : undefined) || [];
+
+  for (let idx = 0; idx !== requiredKeys.length; ++idx) {
+    if (!(requiredKeys[idx] in recordModel)) {
+      throw new Error(`requiredKeys cannot reference keys that have not been defined in recordModel`);
+    }
+  }
+
   for (const k in recordModel) {
-    updatedRecordModel[k] = option(recordModel[k].map((v) => ({ value: v })));
+    const requiredArbitrary = recordModel[k].map((v) => ({ value: v }));
+    if (requiredKeys.indexOf(k) !== -1) updatedRecordModel[k] = requiredArbitrary;
+    else updatedRecordModel[k] = option(requiredArbitrary);
   }
   return rawRecord(updatedRecordModel).map((obj) => {
     const nobj: { [key: string]: T[keyof T] } = {};
