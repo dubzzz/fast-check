a39adbca1042e65883deabe8b5df821d20493ee3
Nicolas DUBIEN <github@dubien.org>
2020-12-29 17:40:34 +0100

    👷 Comment with links to install the package defined by the PR (#1296)
    * Try to add more context through comments on PR
    
    * Add missing checkout
    
    * Install dependencies
    
    * Get back token from the context
    
    * Add error handling
    
    * Swith to octokit
    
    * Revert "Get back token from the context"
    
    This reverts commit 9b8a5b4542fb8f5835d7a5df5bd7943b19c37e72.
    
    * Better commit hash
    
    * Add missing new lines
    
    * Revert "Better commit hash"
    
    This reverts commit 85b8b4de83fb7e7de6b7e980e3ea93804e38a136.
    
    * Try to get back the correct commit hash
    
    * Add more context on failure
    
    * Temporary log as warnings
    
    * Only log stringilfied error
    
    * Log as info
    
    * Early exec
    
    * git command based on commit hash
    
    * debug more
    
    * Extract commit hash
    
    * Only comment on succesful bundle build
    
    * Add new action into dependabot

---
+++ .github/actions/comment-on-pr/index.cjs
@@ -0,0 +1,73 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require('child_process');
+
+const verboseLog = (...args) => {
+  core.info(args.join(' '));
+};
+const cleanErr = (err) => {
+  if (!err) return err;
+  const { stack, ...others } = err;
+  return others;
+};
+const execAsync = (command, options) => {
+  const prettyCmd = `exec(${JSON.stringify(command)}, ${JSON.stringify(options)}})`;
+  return new Promise((resolve) => {
+    verboseLog(`Call to ${prettyCmd}`);
+    exec(command, options, (err, stdout, stderr) => {
+      verboseLog(`Answer from ${prettyCmd}`);
+      verboseLog(`err:`, JSON.stringify(cleanErr(err)));
+      verboseLog(`stdout:`, stdout.toString());
+      verboseLog(`stderr:`, stderr.toString());
+      resolve({ err, stdout, stderr });
+    });
+  });
+};
+
+async function run() {
+  const context = github.context;
+  const token = core.getInput('token', { required: true });
+
+  if (context.eventName !== 'pull_request') {
+    core.setFailed(`comment-on-pr can only be used on pull_request`);
+    return;
+  }
+
+  const { err, stdout: commitMessage } = await execAsync('git log -1 --format=%s');
+  if (err && err.code) {
+    core.setFailed(`comment-on-pr failed to get back commit hash, failed with error: ${err}`);
+    return;
+  }
+  const commitMessageRegex = /^Merge ([a-f0-9]+) into ([a-f0-9]+)$/;
+  const m = commitMessageRegex.exec(commitMessage.trim());
+  if (!m) {
+    core.setFailed(`comment-on-pr invalid commit message encountered, got: ${commitMessage.trim()}`);
+    return;
+  }
+
+  const commitHash = m[1];
+  const packageUrl = `https://pkg.csb.dev/dubzzz/fast-check/commit/${commitHash.substring(0, 8)}/fast-check`;
+  const octokit = github.getOctokit(token);
+  const body =
+    `Give a try to https://github.com/dubzzz/fast-check/pull/${context.issue.number}/commits/${commitHash} with:\n\n` +
+    '```bash\n' +
+    `yarn add ${packageUrl}\n` +
+    `npm i ${packageUrl}\n` +
+    '```\n\n' +
+    '<details>\n' +
+    '<summary>More details on this run</summary>\n\n' +
+    `- On Codeclimate: https://codeclimate.com/github/dubzzz/fast-check/pull/${context.issue.number}\n` +
+    `- On Codecov: https://codecov.io/gh/dubzzz/fast-check/pull/${context.issue.number}\n` +
+    `- On CodeSandbox: https://ci.codesandbox.io/status/dubzzz/fast-check/pr/${context.issue.number}\n` +
+    `- On GitHub Actions: https://github.com/dubzzz/fast-check/actions/runs/${context.runId}\n` +
+    '</details>';
+
+  await octokit.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body,
+  });
+}
+
+run().catch((err) => core.setFailed(`Failed with error: ${err}`));
