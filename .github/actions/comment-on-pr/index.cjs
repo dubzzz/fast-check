ed04e2492adcf5d883a33482da975afcf85d342d
Nicolas DUBIEN <github@dubien.org>
2020-12-29 16:31:34 +0100

    Swith to octokit

--- .github/actions/comment-on-pr/index.cjs
+++ .github/actions/comment-on-pr/index.cjs
@@ -1,13 +1,14 @@
 const core = require('@actions/core');
-const { context, GitHub } = require('@actions/github');
+const github = require('@actions/github');
 
 async function run() {
+  const context = github.context;
   if (context.eventName !== 'pull_request') {
     core.setFailed(`comment-on-pr can only be used on pull_request`);
     return;
   }
   const packageUrl = `https://pkg.csb.dev/dubzzz/fast-check/commit/${context.sha.substring(0, 8)}/fast-check`;
-  const githubClient = new GitHub(context.token);
+  const octokit = github.getOctokit(context.token);
   const body =
     `Give a try to https://github.com/dubzzz/fast-check/pull/${context.issue.number}/commits/${context.sha} with:\n\n` +
     '```bash\n' +
@@ -22,7 +23,7 @@ async function run() {
     `- On GitHub Actions: https://github.com/dubzzz/fast-check/actions/runs/${context.runId}` +
     '</details>';
 
-  await githubClient.issues.createComment({
+  await octokit.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
