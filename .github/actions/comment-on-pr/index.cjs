96805ecb3fc7f8980df83149cba03adc0bb66c3f
Nicolas DUBIEN <github@dubien.org>
2020-12-30 10:57:46 +0100

    Update .github/actions/comment-on-pr/index.cjs

--- .github/actions/comment-on-pr/index.cjs
+++ .github/actions/comment-on-pr/index.cjs
@@ -54,6 +54,7 @@ async function run() {
     `yarn add ${packageUrl}\n` +
     `npm i ${packageUrl}\n` +
     '```\n\n' +
+    '⚠️ Package might not be accessible yet. Wait for CodeSandbox.\n\n' +
     '⚠️ By running one of these commands you will install the package defined by the head of the PR. But, tests are run against the result of the merge of the PR, not against the head of the PR*. *_If needed, you can install the package used for the tests by manually retrieving and installing the artifact bundle stored into GitHub Actions._\n\n' +
     'See also: ' +
     `[Codeclimate](https://codeclimate.com/github/dubzzz/fast-check/pull/${context.issue.number}), ` +
