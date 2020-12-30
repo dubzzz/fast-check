7edf5c551ce964eb287f362f66817b9c89ac9e1e
Nicolas DUBIEN <github@dubien.org>
2020-12-30 18:20:32 +0100

    Update Pre.spec.ts

--- test/unit/check/precondition/Pre.spec.ts
+++ test/unit/check/precondition/Pre.spec.ts
@@ -1,5 +1,6 @@
 import { pre } from '../../../../src/check/precondition/Pre';
 import { PreconditionFailure } from '../../../../src/check/precondition/PreconditionFailure';
+import * as fc from '../../../../lib/fast-check';
 
 describe('pre', () => {
   it('should not throw on thruthy condition', () => {
@@ -12,6 +13,16 @@ describe('pre', () => {
     } catch (err) {
       failed = true;
       expect(PreconditionFailure.isFailure(err)).toBe(true);
+    }
+    expect(failed).toBe(true);
+  });
+  it('should not understand PreconditionFailure thrown by other instances', () => {
+    let failed = false;
+    try {
+      fc.pre(false);
+    } catch (err) {
+      failed = true;
+      expect(PreconditionFailure.isFailure(err)).toBe(false);
     }
     expect(failed).toBe(true);
   });
