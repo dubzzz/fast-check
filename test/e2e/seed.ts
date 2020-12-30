5d770a33f4b245b6c17219e42fd076bfa7685cbd
Nicolas DUBIEN <github@dubien.org>
2020-12-30 19:53:39 +0100

    Update seed.ts

--- test/e2e/seed.ts
+++ test/e2e/seed.ts
@@ -1,6 +1,6 @@
 import * as fc from '../../lib/fast-check';
 
-const globalConfig = fc.readGlobalConfiguration() || {};
+const globalConfig = fc.readConfigureGlobal() || {};
 const globalSeed = globalConfig.seed;
 
 export const seed = globalSeed !== undefined ? globalSeed : Date.now();
