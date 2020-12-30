324bcdcfd974ff6257585775a1f17146e18d48e9
Nicolas DUBIEN <github@dubien.org>
2020-12-30 19:16:08 +0100

    Seed runs with GITHUB_RUN_ID (by default)

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
+const process = require('process');
+const fc = require('./lib/fast-check');
+
 // Default timeout of 120s
 jest.setTimeout(120000);
+
+// Use GITHUB_RUN_ID as default seed
+if (process.env.GITHUB_RUN_ID) {
+  fc.configureGlobal({ seed: +process.env.GITHUB_RUN_ID });
+}
