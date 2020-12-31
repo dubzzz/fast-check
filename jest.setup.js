8529733c541b92f8617e8edfa90b65c5b5733d0e
Nicolas DUBIEN <github@dubien.org>
2020-12-31 11:34:42 +0100

    Apply suggestions from code review

--- jest.setup.js
+++ jest.setup.js
@@ -4,11 +4,11 @@ const fc = require('./lib/fast-check');
 // Default timeout of 120s
 jest.setTimeout(120000);
 
-// Use GITHUB_RUN_ID + GITHUB_ACTION as default seed
+// Use GITHUB_RUN_ID + GITHUB_RUN_NUMBER as default seed
 const runId = process.env.GITHUB_RUN_ID;
-const action = process.env.GITHUB_ACTION;
-if (runId != null && action != null) {
+const runNumber = process.env.GITHUB_RUN_NUMBER;
+if (runId != null && runNumber != null) {
   const runIdValue = +runId
-  const actionValue = +action;
-  fc.configureGlobal({ seed: runIdValue + actionValue });
+  const runNumberValue = +runNumber;
+  fc.configureGlobal({ seed: runIdValue + runNumberValue });
 }
