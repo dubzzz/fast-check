306428cc412033bd398ea35b1d49beb855383a6f
Nicolas DUBIEN <github@dubien.org>
2020-12-31 12:53:19 +0100

    Apply suggestions from code review

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,8 @@ const fc = require('./lib/fast-check');
 // Default timeout of 120s
 jest.setTimeout(120000);
 
-const seed = Date.now() ^ (Math.random() * 0x100000000);
-console.log(`Using default seed: ${seed}`);
-fc.configureGlobal({ seed });
+if (process.env.CI) {
+  const seed = Date.now() ^ (Math.random() * 0x100000000);
+  console.log(`Using default seed: ${seed}`);
+  fc.configureGlobal({ seed });
+}
