f14b4bb15d39ed32b0a4f82f81c36a9f3389037a
Nicolas DUBIEN <github@dubien.org>
2021-01-01 13:24:25 +0100

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -4,8 +4,12 @@ const fc = require('./lib/fast-check');
 // Default timeout of 120s
 jest.setTimeout(120000);
 
-if (process.env.CI) {
-  const seed = Date.now() ^ (Math.random() * 0x100000000);
+// Setup fast-check
+const defaultSeedRaw = process.env.DEFAULT_SEED;
+if (defaultSeedRaw != null) {
+  const seed = +defaultSeedRaw;
   console.log(`Using default seed: ${seed}`);
   fc.configureGlobal({ seed });
+} else if (process.env.CI) {
+  throw new Error('Missing env variable for DEFAULT_SEED in CI context');
 }
