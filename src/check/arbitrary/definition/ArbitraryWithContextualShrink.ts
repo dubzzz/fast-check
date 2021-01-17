dubzzz/fast-check

commit bc17ccf42d9a1156147a8b6eba8e35b17d1f6358

✨ Introduce new helper ArbitraryWithContextualShrink (#1355)
Why?

ArbitraryWithShrink do not provide enough details to the shrinker. Thus it leads to slow shrink scenarios as the only useful information we got on shrinker side is whether or not we already shrunk one.

The problem is that knowing whether we already shrunk or not is useful, but not enough to shrink in an efficient way. Thus we will introduce a new generation of shrinkers based on this more powerful helper.

src/check/arbitrary/definition/ArbitraryWithContextualShrink.ts
@@ -1,0 +1,96 @@
+import { Random } from '../../../random/generator/Random';
+import { Stream } from '../../../stream/Stream';
+import { Arbitrary } from './Arbitrary';
+import { Shrinkable } from './Shrinkable';
+
+/**
+ * Extract value from tuple [value, context]
+ * @internal
+ */
+function removeContextFromContextualValue<T>(contextualValue: [T, unknown]): T {
+  return contextualValue[0];
+}
+
+/**
+ * Abstract class able to generate and shrink values on type `T`
+ *
+ * It can shrink a value that it has not produced through `generate` (contrary to {@link Arbitrary}).
+ * In the case of classical {@link Arbitrary} there is no `contextualShrink`, `contextualShrinkableFor` methods directly on the {@link Arbitrary},
+ * the users have to call `shrink` on the instance of {@link Shrinkable} produced by `generate`.
+ *
+ * @public
+ */
+abstract class ArbitraryWithContextualShrink<T> extends Arbitrary<T> {
+  /**
+   * Generate a value of type `T` along with its shrink method
+   * based on the provided random number generator
+   *
+   * @param mrng - Random number generator
+   * @returns Random value of type `T` and its shrinker
+   */
+  abstract generate(mrng: Random): Shrinkable<T>;
+
+  /**
+   * Produce a stream of shrinks of value
+   *
+   * @param value - Value to shrink
+   * @param context -
+   * @returns Stream of shrinks associated to value
+   */
+  abstract contextualShrink(value: T, context?: unknown): Stream<[T, unknown]>;
+
+  /**
+   * Build the Shrinkable associated to value
+   *
+   * @param value - Value to shrink
+   * @param shrunkOnce - Indicate whether its the first shrink
+   * @returns Shrinkable associated to value
+   */
+  contextualShrinkableFor(value: T, context?: unknown): Shrinkable<T> {
+    return new Shrinkable(value, () =>
+      this.contextualShrink(value, context).map((contextualValue) =>
+        this.contextualShrinkableFor(contextualValue[0], contextualValue[1])
+      )
+    );
+  }
+
+  /**
+   * Produce a context for shrunkOnce case
+   * @deprecated Prefer context based variants of shrink and shrinkableFor
+   */
+  abstract shrunkOnceContext(): unknown;
+
+  /**
+   * Produce a stream of shrinks of value
+   *
+   * @deprecated Prefer contextualShrink
+   *
+   * @param value - Value to shrink
+   * @param shrunkOnce - Indicate whether its the first shrink (default: false)
+   * @returns Stream of shrinks associated to value
+   */
+  shrink(value: T, shrunkOnce?: boolean): Stream<T> {
+    const context = shrunkOnce === true ? this.shrunkOnceContext() : undefined;
+    return this.contextualShrink(value, context).map(removeContextFromContextualValue);
+  }
+
+  /**
+   * Build the Shrinkable associated to value
+   *
+   * @deprecated Prefer contextualShrinkableFor
+   *
+   * @param value - Value to shrink
+   * @param shrunkOnce - Indicate whether its the first shrink
+   * @returns Shrinkable associated to value
+   */
+  shrinkableFor(value: T, shrunkOnce?: boolean): Shrinkable<T> {
+    return new Shrinkable(value, () => {
+      const context = shrunkOnce === true ? this.shrunkOnceContext() : undefined;
+      return this.contextualShrink(value, context).map((contextualValue) =>
+        this.contextualShrinkableFor(contextualValue[0], contextualValue[1])
+      );
+    });
+  }
+}
+
+export { ArbitraryWithContextualShrink };

src/fast-check-default.ts
@@ -136,6 +136,7 @@ import {
 } from './check/arbitrary/AsyncSchedulerArbitrary';
 import { defaultReportMessage } from './check/runner/utils/RunDetailsFormatter';
 import { ArbitraryWithShrink } from './check/arbitrary/definition/ArbitraryWithShrink';
+import { ArbitraryWithContextualShrink } from './check/arbitrary/definition/ArbitraryWithContextualShrink';
 import { CommandsContraints } from './check/model/commands/CommandsContraints';
 import { PreconditionFailure } from './check/precondition/PreconditionFailure';
 import { RandomType } from './check/runner/configuration/RandomType';
@@ -317,6 +318,7 @@ export {
   // extend the framework
   Arbitrary,
   ArbitraryWithShrink,
+  ArbitraryWithContextualShrink,
   Shrinkable,
   cloneMethod,
   // print values

commit 2aa95ed547b90ed234412529f5095471523ea1da

🐛 Calling `shrinkableFor` should never apply contextual shrinker
This potential regression has been introduced by #1355.
For the moment, we want to limit the impact on user still relying on legacy version of the shrinkers.

src/check/arbitrary/definition/ArbitraryWithContextualShrink.ts
@@ -87,7 +87,7 @@ abstract class ArbitraryWithContextualShrink<T> extends Arbitrary<T> {
     return new Shrinkable(value, () => {
       const context = shrunkOnce === true ? this.shrunkOnceContext() : undefined;
       return this.contextualShrink(value, context).map((contextualValue) =>
-        this.contextualShrinkableFor(contextualValue[0], contextualValue[1])
+        this.shrinkableFor(contextualValue[0], true)
       );
     });
   }

test/unit/check/arbitrary/IntegerArbitrary.spec.ts
@@ -7,28 +7,12 @@ import * as genericHelper from './generic/GenericArbitraryHelper';
 
 import * as stubRng from '../../stubs/generators';
 import { generateOneValue } from './generic/GenerateOneValue';
-import { ArbitraryWithShrink } from '../../../../src/check/arbitrary/definition/ArbitraryWithShrink';
 
 const isStrictlySmallerInteger = (v1: number, v2: number) => Math.abs(v1) < Math.abs(v2);
 
 type ShrinkTree<T> = [T, ShrinkTree<T>[]];
 function buildShrinkTree(s: Shrinkable<number, number>): ShrinkTree<number> {
   return [s.value_, [...s.shrink().map((ss) => buildShrinkTree(ss))]];
-}
-function buildShrinkTreeWithShrunkOnce(
-  arb: ArbitraryWithShrink<number>,
-  value: number,
-  shrunkOnce = false
-): ShrinkTree<number> {
-  return [
-    value,
-    [
-      ...arb
-        .shrinkableFor(value, shrunkOnce)
-        .shrink()
-        .map((ss) => buildShrinkTreeWithShrunkOnce(arb, ss.value_, true)),
-    ],
-  ];
 }
 function renderTree(tree: ShrinkTree<number>): string[] {
   const [current, subTrees] = tree;
@@ -340,7 +324,7 @@ describe('IntegerArbitrary', () => {
       it('Should shrink strictly positive value for positive range including zero', () => {
         const arb = integer({ min: 0, max: 7 });
 
-        const tree = buildShrinkTreeWithShrunkOnce(arb, 6);
+        const tree = buildShrinkTree(arb.shrinkableFor(6));
 
         // prettier-ignore
         expect(renderTree(tree).join('\n')).toBe(
@@ -365,7 +349,7 @@ describe('IntegerArbitrary', () => {
       it('Should shrink strictly positive value for range not included zero', () => {
         const arb = integer({ min: 2, max: 9 });
 
-        const tree = buildShrinkTreeWithShrunkOnce(arb, 8);
+        const tree = buildShrinkTree(arb.shrinkableFor(8));
 
         // prettier-ignore
         expect(renderTree(tree).join('\n')).toBe(
@@ -390,7 +374,7 @@ describe('IntegerArbitrary', () => {
       it('Should shrink strictly negative value for negative range including zero', () => {
         const arb = integer({ min: -7, max: 0 });
 
-        const tree = buildShrinkTreeWithShrunkOnce(arb, -6);
+        const tree = buildShrinkTree(arb.shrinkableFor(-6));
 
         // prettier-ignore
         expect(renderTree(tree).join('\n')).toBe(