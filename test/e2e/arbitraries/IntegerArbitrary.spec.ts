e32c346de6d9bfb4703f5d1f7c9ff66acb42ec6c
Nicolas DUBIEN <github@dubien.org>
2021-01-18 20:45:15 +0100

    Remove duplicated test

--- test/e2e/arbitraries/IntegerArbitrary.spec.ts
+++ test/e2e/arbitraries/IntegerArbitrary.spec.ts
@@ -18,20 +18,6 @@ describe(`IntegerArbitrary (seed: ${seed})`, () => {
       expect(out.failed).toBe(true);
       expect(out.counterexample).toEqual([-100]);
     });
-    it('Should shrink two integers up to the minimal failing case', () => {
-      const out = fc.check(
-        fc.property(fc.integer(), fc.integer(), (a: number, b: number) => {
-          if (a < 1000) return true;
-          if (b < 1000) return true;
-          return Math.abs(a - b) < 10;
-        }),
-        { seed: seed }
-      );
-      expect(out.failed).toBe(true);
-      const [a, b] = out.counterexample!;
-      const reorderedCounterexample = a < b ? [a, b] : [b, a];
-      expect(reorderedCounterexample).toEqual([1000, 1010]);
-    });
   });
   describe('nat', () => {
     it('Should generate natural numbers', () => {
