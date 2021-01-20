2f2b1f85195ae1dd78671610643efb21952a4e41
Nicolas DUBIEN <github@dubien.org>
2021-01-20 20:29:23 +0100

    ✅ Tests on typings should fail with explicit errors

--- test/type/type-checker.ts
+++ test/type/type-checker.ts
@@ -82,7 +82,7 @@ const Test_IsSame_tuple_number_tuple_string: IsSame<[number], [string]> = false;
 export function expectType<TExpectedType>() {
   return function <TReal>(
     arg: TReal,
-    ...noArgs: IsSame<TExpectedType, TReal> extends true ? [] : [{ expected: TExpectedType; got: TReal }]
+    ...noArgs: IsSame<TExpectedType, TReal> extends true ? [string] : [{ expected: TExpectedType; got: TReal }]
   ) {
     // no code
   };
@@ -92,79 +92,79 @@ function type<T>() {
   return (null as any) as T;
 }
 
-expectType<5>()(type<5>());
-expectType<number>()(type<number>());
-expectType<never>()(type<never>());
-expectType<any>()(type<any>());
-expectType<unknown>()(type<unknown>());
-expectType<{ a: number }>()(type<{ a: number }>());
-expectType<Promise<5>>()(type<Promise<5>>());
-expectType<[5]>()(type<[5]>());
-expectType<[any]>()(type<[any]>());
+expectType<5>()(type<5>(), '5 is 5');
+expectType<number>()(type<number>(), 'number is number');
+expectType<never>()(type<never>(), 'never is never');
+expectType<any>()(type<any>(), 'any is any');
+expectType<unknown>()(type<unknown>(), 'unknown is unknown');
+expectType<{ a: number }>()(type<{ a: number }>(), '{a:number} is {a:number}');
+expectType<Promise<5>>()(type<Promise<5>>(), 'Promise<5> is Promise<5>');
+expectType<[5]>()(type<[5]>(), '[5] is [5]');
+expectType<[any]>()(type<[any]>(), '[any] is [any]');
 
-// @ts-expect-error
-expectType<number>()(type<5>());
-// @ts-expect-error
-expectType<5>()(type<number>());
-// @ts-expect-error
-expectType<never>()(type<number>());
-// @ts-expect-error
-expectType<never>()(type<any>());
-// @ts-expect-error
-expectType<never>()(type<unknown>());
-// @ts-expect-error
-expectType<any>()(type<number>());
-// @ts-expect-error
-expectType<any>()(type<never>());
-// @ts-expect-error
-expectType<any>()(type<unknown>());
-// @ts-expect-error
-expectType<unknown>()(type<number>());
-// @ts-expect-error
-expectType<unknown>()(type<never>());
-// @ts-expect-error
-expectType<unknown>()(type<any>());
-// @ts-expect-error
-expectType<{ a: number }>()(type<{ a?: number }>());
-// @ts-expect-error
-expectType<{ a?: number }>()(type<{ a: number }>());
-// @ts-expect-error
-expectType<{}>()(type<{ a: number }>());
-// @ts-expect-error
-expectType<Promise<5>>()(type<Promise<number>>());
-// @ts-expect-error
-expectType<Promise<number>>()(type<Promise<5>>());
-// @ts-expect-error
-expectType<[5]>()(type<[15]>());
-//// @ts-expect-error
-//expectType<[any]>()(type<[unknown]>());
-//// @ts-expect-error
-//expectType<[unknown]>()(type<[any]>());
+// @ts-expect-error - 5 is not number
+expectType<number>()(type<5>(), '5 is not number');
+// @ts-expect-error - number is not 5
+expectType<5>()(type<number>(), 'number is not 5');
+// @ts-expect-error - number is not never
+expectType<never>()(type<number>(), 'number is not never');
+// @ts-expect-error - any is not never
+expectType<never>()(type<any>(), 'any is not never');
+// @ts-expect-error - unknown is not never
+expectType<never>()(type<unknown>(), 'unknown is not never');
+// @ts-expect-error - number is not any
+expectType<any>()(type<number>(), 'number is not any');
+// @ts-expect-error - never is not any
+expectType<any>()(type<never>(), 'never is not any');
+// @ts-expect-error - unknown is not any
+expectType<any>()(type<unknown>(), 'unknown is not any');
+// @ts-expect-error - number is not unknown
+expectType<unknown>()(type<number>(), 'number is not unknown');
+// @ts-expect-error - never is not unknown
+expectType<unknown>()(type<never>(), 'never is not unknown');
+// @ts-expect-error - any is not unknown
+expectType<unknown>()(type<any>(), 'any is not unknown');
+// @ts-expect-error - {a?:number} is not {a:number}
+expectType<{ a: number }>()(type<{ a?: number }>(), '{a?:number} is not {a:number}');
+// @ts-expect-error - {a:number} is not {a?:number}
+expectType<{ a?: number }>()(type<{ a: number }>(), '{a:number} is not {a?:number}');
+// @ts-expect-error - {a:number} is not {}
+expectType<{}>()(type<{ a: number }>(), '{a:number} is not {}');
+// @ts-expect-error - Promise<number> is not Promise<5>
+expectType<Promise<5>>()(type<Promise<number>>(), 'Promise<number> is not Promise<5>');
+// @ts-expect-error - Promise<5> is not Promise<number>
+expectType<Promise<number>>()(type<Promise<5>>(), 'Promise<5> is not Promise<number>');
+// @ts-expect-error - [15] is not [5]
+expectType<[5]>()(type<[15]>(), '[15] is not [5]');
+//// @ts-expect-error - [unknown] is not [any]
+//expectType<[any]>()(type<[unknown]>(), '[unknown] is not [any]');
+//// @ts-expect-error - [any] is not [unknown]
+//expectType<[unknown]>()(type<[any]>(), '[any] is not [unknown]');
 
 export function expectTypeAssignable<TExpectedType>() {
   return function <TReal>(
     arg: TReal,
-    ...noArgs: Extends<TReal, TExpectedType> extends true ? [] : [{ expected: TExpectedType; got: TReal }]
+    ...noArgs: Extends<TReal, TExpectedType> extends true ? [string] : [{ expected: TExpectedType; got: TReal }]
   ) {
     // no code
   };
 }
 
-expectTypeAssignable<number>()(type<number>());
-expectTypeAssignable<number>()(type<5>());
-expectTypeAssignable<3 | 5>()(type<5>());
-expectTypeAssignable<unknown>()(type<number>());
-expectTypeAssignable<any>()(type<number>());
-expectTypeAssignable<{ a?: number }>()(type<{ a: number }>());
-expectTypeAssignable<{}>()(type<{ a: number }>());
+expectTypeAssignable<number>()(type<number>(), 'number is assignable to number');
+expectTypeAssignable<number>()(type<5>(), '5 is assignable to number');
+expectTypeAssignable<3 | 5>()(type<5>(), '5 is assignable to 3|5');
+expectTypeAssignable<unknown>()(type<number>(), 'number is assignable to unknown');
+expectTypeAssignable<any>()(type<number>(), 'number is assignable to any');
+expectTypeAssignable<{ a?: number }>()(type<{ a: number }>(), '{a:number} is assignable to {a?:number}');
+expectTypeAssignable<{}>()(type<{ a: number }>(), '{a:number} is assignable to {}');
 
-// @ts-expect-error
-expectTypeAssignable<5>()(type<number>());
-// @ts-expect-error
-expectTypeAssignable<5>()(type<3 | 5>());
-// @ts-expect-error
-expectTypeAssignable<{ a: number }>()(type<{ a?: number }>());
-// @ts-expect-error
-expectTypeAssignable<number>()(type<unknown>());
-// @ts-expect-error
-expectTypeAssignable<never>()(type<unknown>());
+// @ts-expect-error - number is not assignable to 5
+expectTypeAssignable<5>()(type<number>(), 'number is not assignable to 5');
+// @ts-expect-error - 3|5 is not assignable to 5
+expectTypeAssignable<5>()(type<3 | 5>(), '3|5 is not assignable to 5');
+// @ts-expect-error - {a?:number} is not assignable to {a:number}
+expectTypeAssignable<{ a: number }>()(type<{ a?: number }>(), '{a?:number} is not assignable to {a:number}');
+// @ts-expect-error - unknown is not assignable to number
+expectTypeAssignable<number>()(type<unknown>(), 'unknown is not assignable to number');
+// @ts-expect-error - unknown is not assignable to never
+expectTypeAssignable<never>()(type<unknown>(), 'unknown is not assignable to never');
