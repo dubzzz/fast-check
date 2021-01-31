b3400faba2be0de0396498d27bdc60e36d2e83f3
Tomas Carnecky <tomc@caurea.org>
2021-01-31 14:06:04 +0100

    And also make prettier happy

--- src/utils/globalThis.ts
+++ src/utils/globalThis.ts
@@ -1,11 +1,10 @@
-// tslint:disable
-
-declare const self: any
-declare const window: any
-declare const global: any
-
 // See https://github.com/paulmillr/es6-shim/commit/2367e0953edd01ae9a5628e1f47cf14b0377a7d6
 // and https://github.com/tc39/proposal-global/blob/269dd7c646451e72e3e3edc573b57ce766aac86a/README.md#rationale
+
+// tslint:disable
+declare const self: any;
+declare const window: any;
+declare const global: any;
 
 /**
  * Internal polyfill for `globalThis`
@@ -16,12 +15,18 @@ export let getGlobal = () => {
     // the only reliable means to get the global object is
     // `Function('return this')()`
     // However, this causes CSP violations in Chrome apps.
-    if (typeof self !== 'undefined') { return self; }
-    if (typeof window !== 'undefined') { return window; }
-    if (typeof global !== 'undefined') { return global; }
+    if (typeof self !== 'undefined') {
+      return self;
+    }
+    if (typeof window !== 'undefined') {
+      return window;
+    }
+    if (typeof global !== 'undefined') {
+      return global;
+    }
     throw new Error('unable to locate global object');
   })();
-  
-  getGlobal = () => globalThis
+
+  getGlobal = () => globalThis;
   return globalThis;
 };
