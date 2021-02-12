2afff3ffcdcf0d90850d0713b534b19091f2803f
Nicolas DUBIEN <github@dubien.org>
2021-02-12 22:42:15 +0100

    FIX Typing test

--- test/type/main.ts
+++ test/type/main.ts
@@ -128,17 +128,13 @@ expectType<fc.Arbitrary<never>>()(
 type Query = { data: { field: 'X' } };
 expectType<fc.Arbitrary<Query>>()(
   // issue 1453
-  // @fc-ignore-if-3.2
-  fc.record<Query>({ data: fc.record({ field: fc.constant('X') }) }),
-  // @fc-ignore-if-3.2
-  '"record" can be passed the requested type in <*>'
+  // @fc-ignore-if-no-const
+  fc.record<Query>({ data: fc.record({ field: fc.constant('X') }) })
 );
 expectType<fc.Arbitrary<Partial<Query>>>()(
   // issue 1453
-  // @fc-ignore-if-3.2
-  fc.record<Partial<Query>>({ data: fc.record({ field: fc.constant('X') }) }),
-  // @fc-ignore-if-3.2
-  '"record" can be passed something assignable to the requested type in <*>'
+  // @fc-ignore-if-no-const
+  fc.record<Partial<Query>>({ data: fc.record({ field: fc.constant('X') }) })
 );
 // @ts-expect-error - requiredKeys references an unknown key
 fc.record({ a: fc.nat(), b: fc.string() }, { requiredKeys: ['c'] });
