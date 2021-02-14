969250ef20cae39627905a21fdd99d4c9596f3c5
Nicolas DUBIEN <github@dubien.org>
2021-02-15 00:10:47 +0100

    💥 Remove deprecated signatures of `fc.string` (#1496)

--- src/arbitrary/base64String.ts
+++ src/arbitrary/base64String.ts
@@ -3,18 +3,13 @@ import { convertFromNext, convertToNext } from '../check/arbitrary/definition/Co
 import { array } from './array';
 import { base64 } from './base64';
 import { maxLengthFromMinLength } from './_internals/helpers/MaxLengthFromMinLength';
-import {
-  extractStringConstraints,
-  StringFullConstraintsDefinition,
-  StringSharedConstraints,
-} from './_internals/helpers/StringConstraintsExtractor';
+import { StringSharedConstraints } from './_shared/StringSharedConstraints';
 import { codePointsToStringMapper, codePointsToStringUnmapper } from './_internals/mappers/CodePointsToString';
 import { stringToBase64Mapper, stringToBase64Unmapper } from './_internals/mappers/StringToBase64';
-export { StringSharedConstraints } from './_internals/helpers/StringConstraintsExtractor';
+export { StringSharedConstraints } from './_shared/StringSharedConstraints';
 
 /** @internal */
-function extractMinMaxConstraints(args: StringFullConstraintsDefinition) {
-  const constraints = extractStringConstraints(args);
+function extractMinMaxConstraints(constraints: StringSharedConstraints) {
   const minLength = constraints.minLength !== undefined ? constraints.minLength : 0;
   const maxLength = constraints.maxLength !== undefined ? constraints.maxLength : maxLengthFromMinLength(minLength);
   return { minLength, maxLength };
@@ -25,56 +20,15 @@ function extractMinMaxConstraints(args: StringFullConstraintsDefinition) {
  *
  * A base64 string will always have a length multiple of 4 (padded with =)
  *
- * @remarks Since 0.0.1
- * @public
- */
-function base64String(): Arbitrary<string>;
-/**
- * For base64 strings
- *
- * A base64 string will always have a length multiple of 4 (padded with =)
- *
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.base64String({maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Constraints to apply when building instances (since 2.4.0)
  *
  * @remarks Since 0.0.1
  * @public
  */
-function base64String(maxLength: number): Arbitrary<string>;
-/**
- * For base64 strings
- *
- * A base64 string will always have a length multiple of 4 (padded with =)
- *
- * @param minLength - Lower bound of the generated string length
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.base64String({minLength, maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.11
- * @public
- */
-function base64String(minLength: number, maxLength: number): Arbitrary<string>;
-/**
- * For base64 strings
- *
- * A base64 string will always have a length multiple of 4 (padded with =)
- *
- * @param constraints - Constraints to apply when building instances
- *
- * @remarks Since 2.4.0
- * @public
- */
-function base64String(constraints: StringSharedConstraints): Arbitrary<string>;
-function base64String(...args: StringFullConstraintsDefinition): Arbitrary<string> {
-  const constraints = extractMinMaxConstraints(args);
-  const unscaledMinLength = constraints.minLength;
-  const unscaledMaxLength = constraints.maxLength;
+function base64String(constraints: StringSharedConstraints = {}): Arbitrary<string> {
+  const finalConstraints = extractMinMaxConstraints(constraints);
+  const unscaledMinLength = finalConstraints.minLength;
+  const unscaledMaxLength = finalConstraints.maxLength;
 
   // base64 length is always a multiple of 4
   const minLength = unscaledMinLength + 3 - ((unscaledMinLength + 3) % 4);
