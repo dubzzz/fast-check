1df63d6d79bf360364d74df0f8689bc677d41bd2
Nicolas DUBIEN <github@dubien.org>
2021-02-14 23:39:03 +0100

    💥 Remove deprecated signatures of `fc.array` (#1494)
    * Adapt for 2.18.0 and later
    * Adapt for 2.15.0 and later
    * Adapt for 2.22.0 and later

--- src/arbitrary/array.ts
+++ src/arbitrary/array.ts
@@ -1,5 +1,4 @@
 import { Arbitrary } from '../check/arbitrary/definition/Arbitrary';
-import { NextArbitrary } from '../check/arbitrary/definition/NextArbitrary';
 import { convertFromNext, convertToNext } from '../check/arbitrary/definition/Converters';
 import { ArrayArbitrary } from './_internals/ArrayArbitrary';
 import {
@@ -36,80 +35,23 @@ export interface ArrayConstraints {
   size?: SizeForArbitrary;
 }
 
-/** @internal */
-function createArrayArbitrary<T>(
-  nextArb: NextArbitrary<T>,
-  size: SizeForArbitrary | undefined,
-  minLength: number,
-  maxLengthOrUnset: number | undefined
-): Arbitrary<T[]> {
+/**
+ * For arrays of values coming from `arb`
+ *
+ * @param arb - Arbitrary used to generate the values inside the array
+ * @param constraints - Constraints to apply when building instances (since 2.4.0)
+ *
+ * @remarks Since 0.0.1
+ * @public
+ */
+function array<T>(arb: Arbitrary<T>, constraints: ArrayConstraints = {}): Arbitrary<T[]> {
+  const nextArb = convertToNext(arb);
+  const size = constraints.size;
+  const minLength = constraints.minLength || 0;
+  const maxLengthOrUnset = constraints.maxLength;
   const maxLength = maxLengthOrUnset !== undefined ? maxLengthOrUnset : MaxLengthUpperBound;
   const specifiedMaxLength = maxLengthOrUnset !== undefined;
   const maxGeneratedLength = maxGeneratedLengthFromSizeForArbitrary(size, minLength, maxLength, specifiedMaxLength);
   return convertFromNext(new ArrayArbitrary<T>(nextArb, minLength, maxGeneratedLength, maxLength));
-}
-
-/**
- * For arrays of values coming from `arb`
- * @param arb - Arbitrary used to generate the values inside the array
- * @remarks Since 0.0.1
- * @public
- */
-function array<T>(arb: Arbitrary<T>): Arbitrary<T[]>;
-/**
- * For arrays of values coming from `arb` having an upper bound size
- *
- * @param arb - Arbitrary used to generate the values inside the array
- * @param maxLength - Upper bound of the generated array size
- *
- * @deprecated
- * Superceded by `fc.array(arb, {maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.1
- * @public
- */
-function array<T>(arb: Arbitrary<T>, maxLength: number): Arbitrary<T[]>;
-/**
- * For arrays of values coming from `arb` having lower and upper bound size
- *
- * @param arb - Arbitrary used to generate the values inside the array
- * @param minLength - Lower bound of the generated array size
- * @param maxLength - Upper bound of the generated array size
- *
- * @deprecated
- * Superceded by `fc.array(arb, {minLength, maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.7
- * @public
- */
-function array<T>(arb: Arbitrary<T>, minLength: number, maxLength: number): Arbitrary<T[]>;
-/**
- * For arrays of values coming from `arb` having lower and upper bound size
- *
- * @param arb - Arbitrary used to generate the values inside the array
- * @param constraints - Constraints to apply when building instances
- *
- * @remarks Since 2.4.0
- * @public
- */
-function array<T>(arb: Arbitrary<T>, constraints: ArrayConstraints): Arbitrary<T[]>;
-function array<T>(arb: Arbitrary<T>, ...args: [] | [number] | [number, number] | [ArrayConstraints]): Arbitrary<T[]> {
-  const nextArb = convertToNext(arb);
-  // fc.array(arb)
-  if (args[0] === undefined) {
-    return createArrayArbitrary(nextArb, undefined, 0, undefined); // no size, no maxLength
-  }
-  // fc.array(arb, constraints)
-  if (typeof args[0] === 'object') {
-    return createArrayArbitrary(nextArb, args[0].size, args[0].minLength || 0, args[0].maxLength);
-  }
-  // fc.array(arb, minLength, maxLength)
-  if (args[1] !== undefined) {
-    return createArrayArbitrary(nextArb, undefined, args[0], args[1]); // no size
-  }
-  // fc.array(arb, maxLength)
-  return createArrayArbitrary(nextArb, undefined, 0, args[0]); // no size
 }
 export { array };
