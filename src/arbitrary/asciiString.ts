e0509e029dd7e514ec2a21a19157dacf06d94174
Nicolas DUBIEN <github@dubien.org>
2021-02-15 00:10:47 +0100

    💥 Remove deprecated signatures of `fc.string` (#1496)
    * Adapted for 2.22.0

--- src/arbitrary/asciiString.ts
+++ src/arbitrary/asciiString.ts
@@ -2,60 +2,20 @@ import { Arbitrary } from '../check/arbitrary/definition/Arbitrary';
 import { convertFromNext, convertToNext } from '../check/arbitrary/definition/Converters';
 import { array } from './array';
 import { ascii } from './ascii';
-import {
-  extractStringConstraints,
-  StringFullConstraintsDefinition,
-  StringSharedConstraints,
-} from './_internals/helpers/StringConstraintsExtractor';
+import { StringSharedConstraints } from './_shared/StringSharedConstraints';
 import { codePointsToStringMapper, codePointsToStringUnmapper } from './_internals/mappers/CodePointsToString';
-export { StringSharedConstraints } from './_internals/helpers/StringConstraintsExtractor';
+export { StringSharedConstraints } from './_shared/StringSharedConstraints';
 
 /**
  * For strings of {@link ascii}
- * @remarks Since 0.0.1
- * @public
- */
-function asciiString(): Arbitrary<string>;
-/**
- * For strings of {@link ascii}
  *
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.asciiString({maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Constraints to apply when building instances (since 2.4.0)
  *
  * @remarks Since 0.0.1
  * @public
  */
-function asciiString(maxLength: number): Arbitrary<string>;
-/**
- * For strings of {@link ascii}
- *
- * @param minLength - Lower bound of the generated string length
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.asciiString({minLength, maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.11
- * @public
- */
-function asciiString(minLength: number, maxLength: number): Arbitrary<string>;
-/**
- * For strings of {@link ascii}
- *
- * @param constraints - Constraints to apply when building instances
- *
- * @remarks Since 2.4.0
- * @public
- */
-function asciiString(constraints: StringSharedConstraints): Arbitrary<string>;
-function asciiString(...args: StringFullConstraintsDefinition): Arbitrary<string> {
-  const constraints = extractStringConstraints(args);
+export function asciiString(constraints: StringSharedConstraints = {}): Arbitrary<string> {
   return convertFromNext(
     convertToNext(array(ascii(), constraints)).map(codePointsToStringMapper, codePointsToStringUnmapper)
   );
 }
-export { asciiString };
