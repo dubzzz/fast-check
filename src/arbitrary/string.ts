a378994ee55162e43fab548e59c51f80a80e8873
Nicolas DUBIEN <github@dubien.org>
2021-02-15 00:10:47 +0100

    💥 Remove deprecated signatures of `fc.string` (#1496)

--- src/arbitrary/string.ts
+++ src/arbitrary/string.ts
@@ -2,60 +2,20 @@ import { Arbitrary } from '../check/arbitrary/definition/Arbitrary';
 import { convertFromNext, convertToNext } from '../check/arbitrary/definition/Converters';
 import { array } from './array';
 import { char } from './char';
-import {
-  extractStringConstraints,
-  StringFullConstraintsDefinition,
-  StringSharedConstraints,
-} from './_internals/helpers/StringConstraintsExtractor';
+import { StringSharedConstraints } from './_shared/StringSharedConstraints';
 import { codePointsToStringMapper, codePointsToStringUnmapper } from './_internals/mappers/CodePointsToString';
-export { StringSharedConstraints } from './_internals/helpers/StringConstraintsExtractor';
+export { StringSharedConstraints } from './_shared/StringSharedConstraints';
 
 /**
  * For strings of {@link char}
- * @remarks Since 0.0.1
- * @public
- */
-function string(): Arbitrary<string>;
-/**
- * For strings of {@link char}
  *
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.string({maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Constraints to apply when building instances (since 2.4.0)
  *
  * @remarks Since 0.0.1
  * @public
  */
-function string(maxLength: number): Arbitrary<string>;
-/**
- * For strings of {@link char}
- *
- * @param minLength - Lower bound of the generated string length
- * @param maxLength - Upper bound of the generated string length
- *
- * @deprecated
- * Superceded by `fc.string({minLength, maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.11
- * @public
- */
-function string(minLength: number, maxLength: number): Arbitrary<string>;
-/**
- * For strings of {@link char}
- *
- * @param constraints - Constraints to apply when building instances
- *
- * @remarks Since 2.4.0
- * @public
- */
-function string(constraints: StringSharedConstraints): Arbitrary<string>;
-function string(...args: StringFullConstraintsDefinition): Arbitrary<string> {
-  const constraints = extractStringConstraints(args);
+export function string(constraints: StringSharedConstraints = {}): Arbitrary<string> {
   return convertFromNext(
     convertToNext(array(char(), constraints)).map(codePointsToStringMapper, codePointsToStringUnmapper)
   );
 }
-export { string };
