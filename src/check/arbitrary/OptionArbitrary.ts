d2d213532dfefb3b46610fcd0a333fe7ad970f44
Nicolas DUBIEN <github@dubien.org>
2021-02-15 21:49:17 +0100

    💥 Remove deprecated signatures of `fc.option` (#1509)
    * 💥 Remove deprecated signatures of `fc.option`
    * FIX Init for nil
    * FIX Update code follwoing update from 2.13 to 2.14

--- src/check/arbitrary/OptionArbitrary.ts
+++ src/check/arbitrary/OptionArbitrary.ts
@@ -45,48 +45,16 @@ export interface OptionConstraints<TNil = null> {
   depthIdentifier?: string;
 }
 
-/** @internal */
-function extractOptionConstraints<TNil>(constraints?: number | OptionConstraints<TNil>): OptionConstraints<TNil> {
-  if (!constraints) return {};
-  if (typeof constraints === 'number') return { freq: constraints };
-  return constraints;
-}
-
-/**
- * For either null or a value coming from `arb`
- *
- * @param arb - Arbitrary that will be called to generate a non null value
- *
- * @remarks Since 0.0.6
- * @public
- */
-function option<T>(arb: Arbitrary<T>): Arbitrary<T | null>;
-/**
- * For either null or a value coming from `arb` with custom frequency
- *
- * @param arb - Arbitrary that will be called to generate a non null value
- * @param freq - The probability to build a null value is of `1 / freq`
- *
- * @deprecated
- * Superceded by `fc.option(arb, {freq})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.6
- * @public
- */
-function option<T>(arb: Arbitrary<T>, freq: number): Arbitrary<T | null>;
 /**
  * For either nil or a value coming from `arb` with custom frequency
  *
  * @param arb - Arbitrary that will be called to generate a non nil value
- * @param constraints - Constraints on the option
+ * @param constraints - Constraints on the option (since 1.17.0)
  *
- * @remarks Since 1.17.0
+ * @remarks Since 0.0.6
  * @public
  */
-function option<T, TNil = null>(arb: Arbitrary<T>, constraints: OptionConstraints<TNil>): Arbitrary<T | TNil>;
-function option<T, TNil>(arb: Arbitrary<T>, rawConstraints?: number | OptionConstraints<TNil>): Arbitrary<T | TNil> {
-  const constraints = extractOptionConstraints(rawConstraints);
+function option<T, TNil = null>(arb: Arbitrary<T>, constraints: OptionConstraints<TNil> = {}): Arbitrary<T | TNil> {
   const freq = constraints.freq == null ? 5 : constraints.freq;
   const nilArb = constant(Object.prototype.hasOwnProperty.call(constraints, 'nil') ? constraints.nil : (null as any));
   const weightedArbs = [
