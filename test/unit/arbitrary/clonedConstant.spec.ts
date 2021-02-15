0bc0e8b01da0bba8b9105416d7ab77fc05bfaa05
Nicolas DUBIEN <github@dubien.org>
2021-02-15 21:48:32 +0100

    💥 Remove deprecated aliases (#1510)
    * 💥 Remove deprecated aliases
    * Remove doc for `fc.dedup`
    * Adapt for 2.15.0

--- test/unit/arbitrary/clonedConstant.spec.ts
+++
@@ -1,8 +0,0 @@
-import { constant } from '../../../src/arbitrary/constant';
-import { clonedConstant } from '../../../src/arbitrary/clonedConstant';
-
-describe('clonedConstant', () => {
-  it('should be the same function as constant', () => {
-    expect(clonedConstant).toBe(constant);
-  });
-});
