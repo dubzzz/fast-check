d50f5126b221410410e32238782888d9cf5d38f4
Nicolas DUBIEN <github@dubien.org>
2021-02-15 21:52:27 +0100

    💥 Remove deprecated signatures of `fc.json` (#1508)
    Adapted for 2.18.0

--- src/arbitrary/json.ts
+++ src/arbitrary/json.ts
@@ -9,40 +9,12 @@ export { JsonSharedConstraints };
  *
  * Keys and string values rely on {@link string}
  *
- * @remarks Since 0.0.7
- * @public
- */
-function json(): Arbitrary<string>;
-/**
- * For any JSON strings with a maximal depth
- *
- * Keys and string values rely on {@link string}
- *
- * @param maxDepth - Maximal depth of the generated objects
- *
- * @deprecated
- * Superceded by `fc.json({maxDepth})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Constraints to be applied onto the generated instance (since 2.5.0)
  *
  * @remarks Since 0.0.7
  * @public
  */
-function json(maxDepth: number): Arbitrary<string>;
-/**
- * For any JSON strings
- *
- * Keys and string values rely on {@link string}
- *
- * @param constraints - Constraints to be applied onto the generated instance
- *
- * @remarks Since 2.5.0
- * @public
- */
-function json(constraints: JsonSharedConstraints): Arbitrary<unknown>;
-function json(constraints?: number | JsonSharedConstraints): Arbitrary<string> {
-  // Rq: Explicit 'as any' as 'number | JsonConstraints' cannot be passed to 'unicodeJsonObject(number)'
-  //     and cannot be passed to 'unicodeJsonObject(JsonConstraints)' (both are too strict)
-  const arb = constraints != null ? jsonObject(constraints as any) : jsonObject();
+export function json(constraints: JsonSharedConstraints = {}): Arbitrary<string> {
+  const arb = jsonObject(constraints);
   return arb.map(JSON.stringify);
 }
-export { json };
--- src/arbitrary/jsonObject.ts
+++ src/arbitrary/jsonObject.ts
@@ -13,43 +13,11 @@ export { JsonSharedConstraints };
  * As `JSON.parse` preserves `-0`, `jsonObject` can also have `-0` as a value.
  * `jsonObject` must be seen as: any value that could have been built by doing a `JSON.parse` on a given string.
  *
- * @remarks Since 1.2.3
- * @public
- */
-function jsonObject(): Arbitrary<unknown>;
-/**
- * For any JSON compliant values with a maximal depth
- *
- * Keys and string values rely on {@link string}
- *
- * As `JSON.parse` preserves `-0`, `jsonObject` can also have `-0` as a value.
- * `jsonObject` must be seen as: any value that could have been built by doing a `JSON.parse` on a given string.
- *
- * @param maxDepth - Maximal depth of the generated values
- *
- * @deprecated
- * Superceded by `fc.jsonObject({maxDepth})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Constraints to be applied onto the generated instance (since 2.5.0)
  *
  * @remarks Since 1.2.3
  * @public
  */
-function jsonObject(maxDepth: number): Arbitrary<unknown>;
-/**
- * For any JSON compliant values
- *
- * Keys and string values rely on {@link string}
- *
- * As `JSON.parse` preserves `-0`, `jsonObject` can also have `-0` as a value.
- * `jsonObject` must be seen as: any value that could have been built by doing a `JSON.parse` on a given string.
- *
- * @param constraints - Constraints to be applied onto the generated instance
- *
- * @remarks Since 2.5.0
- * @public
- */
-function jsonObject(constraints: JsonSharedConstraints): Arbitrary<unknown>;
-function jsonObject(constraints?: number | JsonSharedConstraints): Arbitrary<unknown> {
+export function jsonObject(constraints: JsonSharedConstraints = {}): Arbitrary<unknown> {
   return anything(jsonConstraintsBuilder(string(), constraints));
 }
-export { jsonObject };
