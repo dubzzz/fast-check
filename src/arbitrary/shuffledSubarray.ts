d47ca8664a47473872a37e539bac5ea2e41cfa5a
Nicolas DUBIEN <github@dubien.org>
2021-02-15 20:33:07 +0100

    💥 Remove deprecated signatures of `fc.*subarray` (#1505)
    Adapted for 2.18.0

--- src/arbitrary/shuffledSubarray.ts
+++ src/arbitrary/shuffledSubarray.ts
@@ -26,46 +26,12 @@ export interface ShuffledSubarrayConstraints {
  * For subarrays of `originalArray`
  *
  * @param originalArray - Original array
+ * @param constraints - Constraints to apply when building instances (since 2.4.0)
  *
  * @remarks Since 1.5.0
  * @public
  */
-function shuffledSubarray<T>(originalArray: T[]): Arbitrary<T[]>;
-/**
- * For subarrays of `originalArray`
- *
- * @param originalArray - Original array
- * @param minLength - Lower bound of the generated array size
- * @param maxLength - Upper bound of the generated array size
- *
- * @deprecated
- * Superceded by `fc.shuffledSubarray(originalArray, {minLength, maxLength})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 1.5.0
- * @public
- */
-function shuffledSubarray<T>(originalArray: T[], minLength: number, maxLength: number): Arbitrary<T[]>;
-/**
- * For subarrays of `originalArray`
- *
- * @param originalArray - Original array
- * @param constraints - Constraints to apply when building instances
- *
- * @remarks Since 2.4.0
- * @public
- */
-function shuffledSubarray<T>(originalArray: T[], constraints: ShuffledSubarrayConstraints): Arbitrary<T[]>;
-function shuffledSubarray<T>(
-  originalArray: T[],
-  ...args: [] | [number, number] | [ShuffledSubarrayConstraints]
-): Arbitrary<T[]> {
-  if (typeof args[0] === 'number' && typeof args[1] === 'number') {
-    return convertFromNext(new SubarrayArbitrary(originalArray, false, args[0], args[1]));
-  }
-  const ct = args[0] as ShuffledSubarrayConstraints | undefined;
-  const minLength = ct !== undefined && ct.minLength !== undefined ? ct.minLength : 0;
-  const maxLength = ct !== undefined && ct.maxLength !== undefined ? ct.maxLength : originalArray.length;
+export function shuffledSubarray<T>(originalArray: T[], constraints: ShuffledSubarrayConstraints = {}): Arbitrary<T[]> {
+  const { minLength = 0, maxLength = originalArray.length } = constraints;
   return convertFromNext(new SubarrayArbitrary(originalArray, false, minLength, maxLength));
 }
-export { shuffledSubarray };
