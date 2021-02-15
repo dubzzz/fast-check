64b108a53aa73379d2768d0134924043240acb6b
Nicolas DUBIEN <github@dubien.org>
2021-02-15 20:33:51 +0100

    💥 Remove deprecated signatures of `fc.commands` (#1507)
    Adapted for 2.18.0

--- src/arbitrary/commands.ts
+++ src/arbitrary/commands.ts
@@ -17,51 +17,9 @@ import {
  * It should shrink more efficiently than {@link array} for {@link AsyncCommand} arrays.
  *
  * @param commandArbs - Arbitraries responsible to build commands
- * @param maxCommands - Maximal number of commands to build
- *
- * @deprecated
- * Superceded by `fc.commands(commandArbs, {maxCommands})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
+ * @param constraints - Contraints to be applied when generating the commands (since 1.11.0)
  *
  * @remarks Since 1.5.0
- * @public
- */
-// eslint-disable-next-line @typescript-eslint/ban-types
-function commands<Model extends object, Real, CheckAsync extends boolean>(
-  commandArbs: Arbitrary<AsyncCommand<Model, Real, CheckAsync>>[],
-  maxCommands?: number
-): Arbitrary<Iterable<AsyncCommand<Model, Real, CheckAsync>>>;
-/**
- * For arrays of {@link Command} to be executed by {@link modelRun}
- *
- * This implementation comes with a shrinker adapted for commands.
- * It should shrink more efficiently than {@link array} for {@link Command} arrays.
- *
- * @param commandArbs - Arbitraries responsible to build commands
- * @param maxCommands - Maximal number of commands to build
- *
- * @deprecated
- * Superceded by `fc.commands(commandArbs, {maxCommands})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 1.5.0
- * @public
- */
-// eslint-disable-next-line @typescript-eslint/ban-types
-function commands<Model extends object, Real>(
-  commandArbs: Arbitrary<Command<Model, Real>>[],
-  maxCommands?: number
-): Arbitrary<Iterable<Command<Model, Real>>>;
-/**
- * For arrays of {@link AsyncCommand} to be executed by {@link asyncModelRun}
- *
- * This implementation comes with a shrinker adapted for commands.
- * It should shrink more efficiently than {@link array} for {@link AsyncCommand} arrays.
- *
- * @param commandArbs - Arbitraries responsible to build commands
- * @param constraints - Contraints to be applied when generating the commands
- *
- * @remarks Since 1.11.0
  * @public
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -76,9 +34,9 @@ function commands<Model extends object, Real, CheckAsync extends boolean>(
  * It should shrink more efficiently than {@link array} for {@link Command} arrays.
  *
  * @param commandArbs - Arbitraries responsible to build commands
- * @param constraints - Constraints to be applied when generating the commands
+ * @param constraints - Contraints to be applied when generating the commands (since 1.11.0)
  *
- * @remarks Since 1.11.0
+ * @remarks Since 1.5.0
  * @public
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
