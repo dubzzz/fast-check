29d27275a754a6f80a6e15955676ffdd9d2e568b
Nicolas DUBIEN <github@dubien.org>
2021-02-15 20:25:13 +0100

    💥 Remove deprecated signatures of `fc.option`

--- src/check/arbitrary/OptionArbitrary.ts
+++ src/check/arbitrary/OptionArbitrary.ts
@@ -50,47 +50,17 @@ class OptionArbitrary<T, TNil> extends Arbitrary<T | TNil> {
 }
 
 /**
- * For either null or a value coming from `arb`
- *
- * @param arb - Arbitrary that will be called to generate a non null value
- *
- * @remarks Since 0.0.6
- * @public
- */
-function option<T>(arb: Arbitrary<T>): Arbitrary<T | null>;
-/**
- * For either null or a value coming from `arb` with custom frequency
- *
- * @param arb - Arbitrary that will be called to generate a non null value
- * @param freq - The probability to build a null value is of `1 / freq`
- *
- * @deprecated
- * Superceded by `fc.option(arb, {freq})` - see {@link https://github.com/dubzzz/fast-check/issues/992 | #992}.
- * Ease the migration with {@link https://github.com/dubzzz/fast-check/tree/main/codemods/unify-signatures | our codemod script}.
- *
- * @remarks Since 0.0.6
- * @public
- */
-function option<T>(arb: Arbitrary<T>, freq: number): Arbitrary<T | null>;
-/**
  * For either nil or a value coming from `arb` with custom frequency
  *
  * @param arb - Arbitrary that will be called to generate a non nil value
- * @param constraints - Constraints on the option
+ * @param constraints - Constraints on the option (since 1.17.0)
  *
- * @remarks Since 1.17.0
+ * @remarks Since 0.0.6
  * @public
  */
-function option<T, TNil = null>(arb: Arbitrary<T>, constraints: OptionConstraints<TNil>): Arbitrary<T | TNil>;
-function option<T, TNil>(arb: Arbitrary<T>, constraints?: number | OptionConstraints<TNil>): Arbitrary<T | TNil> {
-  if (!constraints) return new OptionArbitrary(arb, 5, null as any);
-  if (typeof constraints === 'number') return new OptionArbitrary(arb, constraints, null as any);
-
-  return new OptionArbitrary(
-    arb,
-    constraints.freq == null ? 5 : constraints.freq,
-    Object.prototype.hasOwnProperty.call(constraints, 'nil') ? constraints.nil : (null as any)
-  );
+function option<T, TNil = null>(arb: Arbitrary<T>, constraints: OptionConstraints<TNil> = {}): Arbitrary<T | TNil> {
+  const { freq = 5, nil = null } = constraints;
+  return new OptionArbitrary(arb, freq, nil as TNil);
 }
 
 export { option };
