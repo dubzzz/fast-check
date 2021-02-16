c5c6c6118d3cbe39aadd0fc49f6c58de66d3f211
Nicolas DUBIEN <github@dubien.org>
2021-02-16 23:42:50 +0100

    🏷️ Adopt variadic tuples for signatures of property (#1518)
    * Drop prebuild script
    * FIX Typings in tests
    * FIX E2E Typings
    * Adapt typings tests
    * FIX Missing argument
    * FIX FIbo test
    * Merge conflict from 2.14 to 2.15
    * Adapted for 2.19.0 and later

--- src/check/property/AsyncProperty.ts
+++ src/check/property/AsyncProperty.ts
@@ -1,4 +1,33 @@
-import { asyncProperty } from './AsyncProperty.generated';
-import { IAsyncProperty, IAsyncPropertyWithHooks, AsyncPropertyHookFunction } from './AsyncProperty.generic';
+import { Arbitrary } from '../arbitrary/definition/Arbitrary';
+import { tuple } from '../../arbitrary/tuple';
+import {
+  AsyncProperty,
+  IAsyncProperty,
+  IAsyncPropertyWithHooks,
+  AsyncPropertyHookFunction,
+} from './AsyncProperty.generic';
+import { AlwaysShrinkableArbitrary } from '../../arbitrary/_internals/AlwaysShrinkableArbitrary';
+import { convertFromNext, convertToNext } from '../arbitrary/definition/Converters';
+import { convertFromNextAsyncPropertyWithHooks } from './ConvertersProperty';
+
+/**
+ * Instantiate a new {@link fast-check#IAsyncProperty}
+ * @param predicate - Assess the success of the property. Would be considered falsy if it throws or if its output evaluates to false
+ * @remarks Since 0.0.7
+ * @public
+ */
+function asyncProperty<Ts extends [unknown, ...unknown[]]>(
+  ...args: [...arbitraries: { [K in keyof Ts]: Arbitrary<Ts[K]> }, predicate: (...args: Ts) => Promise<boolean | void>]
+): IAsyncPropertyWithHooks<Ts> {
+  if (args.length < 2) {
+    throw new Error('asyncProperty expects at least two parameters');
+  }
+  const arbs = args.slice(0, args.length - 1) as { [K in keyof Ts]: Arbitrary<Ts[K]> };
+  const p = args[args.length - 1] as (...args: Ts) => Promise<boolean | void>;
+  const mappedArbs = arbs.map((arb): typeof arb =>
+    convertFromNext(new AlwaysShrinkableArbitrary(convertToNext(arb)))
+  ) as typeof arbs;
+  return convertFromNextAsyncPropertyWithHooks(new AsyncProperty(tuple<Ts>(...mappedArbs), (t) => p(...t)));
+}
 
 export { asyncProperty, IAsyncProperty, IAsyncPropertyWithHooks, AsyncPropertyHookFunction };
