0e54c1e921b6ed69161995dafec02a9d3d39db07
Nicolas DUBIEN <github@dubien.org>
2021-02-16 23:42:50 +0100

    🏷️ Adopt variadic tuples for signatures of property (#1518)
    * 🏷️ Adopt variadic tuples for signatures of property
    * Drop prebuild script
    * FIX Typings in tests
    * FIX E2E Typings
    * Adapt typings tests
    * FIX Missing argument
    * FIX FIbo test
    * Merge conflict from 2.14 to 2.15

--- src/check/property/AsyncProperty.ts
+++ src/check/property/AsyncProperty.ts
@@ -1,4 +1,27 @@
-import { asyncProperty } from './AsyncProperty.generated';
-import { IAsyncProperty, IAsyncPropertyWithHooks, AsyncPropertyHookFunction } from './AsyncProperty.generic';
+import { Arbitrary } from '../arbitrary/definition/Arbitrary';
+import { tuple } from '../arbitrary/TupleArbitrary';
+import {
+  AsyncProperty,
+  IAsyncProperty,
+  IAsyncPropertyWithHooks,
+  AsyncPropertyHookFunction,
+} from './AsyncProperty.generic';
+
+/**
+ * Instantiate a new {@link fast-check#IAsyncProperty}
+ * @param predicate - Assess the success of the property. Would be considered falsy if it throws or if its output evaluates to false
+ * @remarks Since 0.0.7
+ * @public
+ */
+function asyncProperty<Ts extends [unknown, ...unknown[]]>(
+  ...args: [...arbitraries: { [K in keyof Ts]: Arbitrary<Ts[K]> }, predicate: (...args: Ts) => Promise<boolean | void>]
+): IAsyncPropertyWithHooks<Ts> {
+  if (args.length < 2) {
+    throw new Error('asyncProperty expects at least two parameters');
+  }
+  const arbs = args.slice(0, args.length - 1) as { [K in keyof Ts]: Arbitrary<Ts[K]> };
+  const p = args[args.length - 1] as (...args: Ts) => Promise<boolean | void>;
+  return new AsyncProperty(tuple<Ts>(...arbs), (t) => p(...t));
+}
 
 export { asyncProperty, IAsyncProperty, IAsyncPropertyWithHooks, AsyncPropertyHookFunction };
--- src/check/property/Property.ts
+++ src/check/property/Property.ts
@@ -1,4 +1,22 @@
-import { property } from './Property.generated';
-import { IProperty, IPropertyWithHooks, PropertyHookFunction } from './Property.generic';
+import { Arbitrary } from '../arbitrary/definition/Arbitrary';
+import { tuple } from '../arbitrary/TupleArbitrary';
+import { Property, IProperty, IPropertyWithHooks, PropertyHookFunction } from './Property.generic';
+
+/**
+ * Instantiate a new {@link fast-check#IProperty}
+ * @param predicate - Assess the success of the property. Would be considered falsy if it throws or if its output evaluates to false
+ * @remarks Since 0.0.1
+ * @public
+ */
+function property<Ts extends [unknown, ...unknown[]]>(
+  ...args: [...arbitraries: { [K in keyof Ts]: Arbitrary<Ts[K]> }, predicate: (...args: Ts) => boolean | void]
+): IPropertyWithHooks<Ts> {
+  if (args.length < 2) {
+    throw new Error('property expects at least two parameters');
+  }
+  const arbs = args.slice(0, args.length - 1) as { [K in keyof Ts]: Arbitrary<Ts[K]> };
+  const p = args[args.length - 1] as (...args: Ts) => boolean | void;
+  return new Property(tuple<Ts>(...arbs), (t) => p(...t));
+}
 
 export { property, IProperty, IPropertyWithHooks, PropertyHookFunction };
