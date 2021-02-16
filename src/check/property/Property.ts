fb4ecb05fdd6420d96da47a6f5ee3742fd38b8f2
Nicolas DUBIEN <github@dubien.org>
2021-02-16 23:42:50 +0100

    🏷️ Adopt variadic tuples for signatures of property (#1518)
    * Drop prebuild script
    * FIX Typings in tests
    * FIX E2E Typings
    * Adapt typings tests
    * FIX Missing argument
    * FIX FIbo test
    * Merge conflict from 2.14 to 2.15
    * Adapted for 2.19.0 and later

--- src/check/property/Property.ts
+++ src/check/property/Property.ts
@@ -1,4 +1,28 @@
-import { property } from './Property.generated';
-import { IProperty, IPropertyWithHooks, PropertyHookFunction } from './Property.generic';
+import { Arbitrary } from '../arbitrary/definition/Arbitrary';
+import { tuple } from '../../arbitrary/tuple';
+import { Property, IProperty, IPropertyWithHooks, PropertyHookFunction } from './Property.generic';
+import { AlwaysShrinkableArbitrary } from '../../arbitrary/_internals/AlwaysShrinkableArbitrary';
+import { convertFromNext, convertToNext } from '../arbitrary/definition/Converters';
+import { convertFromNextPropertyWithHooks } from './ConvertersProperty';
+
+/**
+ * Instantiate a new {@link fast-check#IProperty}
+ * @param predicate - Assess the success of the property. Would be considered falsy if it throws or if its output evaluates to false
+ * @remarks Since 0.0.1
+ * @public
+ */
+function property<Ts extends [unknown, ...unknown[]]>(
+  ...args: [...arbitraries: { [K in keyof Ts]: Arbitrary<Ts[K]> }, predicate: (...args: Ts) => boolean | void]
+): IPropertyWithHooks<Ts> {
+  if (args.length < 2) {
+    throw new Error('property expects at least two parameters');
+  }
+  const arbs = args.slice(0, args.length - 1) as { [K in keyof Ts]: Arbitrary<Ts[K]> };
+  const p = args[args.length - 1] as (...args: Ts) => boolean | void;
+  const mappedArbs = arbs.map((arb): typeof arb =>
+    convertFromNext(new AlwaysShrinkableArbitrary(convertToNext(arb)))
+  ) as typeof arbs;
+  return convertFromNextPropertyWithHooks(new Property(tuple<Ts>(...mappedArbs), (t) => p(...t)));
+}
 
 export { property, IProperty, IPropertyWithHooks, PropertyHookFunction };
