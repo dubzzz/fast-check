b525540e09b0588676a352b54c14841bdbba9086
Nicolas DUBIEN <github@dubien.org>
2021-03-29 23:00:10 +0200

    Migrate LetRec to NextArbitrary

--- src/check/arbitrary/LetRecArbitrary.ts
+++ src/check/arbitrary/LetRecArbitrary.ts
@@ -1,29 +1,45 @@
+import { Stream } from '../../fast-check-default';
 import { Random } from '../../random/generator/Random';
 import { Arbitrary } from './definition/Arbitrary';
-import { Shrinkable } from './definition/Shrinkable';
+import { convertFromNext, convertToNext } from './definition/Converters';
+import { NextArbitrary } from './definition/NextArbitrary';
+import { NextValue } from './definition/NextValue';
 
 /** @internal */
-export class LazyArbitrary extends Arbitrary<any> {
+export class LazyArbitrary extends NextArbitrary<any> {
   private static readonly MaxBiasLevels = 5;
   private numBiasLevels = 0;
   private lastBiasedArbitrary: {
-    biasedArb: Arbitrary<any>;
-    arb: Arbitrary<any>;
+    biasedArb: NextArbitrary<any>;
+    arb: NextArbitrary<any>;
     lvl: number;
     freq: number;
   } | null = null;
-  underlying: Arbitrary<any> | null = null;
+  underlying: NextArbitrary<any> | null = null;
 
   constructor(readonly name: string) {
     super();
   }
-  generate(mrng: Random): Shrinkable<any> {
+  generate(mrng: Random): NextValue<any> {
     if (!this.underlying) {
       throw new Error(`Lazy arbitrary ${JSON.stringify(this.name)} not correctly initialized`);
     }
     return this.underlying.generate(mrng);
   }
-  withBias(freq: number): Arbitrary<any> {
+  canGenerate(value: unknown): value is any {
+    if (!this.underlying) {
+      throw new Error(`Lazy arbitrary ${JSON.stringify(this.name)} not correctly initialized`);
+    }
+    return this.underlying.canGenerate(value);
+  }
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  shrink(value: any, context?: unknown): Stream<NextValue<any>> {
+    if (!this.underlying) {
+      throw new Error(`Lazy arbitrary ${JSON.stringify(this.name)} not correctly initialized`);
+    }
+    return this.underlying.shrink(value, context);
+  }
+  withBias(freq: number): NextArbitrary<any> {
     if (!this.underlying) {
       throw new Error(`Lazy arbitrary ${JSON.stringify(this.name)} not correctly initialized`);
     }
@@ -52,7 +68,7 @@ export class LazyArbitrary extends Arbitrary<any> {
 }
 
 /** @internal */
-function isLazyArbitrary(arb: Arbitrary<any> | undefined): arb is LazyArbitrary {
+function isLazyArbitrary(arb: NextArbitrary<any> | undefined): arb is LazyArbitrary {
   return typeof arb === 'object' && arb !== null && Object.prototype.hasOwnProperty.call(arb, 'underlying');
 }
 
@@ -78,12 +94,12 @@ function isLazyArbitrary(arb: Arbitrary<any> | undefined): arb is LazyArbitrary 
 export function letrec<T>(
   builder: (tie: (key: string) => Arbitrary<unknown>) => { [K in keyof T]: Arbitrary<T[K]> }
 ): { [K in keyof T]: Arbitrary<T[K]> } {
-  const lazyArbs: { [K in keyof T]?: Arbitrary<T[K]> } = Object.create(null);
+  const lazyArbs: { [K in keyof T]?: NextArbitrary<T[K]> } = Object.create(null);
   const tie = (key: keyof T): Arbitrary<any> => {
     if (!Object.prototype.hasOwnProperty.call(lazyArbs, key)) lazyArbs[key] = new LazyArbitrary(key as any);
     // Call to hasOwnProperty ensures that the property key will be defined
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return lazyArbs[key]!;
+    return convertFromNext(lazyArbs[key]!);
   };
   const strictArbs = builder(tie as any);
   for (const key in strictArbs) {
@@ -93,7 +109,7 @@ export function letrec<T>(
     }
     const lazyAtKey = lazyArbs[key];
     const lazyArb = isLazyArbitrary(lazyAtKey) ? lazyAtKey : new LazyArbitrary(key);
-    lazyArb.underlying = strictArbs[key];
+    lazyArb.underlying = convertToNext(strictArbs[key]);
     lazyArbs[key] = lazyArb;
   }
   return strictArbs;
