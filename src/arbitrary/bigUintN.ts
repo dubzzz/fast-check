118566477632dcc3e04af0e06da7a80fda26525e
Nicolas DUBIEN <github@dubien.org>
2021-04-12 18:22:20 +0200

    Change condition

--- src/arbitrary/bigUintN.ts
+++ src/arbitrary/bigUintN.ts
@@ -13,8 +13,8 @@ import { BigIntArbitrary } from './_internals/BigIntArbitrary';
  * @public
  */
 export function bigUintN(n: number): ArbitraryWithContextualShrink<bigint> {
-  if (n < 1) {
-    throw new Error('fc.bigUintN expects requested number of bits to be superior or equal to 1');
+  if (n < 0) {
+    throw new Error('fc.bigUintN expects requested number of bits to be superior or equal to 0');
   }
   const min = BigInt(0);
   const max = (BigInt(1) << BigInt(n)) - BigInt(1);
