ac9bd526699b01b5ea9ef18f2d59682d6ce34b43
Nicolas DUBIEN <github@dubien.org>
2021-04-21 21:26:48 +0200

    ✅ Rewrite tests dealing with `infiniteStream`

---
+++ test/unit/arbitrary/infiniteStream.spec.ts
@@ -0,0 +1,29 @@
+import { infiniteStream } from '../../../src/arbitrary/infiniteStream';
+
+import { convertFromNext, convertToNext } from '../../../src/check/arbitrary/definition/Converters';
+import { fakeNextArbitrary } from '../check/arbitrary/generic/NextArbitraryHelpers';
+
+import * as StreamArbitraryMock from '../../../src/arbitrary/_internals/StreamArbitrary';
+
+function beforeEachHook() {
+  jest.resetModules();
+  jest.restoreAllMocks();
+}
+beforeEach(beforeEachHook);
+
+describe('infiniteStream', () => {
+  it('should instantiate StreamArbitrary(arb, numValues) for infiniteStream(arb)', () => {
+    // Arrange
+    const { instance: sourceArbitrary } = fakeNextArbitrary();
+    const { instance } = fakeNextArbitrary();
+    const StreamArbitrary = jest.spyOn(StreamArbitraryMock, 'StreamArbitrary');
+    StreamArbitrary.mockImplementation(() => instance as StreamArbitraryMock.StreamArbitrary<unknown>);
+
+    // Act
+    const arb = infiniteStream(convertFromNext(sourceArbitrary));
+
+    // Assert
+    expect(StreamArbitrary).toHaveBeenCalledWith(sourceArbitrary);
+    expect(convertToNext(arb)).toBe(instance);
+  });
+});
