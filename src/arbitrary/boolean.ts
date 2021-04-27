80e9a8353a73951814c742a6ae6b4d369927265a
Nicolas DUBIEN <github@dubien.org>
2021-04-27 08:44:07 +0200

    🚚 Move `boolean` arbitrary into /arbitrary

---
+++ src/arbitrary/boolean.ts
@@ -0,0 +1,29 @@
+import { Arbitrary } from '../check/arbitrary/definition/Arbitrary';
+import { convertFromNext, convertToNext } from '../check/arbitrary/definition/Converters';
+import { integer } from './integer';
+
+/** @internal */
+function booleanMapper(v: number): boolean {
+  return v === 1;
+}
+
+/** @internal */
+function booleanUnmapper(v: unknown): number {
+  if (typeof v !== 'boolean') throw new Error('Unsupported input type');
+  return v === true ? 1 : 0;
+}
+
+/**
+ * For boolean values - `true` or `false`
+ * @remarks Since 0.0.6
+ * @public
+ */
+function boolean(): Arbitrary<boolean> {
+  return convertFromNext(
+    convertToNext(integer({ min: 0, max: 1 }))
+      .map(booleanMapper, booleanUnmapper)
+      .noBias()
+  );
+}
+
+export { boolean };
