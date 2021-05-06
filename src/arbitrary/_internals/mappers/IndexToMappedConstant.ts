1735904c8769b6e8d6e5995d1cf52bfe6f1168e8
Nicolas DUBIEN <github@dubien.org>
2021-05-07 00:29:28 +0200

    ✨ Add built-in unmapper support onto `mapToConstant` (#1836)
    Ideally `mapToConstant` should provide a more efficient way to compute the unmapping. For the moment the unmapping will be good enough given it will never be called except in explicit calls to canGenerate or shrink without context.

--- src/arbitrary/_internals/mappers/IndexToMappedConstant.ts
+++ src/arbitrary/_internals/mappers/IndexToMappedConstant.ts
@@ -1,7 +1,7 @@
 /** @internal */
 export function indexToMappedConstantMapperFor<T>(
   entries: { num: number; build: (idInGroup: number) => T }[]
-): (index: number) => T {
+): (choiceIndex: number) => T {
   return function indexToMappedConstantMapper(choiceIndex: number): T {
     let idx = -1;
     let numSkips = 0;
@@ -9,5 +9,44 @@ export function indexToMappedConstantMapperFor<T>(
       numSkips += entries[++idx].num;
     }
     return entries[idx].build(choiceIndex - numSkips + entries[idx].num);
+  };
+}
+
+/** @internal */
+type ReverseMapping = { mapping: Map<unknown, number>; negativeZeroIndex: number | undefined };
+
+/** @internal */
+function buildReverseMapping(entries: { num: number; build: (idInGroup: number) => unknown }[]): ReverseMapping {
+  const reverseMapping: ReverseMapping = { mapping: new Map(), negativeZeroIndex: undefined };
+  let choiceIndex = 0;
+  for (let entryIdx = 0; entryIdx !== entries.length; ++entryIdx) {
+    const entry = entries[entryIdx];
+    for (let idxInEntry = 0; idxInEntry !== entry.num; ++idxInEntry) {
+      const value = entry.build(idxInEntry);
+      if (Object.is(value, -0)) {
+        reverseMapping.negativeZeroIndex = choiceIndex;
+      } else {
+        reverseMapping.mapping.set(value, choiceIndex);
+      }
+      ++choiceIndex;
+    }
+  }
+  return reverseMapping;
+}
+
+/** @internal */
+export function indexToMappedConstantUnmapperFor<T>(
+  entries: { num: number; build: (idInGroup: number) => T }[]
+): (value: unknown) => number {
+  let reverseMapping: ReverseMapping | null = null;
+  return function indexToMappedConstantUnmapper(value: unknown): number {
+    if (reverseMapping === null) {
+      reverseMapping = buildReverseMapping(entries);
+    }
+    const choiceIndex = Object.is(value, -0) ? reverseMapping.negativeZeroIndex : reverseMapping.mapping.get(value);
+    if (choiceIndex === undefined) {
+      throw new Error('Unknown value encountered cannot be built using this mapToConstant');
+    }
+    return choiceIndex;
   };
 }
