09ce57c54317140a5b984b1486f94f016c136493
Nicolas DUBIEN <github@dubien.org>
2021-05-26 20:45:43 +0200

    🐛 Stricter unmapper for `dictionary` to prevent unmapping unrelated values (#1910)

--- test/unit/arbitrary/_internals/mappers/KeyValuePairsToObject.spec.ts
+++ test/unit/arbitrary/_internals/mappers/KeyValuePairsToObject.spec.ts
@@ -44,14 +44,20 @@ describe('keyValuePairsToObjectUnmapper', () => {
   });
 
   it.each`
-    value                  | condition
-    ${Object.create(null)} | ${'it has no prototype'}
-    ${new (class A {})()}  | ${'it is not just a simple object but a more complex type'}
-    ${[]}                  | ${'it is an Array'}
-    ${new Number(0)}       | ${'it is a boxed-Number'}
-    ${0}                   | ${'it is a number'}
-    ${null}                | ${'it is null'}
-    ${undefined}           | ${'it is undefined'}
+    value                                                                | condition
+    ${Object.create(null)}                                               | ${'it has no prototype'}
+    ${new (class A {})()}                                                | ${'it is not just a simple object but a more complex type'}
+    ${[]}                                                                | ${'it is an Array'}
+    ${new Number(0)}                                                     | ${'it is a boxed-Number'}
+    ${0}                                                                 | ${'it is a number'}
+    ${null}                                                              | ${'it is null'}
+    ${undefined}                                                         | ${'it is undefined'}
+    ${{ [Symbol('my-symbol')]: 5 }}                                      | ${'it contains a symbol property'}
+    ${Object.defineProperty({}, 'a', { value: 5, configurable: false })} | ${'it contains a non-configurable property'}
+    ${Object.defineProperty({}, 'a', { value: 5, enumerable: false })}   | ${'it contains a non-enumerable property'}
+    ${Object.defineProperty({}, 'a', { value: 5, writable: false })}     | ${'it contains a non-writable property'}
+    ${Object.defineProperty({}, 'a', { get: () => 5 })}                  | ${'it contains a get property'}
+    ${Object.defineProperty({}, 'a', { set: () => {} })}                 | ${'it contains a set property'}
   `('should reject unmap on instance when $condition', ({ value }) => {
     // Arrange / Act / Assert
     expect(() => keyValuePairsToObjectUnmapper(value)).toThrowError();
