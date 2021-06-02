8bfb611b59ea6c25c624ea3afcb362df0831686e
Nicolas DUBIEN <github@dubien.org>
2021-06-02 23:38:37 +0200

    ⚡️ Replace `yield`-based code in tosser by `class`-based

--- src/check/runner/Tosser.ts
+++ src/check/runner/Tosser.ts
@@ -10,17 +10,40 @@ function lazyGenerate<Ts>(generator: IRawProperty<Ts>, rng: prand.RandomGenerato
 }
 
 /** @internal */
-export function* toss<Ts>(
+class TosserGenerator<Ts> implements IterableIterator<() => Shrinkable<Ts>> {
+  private rng: prand.RandomGenerator;
+  private currentIndex: number;
+  constructor(
+    readonly generator: IRawProperty<Ts>,
+    readonly seed: number,
+    readonly random: (seed: number) => prand.RandomGenerator,
+    readonly examples: Ts[]
+  ) {
+    this.rng = random(seed);
+    this.currentIndex = -1;
+  }
+  [Symbol.iterator](): IterableIterator<() => Shrinkable<Ts>> {
+    if (this.currentIndex === -1) {
+      return this;
+    }
+    return new TosserGenerator(this.generator, this.seed, this.random, this.examples);
+  }
+  next(): IteratorResult<() => Shrinkable<Ts>> {
+    ++this.currentIndex;
+    if (this.currentIndex < this.examples.length) {
+      return { value: () => new Shrinkable(this.examples[this.currentIndex]), done: false };
+    }
+    this.rng = this.rng.jump ? this.rng.jump() : prand.skipN(this.rng, 42);
+    return { value: lazyGenerate(this.generator, this.rng, this.currentIndex - this.examples.length) };
+  }
+}
+
+/** @internal */
+export function toss<Ts>(
   generator: IRawProperty<Ts>,
   seed: number,
   random: (seed: number) => prand.RandomGenerator,
   examples: Ts[]
 ): IterableIterator<() => Shrinkable<Ts>> {
-  yield* examples.map((e) => () => new Shrinkable(e));
-  let idx = 0;
-  let rng = random(seed);
-  for (;;) {
-    rng = rng.jump ? rng.jump() : prand.skipN(rng, 42);
-    yield lazyGenerate(generator, rng, idx++);
-  }
+  return new TosserGenerator(generator, seed, random, examples);
 }
