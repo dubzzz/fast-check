10da7bb8e5a69dfd60b2d09f3ad67bcfd1fe0ef6
kazuki <pharcydetip@gmail.com>
2021-06-08 01:30:00 +0900

    create overload constantFrom

--- src/arbitrary/constantFrom.ts
+++ src/arbitrary/constantFrom.ts
@@ -8,13 +8,24 @@ import { ConstantArbitrary } from './_internals/ConstantArbitrary';
  * **WARNING**: It expects at least one value, otherwise it should throw
  *
  * @param values - Constant values to be produced (all values shrink to the first one)
+ */
+function constantFrom<T>(...values: T[]): Arbitrary<T>;
+
+/**
+ * For one `...values` values - all equiprobable
+ *
+ * **WARNING**: It expects at least one value, otherwise it should throw
+ *
+ * @param values - Constant values to be produced (all values shrink to the first one)
  *
  * @remarks Since 0.0.12
  * @public
  */
-export function constantFrom<TArgs extends any[] | [any]>(...values: TArgs): Arbitrary<TArgs[number]> {
+function constantFrom<TArgs extends any[] | [any]>(...values: TArgs): Arbitrary<TArgs[number]> {
   if (values.length === 0) {
     throw new Error('fc.constantFrom expects at least one parameter');
   }
   return convertFromNext(new ConstantArbitrary(values));
 }
+
+export { constantFrom };
