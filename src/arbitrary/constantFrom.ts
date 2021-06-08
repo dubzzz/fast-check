2bf5b0c93919a73f5bca2aa9bfeb276e3aa2f20d
kazuki <pharcydetip@gmail.com>
2021-06-08 16:46:30 +0900

    add jsdoc

--- src/arbitrary/constantFrom.ts
+++ src/arbitrary/constantFrom.ts
@@ -16,7 +16,19 @@ type Elem<T> = T extends any[] ? T[number] : T;
  * @public
  */
 function constantFrom<T>(...values: T[]): Arbitrary<T>;
+
+/**
+ * For one `...values` values - all equiprobable
+ *
+ * **WARNING**: It expects at least one value, otherwise it should throw
+ *
+ * @param values - Constant values to be produced (all values shrink to the first one)
+ *
+ * @remarks Since 0.0.12
+ * @public
+ */
 function constantFrom<TArgs extends any[] | [any]>(...values: TArgs): Arbitrary<TArgs[number]>;
+
 function constantFrom<TArgs extends any[] | [any] | any>(...values: Arrayfy<TArgs>): Arbitrary<Elem<TArgs>> {
   if (values.length === 0) {
     throw new Error('fc.constantFrom expects at least one parameter');
