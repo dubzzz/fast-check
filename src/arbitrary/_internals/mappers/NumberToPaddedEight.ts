31037e7d3bda34978fdcee6302103773e5521c47
Nicolas DUBIEN <github@dubien.org>
2021-06-24 01:29:21 +0200

    Reject invalid lengths

--- src/arbitrary/_internals/mappers/NumberToPaddedEight.ts
+++ src/arbitrary/_internals/mappers/NumberToPaddedEight.ts
@@ -8,9 +8,12 @@ export function numberToPaddedEightUnmapper(value: unknown): number {
   if (typeof value !== 'string') {
     throw new Error('Unsupported type');
   }
+  if (value.length !== 8) {
+    throw new Error('Unsupported value: invalid length');
+  }
   const n = parseInt(value, 16);
   if (value !== numberToPaddedEightMapper(n)) {
-    throw new Error('Unsupported value');
+    throw new Error('Unsupported value: invalid content');
   }
   return n;
 }
