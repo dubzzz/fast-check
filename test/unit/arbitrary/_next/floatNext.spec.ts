e4f4971a89caafe85f8823886144ab91d88102a8
Nicolas DUBIEN <github@dubien.org>
2021-07-08 22:18:27 +0200

    Migrate units on floatNext

--- test/unit/arbitrary/_next/floatNext.spec.ts
+++ test/unit/arbitrary/_next/floatNext.spec.ts
@@ -9,16 +9,39 @@ import {
   isStrictlySmaller,
   defaultFloatRecordConstraints,
 } from '../../check/arbitrary/generic/FloatingPointHelpers';
+import { floatToIndex, indexToFloat, MAX_VALUE_32 } from '../../../../src/arbitrary/_internals/helpers/FloatHelpers';
+import { convertFromNextWithShrunkOnce } from '../../../../src/check/arbitrary/definition/Converters';
 
-import { mocked } from 'ts-jest/utils';
-import { arbitraryFor } from '../../check/arbitrary/generic/ArbitraryBuilder';
-import * as stubRng from '../../stubs/generators';
+import { fakeNextArbitrary, fakeNextArbitraryStaticValue } from '../../check/arbitrary/generic/NextArbitraryHelpers';
+import { fakeRandom } from '../../check/arbitrary/generic/RandomHelpers';
 
 import * as IntegerMock from '../../../../src/arbitrary/integer';
-import { floatToIndex, indexToFloat, MAX_VALUE_32 } from '../../../../src/arbitrary/_internals/helpers/FloatHelpers';
-jest.mock('../../../src/arbitrary/integer');
 
-const mrng = () => stubRng.mutable.nocall();
+function spyInteger() {
+  const { instance, map } = fakeNextArbitrary<number>();
+  const { instance: mappedInstance } = fakeNextArbitrary();
+  const integer = jest.spyOn(IntegerMock, 'integer');
+  integer.mockImplementation(() => convertFromNextWithShrunkOnce(instance, undefined));
+  map.mockReturnValue(mappedInstance);
+  return integer;
+}
+
+function spyIntegerWithValue(value: () => number) {
+  const { instance } = fakeNextArbitraryStaticValue<number>(value);
+  const integer = jest.spyOn(IntegerMock, 'integer');
+  integer.mockImplementation(() => convertFromNextWithShrunkOnce(instance, undefined));
+  return integer;
+}
+
+function beforeEachHook() {
+  jest.resetModules();
+  jest.restoreAllMocks();
+}
+beforeEach(beforeEachHook);
+fc.configureGlobal({
+  ...fc.readConfigureGlobal(),
+  beforeEach: beforeEachHook,
+});
 
 function minMaxForConstraints(ct: FloatNextConstraints) {
   const noDefaultInfinity = ct.noDefaultInfinity;
@@ -29,94 +52,112 @@ function minMaxForConstraints(ct: FloatNextConstraints) {
   return { min, max };
 }
 
-function mockNoOpIntegerArb(opts: { single?: boolean } = {}) {
-  // Mocking integer: not expecting any call there
-  const { integer } = mocked(IntegerMock);
-  if (opts.single) integer.mockImplementationOnce(() => arbitraryFor([]));
-  else integer.mockImplementation(() => arbitraryFor([]));
-  return integer;
-}
-
-beforeEach(() => {
-  jest.clearAllMocks();
-  jest.resetAllMocks();
-});
-const previousGlobal = fc.readConfigureGlobal();
-fc.configureGlobal({
-  ...previousGlobal,
-  beforeEach: () => {
-    jest.clearAllMocks();
-    jest.resetAllMocks();
-  },
-});
-
 describe('floatNext', () => {
-  it('Should accept any valid range of 32-bit floating point numbers (including infinity)', () =>
+  it('should accept any valid range of 32-bit floating point numbers (including infinity)', () => {
     fc.assert(
       fc.property(floatNextConstraints(), (ct) => {
-        mockNoOpIntegerArb();
-        expect(floatNext(ct)).toBeDefined();
+        // Arrange
+        spyInteger();
+
+        // Act
+        const arb = floatNext(ct);
+
+        // Assert
+        expect(arb).toBeDefined();
       })
-    ));
-  it('Should accept any constraits defining min (32-bit float not-NaN) equal to max', () =>
+    );
+  });
+
+  it('should accept any constraits defining min (32-bit float not-NaN) equal to max', () => {
     fc.assert(
       fc.property(
         float32raw(),
         fc.record({ noDefaultInfinity: fc.boolean(), noNaN: fc.boolean() }, { withDeletedKeys: true }),
         (f, otherCt) => {
+          // Arrange
           fc.pre(isNotNaN32bits(f));
-          mockNoOpIntegerArb();
-          expect(floatNext({ ...otherCt, min: f, max: f })).toBeDefined();
+          spyInteger();
+
+          // Act
+          const arb = floatNext({ ...otherCt, min: f, max: f });
+
+          // Assert
+          expect(arb).toBeDefined();
         }
       )
-    ));
-  it('Should reject non-32-bit or NaN floating point numbers if specified for min', () =>
+    );
+  });
+
+  it('should reject non-32-bit or NaN floating point numbers if specified for min', () => {
     fc.assert(
       fc.property(float64raw(), (f64) => {
+        // Arrange
         fc.pre(!isNotNaN32bits(f64));
-        mockNoOpIntegerArb();
+        const integer = spyInteger();
+
+        // Act / Assert
         expect(() => floatNext({ min: f64 })).toThrowError();
+        expect(integer).not.toHaveBeenCalled();
       })
-    ));
-  it('Should reject non-32-bit or NaN floating point numbers if specified for max', () =>
+    );
+  });
+
+  it('should reject non-32-bit or NaN floating point numbers if specified for max', () => {
     fc.assert(
       fc.property(float64raw(), (f64) => {
+        // Arrange
         fc.pre(!isNotNaN32bits(f64));
-        mockNoOpIntegerArb();
+        const integer = spyInteger();
+
+        // Act / Assert
         expect(() => floatNext({ max: f64 })).toThrowError();
+        expect(integer).not.toHaveBeenCalled();
       })
-    ));
-  it('Should reject if specified min is strictly greater than max', () =>
+    );
+  });
+
+  it('should reject if specified min is strictly greater than max', () => {
     fc.assert(
       fc.property(float32raw(), float32raw(), (fa32, fb32) => {
+        // Arrange
         fc.pre(isNotNaN32bits(fa32));
         fc.pre(isNotNaN32bits(fb32));
         fc.pre(!Object.is(fa32, fb32)); // Object.is can distinguish -0 from 0, while !== cannot
-        mockNoOpIntegerArb();
+        const integer = spyInteger();
         const min = isStrictlySmaller(fa32, fb32) ? fb32 : fa32;
         const max = isStrictlySmaller(fa32, fb32) ? fa32 : fb32;
+
+        // Act / Assert
         expect(() => floatNext({ min, max })).toThrowError();
+        expect(integer).not.toHaveBeenCalled();
       })
-    ));
-  it('Should reject impossible noDefaultInfinity-based ranges', () => {
-    mockNoOpIntegerArb();
+    );
+  });
+
+  it('should reject impossible noDefaultInfinity-based ranges', () => {
+    // Arrange
+    const integer = spyInteger();
+
+    // Act / Assert
     expect(() => floatNext({ min: Number.POSITIVE_INFINITY, noDefaultInfinity: true })).toThrowError();
     expect(() => floatNext({ max: Number.NEGATIVE_INFINITY, noDefaultInfinity: true })).toThrowError();
+    expect(integer).not.toHaveBeenCalled();
   });
-  it('Should properly convert integer value for index between min and max into its associated float value', () =>
+
+  it('should properly convert integer value for index between min and max into its associated float value', () =>
     fc.assert(
       fc.property(fc.option(floatNextConstraints(), { nil: undefined }), fc.maxSafeNat(), (ct, mod) => {
         // Arrange
-        const { integer } = mocked(IntegerMock);
+        const { instance: mrng } = fakeRandom();
         const { min, max } = minMaxForConstraints(ct || {});
         const minIndex = floatToIndex(min);
         const maxIndex = floatToIndex(max);
         const arbitraryGeneratedIndex = (mod % (maxIndex - minIndex + 1)) + minIndex;
-        integer.mockImplementationOnce(() => arbitraryFor([{ value: arbitraryGeneratedIndex }]));
+        spyIntegerWithValue(() => arbitraryGeneratedIndex);
 
         // Act
         const arb = floatNext(ct);
-        const { value_: f } = arb.generate(mrng());
+        const { value_: f } = arb.generate(mrng);
 
         // Assert
         expect(f).toBe(indexToFloat(arbitraryGeneratedIndex));
@@ -126,12 +167,12 @@ describe('floatNext', () => {
   describe('with NaN', () => {
     const withNaNRecordConstraints = { ...defaultFloatRecordConstraints, noNaN: fc.constant(false) };
 
-    it('Should ask for a range with one extra value (far from zero)', () =>
+    it('should ask for a range with one extra value (far from zero)', () => {
       fc.assert(
         fc.property(floatNextConstraints(withNaNRecordConstraints), (ct) => {
           // Arrange
           const { max } = minMaxForConstraints(ct);
-          const integer = mockNoOpIntegerArb();
+          const integer = spyInteger();
 
           // Act
           floatNext({ ...ct, noNaN: true });
@@ -151,15 +192,17 @@ describe('floatNext', () => {
             expect(integerConstraintsWithNaN.max).toBe(integerConstraintsNoNaN.max);
           }
         })
-      ));
-    it('Should properly convert the extra value to NaN', () =>
+      );
+    });
+
+    it('should properly convert the extra value to NaN', () =>
       fc.assert(
         fc.property(floatNextConstraints(withNaNRecordConstraints), (ct) => {
           // Arrange
           // Setup mocks for integer
+          const { instance: mrng } = fakeRandom();
           const arbitraryGenerated = { value: Number.NaN };
-          const integer = mockNoOpIntegerArb({ single: true });
-          integer.mockImplementationOnce(() => arbitraryFor([arbitraryGenerated]));
+          const integer = spyIntegerWithValue(() => arbitraryGenerated.value);
           // Call float next to find out the value required for NaN
           floatNext({ ...ct, noNaN: true });
           const arb = floatNext(ct);
@@ -171,7 +214,7 @@ describe('floatNext', () => {
           arbitraryGenerated.value = indexForNaN;
 
           // Act
-          const { value_: f } = arb.generate(mrng());
+          const { value_: f } = arb.generate(mrng);
 
           // Assert
           expect(f).toBe(Number.NaN);
@@ -183,12 +226,12 @@ describe('floatNext', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { noNaN, ...noNaNRecordConstraints } = defaultFloatRecordConstraints;
 
-    it('Should ask integers between the indexes corresponding to min and max', () =>
+    it('should ask integers between the indexes corresponding to min and max', () => {
       fc.assert(
         fc.property(floatNextConstraints(noNaNRecordConstraints), (ctDraft) => {
           // Arrange
           const ct = { ...ctDraft, noNaN: true };
-          const integer = mockNoOpIntegerArb();
+          const integer = spyInteger();
           const { min, max } = minMaxForConstraints(ct);
           const minIndex = floatToIndex(min);
           const maxIndex = floatToIndex(max);
@@ -200,6 +243,7 @@ describe('floatNext', () => {
           expect(integer).toHaveBeenCalledTimes(1);
           expect(integer).toHaveBeenCalledWith({ min: minIndex, max: maxIndex });
         })
-      ));
+      );
+    });
   });
 });
