e406304f818ca1f21f8f3ec349fecde287c57cf4
Nicolas DUBIEN <github@dubien.org>
2021-10-01 21:19:29 +0200

    ✅ Add tests for JSON arbitraries (#2338)

---
+++ test/unit/arbitrary/jsonObject.spec.ts
@@ -0,0 +1,46 @@
+import fc from '../../../lib/fast-check';
+
+import { jsonObject, JsonSharedConstraints } from '../../../src/arbitrary/jsonObject';
+import { convertToNext } from '../../../src/check/arbitrary/definition/Converters';
+import {
+  assertProduceCorrectValues,
+  assertProduceSameValueGivenSameSeed,
+  assertProduceValuesShrinkableWithoutContext,
+  assertShrinkProducesSameValueWithoutInitialContext,
+} from '../check/arbitrary/generic/NextArbitraryAssertions';
+import { computeObjectDepth } from './__test-helpers__/ComputeObjectDepth';
+
+describe('jsonObject (integration)', () => {
+  type Extra = JsonSharedConstraints | undefined;
+  const extraParameters: fc.Arbitrary<Extra> = fc.option(
+    fc.record({
+      maxDepth: fc.nat({ max: 5 }),
+    }),
+    { nil: undefined }
+  );
+
+  const isCorrect = (v: unknown, extra: Extra) => {
+    expect(JSON.parse(fc.stringify(v))).toEqual(v); // JSON.stringify does not handle the -0 properly
+    if (extra !== undefined && extra.maxDepth !== undefined) {
+      expect(computeObjectDepth(v)).toBeLessThanOrEqual(extra.maxDepth);
+    }
+  };
+
+  const jsonObjectBuilder = (extra: Extra) => convertToNext(extra !== undefined ? jsonObject(extra) : jsonObject());
+
+  it('should produce the same values given the same seed', () => {
+    assertProduceSameValueGivenSameSeed(jsonObjectBuilder, { extraParameters });
+  });
+
+  it('should only produce correct values', () => {
+    assertProduceCorrectValues(jsonObjectBuilder, isCorrect, { extraParameters });
+  });
+
+  it('should produce values seen as shrinkable without any context', () => {
+    assertProduceValuesShrinkableWithoutContext(jsonObjectBuilder, { extraParameters });
+  });
+
+  it('should be able to shrink to the same values without initial context', () => {
+    assertShrinkProducesSameValueWithoutInitialContext(jsonObjectBuilder, { extraParameters });
+  });
+});
