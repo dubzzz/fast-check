6259035bc3e901146c9de7fd264975265c3047c1
Nicolas DUBIEN <github@dubien.org>
2021-10-11 08:28:39 +0200

    ♻️ Migrate IgnoreEqualValuesProperty to next gen property (#2374)

--- src/check/property/IgnoreEqualValuesProperty.ts
+++ src/check/property/IgnoreEqualValuesProperty.ts
@@ -1,28 +1,31 @@
-import { IRawProperty } from './IRawProperty';
+import { INextRawProperty } from './INextRawProperty';
 import { Random } from '../../random/generator/Random';
-import { Shrinkable } from '../arbitrary/definition/Shrinkable';
 import { stringify } from '../../utils/stringify';
 import { PreconditionFailure } from '../precondition/PreconditionFailure';
+import { NextValue } from '../arbitrary/definition/NextValue';
+import { Stream } from '../../stream/Stream';
 
 /** @internal */
 function fromSyncCached<Ts>(
-  cachedValue: ReturnType<IRawProperty<Ts, false>['run']>
-): ReturnType<IRawProperty<Ts, false>['run']> {
+  cachedValue: ReturnType<INextRawProperty<Ts, false>['run']>
+): ReturnType<INextRawProperty<Ts, false>['run']> {
   return cachedValue === null ? new PreconditionFailure() : cachedValue;
 }
 
 /** @internal */
 function fromCached<Ts>(
-  cachedValue: ReturnType<IRawProperty<Ts, false>['run']>,
+  cachedValue: ReturnType<INextRawProperty<Ts, false>['run']>,
   isAsync: false
-): ReturnType<IRawProperty<Ts, false>['run']>;
+): ReturnType<INextRawProperty<Ts, false>['run']>;
 /** @internal */
 function fromCached<Ts>(
-  cachedValue: ReturnType<IRawProperty<Ts, true>['run']>,
+  cachedValue: ReturnType<INextRawProperty<Ts, true>['run']>,
   isAsync: true
-): ReturnType<IRawProperty<Ts, true>['run']>;
+): ReturnType<INextRawProperty<Ts, true>['run']>;
 function fromCached<Ts>(
-  ...data: [ReturnType<IRawProperty<Ts, true>['run']>, true] | [ReturnType<IRawProperty<Ts, false>['run']>, false]
+  ...data:
+    | [ReturnType<INextRawProperty<Ts, true>['run']>, true]
+    | [ReturnType<INextRawProperty<Ts, false>['run']>, false]
 ) {
   if (data[1]) return data[0].then(fromSyncCached);
   return fromSyncCached(data[0]);
@@ -30,24 +33,34 @@ function fromCached<Ts>(
 
 /** @internal */
 function fromCachedUnsafe<Ts, IsAsync extends boolean>(
-  cachedValue: ReturnType<IRawProperty<Ts, IsAsync>['run']>,
+  cachedValue: ReturnType<INextRawProperty<Ts, IsAsync>['run']>,
   isAsync: IsAsync
-): ReturnType<IRawProperty<Ts, IsAsync>['run']> {
+): ReturnType<INextRawProperty<Ts, IsAsync>['run']> {
   return fromCached(cachedValue as any, isAsync as any) as any;
 }
 
 /** @internal */
-export class IgnoreEqualValuesProperty<Ts, IsAsync extends boolean> implements IRawProperty<Ts, IsAsync> {
-  private coveredCases: Map<string, ReturnType<IRawProperty<Ts, IsAsync>['run']>> = new Map();
+export class IgnoreEqualValuesProperty<Ts, IsAsync extends boolean> implements INextRawProperty<Ts, IsAsync> {
+  private coveredCases: Map<string, ReturnType<INextRawProperty<Ts, IsAsync>['run']>> = new Map();
 
-  constructor(readonly property: IRawProperty<Ts, IsAsync>, readonly skipRuns: boolean) {}
+  constructor(readonly property: INextRawProperty<Ts, IsAsync>, readonly skipRuns: boolean) {}
 
-  isAsync = (): IsAsync => this.property.isAsync();
-  generate = (mrng: Random, runId?: number): Shrinkable<Ts> => this.property.generate(mrng, runId);
-  run = (v: Ts): ReturnType<IRawProperty<Ts, IsAsync>['run']> => {
+  isAsync(): IsAsync {
+    return this.property.isAsync();
+  }
+
+  generate(mrng: Random, runId?: number): NextValue<Ts> {
+    return this.property.generate(mrng, runId);
+  }
+
+  shrink(value: NextValue<Ts>): Stream<NextValue<Ts>> {
+    return this.property.shrink(value);
+  }
+
+  run(v: Ts): ReturnType<INextRawProperty<Ts, IsAsync>['run']> {
     const stringifiedValue = stringify(v);
     if (this.coveredCases.has(stringifiedValue)) {
-      const lastOutput = this.coveredCases.get(stringifiedValue) as ReturnType<IRawProperty<Ts, IsAsync>['run']>;
+      const lastOutput = this.coveredCases.get(stringifiedValue) as ReturnType<INextRawProperty<Ts, IsAsync>['run']>;
       if (!this.skipRuns) {
         return lastOutput;
       }
@@ -56,5 +69,5 @@ export class IgnoreEqualValuesProperty<Ts, IsAsync extends boolean> implements I
     const out = this.property.run(v);
     this.coveredCases.set(stringifiedValue, out);
     return out;
-  };
+  }
 }
