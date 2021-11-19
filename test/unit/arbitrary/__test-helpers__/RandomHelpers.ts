127705988b11003b07008eaa334348b8979f635a
Nicolas DUBIEN <github@dubien.org>
2021-11-19 08:53:42 +0100

    FIX clone related error in tests

--- test/unit/arbitrary/__test-helpers__/RandomHelpers.ts
+++ test/unit/arbitrary/__test-helpers__/RandomHelpers.ts
@@ -2,7 +2,7 @@ import { MaybeMocked } from 'ts-jest/dist/utils/testing';
 import { Random } from '../../../../src/random/generator/Random';
 
 export function fakeRandom(): { instance: Random } & Omit<MaybeMocked<Random>, 'internalRng' | 'uniformIn'> {
-  const clone = jest.fn().mockImplementation(() => new MyRandom({} as any));
+  const clone = jest.fn();
   const next = jest.fn();
   const nextBoolean = jest.fn();
   const nextInt = jest.fn();
@@ -19,14 +19,8 @@ export function fakeRandom(): { instance: Random } & Omit<MaybeMocked<Random>, '
     nextDouble = nextDouble;
   }
 
-  return {
-    instance: new MyRandom({} as any),
-    clone,
-    next,
-    nextBoolean,
-    nextInt,
-    nextBigInt,
-    nextArrayInt,
-    nextDouble,
-  };
+  // Calling `new MyRandom` triggers a call to the default ctor of `Random`.
+  // As we don't use anything from this base class, we just pass the ctor with a value that looks ok for it.
+  const instance = new MyRandom({ clone: () => null } as any);
+  return { instance, clone, next, nextBoolean, nextInt, nextBigInt, nextArrayInt, nextDouble };
 }
