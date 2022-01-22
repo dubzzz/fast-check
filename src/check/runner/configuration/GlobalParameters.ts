926cf5840b6f965477cce2eea7c677d228161fde
Nicolas DUBIEN <github@dubien.org>
2022-01-22 20:46:45 +0100

    ✨ Add ability to configure a base size (#2669)

--- src/check/runner/configuration/GlobalParameters.ts
+++ src/check/runner/configuration/GlobalParameters.ts
@@ -1,3 +1,4 @@
+import { Size } from '../../../arbitrary/_internals/helpers/MaxLengthFromMinLength';
 import { Parameters } from './Parameters';
 
 /** @internal */
@@ -62,6 +63,28 @@ export type GlobalParameters = Pick<Parameters<unknown>, Exclude<keyof Parameter
    * @remarks Since 2.3.0
    */
   asyncAfterEach?: GlobalAsyncPropertyHookFunction;
+  /**
+   * Define the base size to be used by arbitraries.
+   *
+   * By default arbitraries not specifying any size will default to it (except in some cases when used defaultSizeToMaxWhenMaxSpecified is true).
+   * For some arbitraries users will want to override the default and either define another size relative to this one,
+   * or a fixed one.
+   *
+   * @defaultValue `"small"`
+   * @remarks Since 2.22.0
+   */
+  baseSize?: Size;
+  /**
+   * When set to `true` and if the size has not been defined for this precise instance,
+   * it will automatically default to `"max"` if the user specified a upper bound for the range.
+   *
+   * When `false`, the size will be defaulted to `baseSize` even if the user specified
+   * a upper bound for the range.
+   *
+   * @defaultValue `true` - for backward compatibility reasons
+   * @remarks Since 2.22.0
+   */
+  defaultSizeToMaxWhenMaxSpecified?: boolean;
 };
 /**
  * Define global parameters that will be used by all the runners
