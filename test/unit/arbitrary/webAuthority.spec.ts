ac075349b9f33b8dbb422c68f45ba384df138249
Nicolas DUBIEN <github@dubien.org>
2022-02-07 23:16:06 +0100

    Update webAuthority tests

--- test/unit/arbitrary/webAuthority.spec.ts
+++ test/unit/arbitrary/webAuthority.spec.ts
@@ -1,12 +1,15 @@
 import fc from '../../../lib/fast-check';
 import { webAuthority, WebAuthorityConstraints } from '../../../src/arbitrary/webAuthority';
 import { convertToNext } from '../../../src/check/arbitrary/definition/Converters';
+import { URL } from 'url';
 
 import {
+  assertProduceCorrectValues,
   assertProduceSameValueGivenSameSeed,
   assertProduceValuesShrinkableWithoutContext,
   assertShrinkProducesSameValueWithoutInitialContext,
 } from './__test-helpers__/NextArbitraryAssertions';
+import { sizeArb } from './__test-helpers__/SizeHelpers';
 
 function beforeEachHook() {
   jest.resetModules();
@@ -24,14 +27,23 @@ describe('webAuthority (integration)', () => {
       withIPv6: fc.boolean(),
       withPort: fc.boolean(),
       withUserInfo: fc.boolean(),
+      size: sizeArb,
     },
     { requiredKeys: [] }
   );
+
+  const isCorrectForURL = (webAuthority: string) => {
+    expect(() => new URL(`http://${webAuthority}`)).not.toThrow();
+  };
 
   const webAuthorityBuilder = (extra: Extra) => convertToNext(webAuthority(extra));
 
   it('should produce the same values given the same seed', () => {
     assertProduceSameValueGivenSameSeed(webAuthorityBuilder, { extraParameters });
+  });
+
+  it('should only produce correct values regarding `new URL`', () => {
+    assertProduceCorrectValues(webAuthorityBuilder, isCorrectForURL, { extraParameters });
   });
 
   it('should produce values seen as shrinkable without any context', () => {
