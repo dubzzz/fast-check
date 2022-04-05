d90e9b354ac4e7dba72f95987c40674b3884c040
Nicolas DUBIEN <github@dubien.org>
2022-04-06 00:07:03 +0200

    ✨ Adopt better way to bias oneof with depth (#2828)
    The "depth benefit" is an internal value used within frequency or oneof in order to bias the generated value towards the base one as we go deeper in the generated structure.
    
    Before that change `oneof(base, tie(self), tie(self))` had more chance to produce deeper structures than `oneof(base, tie(self))`. Now it should be different.
    
    Stats will be attached into the PR.

--- src/arbitrary/_internals/FrequencyArbitrary.ts
+++ src/arbitrary/_internals/FrequencyArbitrary.ts
@@ -188,14 +188,14 @@ export class FrequencyArbitrary<T> extends NextArbitrary<T> {
   /** Compute the benefit for the current depth */
   private computeNegDepthBenefit(): number {
     const depthFactor = this.constraints.depthFactor;
-    if (depthFactor === undefined || depthFactor <= 0) {
+    if (depthFactor === undefined || depthFactor <= 0 || this.warbs[0].weight === 0) {
       return 0;
     }
     // We use a pow-based biased benefit as the deeper we go the more chance we have
     // to encounter thousands of instances of the current arbitrary.
     const depthBenefit = Math.floor(Math.pow(1 + depthFactor, this.context.depth)) - 1;
     // -0 has to be converted into 0 thus we call ||0
-    return -Math.min(this.warbs[0].weight * depthBenefit, Number.MAX_SAFE_INTEGER) || 0;
+    return -Math.min(this.totalWeight * depthBenefit, Number.MAX_SAFE_INTEGER) || 0;
   }
 }
 
