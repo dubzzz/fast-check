fc40f9a06bb14717a4e40059bad15e0ae0c30b12
Nicolas DUBIEN <github@dubien.org>
2022-05-05 19:31:36 +0200

    Reduce requested size

--- example/babel.config.cjs
+++ example/babel.config.cjs
@@ -1,16 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        // Don't add polyfills automatically per file, and don't transform import "core-js"
-        // or import "@babel/polyfill" to individual polyfills.
-        useBuiltIns: false,
-        // This option is useful for "blacklisting" a transform like @babel/plugin-transform-regenerator
-        // if you don't use generators and don't want to include regeneratorRuntime (when using useBuiltIns)
-        exclude: ['@babel/plugin-transform-regenerator'],
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
+  plugins: ['@babel/plugin-transform-modules-commonjs'],
+  presets: ['@babel/preset-typescript'],
 };
