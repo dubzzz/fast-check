d5c56242f9c8ad3304d66d507a4b290a4bd6bb71
Nicolas DUBIEN <github@dubien.org>
2022-05-05 20:03:31 +0200

    Add babel/preset-react for examples

--- example/babel.config.cjs
+++ example/babel.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   plugins: [['@babel/plugin-transform-modules-commonjs', { importInterop: 'node' }]],
-  presets: ['@babel/preset-typescript'],
+  presets: ['@babel/preset-react', '@babel/preset-typescript'],
 };
