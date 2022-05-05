6af9f2fd8f406c89322bd0c849668f1f21a8fcea
Nicolas DUBIEN <github@dubien.org>
2022-05-05 19:25:12 +0200

    ♻️ Replace deprecated usages of `integer` by constraint-based ones (#2921)

--- test/unit/arbitrary/date.spec.ts
+++ test/unit/arbitrary/date.spec.ts
@@ -12,7 +12,8 @@ import {
 
 import * as _IntegerMock from '../../../src/arbitrary/integer';
 import { ArbitraryWithShrink } from '../../../src/check/arbitrary/definition/ArbitraryWithShrink';
-const IntegerMock: { integer: (min: number, max: number) => ArbitraryWithShrink<number> } = _IntegerMock;
+const IntegerMock: { integer: (constraints?: _IntegerMock.IntegerConstraints) => ArbitraryWithShrink<number> } =
+  _IntegerMock;
 
 function beforeEachHook() {
   jest.resetModules();
@@ -55,9 +56,9 @@ describe('date', () => {
 
         // Act
         date(constraints);
-        const [rangeMin] = integer.mock.calls[0];
+        const { min: rangeMin } = integer.mock.calls[0][0]!;
         const [mapper] = map.mock.calls[0];
-        const minDate = mapper(rangeMin) as Date;
+        const minDate = mapper(rangeMin!) as Date;
 
         // Assert
         if (constraints.min !== undefined) {
@@ -85,9 +86,9 @@ describe('date', () => {
 
         // Act
         date(constraints);
-        const [, rangeMax] = integer.mock.calls[0];
+        const { max: rangeMax } = integer.mock.calls[0][0]!;
         const [mapper] = map.mock.calls[0];
-        const maxDate = mapper(rangeMax) as Date;
+        const maxDate = mapper(rangeMax!) as Date;
 
         // Assert
         if (constraints.max !== undefined) {
@@ -115,9 +116,9 @@ describe('date', () => {
 
         // Act
         date(constraints);
-        const [rangeMin, rangeMax] = integer.mock.calls[0];
+        const { min: rangeMin, max: rangeMax } = integer.mock.calls[0][0]!;
         const [mapper] = map.mock.calls[0];
-        const d = mapper(rangeMin + (mod % (rangeMax - rangeMin + 1))) as Date;
+        const d = mapper(rangeMin! + (mod % (rangeMax! - rangeMin! + 1))) as Date;
 
         // Assert
         expect(d.getTime()).not.toBe(Number.NaN);
