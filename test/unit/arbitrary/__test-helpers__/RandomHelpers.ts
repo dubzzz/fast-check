ff954442d51e00269fb8662c05412ff165df7701
Nicolas DUBIEN <github@dubien.org>
2022-05-06 19:11:12 +0200

    💥 Remove forked typings for `pure-rand` (#2941)
    Remove forked typings of `pure-rand` and only expose the ones coming with the library `pure-rand`.
    Change initiated during the alpha release cycle for v3 under #2462.

--- test/unit/arbitrary/__test-helpers__/RandomHelpers.ts
+++ test/unit/arbitrary/__test-helpers__/RandomHelpers.ts
@@ -19,14 +19,8 @@ export function fakeRandom(): { instance: Random } & Omit<MaybeMocked<Random>, '
     nextDouble = nextDouble;
   }
 
-  return {
-    instance: new MyRandom({} as any),
-    clone,
-    next,
-    nextBoolean,
-    nextInt,
-    nextBigInt,
-    nextArrayInt,
-    nextDouble,
-  };
+  // Calling `new MyRandom` triggers a call to the default ctor of `Random`.
+  // As we don't use anything from this base class, we just pass the ctor with a value that looks ok for it.
+  const instance = new MyRandom({ clone: () => null } as any);
+  return { instance, clone, next, nextBoolean, nextInt, nextBigInt, nextArrayInt, nextDouble };
 }
