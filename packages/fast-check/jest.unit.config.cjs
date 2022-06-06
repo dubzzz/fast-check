ac2d4a661d705f23cc1904b04816b51030bca802
Nicolas DUBIEN <github@dubien.org>
2022-06-06 22:07:55 +0200

    👷 Fix script not using the right path

--- packages/fast-check/jest.unit.config.cjs
+++ packages/fast-check/jest.unit.config.cjs
@@ -3,5 +3,5 @@ const conf = require('./jest.config.cjs');
 module.exports = Object.assign(conf, {
   testMatch: ['<rootDir>/test/unit/**/*.spec.ts'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/test/', '<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/test/'],
 });
