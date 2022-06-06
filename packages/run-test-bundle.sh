59048380f46a2135b3c4776fa46a3ddc9b0df743
Nicolas DUBIEN <github@dubien.org>
2022-06-06 21:25:57 +0200

    👷 Simplify test-bundle script following merge of Yarn 3 (#2987)
    Related to #2986

--- packages/run-test-bundle.sh
+++ packages/run-test-bundle.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-
-# Echo commands in the logs
-set -x
-
-# Autoquit on crash
-set -e
+set -x # Echo commands in the logs
+set -e # Autoquit on crash
 
 ## Versions of node >=13.2.0 support es modules without any flag
 ## Versions 12.x of node >=12.18.0 support es modules without any flag
@@ -12,43 +8,14 @@ NODE_MAJOR=$(node --version | cut -d. -f 1 | cut -dv -f 2)
 NODE_MINOR=$(node --version | cut -d. -f 2)
 
 if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ] || [ $NODE_MAJOR -eq 12 ] && [ $NODE_MINOR -ge 18 ]; then
-    cd test-bundle-node-with-import
-    yarn test
-    cd ..
-
-    cd test-bundle-node-extension-mjs
-    yarn test
-    cd ..
+    yarn workspace @fast-check/test-bundle-node-with-import test
+    yarn workspace @fast-check/test-bundle-node-extension-mjs test
 fi
-
-cd test-bundle-node-extension-cjs
-yarn test
-cd ..
-
-cd test-bundle-node-with-require
-yarn test
-cd ..
-
-cd test-bundle-rollup-with-import
-yarn test
-cd ..
-
-cd test-bundle-rollup-with-require
-yarn test
-cd ..
-
-cd test-bundle-webpack-with-import
-yarn test
-cd ..
-
-cd test-bundle-webpack-with-require
-yarn test
-cd ..
-
-cd test-bundle-esbuild-with-require
-yarn test
-cd ..
-
-cd test-bundle-esbuild-with-import
-yarn test
-cd ..
+yarn workspace @fast-check/test-bundle-node-extension-cjs test
+yarn workspace @fast-check/test-bundle-node-with-require test
+yarn workspace @fast-check/test-bundle-rollup-with-import test
+yarn workspace @fast-check/test-bundle-rollup-with-require test
+yarn workspace @fast-check/test-bundle-webpack-with-import test
+yarn workspace @fast-check/test-bundle-webpack-with-require test
+yarn workspace @fast-check/test-bundle-esbuild-with-require test
+yarn workspace @fast-check/test-bundle-esbuild-with-import test
