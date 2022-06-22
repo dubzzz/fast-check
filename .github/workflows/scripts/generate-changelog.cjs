99d5ca6e54f6ffe6cc1e38f4782a1406e0cdfc87
Nicolas DUBIEN <github@dubien.org>
2022-06-23 01:05:49 +0200

    👷 Handle monorepo in generate changelog (#3032)

--- .github/workflows/scripts/generate-changelog.cjs
+++ .github/workflows/scripts/generate-changelog.cjs
@@ -21,6 +21,14 @@ function buildPrLine(pr, title) {
  * @returns {Promise<{breakingSection:string[], newFeaturesSection:string[], maintenanceSection:{type:string,pr:string,title:string}[], errors: string[]}>}
  */
 async function extractAndParseDiff(fromIdentifier) {
+  const breakingSection = [];
+  const newFeaturesSection = [];
+  const maintenanceSection = [];
+  const errors = [];
+  if (isInitialTag(fromIdentifier)) {
+    return { breakingSection, newFeaturesSection, maintenanceSection, errors };
+  }
+
   // Extract raw diff log
   const { stdout: diffOutput } = await execFile('git', ['--no-pager', 'log', `${fromIdentifier}..HEAD`, '--format=%s']);
   const diffOutputLines = diffOutput
@@ -29,21 +37,15 @@ async function extractAndParseDiff(fromIdentifier) {
     .filter((line) => line.length !== 0);
 
   // Parse raw diff log
-  const breakingSection = [];
-  const newFeaturesSection = [];
-  const maintenanceSection = [];
-  const errors = [];
   for (const lineDiff of diffOutputLines) {
     const [type, ...titleAndPR] = lineDiff.split(' ');
     const prExtractor = /^(.*) \(#(\d+)\)$/;
-    let title;
-    let pr;
-    try {
-      [, title, pr] = prExtractor.exec(titleAndPR.join(' '));
-    } catch (err) {
+    const m = prExtractor.exec(titleAndPR.join(' '));
+    if (!m) {
       errors.push(`Failed to extract PR/title from: ${JSON.stringify(lineDiff)}`);
       break;
     }
+    const [, title, pr] = m;
     switch (type) {
       case '💥':
       case ':boom:':
@@ -122,6 +124,28 @@ function extractMajorMinorPatch(tagName) {
 }
 
 /**
+ * Compute tag from version
+ * @param {string} versionName
+ * @param {string} packageName
+ * @returns {string}
+ */
+function computeTag(versionName, packageName) {
+  if (packageName === 'fast-check') {
+    return `v${versionName}`;
+  }
+  return `${packageName.split('/')[1]}/v${versionName}`;
+}
+
+/**
+ * Check if tag corresponds to v0
+ * @param {string} tagName
+ * @returns {boolean}
+ */
+function isInitialTag(tagName) {
+  return tagName.endsWith('v0.0.0');
+}
+
+/**
  * Extract the kind of release
  * @param {string} oldTagName
  * @param {string} newTagName
@@ -140,85 +164,82 @@ function extractReleaseKind(oldTagName, newTagName) {
 }
 
 /**
- * @param {{shortDescription:string}} configuration
  * @returns {Promise<{branchName:string, commitName:string, errors:string[]}>}
  */
-async function run({ shortDescription }) {
-  const packageName = 'fast-check';
+async function run() {
+  const allErrors = [];
 
-  // Get next version via yarn
+  // Get packages to be bumped via yarn
   const { stdout: yarnOut } = await execFile('yarn', ['version', 'apply', '--all', '--dry-run', '--json']);
   const allBumps = yarnOut
     .split('\n')
     .filter((line) => line.trim().length !== 0)
     .map((line) => JSON.parse(line));
-  const fastCheckBump = allBumps.find((bump) => bump.ident === packageName);
-  if (fastCheckBump === undefined) {
-    throw new Error(
-      `Unable to find ${packageName} in the list of bumped versions, received:\n${yarnOut
-        .split('\n')
-        .map((line) => `>  ${line}`)}`
-    );
+
+  for (const packageBump of allBumps) {
+    const { oldVersion, newVersion, cwd: packageLocation, ident: packageName } = packageBump;
+
+    // Extract metas for changelog
+    const oldTag = computeTag(oldVersion, packageName);
+    const newTag = computeTag(newVersion, packageName);
+    const releaseKind = extractReleaseKind(oldTag, newTag);
+    const { breakingSection, newFeaturesSection, maintenanceSection, errors } = await extractAndParseDiff(oldTag);
+
+    // Build changelog message
+    const codeUrl = `https://github.com/dubzzz/fast-check/tree/${encodeURIComponent(newTag)}`;
+    const diffUrl = `https://github.com/dubzzz/fast-check/compare/${encodeURIComponent(oldTag)}...${encodeURIComponent(
+      newTag
+    )}`;
+    const breakingBlock = breakingSection
+      .reverse()
+      .map((line) => `- ${line}`)
+      .join('\n');
+    const newFeaturesBlock = newFeaturesSection
+      .reverse()
+      .map((line) => `- ${line}`)
+      .join('\n');
+    const maintenanceBlock = maintenanceSection
+      .reverse()
+      .sort((a, b) => a.type.localeCompare(b.type))
+      .map(({ type, title, pr }) => `- ${buildPrLine(pr, `${type}: ${title}`)}`)
+      .join('\n');
+    const body =
+      `# ${newVersion}\n\n` +
+      `_TODO Description_\n` +
+      `[[Code](${codeUrl})]${!isInitialTag(oldTag) ? `[[Diff](${diffUrl})]` : ''}\n\n` +
+      (breakingBlock.length !== 0 ? '## Breaking changes\n\n' + `${breakingBlock}\n\n` : '') +
+      '## Features\n\n' +
+      `${newFeaturesBlock}\n\n` +
+      '## Fixes\n\n' +
+      `${maintenanceBlock}`;
+
+    // Report in console
+    console.log(`Changelog for ${packageName} is:\n\n${body}\n\n---`);
+    if (errors.length > 0) {
+      allErrors.push(...errors);
+      console.log(`Got errors:\n${errors.join('\n')}`);
+    }
+
+    // Update changelog
+    const changelogPath = path.join(packageLocation, 'CHANGELOG.md');
+    const previousContent = await readFile(changelogPath);
+    await writeFile(changelogPath, `${body}\n\n${releaseKind !== 'patch' ? `---\n\n` : ''}${previousContent}`);
+    await execFile('git', ['add', changelogPath]);
   }
-  const { oldVersion, newVersion, cwd: packageLocation } = fastCheckBump;
 
-  // Extract metas for changelog
-  const oldTag = `v${oldVersion}`;
-  const newTag = `v${newVersion}`;
-  const releaseKind = extractReleaseKind(oldTag, newTag);
-  const { breakingSection, newFeaturesSection, maintenanceSection, errors } = await extractAndParseDiff(oldTag);
-
-  // Build changelog message
-  const codeUrl = `https://github.com/dubzzz/fast-check/tree/${newTag}`;
-  const diffUrl = `https://github.com/dubzzz/fast-check/compare/${oldTag}...${newTag}`;
-  const breakingBlock = breakingSection
-    .reverse()
-    .map((line) => `- ${line}`)
-    .join('\n');
-  const newFeaturesBlock = newFeaturesSection
-    .reverse()
-    .map((line) => `- ${line}`)
-    .join('\n');
-  const maintenanceBlock = maintenanceSection
-    .reverse()
-    .sort((a, b) => a.type.localeCompare(b.type))
-    .map(({ type, title, pr }) => `- ${buildPrLine(pr, `${type}: ${title}`)}`)
-    .join('\n');
-  const body =
-    `# ${newVersion}\n\n` +
-    `_${shortDescription}_\n` +
-    `[[Code](${codeUrl})][[Diff](${diffUrl})]\n\n` +
-    (breakingBlock.length !== 0 ? '## Breaking changes\n\n' + `${breakingBlock}\n\n` : '') +
-    '## Features\n\n' +
-    `${newFeaturesBlock}\n\n` +
-    '## Fixes\n\n' +
-    `${maintenanceBlock}`;
-
-  // Report in console
-  console.log(`Changelog is:\n\n${body}\n\n---`);
-  if (errors.length > 0) {
-    console.log(`Got errors:\n${errors.join('\n')}`);
-  }
-
-  // Update changelog
-  const changelogPath = path.join(packageLocation, 'CHANGELOG.md');
-  const previousContent = await readFile(changelogPath);
-  await writeFile(changelogPath, `${body}\n\n${releaseKind !== 'patch' ? `---\n\n` : ''}${previousContent}`);
-  await execFile('git', ['add', changelogPath]);
-
-  // Update package.json
+  // Update all needed package.json
   await execFile('yarn', ['version', 'apply', '--all']);
-  await execFile('git', ['add', path.join(packageLocation, 'package.json')]);
+  await execFile('git', ['add', './**/package.json']);
 
   // Create another branch and commit on it
-  const branchName = `changelog-${newVersion.replace(/\./g, '-')}-${Math.random().toString(16).substring(2)}`;
-  const commitName = `🔖 Update CHANGELOG.md for ${newVersion}`;
+  const branchName = `changelog-${Math.random().toString(16).substring(2)}`;
+  const commitName = `🔖 Update CHANGELOG.md for ${allBumps.map((b) => `${b.ident}@${b.newVersion}`).join(', ')}`;
   await execFile('git', ['checkout', '-b', branchName]);
   await execFile('git', ['commit', '-m', commitName]);
   await execFile('git', ['push', '--set-upstream', 'origin', branchName]);
 
   // Return useful details
-  return { branchName, commitName, errors };
+  return { branchName, commitName, errors: allErrors };
 }
 
 exports.run = run;
---
+++ packages/packaged/CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.0.0
+
+`@fast-check/packaged` is coming soon...
