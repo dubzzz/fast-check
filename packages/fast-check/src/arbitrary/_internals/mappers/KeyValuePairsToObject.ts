963e1d92b5249c5016b6b6cae3639dede5686020
Nicolas DUBIEN <github@dubien.org>
2022-07-08 08:37:27 +0200

    🐛 Allow strange keys as keys of dictionary
    Up-to-now the instances of `dictionary` were unable to generate key "__proto__" even if they were asked to. This is now not anymore the case and they can easily create them too.

--- packages/fast-check/src/arbitrary/_internals/mappers/KeyValuePairsToObject.ts
+++ packages/fast-check/src/arbitrary/_internals/mappers/KeyValuePairsToObject.ts
@@ -1,9 +1,13 @@
 /** @internal */
 export function keyValuePairsToObjectMapper<T>(items: [string, T][]): { [key: string]: T } {
-  // Equivalent to Object.fromEntries
   const obj: { [key: string]: T } = {};
   for (const keyValue of items) {
-    obj[keyValue[0]] = keyValue[1];
+    Object.defineProperty(obj, keyValue[0], {
+      enumerable: true,
+      configurable: true,
+      writable: true,
+      value: keyValue[1],
+    });
   }
   return obj;
 }
--- packages/fast-check/test/unit/arbitrary/_internals/mappers/KeyValuePairsToObject.spec.ts
+++ packages/fast-check/test/unit/arbitrary/_internals/mappers/KeyValuePairsToObject.spec.ts
@@ -2,18 +2,85 @@ import {
   keyValuePairsToObjectMapper,
   keyValuePairsToObjectUnmapper,
 } from '../../../../../src/arbitrary/_internals/mappers/KeyValuePairsToObject';
+import fc from '../../../../../src/fast-check';
 
 describe('keyValuePairsToObjectMapper', () => {
   it('should create instances with Object prototype', () => {
-    // Arrange
-    const keyValues: [string, unknown][] = [];
+    fc.assert(
+      fc.property(fc.uniqueArray(fc.tuple(fc.string(), fc.anything()), { selector: (kv) => kv[0] }), (keyValues) => {
+        // Arrange / Act
+        const obj = keyValuePairsToObjectMapper(keyValues);
 
-    // Act
-    const obj = keyValuePairsToObjectMapper(keyValues);
+        // Assert
+        expect(Object.getPrototypeOf(obj)).toBe(Object.prototype);
+        if (!keyValues.some(([k]) => k === 'constructor')) {
+          expect(obj.constructor).toBe(Object);
+        }
+        if (!keyValues.some(([k]) => k === '__proto__')) {
+          expect(obj.__proto__).toBe(Object.prototype);
+        }
+      })
+    );
+  });
+
+  it('should create instances with all requested keys', () => {
+    fc.assert(
+      fc.property(fc.uniqueArray(fc.tuple(fc.string(), fc.anything()), { selector: (kv) => kv[0] }), (keyValues) => {
+        // Arrange / Act
+        const obj = keyValuePairsToObjectMapper(keyValues);
+
+        // Assert
+        expect(Object.getPrototypeOf(obj)).toBe(Object.prototype);
+        for (const [key, value] of keyValues) {
+          expect(key in obj).toBe(true);
+          expect(obj[key]).toBe(value);
+        }
+      })
+    );
+  });
+
+  it('should create the same instances as-if we used bracket-based assignment', () => {
+    fc.assert(
+      fc.property(
+        fc.uniqueArray(
+          fc.tuple(
+            fc.string().filter((k) => k !== '__proto__'),
+            fc.anything()
+          ),
+          { selector: (kv) => kv[0] }
+        ),
+        (keyValues) => {
+          // Arrange / Act
+          const obj = keyValuePairsToObjectMapper(keyValues);
+          const refObj = Object.fromEntries(keyValues); // will miss __proto__ if passed as keys
+
+          // Assert
+          expect(obj).toEqual(refObj);
+          expect(Object.keys(obj).sort()).toEqual(Object.keys(refObj).sort());
+          expect(Object.getOwnPropertyNames(obj).sort()).toEqual(Object.getOwnPropertyNames(refObj).sort());
+          expect(Object.getOwnPropertyDescriptors(obj)).toEqual(Object.getOwnPropertyDescriptors(refObj));
+        }
+      )
+    );
+  });
+
+  it('should be able to build instances with dangerous keys', () => {
+    // Arrange / Act
+    const obj = keyValuePairsToObjectMapper([
+      ['__proto__', '1'],
+      ['constructor', '2'],
+      ['toString', '3'],
+    ]);
 
     // Assert
-    expect(obj.constructor).toBe(Object);
-    expect(obj.__proto__).toBe(Object.prototype);
+    expect(Object.getPrototypeOf(obj)).toBe(Object.prototype);
+    // Valid values
+    expect(obj.__proto__).toBe('1');
+    expect(obj.constructor).toBe('2');
+    expect(obj.toString).toBe('3');
+    // Enumerable properties
+    expect(Object.keys(obj).sort()).toEqual(['__proto__', 'constructor', 'toString'].sort());
+    expect(Object.values(obj).sort()).toEqual(['1', '2', '3'].sort());
   });
 });
 
