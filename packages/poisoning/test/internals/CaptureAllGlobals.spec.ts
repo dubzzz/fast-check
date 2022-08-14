e1e4ee39d53d79694f8f10dc9c4ac6915672e954
Nicolas DUBIEN <github@dubien.org>
2022-08-14 20:23:00 +0200

    Add missing checks in capture all

--- packages/poisoning/test/internals/CaptureAllGlobals.spec.ts
+++ packages/poisoning/test/internals/CaptureAllGlobals.spec.ts
@@ -2,12 +2,15 @@ import { captureAllGlobals } from '../../src/internals/CaptureAllGlobals.js';
 
 describe('captureAllGlobals', () => {
   it.each`
-    globalName               | globalValue
-    ${'Array'}               | ${Array}
-    ${'Array.prototype'}     | ${Array.prototype}
-    ${'Array.prototype.map'} | ${Array.prototype.map}
-    ${'Function'}            | ${Function}
-    ${'setTimeout'}          | ${setTimeout}
+    globalName                             | globalValue
+    ${'Array'}                             | ${Array}
+    ${'Array.prototype'}                   | ${Array.prototype}
+    ${'Array.prototype.map'}               | ${Array.prototype.map}
+    ${'Object'}                            | ${Object}
+    ${'Object.entries'}                    | ${Object.entries}
+    ${'Function'}                          | ${Function}
+    ${'setTimeout'}                        | ${setTimeout}
+    ${'Map.prototype[Symbol.toStringTag]'} | ${Map.prototype[Symbol.toStringTag]}
   `('should capture $globalName', ({ globalValue }) => {
     // Arrange / Act
     const globals = captureAllGlobals();
