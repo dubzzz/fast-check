e42fa15f054a17efc1bdf9a0ab914721e558ac48
Nicolas DUBIEN <github@dubien.org>
2022-08-27 01:15:15 +0200

    ⚡️ Drop unneeded copy for full custom `uniqueArray`

--- packages/fast-check/src/arbitrary/_internals/helpers/CustomEqualSet.ts
+++ packages/fast-check/src/arbitrary/_internals/helpers/CustomEqualSet.ts
@@ -1,4 +1,4 @@
-import { safePush, safeSlice } from '../../../utils/globals';
+import { safePush } from '../../../utils/globals';
 import { CustomSet } from '../interfaces/CustomSet';
 
 /**
@@ -28,6 +28,6 @@ export class CustomEqualSet<T> implements CustomSet<T> {
   }
 
   getData(): T[] {
-    return safeSlice(this.data);
+    return this.data;
   }
 }
