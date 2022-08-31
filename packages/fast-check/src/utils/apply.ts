95b9fb9edc9239b73436af92a96421b8880be799
Nicolas DUBIEN <github@dubien.org>
2022-08-31 06:17:48 +0200

    ⚡️ Faster implementation for `safeApply` (#3105)
    * ⚡️ Faster implementation for `safeApply`
    
    Benchmark measurements made at https://github.com/dubzzz/fast-check-benchmarks/blob/main/benchmark-poisoning.js proved that the code for `safeApply` can be speed-up by huge factors if rewritten in a try/catch maner.
    
    We measured:
    - before: from 15,083.692 to 15,573.392 ops/sec
    - after: from 59,929.287 to 61,058.436 ops/sec
    
    On this benchmark. Figures need to be confirmed on fast-check's code but figures looks promising.
    
    * versions

--- packages/fast-check/src/utils/apply.ts
+++ packages/fast-check/src/utils/apply.ts
@@ -1,9 +1,20 @@
-const safeObjectGetPrototypeOf = Object.getPrototypeOf;
-const safeObjectGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
-const untouchedPrototype = Function.prototype;
 const untouchedApply = Function.prototype.apply;
-
 const ApplySymbol = Symbol('apply');
+
+/**
+ * Extract apply or return undefined
+ * @param f - Function to extract apply from
+ * @internal
+ */
+function safeExtractApply<T, TArgs extends unknown[], TReturn>(
+  f: (this: T, ...args: TArgs) => TReturn
+): ((thisArg: T) => TReturn) | undefined {
+  try {
+    return f.apply;
+  } catch (err) {
+    return undefined;
+  }
+}
 
 /**
  * Equivalent to `f.apply(instance, args)` but temporary altering the instance
@@ -30,12 +41,10 @@ export function safeApply<T, TArgs extends unknown[], TReturn>(
   instance: T,
   args: TArgs
 ): TReturn {
-  const fPrototype = safeObjectGetPrototypeOf(f);
-  if (fPrototype === untouchedPrototype && safeObjectGetOwnPropertyDescriptor(f, 'apply') === undefined) {
-    const functionApplyDesc = safeObjectGetOwnPropertyDescriptor(untouchedPrototype, 'apply');
-    if (functionApplyDesc !== undefined && functionApplyDesc.value === untouchedApply) {
-      return f.apply(instance, args);
-    }
+  // Not as safe as checking the descriptor of the property but much faster
+  // Can be by-passed by an appropriate getter property on 'apply'
+  if (safeExtractApply(f) === untouchedApply) {
+    return f.apply(instance, args);
   }
   return safeApplyHacky(f, instance, args);
 }
