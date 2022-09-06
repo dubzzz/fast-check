ec930d8d6b0db5c4305656db5def224a1b419056
Nicolas DUBIEN <github@dubien.org>
2022-09-06 21:03:20 +0200

    Fix compile issues

--- packages/fast-check/src/arbitrary/_internals/helpers/EnumerableKeysExtractor.ts
+++ packages/fast-check/src/arbitrary/_internals/helpers/EnumerableKeysExtractor.ts
@@ -6,7 +6,7 @@ const safeObjectGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 export type EnumerableKeyOf<T> = Extract<keyof T, string | symbol>;
 
 /** @internal */
-export function extractEnumerableKeys<T>(instance: T): EnumerableKeyOf<T>[] {
+export function extractEnumerableKeys<T extends object>(instance: T): EnumerableKeyOf<T>[] {
   const keys = safeObjectKeys(instance) as EnumerableKeyOf<T>[]; // Only enumerable own properties
   const symbols = safeObjectGetOwnPropertySymbols(instance) as EnumerableKeyOf<T>[];
   for (let index = 0; index !== symbols.length; ++index) {
