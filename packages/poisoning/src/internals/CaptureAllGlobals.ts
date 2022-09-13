ae68b452d74fe2b1864a2eaf113f6253b0528fe8
Nicolas DUBIEN <github@dubien.org>
2022-09-13 12:38:05 +0200

    🐛(poisoning) Compute smallest depth for each global (#3188)
    * 🐛(poisoning) Compute smallest depth for each global
    
    * Add some dedicated tests
    
    * Add some more tests
    
    * versions
    
    * Switch to poison safe versions

--- packages/poisoning/src/internals/CaptureAllGlobals.ts
+++ packages/poisoning/src/internals/CaptureAllGlobals.ts
@@ -1,4 +1,4 @@
-import { toPoisoningFreeArray, MapSymbol, SortSymbol } from './PoisoningFreeArray.js';
+import { toPoisoningFreeArray, MapSymbol, SortSymbol, ShiftSymbol, PushSymbol } from './PoisoningFreeArray.js';
 import { HasSymbol, SetSymbol, toPoisoningFreeMap } from './PoisoningFreeMap.js';
 import { AllGlobals, GlobalDetails } from './types/AllGlobals.js';
 
@@ -25,42 +25,53 @@ function extractAllDescriptorsDetails(instance: unknown): [string | symbol, Prop
   return allDescriptorsDetails[SortSymbol](compareKeys);
 }
 
-function captureOneRecursively(knownGlobals: AllGlobals, instance: unknown, name: string, currentDepth: number): void {
-  if (typeof instance !== 'function' && typeof instance !== 'object') {
-    return;
-  }
-  if (instance === null || instance === undefined || knownGlobals[HasSymbol](instance)) {
-    return;
-  }
-  const allDescriptorsDetails = extractAllDescriptorsDetails(instance);
-  const localGlobal: GlobalDetails = {
-    name,
-    depth: currentDepth,
-    properties: toPoisoningFreeMap(new Map<string | symbol, PropertyDescriptor>()),
-  };
-  knownGlobals[SetSymbol](instance, localGlobal);
-  for (let index = 0; index !== allDescriptorsDetails.length; ++index) {
-    const descriptorName = allDescriptorsDetails[index][0];
-    const descriptor = allDescriptorsDetails[index][1];
-    localGlobal.properties[SetSymbol](descriptorName, descriptor);
-    if (typeof descriptorName === 'symbol') {
-      // Do not scan the internal data within keys attached symbol
-      // For instance: do not monitor the content of globalThis.Symbol(JEST_STATE_SYMBOL)
-      continue;
-    }
-    if (currentDepth === 0 && descriptorName[0] === '_') {
-      // Do not scan what's sounds like private properties dropped on globalThis
-      // For instance: do not track the content of globalThis.__coverage__
-      continue;
-    }
-    const subGlobalName = currentDepth !== 0 ? name + '.' + String(descriptorName) : String(descriptorName);
-    captureOneRecursively(knownGlobals, descriptor.value, subGlobalName, currentDepth + 1);
-  }
-}
+type NextCapture = {
+  instance: unknown;
+  name: string;
+  currentDepth: number;
+};
 
 /** Capture all globals accessible from globalThis */
 export function captureAllGlobals(): AllGlobals {
   const knownGlobals = toPoisoningFreeMap(new Map<unknown, GlobalDetails>());
-  captureOneRecursively(knownGlobals, globalThis, 'globalThis', 0);
+  const nextCaptures = toPoisoningFreeArray<NextCapture>([
+    { instance: globalThis, name: 'globalThis', currentDepth: 0 },
+  ]);
+  while (nextCaptures.length !== 0) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const { instance, name, currentDepth } = nextCaptures[ShiftSymbol]()!;
+
+    if (typeof instance !== 'function' && typeof instance !== 'object') {
+      continue;
+    }
+    if (instance === null || instance === undefined || knownGlobals[HasSymbol](instance)) {
+      continue;
+    }
+
+    const allDescriptorsDetails = extractAllDescriptorsDetails(instance);
+    const localGlobal: GlobalDetails = {
+      name,
+      depth: currentDepth,
+      properties: toPoisoningFreeMap(new Map<string | symbol, PropertyDescriptor>()),
+    };
+    knownGlobals[SetSymbol](instance, localGlobal);
+    for (let index = 0; index !== allDescriptorsDetails.length; ++index) {
+      const descriptorName = allDescriptorsDetails[index][0];
+      const descriptor = allDescriptorsDetails[index][1];
+      localGlobal.properties[SetSymbol](descriptorName, descriptor);
+      if (typeof descriptorName === 'symbol') {
+        // Do not scan the internal data within keys attached symbol
+        // For instance: do not monitor the content of globalThis.Symbol(JEST_STATE_SYMBOL)
+        continue;
+      }
+      if (currentDepth === 0 && descriptorName[0] === '_') {
+        // Do not scan what's sounds like private properties dropped on globalThis
+        // For instance: do not track the content of globalThis.__coverage__
+        continue;
+      }
+      const subGlobalName = currentDepth !== 0 ? name + '.' + String(descriptorName) : String(descriptorName);
+      nextCaptures[PushSymbol]({ instance: descriptor.value, name: subGlobalName, currentDepth: currentDepth + 1 });
+    }
+  }
   return knownGlobals;
 }
--- packages/poisoning/test/internals/CaptureAllGlobals.spec.ts
+++ packages/poisoning/test/internals/CaptureAllGlobals.spec.ts
@@ -1,20 +1,71 @@
 import { captureAllGlobals } from '../../src/internals/CaptureAllGlobals.js';
+import { GlobalDetails } from '../../src/internals/types/AllGlobals.js';
 
 describe('captureAllGlobals', () => {
   const expectedGlobals = [
-    { globalName: 'Array', globalValue: Array },
-    { globalName: 'Array.prototype', globalValue: Array.prototype },
-    { globalName: 'Array.prototype.map', globalValue: Array.prototype.map },
-    { globalName: 'Object', globalValue: Object },
-    { globalName: 'Object.entries', globalValue: Object.entries },
-    { globalName: 'Function', globalValue: Function },
-    { globalName: 'Function.prototype.apply', globalValue: Function.prototype.apply },
-    { globalName: 'Function.prototype.call', globalValue: Function.prototype.call },
-    { globalName: 'setTimeout', globalValue: setTimeout },
-    { globalName: 'Map.prototype[Symbol.toStringTag]', globalValue: Map.prototype[Symbol.toStringTag], isSymbol: true },
-    { globalName: 'Object.prototype.toString', globalValue: Object.prototype.toString },
-    { globalName: 'Number.prototype.toString', globalValue: Number.prototype.toString }, // not the same as Object one
+    {
+      globalName: 'Array',
+      globalValue: Array,
+      expectedDepth: 1,
+    },
+    {
+      globalName: 'Array.prototype',
+      globalValue: Array.prototype,
+      expectedDepth: 2,
+    },
+    {
+      globalName: 'Array.prototype.map',
+      globalValue: Array.prototype.map,
+      expectedDepth: 3,
+    },
+    {
+      globalName: 'Object',
+      globalValue: Object,
+      expectedDepth: 1,
+    },
+    {
+      globalName: 'Object.entries',
+      globalValue: Object.entries,
+      expectedDepth: 2,
+    },
+    {
+      globalName: 'Function',
+      globalValue: Function,
+      expectedDepth: 1,
+    },
+    {
+      globalName: 'Function.prototype.apply',
+      globalValue: Function.prototype.apply,
+      expectedDepth: 3,
+    },
+    {
+      globalName: 'Function.prototype.call',
+      globalValue: Function.prototype.call,
+      expectedDepth: 3,
+    },
+    {
+      globalName: 'setTimeout',
+      globalValue: setTimeout,
+      expectedDepth: 1,
+    },
+    {
+      globalName: 'Map.prototype[Symbol.toStringTag]',
+      globalValue: Map.prototype[Symbol.toStringTag],
+      expectedDepth: 3,
+      isSymbol: true,
+    },
+    {
+      globalName: 'Object.prototype.toString',
+      globalValue: Object.prototype.toString,
+      expectedDepth: 3,
+    },
+    {
+      globalName: 'Number.prototype.toString', // not the same as Object one
+      globalValue: Number.prototype.toString,
+      expectedDepth: 3,
+    },
   ];
+  // For the moment, internal data for globals linked to symbols is not tracked
   const expectedGlobalsExcludingSymbols = expectedGlobals.filter((item) => !item.isSymbol);
 
   it.each(expectedGlobals)('should capture value for $globalName', ({ globalValue }) => {
@@ -28,7 +79,6 @@ describe('captureAllGlobals', () => {
     expect(flattenGlobalsValues).toContainEqual(globalValue);
   });
 
-  // For the moment, internal data for globals linked to symbols is not tracked
   it.each(expectedGlobalsExcludingSymbols)('should track the content of $globalName', ({ globalName, globalValue }) => {
     // Arrange / Act
     const globals = captureAllGlobals();
@@ -47,6 +97,90 @@ describe('captureAllGlobals', () => {
         throw new Error(errorMessage, { cause: err });
       }
       throw err;
+    }
+  });
+
+  it.each(expectedGlobalsExcludingSymbols)(
+    'should attach the right depth to $globalName',
+    ({ globalValue, expectedDepth }) => {
+      // Arrange / Act
+      const globals = captureAllGlobals();
+
+      // Assert
+      expect(globals.get(globalValue)?.depth).toBe(expectedDepth);
+    }
+  );
+
+  it('should attach the minimal depth from globalThis to each global', () => {
+    // Arrange
+    const dataB = { c: { d: 5 } };
+    const dataA = { a: { b: dataB } };
+    const dataC = { e: dataB };
+    const dataD = { f: dataA, g: dataC, h: { i: { j: { k: { l: 1 } } } } };
+    (globalThis as any).dataA = dataA;
+    (globalThis as any).dataB = dataB;
+    (globalThis as any).dataC = dataC;
+    (globalThis as any).dataD = dataD;
+    const expectedExtractedGlobalThis: GlobalDetails = {
+      name: 'globalThis',
+      depth: 0,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedDataA: GlobalDetails = {
+      name: 'dataA',
+      depth: 1,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedDataB: GlobalDetails = {
+      name: 'dataB',
+      depth: 1,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedDataC: GlobalDetails = {
+      name: 'dataC',
+      depth: 1,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedDataD: GlobalDetails = {
+      name: 'dataD',
+      depth: 1,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedC: GlobalDetails = {
+      name: 'dataB.c', // shortest path to c
+      depth: 2,
+      properties: expect.any(Map),
+    };
+    const expectedExtractedK: GlobalDetails = {
+      name: 'dataD.h.i.j.k', // shortest and only path to k
+      depth: 5,
+      properties: expect.any(Map),
+    };
+
+    try {
+      // Act
+      const globals = captureAllGlobals();
+
+      // Assert
+      const extractedGlobalThis = globals.get(globalThis);
+      expect(extractedGlobalThis).toEqual(expect.objectContaining(expectedExtractedGlobalThis));
+      const extractedDataA = globals.get(dataA);
+      expect(extractedDataA).toEqual(expect.objectContaining(expectedExtractedDataA));
+      const extractedDataB = globals.get(dataB);
+      expect(extractedDataB).toEqual(expect.objectContaining(expectedExtractedDataB));
+      const extractedDataC = globals.get(dataC);
+      expect(extractedDataC).toEqual(expect.objectContaining(expectedExtractedDataC));
+      const extractedDataD = globals.get(dataD);
+      expect(extractedDataD).toEqual(expect.objectContaining(expectedExtractedDataD));
+      const extractedC = globals.get(dataB.c);
+      expect(extractedC).toEqual(expect.objectContaining(expectedExtractedC));
+      const extractedK = globals.get(dataD.h.i.j.k);
+      expect(extractedK).toEqual(expect.objectContaining(expectedExtractedK));
+    } finally {
+      delete (globalThis as any).dataA;
+      delete (globalThis as any).dataB;
+      delete (globalThis as any).dataC;
+      delete (globalThis as any).dataD;
     }
   });
 });
