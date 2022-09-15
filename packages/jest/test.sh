9690032ecac456dcdff91b756a5ba22bfdaf022d
Nicolas DUBIEN <github@dubien.org>
2022-09-15 21:36:51 +0200

    ✨(jest) Add `it.failing` and `it.concurrent` and combinations (#3203)
    Jest comes with many ways to combine all the `it`, `test` with `skip`, `failing` and others altogether. While so far we decided not to support `each`, we added support for all the other ones.
    
    So you'll now be able to run any of those:
    ```ts
    itProp(...)
    itProp.only(...)
    itProp.only.failing(...)
    itProp.failing(...)
    itProp.skip(...)
    itProp.skip.failing(...)
    itProp.todo(...)
    itProp.concurrent(...)
    itProp.concurrent.only(...)
    itProp.concurrent.only.failing(...)
    itProp.concurrent.failing(...)
    itProp.concurrent.skip(...)
    itProp.concurrent.skip.failing(...)
    testProp(...)
    testProp.only(...)
    testProp.only.failing(...)
    testProp.failing(...)
    testProp.skip(...)
    testProp.skip.failing(...)
    testProp.todo(...)
    testProp.concurrent(...)
    testProp.concurrent.only(...)
    testProp.concurrent.only.failing(...)
    testProp.concurrent.failing(...)
    testProp.concurrent.skip(...)
    testProp.concurrent.skip.failing(...)
    ```
    
    Fixes #3202

--- packages/jest/test.sh
+++ packages/jest/test.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-npm run jest-test >test/out.log 2>&1
+yarn jest-test >test/out.log 2>&1
 
 for expectedContent in \
         "● should fail on falsy synchronous property" \
         "● should fail on falsy asynchronous property" \
         "● should fail with seed=4242 and path=\"25\" (with seed=4242)" \
+        "● should fail because passing (with seed=48)" \
         "● itProp › should fail on falsy synchronous property" \
         "● itProp › should fail on falsy asynchronous property" \
-        "● itProp › should fail with seed=4242 and path=\"25\" (with seed=4242)"
+        "● itProp › should fail with seed=4242 and path=\"25\" (with seed=4242)" \
+        "● itProp › should fail because passing (with seed=48)" \
+        "● itProp › should fail for concurrent (with seed=4848)" \
+        "● itProp › should fail for concurrent because passing (with seed=4848)"
 do
     cat test/out.log | grep "${expectedContent}" >/dev/null 2>&1
     if [ $? -ne 0 ]; then
