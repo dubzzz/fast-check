52573a6dd0f1c59f9ebd8b01c13a45056970648b
Nicolas DUBIEN <github@dubien.org>
2022-09-15 08:52:41 +0200

    ✨(poisoning) Add ability to ignore some roots (#3198)

--- packages/poisoning/src/main.ts
+++ packages/poisoning/src/main.ts
@@ -1,15 +1,41 @@
 import { captureAllGlobals } from './internals/CaptureAllGlobals.js';
+import { filterNonEligibleDiffs } from './internals/FilterNonEligibleDiffs.js';
 import { trackDiffsOnGlobals } from './internals/TrackDiffsOnGlobal.js';
 
 const initialGlobals = captureAllGlobals();
+
+/**
+ * Some extra options for {@link restoreGlobals} and {@link assertNoPoisoning}
+ */
+export type ExtraOptions = {
+  /**
+   * Discard any changes occuring on any root matching the regex or any of its children.
+   * Elements being child of several roots must have all their roots ignored not be tracked.
+   *
+   * eg.: If pattern is /^_/ then any change of globalThis._ignored or children of it will be omitted.
+   * Except changes on prop if globalThis._ignored.prop === globalThis.nonIgnored.prop as prop is part of a tracked root too.
+   *
+   * Remark: a root is a property directly accessible from globalThis
+   */
+  ignoredRootRegex?: RegExp;
+};
+
+/** Internal helper to share the extraction logic */
+function trackDiffsOnGlobalsBasedOnOptions(options: ExtraOptions | undefined) {
+  const allDiffs = trackDiffsOnGlobals(initialGlobals);
+  if (options === undefined || options.ignoredRootRegex === undefined) {
+    return allDiffs;
+  }
+  return filterNonEligibleDiffs(allDiffs, options.ignoredRootRegex);
+}
 
 /**
  * Restore all globals as they were when first importing this package.
  *
  * Remark: At least, it attempts to do so
  */
-export function restoreGlobals(): void {
-  const diffs = trackDiffsOnGlobals(initialGlobals);
+export function restoreGlobals(options?: ExtraOptions): void {
+  const diffs = trackDiffsOnGlobalsBasedOnOptions(options);
   for (let index = 0; index !== diffs.length; ++index) {
     diffs[index].patch();
   }
@@ -27,12 +53,12 @@ export function restoreGlobals(): void {
  * - someone added a new global on `window` (browser case) or `global` (node case) or modern `globalThis` (everywhere)
  * - someone changed `Array.prototype.map` into another function
  */
-export function assertNoPoisoning(): void {
-  const diffs = trackDiffsOnGlobals(initialGlobals);
+export function assertNoPoisoning(options?: ExtraOptions): void {
+  const diffs = trackDiffsOnGlobalsBasedOnOptions(options);
   if (diffs.length !== 0) {
-    let impactedElements = diffs[0].keyName;
+    let impactedElements = diffs[0].fullyQualifiedKeyName;
     for (let index = 1; index !== diffs.length; ++index) {
-      impactedElements += ', ' + diffs[index].keyName;
+      impactedElements += ', ' + diffs[index].fullyQualifiedKeyName;
     }
     throw new Error('Poisoning detected on ' + impactedElements);
   }
