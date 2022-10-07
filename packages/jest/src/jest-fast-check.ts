d075a058a0b6860acf083dbeeaf574939138ca4f
Nicolas DUBIEN <github@dubien.org>
2022-10-07 19:45:36 +0200

    🐛(jest) Fallback on the seed coming from globals if any (#3281)

--- packages/jest/src/jest-fast-check.ts
+++ packages/jest/src/jest-fast-check.ts
@@ -24,7 +24,14 @@ function internalTestPropExecute<Ts extends [any] | any[]>(
   params?: fc.Parameters<Ts>
 ): void {
   const customParams: fc.Parameters<Ts> = { ...params };
-  if (customParams.seed === undefined) customParams.seed = Date.now();
+  if (customParams.seed === undefined) {
+    const seedFromGlobals = fc.readConfigureGlobal().seed;
+    if (seedFromGlobals !== undefined) {
+      customParams.seed = seedFromGlobals;
+    } else {
+      customParams.seed = Date.now();
+    }
+  }
 
   const promiseProp = wrapProp(prop);
   testFn(`${label} (with seed=${customParams.seed})`, async () => {
