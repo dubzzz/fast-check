7e6ddbdfaec6c5c1a584b67556d420fc7dc11130
Nicolas DUBIEN <github@dubien.org>
2022-10-16 22:20:15 +0200

    ✅(ava) Internalize bundle checks for ava package (#3319)
    * ✅(ava) Internalize bundle checks for ava package
    
    * versions

--- packages/test-ava-bundle-cjs/index.test.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
-const { testProp, fc } = require('@fast-check/ava');
-
-// for all a, b, c strings
-// b is a substring of a + b + c
-testProp('should detect the substring', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
-  t.true((a + b + c).includes(b));
-});
