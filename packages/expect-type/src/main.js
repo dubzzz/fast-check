a691aa5e931b82edb9940a8045eeac6def67e572
Nicolas DUBIEN <github@dubien.org>
2022-10-22 14:30:31 +0200

    Extract expect-type

---
+++ packages/expect-type/src/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.expectType = function expectType() {
+  return function () {};
+};
+exports.expectTypeAssignable = function expectTypeAssignable() {
+  return function () {};
+};
