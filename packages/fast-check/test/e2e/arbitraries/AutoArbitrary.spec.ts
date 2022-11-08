4f6540490a82e5cfced4a8df8951166a78383c35
Nicolas DUBIEN <github@dubien.org>
2022-11-08 21:33:59 +0100

    switch to builder based

--- packages/fast-check/test/e2e/arbitraries/AutoArbitrary.spec.ts
+++ packages/fast-check/test/e2e/arbitraries/AutoArbitrary.spec.ts
@@ -4,10 +4,9 @@ import { seed } from '../seed';
 describe(`AutoArbitrary (seed: ${seed})`, () => {
   describe('auto', () => {
     it('should be able to shrink a single arbitrary', () => {
-      const integerArb = fc.integer();
       const out = fc.check(
         fc.property(fc.auto(), (auto) => {
-          const v1 = auto.builder(integerArb);
+          const v1 = auto.builder(fc.integer);
           expect(v1).toBeLessThanOrEqual(10);
         }),
         { seed: seed }
@@ -17,11 +16,10 @@ describe(`AutoArbitrary (seed: ${seed})`, () => {
     });
 
     it('should be able to shrink two unrelated arbitraries', () => {
-      const natArb = fc.nat();
       const out = fc.check(
         fc.property(fc.auto(), (auto) => {
-          const v1 = auto.builder(natArb);
-          const v2 = auto.builder(natArb); // unrelated because does not depend on v1
+          const v1 = auto.builder(fc.integer);
+          const v2 = auto.builder(fc.integer); // unrelated because does not depend on v1
           expect(v1).toBeLessThanOrEqual(v2);
         }),
         { seed: seed }
