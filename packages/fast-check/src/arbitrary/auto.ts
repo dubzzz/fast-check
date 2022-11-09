b295698d3b4ce260a6c7f6eb1c6915bf88de232f
Nicolas DUBIEN <github@dubien.org>
2022-11-09 22:53:58 +0100

    Share buildAutoValue code

--- packages/fast-check/src/arbitrary/auto.ts
+++ packages/fast-check/src/arbitrary/auto.ts
@@ -17,24 +17,39 @@ export type AutoValueFunction = <T>(arb: Arbitrary<T>) => T;
 export type AutoValueMethods = { values: () => unknown[] };
 export type AutoValue = AutoValueFunction & AutoValueMethods;
 
+type PreBuiltValue = { arb: Arbitrary<unknown>; value: unknown; context: unknown };
+function buildAutoValue(
+  mrng: Random,
+  biasFactor: number | undefined,
+  preBuiltValues: PreBuiltValue[]
+): Value<AutoValue> {
+  const clonedMrng = mrng.clone();
+  const context: AutoContext = { mrng: clonedMrng, biasFactor, history: [] };
+  const value: AutoValue & WithToStringMethod = Object.assign(
+    <T>(arb: Arbitrary<T>): T => {
+      const preBuiltValue = preBuiltValues[context.history.length];
+      if (preBuiltValue !== undefined && preBuiltValue.arb === arb) {
+        const value = preBuiltValue.value;
+        context.history.push({ arb, value, context: preBuiltValue.context });
+        return value as T;
+      }
+      const g = arb.generate(clonedMrng, biasFactor);
+      context.history.push({ arb, value: g.value_, context: g.context });
+      return g.value;
+    },
+    {
+      values: () => context.history.map((c) => c.value),
+      [toStringMethod]: () => {
+        return stringify(context.history.map((c) => c.value));
+      },
+    }
+  );
+  return new Value(value, context);
+}
+
 class AutoArbitrary extends Arbitrary<AutoValue> {
   generate(mrng: Random, biasFactor: number | undefined): Value<AutoValue> {
-    const clonedMrng = mrng.clone();
-    const context: AutoContext = { mrng: clonedMrng, biasFactor, history: [] };
-    const value: AutoValue & WithToStringMethod = Object.assign(
-      <T>(arb: Arbitrary<T>): T => {
-        const g = arb.generate(clonedMrng, biasFactor);
-        context.history.push({ arb, value: g.value_, context: g.context });
-        return g.value;
-      },
-      {
-        values: () => context.history.map((c) => c.value),
-        [toStringMethod]: () => {
-          return stringify(context.history.map((c) => c.value));
-        },
-      }
-    );
-    return new Value(value, context);
+    return buildAutoValue(mrng, biasFactor, []);
   }
   canShrinkWithoutContext(value: unknown): value is AutoValue {
     return false;
@@ -46,36 +61,28 @@ class AutoArbitrary extends Arbitrary<AutoValue> {
     const safeContext = context as AutoContext;
     const mrng = safeContext.mrng;
     const biasFactor = safeContext.biasFactor;
-    const tupleArb = new TupleArbitrary(safeContext.history.map((c) => c.arb));
+    const history = safeContext.history;
+    const tupleArb = new TupleArbitrary(history.map((c) => c.arb));
     return tupleArb
       .shrink(
-        safeContext.history.map((c) => c.value),
-        safeContext.history.map((c) => c.context) // HACKY!!!
+        history.map((c) => c.value),
+        history.map((c) => c.context) // HACKY!!!
       )
       .map((shrink): Value<AutoValue> => {
-        const clonedMrng = mrng.clone();
-        const newContext: AutoContext = { mrng: clonedMrng, biasFactor, history: [] };
-        const newValue: AutoValue & WithToStringMethod = Object.assign(
-          <T>(arb: Arbitrary<T>): T => {
-            const fromOldContext = safeContext.history[newContext.history.length];
-            if (fromOldContext !== undefined && fromOldContext.arb === arb) {
-              const value = shrink.value_[newContext.history.length];
-              const context = (shrink.context as unknown[])[newContext.history.length]; // HACKY!!!
-              newContext.history.push({ arb, value, context });
-              return value as T;
-            }
-            const g = arb.generate(clonedMrng, biasFactor);
-            newContext.history.push({ arb, value: g.value_, context: g.context });
-            return g.value;
-          },
-          {
-            values: () => newContext.history.map((c) => c.value),
-            [toStringMethod]: () => {
-              return stringify(newContext.history.map((c) => c.value));
-            },
-          }
-        );
-        return new Value(newValue, newContext);
+        if (
+          shrink.value_.length !== history.length ||
+          !Array.isArray(shrink.context) ||
+          shrink.context.length !== history.length
+        ) {
+          return buildAutoValue(mrng, biasFactor, []);
+        }
+        const shrinkContext = shrink.context;
+        const preBuiltValues: PreBuiltValue[] = safeContext.history.map((entry, index) => ({
+          arb: entry.arb,
+          value: shrink.value_[index],
+          context: shrinkContext[index], // HACKY!!!
+        }));
+        return buildAutoValue(mrng, biasFactor, preBuiltValues);
       });
   }
 }
