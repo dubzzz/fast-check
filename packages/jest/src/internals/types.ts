3c18bdae710724b0946e0b98417d8a6c6474879e
Nicolas DUBIEN <github@dubien.org>
2022-12-06 22:06:24 +0000

    ♻️(jest) Take `jest` and `fc` as input for main internals

--- packages/jest/src/internals/types.ts
+++ packages/jest/src/internals/types.ts
@@ -1,15 +1,28 @@
 import type { it as itJest } from '@jest/globals';
-import type * as fc from 'fast-check';
+import type { Arbitrary, asyncProperty, assert } from 'fast-check';
+
+export type FcExtra = {
+  asyncProperty: typeof asyncProperty;
+  assert: typeof assert;
+};
+
+export type JestExtra = {
+  /**
+   * This option is only available since v29.2.0 of Jest
+   * See official release note: https://github.com/facebook/jest/releases/tag/v29.2.0
+   */
+  getSeed?: () => number;
+};
 
 export type It = typeof itJest;
 
 // Pre-requisite: https://github.com/Microsoft/TypeScript/pull/26063
 // Require TypeScript 3.1
 export type ArbitraryTuple<Ts extends [any] | any[]> = {
-  [P in keyof Ts]: fc.Arbitrary<Ts[P]>;
+  [P in keyof Ts]: Arbitrary<Ts[P]>;
 };
 export type ArbitraryRecord<Ts> = {
-  [P in keyof Ts]: fc.Arbitrary<Ts[P]>;
+  [P in keyof Ts]: Arbitrary<Ts[P]>;
 };
 
 export type Prop<Ts extends [any] | any[]> = (...args: Ts) => boolean | void | PromiseLike<boolean | void>;
