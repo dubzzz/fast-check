745d612e02ada18f20e14d554c91585bc00bd9d5
Nicolas DUBIEN <github@dubien.org>
2022-12-10 15:26:58 +0100

    ✨(worker) Terminate workers if property timeout-ed
    Up-to-now ill workers stayed alive until the property has been executed on all the possible values. In other words, if one property was leading to infinite loops for some values, it would have created many workers consumming lots of memory and would have killed them only at the end of the whole shrinking process. It will now kill them as soon as possible to avoid keeping unneeded code running in background.

---
+++ packages/worker/test/internals/worker-pool/Lock.spec.ts
@@ -0,0 +1,53 @@
+import { AcquiredLock, Lock } from '../../../src/internals/worker-pool/Lock.js';
+
+describe('Lock', () => {
+  it('should be able to take the first lock', async () => {
+    // Arrange
+    const lock = new Lock();
+
+    // Act
+    const { release } = await lock.acquire();
+
+    // Assert
+    await release();
+  });
+
+  it('should wait until previous lock gets released to access to it', async () => {
+    // Arrange
+    const lock = new Lock();
+    const seenValues: number[] = [];
+    let lastAcquiredLock: AcquiredLock | undefined = undefined;
+
+    // Act
+    lock.acquire().then((acquired) => {
+      seenValues.push(1);
+      lastAcquiredLock = acquired;
+    });
+    lock.acquire().then((acquired) => {
+      seenValues.push(1);
+      lastAcquiredLock = acquired;
+    });
+    lock.acquire().then((acquired) => {
+      seenValues.push(1);
+      lastAcquiredLock = acquired;
+    });
+
+    // Assert
+    expect(seenValues).toEqual([]);
+    await delay0();
+    expect(seenValues).toEqual([1]);
+    lastAcquiredLock!.release();
+    await delay0();
+    expect(seenValues).toEqual([1, 2]);
+    lastAcquiredLock!.release();
+    await delay0();
+    expect(seenValues).toEqual([1, 2, 3]);
+    lastAcquiredLock!.release();
+  });
+});
+
+// Helpers
+
+async function delay0() {
+  await new Promise((r) => setTimeout(r, 0));
+}
