bd086027617dad47aaa090e4e1f5375ef5a22634
Nicolas DUBIEN <github@dubien.org>
2022-12-25 17:37:53 +0000

    do not run test for local on testProp

--- packages/jest/test/jest-fast-check.spec.ts
+++ packages/jest/test/jest-fast-check.spec.ts
@@ -495,27 +495,29 @@ describe.each<{ specName: string; runnerName: RunnerType; useLegacySignatures: b
       });
     });
 
-    it.concurrent('should fail but favor local Jest timeout over Jest setTimeout', async () => {
-      // Arrange
-      const { specFileName, jestConfigRelativePath } = await writeToFile(runnerName, options, () => {
-        jest.setTimeout(2000);
-        runner.prop([fc.nat()])(
-          'property favor local Jest timeout over Jest setTimeout',
-          async () => {
-            await new Promise(() => {}); // never resolving
-          },
-          1000
-        );
+    if (!useLegacySignatures) {
+      it.concurrent('should fail but favor local Jest timeout over Jest setTimeout', async () => {
+        // Arrange
+        const { specFileName, jestConfigRelativePath } = await writeToFile(runnerName, options, () => {
+          jest.setTimeout(2000);
+          runner.prop([fc.nat()])(
+            'property favor local Jest timeout over Jest setTimeout',
+            async () => {
+              await new Promise(() => {}); // never resolving
+            },
+            1000
+          );
+        });
+
+        // Act
+        const out = await runSpec(jestConfigRelativePath);
+
+        // Assert
+        expectFail(out, specFileName);
+        expectTimeout(out, 1000); // neither 2000 (setTimeout), nor 5000 (default)
+        expect(out).toMatch(/[×✕] property favor local Jest timeout over Jest setTimeout/);
       });
-
-      // Act
-      const out = await runSpec(jestConfigRelativePath, { testTimeoutCLI: 2000 });
-
-      // Assert
-      expectFail(out, specFileName);
-      expectTimeout(out, 1000); // neither 2000 (setTimeout), nor 5000 (default)
-      expect(out).toMatch(/[×✕] property favor local Jest timeout over Jest setTimeout/);
-    });
+    }
 
     it.concurrent('should fail but favor Jest setTimeout over Jest CLI timeout', async () => {
       // Arrange
