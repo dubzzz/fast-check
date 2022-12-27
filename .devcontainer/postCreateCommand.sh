b908d77ecee5b1ca3a428a5b2fe3185c0780698c
Nicolas DUBIEN <github@dubien.org>
2022-12-27 15:46:58 +0100

    🔨 Install and setup nvs inside Dockerfile (#3524)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,4 @@
 set -x
 
 # Install locked dependencies
-#corepack enable
 yarn install --immutable
-
-# Pre-download node version in nvs
-CURRENT_NODE_VERSION=`node --version`
-nvs add "$CURRENT_NODE_VERSION"
