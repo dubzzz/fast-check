a61e51aee6ab95dfb96e6ecbfc15e69062db69d1
Nicolas DUBIEN <github@dubien.org>
2023-01-19 08:12:27 +0000

    simplified toss

--- packages/fast-check/src/check/runner/Tosser.ts
+++ packages/fast-check/src/check/runner/Tosser.ts
@@ -6,45 +6,28 @@ import { Value } from '../arbitrary/definition/Value';
 import { safeMap } from '../../utils/globals';
 import { QualifiedRandomGenerator } from './configuration/QualifiedParameters';
 
-/** @internal */
+/**
+ * Extracting tossNext out of toss was dropping some bailout reasons on v8 side
+ * @internal
+ */
 function tossNext<Ts>(generator: IRawProperty<Ts>, rng: QualifiedRandomGenerator, index: number): Value<Ts> {
   rng.unsafeJump();
   return generator.generate(new Random(rng), index);
 }
 
 /** @internal */
-function* tossNoExamples<Ts>(
+export function *toss<Ts>(
   generator: IRawProperty<Ts>,
   seed: number,
-  random: (seed: number) => QualifiedRandomGenerator
+  random: (seed: number) => QualifiedRandomGenerator,
+  examples: Ts[]
 ): IterableIterator<Value<Ts>> {
+  for (let idx = 0 ; idx !== examples.length ; ++idx) {
+    yield new Value(examples[idx], undefined)
+  }
   for (let idx = 0, rng = random(seed); ; ++idx) {
     yield tossNext(generator, rng, idx);
   }
-}
-
-/** @internal */
-function* tossExamples<Ts>(
-  generator: IRawProperty<Ts>,
-  seed: number,
-  random: (seed: number) => QualifiedRandomGenerator,
-  examples: Ts[]
-): IterableIterator<Value<Ts>> {
-  yield* safeMap(examples, (e) => new Value(e, undefined));
-  yield* tossNoExamples(generator, seed, random);
-}
-
-/** @internal */
-export function toss<Ts>(
-  generator: IRawProperty<Ts>,
-  seed: number,
-  random: (seed: number) => QualifiedRandomGenerator,
-  examples: Ts[]
-): IterableIterator<Value<Ts>> {
-  // The split into multiple sub-toss has been done to drop some bailout linked to V8 when running this code
-  // it used to be the source of many performance issues
-  if (examples.length === 0) return tossNoExamples(generator, seed, random);
-  else return tossExamples(generator, seed, random, examples);
 }
 
 /** @internal */
