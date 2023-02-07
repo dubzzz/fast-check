dubzzz/fast-check

commit 7b8948786ae7f15a65a485852ee7e5bd25346f5a

⚡️ Tosser must immediately produce values (#3554)
* ⚡️ Tosser must immediately produce values

...instead of producing generators that will be call to produce the values.

* Update packages/fast-check/src/check/runner/Sampler.ts

* versions

* fix code and compile

* Update packages/fast-check/src/check/runner/Runner.ts

* Update packages/fast-check/src/check/runner/Runner.ts

* Update SourceValuesIterator.spec.ts

* Update packages/fast-check/test/unit/check/runner/SourceValuesIterator.spec.ts

* Revert "Update packages/fast-check/test/unit/check/runner/SourceValuesIterator.spec.ts"

This reverts commit a983cc1e34e4f42e52acd136dafd3475e771afce.

* Revert "Update SourceValuesIterator.spec.ts"

This reverts commit b3039e817d715f410663ea6c96b7d201ae141e18.

* fix tests

* rework sampler

* move back to toss

* directly pass path

packages/fast-check/src/check/runner/Tosser.ts
@@ -6,12 +6,23 @@ import { Value } from '../arbitrary/definition/Value';
 import { safeMap } from '../../utils/globals';
 
 /** @internal */
-function lazyGenerate<Ts>(generator: IRawProperty<Ts>, rng: RandomGenerator, idx: number): () => Value<Ts> {
-  return () => generator.generate(new Random(rng), idx);
+export function* toss<Ts>(
+  generator: IRawProperty<Ts>,
+  seed: number,
+  random: (seed: number) => RandomGenerator,
+  examples: Ts[]
+): IterableIterator<Value<Ts>> {
+  yield* safeMap(examples, (e) => new Value(e, undefined));
+  let idx = 0;
+  let rng = random(seed);
+  for (;;) {
+    rng = rng.jump ? rng.jump() : skipN(rng, 42);
+    yield generator.generate(new Random(rng), idx++);
+  }
 }
 
 /** @internal */
-export function* toss<Ts>(
+export function* lazyToss<Ts>(
   generator: IRawProperty<Ts>,
   seed: number,
   random: (seed: number) => RandomGenerator,
@@ -22,6 +33,6 @@ export function* toss<Ts>(
   let rng = random(seed);
   for (;;) {
     rng = rng.jump ? rng.jump() : skipN(rng, 42);
-    yield lazyGenerate(generator, rng, idx++);
+    yield () => generator.generate(new Random(rng), idx++);
   }
 }

.yarn/versions/aae163da.yml
@@ -1,0 +1,7 @@
+releases:
+  fast-check: minor
+
+declined:
+  - "@fast-check/ava"
+  - "@fast-check/jest"
+  - "@fast-check/worker"

packages/fast-check/src/check/runner/Runner.ts
@@ -10,7 +10,7 @@ import { RunDetails } from './reporter/RunDetails';
 import { RunExecution } from './reporter/RunExecution';
 import { RunnerIterator } from './RunnerIterator';
 import { SourceValuesIterator } from './SourceValuesIterator';
-import { toss } from './Tosser';
+import { lazyToss, toss } from './Tosser';
 import { pathWalk } from './utils/PathWalker';
 import { asyncReportRunDetails, reportRunDetails } from './utils/RunDetailsFormatter';
 import { IAsyncProperty } from '../property/AsyncProperty';
@@ -70,29 +70,17 @@ async function asyncRunIt<Ts>(
 }
 
 /** @internal */
-function runnerPathWalker<Ts>(
+function applyPath<Ts>(
   valueProducers: IterableIterator<() => Value<Ts>>,
   shrink: (value: Value<Ts>) => Stream<Value<Ts>>,
-  path: string
-): IterableIterator<() => Value<Ts>> {
-  const pathPoints = path.split(':');
+  nonEmptyPath: string
+): IterableIterator<Value<Ts>> {
+  const pathPoints = nonEmptyPath.split(':');
   const pathStream = stream(valueProducers)
     .drop(pathPoints.length > 0 ? +pathPoints[0] : 0)
     .map((producer) => producer());
   const adaptedPath = ['0', ...pathPoints.slice(1)].join(':');
-  return pathWalk(adaptedPath, pathStream, shrink).map((v) => () => v);
-}
-
-/** @internal */
-function buildInitialValues<Ts>(
-  valueProducers: IterableIterator<() => Value<Ts>>,
-  shrink: (value: Value<Ts>) => Stream<Value<Ts>>,
-  qParams: QualifiedParameters<Ts>
-): IterableIterator<() => Value<Ts>> {
-  if (qParams.path.length === 0) {
-    return valueProducers;
-  }
-  return runnerPathWalker(valueProducers, shrink, qParams.path);
+  return pathWalk(adaptedPath, pathStream, shrink);
 }
 
 /**
@@ -149,12 +137,14 @@ function check<Ts>(rawProperty: IRawProperty<Ts>, params?: Parameters<Ts>): unkn
   if (qParams.asyncReporter !== null && !rawProperty.isAsync())
     throw new Error('Invalid parameters encountered, only asyncProperty can be used when asyncReporter specified');
   const property = decorateProperty(rawProperty, qParams);
-  const generator = toss(property, qParams.seed, qParams.randomType, qParams.examples);
 
   const maxInitialIterations = qParams.path.length === 0 || qParams.path.indexOf(':') === -1 ? qParams.numRuns : -1;
   const maxSkips = qParams.numRuns * qParams.maxSkipsPerRun;
   const shrink: typeof property.shrink = (...args) => property.shrink(...args);
-  const initialValues = buildInitialValues(generator, shrink, qParams);
+  const initialValues =
+    qParams.path.length === 0
+      ? toss(property, qParams.seed, qParams.randomType, qParams.examples)
+      : applyPath(lazyToss(property, qParams.seed, qParams.randomType, qParams.examples), shrink, qParams.path);
   const sourceValues = new SourceValuesIterator(initialValues, maxInitialIterations, maxSkips);
   const finalShrink = !qParams.endOnFailure ? shrink : Stream.nil;
   return property.isAsync()

packages/fast-check/src/check/runner/Sampler.ts
@@ -33,19 +33,13 @@ function streamSample<Ts>(
   const qParams: QualifiedParameters<Ts> = QualifiedParameters.read<Ts>(extendedParams);
   const nextProperty = toProperty(generator, qParams);
   const shrink = nextProperty.shrink.bind(nextProperty);
-  const tossedValues: Stream<() => Value<Ts>> = stream(
+  const tossedValues: Stream<Value<Ts>> = stream(
     toss(nextProperty, qParams.seed, qParams.randomType, qParams.examples)
   );
   if (qParams.path.length === 0) {
-    return tossedValues.take(qParams.numRuns).map((s) => s().value_);
+    return tossedValues.take(qParams.numRuns).map((s) => s.value_);
   }
-  return stream(
-    pathWalk(
-      qParams.path,
-      tossedValues.map((s) => s()),
-      shrink
-    )
-  )
+  return pathWalk(qParams.path, tossedValues, shrink)
     .take(qParams.numRuns)
     .map((s) => s.value_);
 }

packages/fast-check/src/check/runner/SourceValuesIterator.ts
@@ -6,7 +6,7 @@
  */
 export class SourceValuesIterator<Ts> implements IterableIterator<Ts> {
   constructor(
-    readonly initialValues: IterableIterator<() => Ts>,
+    readonly initialValues: IterableIterator<Ts>,
     private maxInitialIterations: number,
     private remainingSkips: number
   ) {}
@@ -16,7 +16,7 @@ export class SourceValuesIterator<Ts> implements IterableIterator<Ts> {
   next(): IteratorResult<Ts> {
     if (--this.maxInitialIterations !== -1 && this.remainingSkips >= 0) {
       const n = this.initialValues.next();
-      if (!n.done) return { value: n.value(), done: false };
+      if (!n.done) return { value: n.value, done: false };
     }
     return { value: undefined, done: true };
   }

packages/fast-check/test/unit/check/runner/SourceValuesIterator.spec.ts
@@ -13,15 +13,11 @@ function iotaN(n: number) {
   return iota().take(n);
 }
 
-function source() {
-  return iota()
-    .map((v) => () => v)
-    [Symbol.iterator]();
+function source(): IterableIterator<number> {
+  return iota()[Symbol.iterator]();
 }
-function sourceN(n: number) {
-  return iotaN(n)
-    .map((v) => () => v)
-    [Symbol.iterator]();
+function sourceN(n: number): IterableIterator<number> {
+  return iotaN(n)[Symbol.iterator]();
 }
 
 function simulateSkips(svIt: SourceValuesIterator<number>, skippedValues: number[]) {
@@ -38,8 +34,8 @@ describe('SourceValuesIterator', () => {
     fc.assert(
       fc.property(fc.nat(100), (askedValues) => {
         const generatedValues: number[] = [];
-        const initialValues = iota()
-          .map((v) => () => {
+        const initialValues: IterableIterator<number> = iota()
+          .map((v) => {
             generatedValues.push(v);
             return v;
           })

packages/fast-check/test/unit/check/runner/Tosser.spec.ts
@@ -29,7 +29,7 @@ describe('Tosser', () => {
             ...s
               .drop(start)
               .take(2)
-              .map((f) => f().value),
+              .map((f) => f.value),
           ];
           expect(g1).not.toStrictEqual(g2);
           return true;
@@ -41,11 +41,11 @@ describe('Tosser', () => {
           expect([
             ...stream(toss(wrap(stubArb.forward()), seed, prand.xorshift128plus, []))
               .take(num)
-              .map((f) => f().value),
+              .map((f) => f.value),
           ]).toStrictEqual([
             ...stream(toss(wrap(stubArb.forward()), seed, prand.xorshift128plus, []))
               .take(num)
-              .map((f) => f().value),
+              .map((f) => f.value),
           ]);
         })
       ));
@@ -57,9 +57,9 @@ describe('Tosser', () => {
           expect(
             onGoingItems2
               .reverse()
-              .map((f) => f().value)
+              .map((f) => f.value)
               .reverse()
-          ).toStrictEqual(onGoingItems1.map((f) => f().value));
+          ).toStrictEqual(onGoingItems1.map((f) => f.value));
         })
       ));
     it('Should offset toss with the provided examples', () =>
@@ -67,10 +67,10 @@ describe('Tosser', () => {
         fc.property(fc.integer(), fc.nat(20), fc.array(fc.integer()), (seed, num, examples) => {
           const noExamplesProvided = [
             ...stream(toss(wrap(stubArb.forward()), seed, prand.xorshift128plus, [])).take(num - examples.length),
-          ].map((f) => f().value);
+          ].map((f) => f.value);
           const examplesProvided = [
             ...stream(toss(wrap(stubArb.forward()), seed, prand.xorshift128plus, examples)).take(num),
-          ].map((f) => f().value);
+          ].map((f) => f.value);
           expect([...examples, ...noExamplesProvided].slice(0, num)).toStrictEqual(examplesProvided);
         })
       ));

commit 8e2e3172693de71886ba015a991f7b2d0791e3cb

🐛 Fix broken replay based on path
The original attempt to optimize the performance of tosser introduced a slightly regression in the code. It broke many replays capabilities.

Regression introduced by #3554.

Fix #3613

packages/fast-check/src/check/runner/Tosser.ts
@@ -22,6 +22,11 @@ export function* toss<Ts>(
 }
 
 /** @internal */
+function lazyGenerate<Ts>(generator: IRawProperty<Ts>, rng: RandomGenerator, idx: number): () => Value<Ts> {
+  return () => generator.generate(new Random(rng), idx);
+}
+
+/** @internal */
 export function* lazyToss<Ts>(
   generator: IRawProperty<Ts>,
   seed: number,
@@ -33,6 +38,6 @@ export function* lazyToss<Ts>(
   let rng = random(seed);
   for (;;) {
     rng = rng.jump ? rng.jump() : skipN(rng, 42);
-    yield () => generator.generate(new Random(rng), idx++);
+    yield lazyGenerate(generator, rng, idx++);
   }
 }