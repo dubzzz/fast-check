30f1e5c2b9f699d06a26f5fd41462d5a5191c77d
Nicolas DUBIEN <github@dubien.org>
2023-02-23 22:32:15 +0000

    re-export AutoValue

--- packages/fast-check/src/arbitrary/auto.ts
+++ packages/fast-check/src/arbitrary/auto.ts
@@ -2,6 +2,7 @@ import { Arbitrary } from '../check/arbitrary/definition/Arbitrary';
 import { AutoArbitrary } from './_internals/AutoArbitrary';
 import { AutoValue } from './_internals/builders/AutoValueBuilder';
 
+export { AutoValue };
 export function auto(): Arbitrary<AutoValue> {
   return new AutoArbitrary();
 }
