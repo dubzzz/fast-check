7ffa5f0a450cb685fbd15333fdc2988f6e3a7090
Nicolas DUBIEN <github@dubien.org>
2023-04-03 20:18:14 +0200

    drop social

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo.png',
       navbar: {
         title: 'fast-check',
         logo: { alt: 'fast-check Logo', src: 'img/mug.svg' },
