0c7edbe15df75d288ac47fc910e64ea620bb9b99
Nicolas DUBIEN <github@dubien.org>
2023-04-04 21:02:40 +0000

    📝 Add search capability to the doc

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -93,6 +93,27 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Nicolas Dubien. Built with Docusaurus.`,
       },
       prism: { theme: lightCodeTheme, darkTheme: darkCodeTheme },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'XIUK9DGBYC',
+        // Public API key: it is safe to commit it
+        apiKey: '3d6340d4de21361b286217d4edd1ef52',
+        indexName: 'fast-check',
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        //replaceSearchResultPathname: {
+        //  from: '/docs/', // or as RegExp: /\/docs\//
+        //  to: '/',
+        //},
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+        //... other Algolia params
+      },
     }),
 };
 
