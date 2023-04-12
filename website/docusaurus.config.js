67172d14e35af16440f4869e00dd5ac15967e0b8
Nicolas DUBIEN <github@dubien.org>
2023-04-12 09:23:52 +0200

    🔧 Update `baseUrl` on the ain documentation (#3751)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://dubzzz.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fast-check/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
