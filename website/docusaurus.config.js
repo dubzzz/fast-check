1f9e25cd0d5067715c85cacb999db58bff202543
Nicolas DUBIEN <github@dubien.org>
2023-04-15 15:03:01 +0000

    edit docusaurus config to always add trailing slashes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
