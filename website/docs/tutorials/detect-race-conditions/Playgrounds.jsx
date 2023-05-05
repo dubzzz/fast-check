0e05da858a4fa886df23abddc222eafc2dc3ae3e
Nicolas DUBIEN <github@dubien.org>
2023-05-05 20:49:03 +0200

    work on the missing part section

--- website/docs/tutorials/detect-race-conditions/Playgrounds.jsx
+++ website/docs/tutorials/detect-race-conditions/Playgrounds.jsx
@@ -38,6 +38,48 @@ const queueCodeV2 = `export function queue(fun) {
   };
 }`;
 
+const queueCodeV3 = `export function queue(fun) {
+  let pastQueries = [];
+  return (...args) => {
+    const currentQuery = fun(...args);
+    const knownPastQueries = pastQueries;
+    const returnedQuery = Promise.all(pastQueries)
+      .finally(() => {
+        if (knownPastQueries === pastQueries)
+          pastQueries = [];
+      })
+      .then(() => currentQuery);
+    pastQueries = [...pastQueries, currentQuery];
+    return returnedQuery;
+  };
+}`;
+
+const queueCodeV4 = `export function queue(fun) {
+  let queryId = 0;
+  let lastQuery = null;
+  return (...args) => {
+    const selfQueryId = ++queryId;
+    if (lastQuery === null) {
+      lastQuery = fun(...args)
+        .finally(() => {
+          if (queryId === selfQueryId)
+            lastQuery = null;
+        });
+    } else {
+      lastQuery = lastQuery
+        .then(
+          () => fun(...args),
+          () => fun(...args),
+        )
+        .finally(() => {
+          if (queryId === selfQueryId)
+            lastQuery = null;
+        });
+    }
+    return lastQuery;
+  };
+}`;
+
 const queueUnitSpecCode = `import {queue} from './queue.js';
 
 test('should resolve in call order', async () => {
@@ -104,6 +146,37 @@ test('should resolve in call order', async () => {
   }))
 })`;
 
+const queueBatchesAlternativePBTSpecCode = `import {queue} from './queue.js';
+import fc from 'fast-check';
+
+test('should resolve in call order', async () => {
+  await fc.assert(fc.asyncProperty(fc.scheduler(), fc.integer({min: 1, max: 10}), async (s, numCalls) => {
+    // Arrange
+    const pendingQueries = [];
+    const seenAnswers = [];
+    const expectedAnswers = [];
+    const call = jest.fn()
+      .mockImplementation(v => Promise.resolve(v));
+  
+    // Act
+    const queued = queue(s.scheduleFunction(call));
+    for (let id = 0 ; id !== numCalls ; ++id) {
+      pendingQueries.push(
+        s.schedule(Promise.resolve(\`Fire the call for \${id}\`))
+          .then(() => {
+            expectedAnswers.push(id);
+            return queued(id);
+          })
+          .then(v => (seenAnswers.push(v)))
+      );
+    }
+    await s.waitFor(Promise.all(pendingQueries));
+  
+    // Assert
+    expect(seenAnswers).toEqual(expectedAnswers);
+  }))
+})`;
+
 const queueFromBatchesPBTSpecCode = `import {queue} from './queue.js';
 import fc from 'fast-check';
 
@@ -135,6 +208,49 @@ test('should resolve in call order', async () => {
   
     // Assert
     expect(seenAnswers).toEqual(expectedAnswers);
+  }))
+})`;
+
+const nextPBTSpecCode = `import {queue} from './queue.js';
+import fc from 'fast-check';
+
+test('should resolve in call order', async () => {
+  await fc.assert(fc.asyncProperty(fc.scheduler(), fc.array(fc.integer({min: 1, max: 10}), {minLength: 1}), async (s, batches) => {
+    // Arrange
+    const pendingQueries = [];
+    const seenAnswers = [];
+    const expectedAnswers = [];
+    const call = jest.fn()
+      .mockImplementation(v => Promise.resolve(v));
+    const scheduledCall = s.scheduleFunction(call);
+    let concurrentQueriesDetected = false;
+    let queryPending = false;
+    const monitoredScheduledCall = (...args) => {
+      concurrentQueriesDetected ||= queryPending;
+      queryPending = true;
+      return scheduledCall(...args).finally(() => (queryPending = false));
+    };
+  
+    // Act
+    const queued = queue(monitoredScheduledCall);
+    let lastId = 0;
+    const { task } = s.scheduleSequence(batches.map((batch, index) => {
+      return {
+        label: \`Fire batch #\${index + 1} (\${batch} calls)\`,
+        builder: async () => {
+          for (let id = 0 ; id !== batch ; ++id, ++lastId) {
+            expectedAnswers.push(lastId);
+            pendingQueries.push(queued(lastId).then(v => (seenAnswers.push(v))));
+          }
+        },
+      }
+    }));
+    await s.waitFor(task);
+    await s.waitFor(Promise.all(pendingQueries));
+  
+    // Assert
+    expect(seenAnswers).toEqual(expectedAnswers);
+    expect(concurrentQueriesDetected).toBe(false);
   }))
 })`;
 
@@ -230,6 +346,18 @@ export function MultipleBatchesOfCalls() {
       fileContent={queueCodeV2}
       startSpecCode={queueMoreThan2CallsPBTSpecCode}
       anwserSpecCode={queueFromBatchesPBTSpecCode}
+    />
+  );
+}
+
+export function MissingPart() {
+  return (
+    <SetupPlayground
+      fileName="queue"
+      fileExtension="js"
+      fileContent={queueCodeV3}
+      startSpecCode={queueFromBatchesPBTSpecCode}
+      anwserSpecCode={nextPBTSpecCode}
     />
   );
 }
