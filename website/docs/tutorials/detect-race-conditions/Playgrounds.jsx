f932cd5354f319fbd34d72f790795bc7d58089a8
Nicolas DUBIEN <github@dubien.org>
2023-05-10 22:47:18 +0200

    Enrich wrapping up

--- website/docs/tutorials/detect-race-conditions/Playgrounds.jsx
+++ website/docs/tutorials/detect-race-conditions/Playgrounds.jsx
@@ -3,6 +3,7 @@ import {
   SandpackProvider,
   SandpackLayout,
   SandpackCodeEditor,
+  SandpackFileExplorer,
   SandpackTests,
   UnstyledOpenInCodeSandboxButton,
 } from '@codesandbox/sandpack-react';
@@ -60,23 +61,47 @@ const queueCodeV4 = `export function queue(fun) {
   return (...args) => {
     const selfQueryId = ++queryId;
     if (lastQuery === null) {
-      lastQuery = fun(...args)
-        .finally(() => {
-          if (queryId === selfQueryId)
-            lastQuery = null;
-        });
+      lastQuery = fun(...args);
     } else {
       lastQuery = lastQuery
         .then(
           () => fun(...args),
           () => fun(...args),
-        )
-        .finally(() => {
-          if (queryId === selfQueryId)
-            lastQuery = null;
-        });
+        );
     }
+    lastQuery
+      .finally(() => {
+        if (queryId === selfQueryId)
+          lastQuery = null;
+      });
     return lastQuery;
+  };
+}`;
+
+const queueCodeV5 = `export function queue(fun) {
+  let pending = false;
+  let onDone = [];
+  function runNext() {
+    if (onDone.length === 0) {
+      pending = false;
+      return;
+    }
+    onDone.shift()();
+  }
+  return (...args) => {
+    if (!pending) {
+      pending = true;
+      const p = fun(...args);
+      p.then(runNext, runNext);
+      return p;
+    }
+    return new Promise((resolve, reject) => {
+      onDone.push(() => {
+        const p = fun(...args);
+        p.then(runNext, runNext);
+        p.then(resolve, reject);
+      });
+    });
   };
 }`;
 
@@ -115,6 +140,27 @@ test('should resolve in call order', async () => {
     pendingQueries.push(queued(1).then(v => (seenAnswers.push(v))));
     pendingQueries.push(queued(2).then(v => (seenAnswers.push(v))));
     await s.waitFor(Promise.all(pendingQueries));
+  
+    // Assert
+    expect(seenAnswers).toEqual([1, 2]);
+  }))
+})`;
+
+const queueBasicPBTWaitAllSpecCode = `import {queue} from './queue.js';
+import fc from 'fast-check';
+
+test('should resolve in call order', async () => {
+  await fc.assert(fc.asyncProperty(fc.scheduler(), async (s) => {
+    // Arrange
+    const seenAnswers = [];
+    const call = jest.fn()
+      .mockImplementation(v => Promise.resolve(v));
+  
+    // Act
+    const queued = queue(s.scheduleFunction(call));
+    queued(1).then(v => (seenAnswers.push(v)));
+    queued(2).then(v => (seenAnswers.push(v)));
+    await s.waitAll();
   
     // Assert
     expect(seenAnswers).toEqual([1, 2]);
@@ -211,7 +257,7 @@ test('should resolve in call order', async () => {
   }))
 })`;
 
-const nextPBTSpecCode = `import {queue} from './queue.js';
+const missingPartPBTSpecCode = `import {queue} from './queue.js';
 import fc from 'fast-check';
 
 test('should resolve in call order', async () => {
@@ -254,8 +300,90 @@ test('should resolve in call order', async () => {
   }))
 })`;
 
+const extendedBackToWaitAllPBTSpecCode = `import {queue} from './queue.js';
+import fc from 'fast-check';
+
+test('should resolve in call order', async () => {
+  await fc.assert(fc.asyncProperty(fc.scheduler(), fc.array(fc.integer({min: 1, max: 10}), {minLength: 1}), async (s, batches) => {
+    // Arrange
+    const seenAnswers = [];
+    const expectedAnswers = [];
+    const call = jest.fn()
+      .mockImplementation(v => Promise.resolve(v));
+    const scheduledCall = s.scheduleFunction(call);
+    let concurrentQueriesDetected = false;
+    let queryPending = false;
+    const monitoredScheduledCall = (...args) => {
+      concurrentQueriesDetected ||= queryPending;
+      queryPending = true;
+      return scheduledCall(...args).finally(() => (queryPending = false));
+    };
+  
+    // Act
+    const queued = queue(monitoredScheduledCall);
+    let lastId = 0;
+    s.scheduleSequence(batches.map((batch, index) => {
+      return {
+        label: \`Fire batch #\${index + 1} (\${batch} calls)\`,
+        builder: async () => {
+          for (let id = 0 ; id !== batch ; ++id, ++lastId) {
+            expectedAnswers.push(lastId);
+            queued(lastId).then(v => (seenAnswers.push(v)));
+          }
+        },
+      }
+    }));
+    await s.waitAll();
+  
+    // Assert
+    expect(seenAnswers).toEqual(expectedAnswers);
+    expect(concurrentQueriesDetected).toBe(false);
+  }))
+})`;
+
+const extendedWithExceptionsPBTSpecCode = `import {queue} from './queue.js';
+import fc from 'fast-check';
+
+test('should resolve in call order', async () => {
+  await fc.assert(fc.asyncProperty(fc.scheduler(), fc.array(fc.integer({min: 1, max: 10}), {minLength: 1}), async (s, batches) => {
+    // Arrange
+    const seenAnswers = [];
+    const expectedAnswers = [];
+    const call = jest.fn()
+      .mockImplementation(v => Promise.resolve(v));
+    const scheduledCall = s.scheduleFunction(call);
+    let concurrentQueriesDetected = false;
+    let queryPending = false;
+    const monitoredScheduledCall = (...args) => {
+      concurrentQueriesDetected ||= queryPending;
+      queryPending = true;
+      return scheduledCall(...args).finally(() => (queryPending = false));
+    };
+  
+    // Act
+    const queued = queue(monitoredScheduledCall);
+    let lastId = 0;
+    s.scheduleSequence(batches.map((batch, index) => {
+      return {
+        label: \`Fire batch #\${index + 1} (\${batch} calls)\`,
+        builder: async () => {
+          for (let id = 0 ; id !== batch ; ++id, ++lastId) {
+            expectedAnswers.push(lastId);
+            queued(lastId).then(v => (seenAnswers.push(v)));
+          }
+        },
+      }
+    }));
+    await s.waitAll();
+  
+    // Assert
+    expect(seenAnswers).toEqual(expectedAnswers);
+    expect(concurrentQueriesDetected).toBe(false);
+  }))
+})`;
+
 function SetupPlayground(props) {
-  const { startSpecCode, anwserSpecCode, fileContent, fileName, fileExtension, specs = [] } = props;
+  const { startSpecCode, anwserSpecCode, fileContent, fileName, fileExtension } = props;
   const [specFile, setSpecFile] = useState({ key: 0, content: startSpecCode });
 
   return (
@@ -275,11 +403,6 @@ function SetupPlayground(props) {
           active: true,
           hidden: false,
         },
-        ...Object.fromEntries(
-          specs.map((specContent, index) => {
-            return [`./${fileName}.v${index}.spec.${fileExtension}`, specContent];
-          })
-        ),
       }}
       customSetup={{
         entry: `/${fileName}.${fileExtension}`,
@@ -357,20 +480,119 @@ export function MissingPart() {
       fileExtension="js"
       fileContent={queueCodeV3}
       startSpecCode={queueFromBatchesPBTSpecCode}
-      anwserSpecCode={nextPBTSpecCode}
+      anwserSpecCode={missingPartPBTSpecCode}
     />
   );
 }
 
-export function DebugPlaygroundsQueue() {
+function codeWithComments(code, comments) {
+  return `${comments.map((line) => `// ${line}`).join('\n')}\n\n${code}`;
+}
+function pastImplementationSnippet(code, partName) {
+  return codeWithComments(code, ['Implementation used within the part:', '> ' + partName]);
+}
+function pastTestSnippet(code, partName, variationName) {
+  return codeWithComments(code, [
+    'Test suggested for the part:',
+    '> ' + partName,
+    ...(variationName !== undefined ? [variationName] : []),
+  ]);
+}
+
+export function WrapUpPlaygroundQueue() {
+  const [reset, setReset] = useState(0);
+  const queueImplementations = {
+    'queue.v0.js': pastImplementationSnippet(queueCodeV0, 'Your first race condition test'),
+    'queue.v1.js': pastImplementationSnippet(queueCodeV1, 'One step close to real usages'),
+    'queue.v2.js': pastImplementationSnippet(queueCodeV2, 'Multiple batches of calls'),
+    'queue.v3.js': pastImplementationSnippet(queueCodeV3, 'The missing part'),
+    'queue.v4.js': codeWithComments(queueCodeV4, ['Pass all the tests of the tutorial']),
+    'queue.v5.js': codeWithComments(queueCodeV5, ['Pass all the tests']),
+  };
+  const defaultQueueImplementation = 'queue.v5.js';
+  const queueTests = {
+    'queue.p0.spec.js': queueUnitSpecCode,
+    'queue.p1.spec.js': pastTestSnippet(queueBasicPBTSpecCode, 'Your first race condition test'),
+    'queue.p1.v2.spec.js': pastTestSnippet(
+      queueBasicPBTWaitAllSpecCode,
+      'Your first race condition test',
+      'With waitAll'
+    ),
+    'queue.p2.spec.js': pastTestSnippet(queueMoreThan2CallsPBTSpecCode, 'One step close to real usages'),
+    'queue.p3.spec.js': pastTestSnippet(queueFromBatchesPBTSpecCode, 'Multiple batches of calls'),
+    'queue.p3.v2.spec.js': pastTestSnippet(
+      queueBatchesAlternativePBTSpecCode,
+      'Multiple batches of calls',
+      'With delayed calls (no batches)'
+    ),
+    'queue.p4.spec.js': pastTestSnippet(missingPartPBTSpecCode, 'The missing part'),
+    'queue.pnext.spec.js': extendedBackToWaitAllPBTSpecCode,
+    'queue.pnext.v2.spec.js': extendedWithExceptionsPBTSpecCode,
+  };
+  const defaultQueueTest = 'queue.p4.spec.js';
   return (
-    <SetupPlayground
-      fileName="queue"
-      fileExtension="js"
-      fileContent={queueCodeV2}
-      startSpecCode={queueUnitSpecCode}
-      anwserSpecCode={queueUnitSpecCode}
-      specs={[queueUnitSpecCode, queueBasicPBTSpecCode, queueMoreThan2CallsPBTSpecCode, queueFromBatchesPBTSpecCode]}
-    />
+    <SandpackProvider
+      key={reset}
+      theme={atomDark}
+      files={{
+        ...Object.fromEntries(
+          Object.entries(queueImplementations).map(([fileName, fileContent]) => {
+            return [
+              `src/${fileName}`,
+              {
+                code: fileContent,
+                readOnly: false,
+                active: false,
+                hidden: true,
+              },
+            ];
+          })
+        ),
+        ...Object.fromEntries(
+          Object.entries(queueTests).map(([fileName, fileContent]) => {
+            return [
+              `tests/${fileName}`,
+              {
+                code: fileContent.replace('./queue.js', './../src/queue.js'),
+                readOnly: false,
+                active: fileName === defaultQueueTest,
+                hidden: fileName !== defaultQueueTest,
+              },
+            ];
+          })
+        ),
+        'src/queue.js': {
+          code: `export {queue} from './${defaultQueueImplementation}'`,
+          readOnly: false,
+          active: false,
+          hidden: false,
+        },
+        'package.json': {
+          code: JSON.stringify({ main: `src/queue.js` }),
+          readOnly: true,
+          active: false,
+          hidden: true,
+        },
+      }}
+      customSetup={{
+        entry: `src/queue.js`,
+        dependencies: {
+          'fast-check': 'latest',
+        },
+      }}
+    >
+      <SandpackLayout>
+        <SandpackFileExplorer style={{ height: 500 }} />
+        <SandpackCodeEditor style={{ height: 500 }} />
+        <SandpackTests
+          actionsChildren={<button onClick={() => setReset((r) => r + 1)}>Reset snippets</button>}
+          verbose
+          style={{ height: 500 }}
+        />
+      </SandpackLayout>
+      <div className={styles.openInCodeSandbox}>
+        <UnstyledOpenInCodeSandboxButton>Open in CodeSandbox</UnstyledOpenInCodeSandboxButton>
+      </div>
+    </SandpackProvider>
   );
 }
