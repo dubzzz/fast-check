12c0c9768a2c37c868cddc5219aa38ddceba0678
Nicolas DUBIEN <github@dubien.org>
2023-05-19 18:03:55 +0200

    Update packages/fast-check/src/arbitrary/_internals/implementations/SchedulerImplem.ts

--- packages/fast-check/src/arbitrary/_internals/implementations/SchedulerImplem.ts
+++ packages/fast-check/src/arbitrary/_internals/implementations/SchedulerImplem.ts
@@ -224,8 +224,7 @@ export class SchedulerImplem<TMetaData> implements Scheduler<TMetaData> {
 
   async waitOne(customAct?: SchedulerAct): Promise<void> {
     const waitAct = customAct || defaultSchedulerAct;
-    const waitOneResult: Promise<unknown> = this.act(() => waitAct(() => this.internalWaitOne()));
-    return waitOneResult as Promise<void>;
+    await this.act(() => waitAct(() => this.internalWaitOne()));
   }
 
   async waitAll(customAct?: SchedulerAct): Promise<void> {
