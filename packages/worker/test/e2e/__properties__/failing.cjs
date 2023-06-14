ae3da6de8e1036f1f6f94e2a8aca9acb9b9329e6
Nicolas DUBIEN <github@dubien.org>
2023-06-14 20:54:55 +0200

    ✅(worker) Restructure E2E tests (#3971)
    * ✅(worker) Restructure E2E tests
    
    * tests only
    
    * update comment

---
+++ packages/worker/test/e2e/__properties__/failing.cjs
@@ -0,0 +1,16 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* global __filename, exports, require */
+const { pathToFileURL } = require('node:url');
+const fc = require('fast-check');
+const { propertyFor } = require('@fast-check/worker');
+
+const property = propertyFor(pathToFileURL(__filename));
+
+exports.failingProperty = property(
+  fc.integer({ min: -1000, max: 1000 }),
+  fc.integer({ min: -1000, max: 1000 }),
+  (_from, _to) => {
+    throw new Error("I'm a failing property");
+  }
+);
---
+++ packages/worker/test/e2e/concurrentAssert.spec.ts
@@ -0,0 +1,33 @@
+import { isMainThread } from 'node:worker_threads';
+import { type Parameters } from 'fast-check';
+import { assert } from '@fast-check/worker';
+
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import { readerAssert, writerAssert } from './__properties__/concurrentAssert.cjs';
+
+if (isMainThread) {
+  describe('@fast-check/worker', () => {
+    const jestTimeout = 10000;
+    const assertTimeout = 1000;
+    const defaultOptions: Parameters<unknown> = { timeout: assertTimeout };
+
+    it(
+      'should be able to run two inter-dependent asserts',
+      async () => {
+        // While this pattern is definitely not recommended, we leverage it in our test suite to make sure
+        // two concurrent workers can be spawn at the same time on the default isolation level (file)
+        // Arrange
+        const options: Parameters<unknown> = { ...defaultOptions, numRuns: 5 };
+
+        // Act
+        const readerRun = assert(readerAssert, options); // reader asks questions to writer and waits for the answer
+        const writerRun = assert(writerAssert, options); // writer waits for questions from reader and sends an answer
+
+        // Assert
+        await expect(Promise.all([readerRun, writerRun])).resolves.not.toThrow();
+      },
+      jestTimeout
+    );
+  });
+}
