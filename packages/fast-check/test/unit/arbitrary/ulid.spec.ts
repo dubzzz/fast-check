c47f208c1940d6e48565b3153dff7a7194bd8279
Nicolas DUBIEN <github@dubien.org>
2023-07-19 19:15:32 +0000

    slow add examples

--- packages/fast-check/test/unit/arbitrary/ulid.spec.ts
+++ packages/fast-check/test/unit/arbitrary/ulid.spec.ts
@@ -2,6 +2,8 @@ import fc from 'fast-check';
 import { ulid } from '../../../src/arbitrary/ulid';
 
 import { assertProduceSameValueGivenSameSeed } from './__test-helpers__/ArbitraryAssertions';
+import { integer } from '../../../src/arbitrary/integer';
+import { tuple } from '../../../src/arbitrary/tuple';
 
 function beforeEachHook() {
   jest.resetModules();
@@ -11,6 +13,31 @@ function beforeEachHook() {
 beforeEach(beforeEachHook);
 
 describe('ulid (integration)', () => {
+  function ulid40() {
+    return integer({ min: 0, max: 0xffffffffff });
+  }
+  function ulid48() {
+    return integer({ min: 0, max: 0xffffffffffff });
+  }
+  function ulidNoMap() {
+    const timestampPartArbitrary = integer({ min: 0, max: 0xffffffffffff });
+    const randomnessPartOneArbitrary = integer({ min: 0, max: 0xffffffffff });
+    const randomnessPartTwoArbitrary = integer({ min: 0, max: 0xffffffffff });
+    return tuple(timestampPartArbitrary, randomnessPartOneArbitrary, randomnessPartTwoArbitrary);
+  }
+
+  it('40bits', () => {
+    assertProduceSameValueGivenSameSeed(ulid40);
+  });
+
+  it('48bits', () => {
+    assertProduceSameValueGivenSameSeed(ulid48);
+  });
+
+  it('ulidNoMap', () => {
+    assertProduceSameValueGivenSameSeed(ulidNoMap);
+  });
+
   it('ulid', () => {
     assertProduceSameValueGivenSameSeed(ulid);
   });
