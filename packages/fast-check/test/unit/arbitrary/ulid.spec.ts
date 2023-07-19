32a222a3c22397d16faf6170625204a62f066e61
Nicolas DUBIEN <github@dubien.org>
2023-07-20 00:10:08 +0200

    Update ulid.spec.ts for slow measurements

--- packages/fast-check/test/unit/arbitrary/ulid.spec.ts
+++ packages/fast-check/test/unit/arbitrary/ulid.spec.ts
@@ -1,17 +1,9 @@
 import fc from 'fast-check';
 import { ulid } from '../../../src/arbitrary/ulid';
-import { fakeArbitraryStaticValue } from './__test-helpers__/ArbitraryHelpers';
 
-import * as _IntegerMock from '../../../src/arbitrary/integer';
-import { Arbitrary } from '../../../src/check/arbitrary/definition/Arbitrary';
-import { fakeRandom } from './__test-helpers__/RandomHelpers';
-import {
-  assertProduceSameValueGivenSameSeed,
-  assertProduceCorrectValues,
-  assertProduceValuesShrinkableWithoutContext,
-  assertShrinkProducesSameValueWithoutInitialContext,
-} from './__test-helpers__/ArbitraryAssertions';
-const IntegerMock: { integer: (ct: { min: number; max: number }) => Arbitrary<number> } = _IntegerMock;
+import { assertProduceSameValueGivenSameSeed } from './__test-helpers__/ArbitraryAssertions';
+import { integer } from '../../../src/arbitrary/integer';
+import { tuple } from '../../../src/arbitrary/tuple';
 
 function beforeEachHook() {
   jest.resetModules();
@@ -20,60 +12,33 @@ function beforeEachHook() {
 }
 beforeEach(beforeEachHook);
 
-describe('ulid', () => {
-  it('should produce the minimal ulid given all minimal generated values', () => {
-    // Arrange
-    const { instance: mrng } = fakeRandom();
-    const integer = jest.spyOn(IntegerMock, 'integer');
-    integer.mockImplementation(({ min }) => {
-      const { instance } = fakeArbitraryStaticValue(() => min);
-      return instance;
-    });
-
-    // Act
-    const arb = ulid();
-    const out = arb.generate(mrng, undefined);
-
-    // Assert
-    expect(out.value).toBe('0'.repeat(26));
-  });
-
-  it('should produce the maximal ulid given all maximal generated values', () => {
-    // Arrange
-    const { instance: mrng } = fakeRandom();
-    const integer = jest.spyOn(IntegerMock, 'integer');
-    integer.mockImplementation(({ max }) => {
-      const { instance } = fakeArbitraryStaticValue(() => max);
-      return instance;
-    });
-
-    // Act
-    const arb = ulid();
-    const out = arb.generate(mrng, undefined);
-
-    // Assert
-    expect(out.value).toBe('7ZZZZZZZZZZZZZZZZZZZZZZZZZ');
-  });
-});
-
 describe('ulid (integration)', () => {
-  const isCorrect = (u: string) => {
-    expect(u).toMatch(/^[0-7][0-9A-HJKMNP-TV-Z]{25}$/);
-  };
+  function ulid40() {
+    return integer({ min: 0, max: 0xffffffffff });
+  }
+  function ulid48() {
+    return integer({ min: 0, max: 0xffffffffffff });
+  }
+  function ulidNoMap() {
+    const timestampPartArbitrary = integer({ min: 0, max: 0xffffffffffff });
+    const randomnessPartOneArbitrary = integer({ min: 0, max: 0xffffffffff });
+    const randomnessPartTwoArbitrary = integer({ min: 0, max: 0xffffffffff });
+    return tuple(timestampPartArbitrary, randomnessPartOneArbitrary, randomnessPartTwoArbitrary);
+  }
 
-  it('should produce the same values given the same seed', () => {
+  it('40bits', () => {
+    assertProduceSameValueGivenSameSeed(ulid40);
+  });
+
+  it('48bits', () => {
+    assertProduceSameValueGivenSameSeed(ulid48);
+  });
+
+  it('ulidNoMap', () => {
+    assertProduceSameValueGivenSameSeed(ulidNoMap);
+  });
+
+  it('ulid', () => {
     assertProduceSameValueGivenSameSeed(ulid);
-  });
-
-  it('should only produce correct values', () => {
-    assertProduceCorrectValues(ulid, isCorrect);
-  });
-
-  it('should produce values seen as shrinkable without any context', () => {
-    assertProduceValuesShrinkableWithoutContext(ulid);
-  });
-
-  it('should be able to shrink to the same values without initial context', () => {
-    assertShrinkProducesSameValueWithoutInitialContext(ulid);
   });
 });
