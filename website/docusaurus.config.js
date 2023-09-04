25f2f6f246f69176742bdc041088b691bab2cd95
Nicolas DUBIEN <github@dubien.org>
2023-09-04 20:15:14 +0000

    🔧 Add some more details onto the PWA

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -153,6 +153,37 @@ const config = {
             name: 'theme-color',
             content: 'rgb(40, 46, 169)',
           },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#000',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/favicon.ico',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/mug.svg',
+            color: 'rgb(40, 46, 169)',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/favicon.ico',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#000',
+          },
         ],
       },
     ],
