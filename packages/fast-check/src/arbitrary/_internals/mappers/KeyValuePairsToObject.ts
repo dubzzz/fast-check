71882c573c4e0c3521f8e7ba8a5e68765cd8deaf
Nicolas DUBIEN <github@dubien.org>
2023-09-06 22:17:44 +0200

    🐛 Better poisoning resiliency for `dictionary` (#4207)
    * 🐛 Better poisoning resiliency for `dictionary`
    
    * versions

--- packages/fast-check/src/arbitrary/_internals/mappers/KeyValuePairsToObject.ts
+++ packages/fast-check/src/arbitrary/_internals/mappers/KeyValuePairsToObject.ts
@@ -1,11 +1,21 @@
+import { Error, safeEvery } from '../../../utils/globals';
+
 type KeyValuePairs<T> = [string, T][];
 type ObjectDefinition<T> = [/*items*/ KeyValuePairs<T>, /*null prototype*/ boolean];
 
+const safeObjectCreate = Object.create;
+const safeObjectDefineProperty = Object.defineProperty;
+const safeObjectGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+const safeObjectGetPrototypeOf = Object.getPrototypeOf;
+const safeObjectGetOwnPropertySymbols = Object.getOwnPropertySymbols;
+const safeObjectGetOwnPropertyNames = Object.getOwnPropertyNames;
+const safeObjectEntries = Object.entries;
+
 /** @internal */
 export function keyValuePairsToObjectMapper<T>(definition: ObjectDefinition<T>): { [key: string]: T } {
-  const obj: { [key: string]: T } = definition[1] ? Object.create(null) : {};
+  const obj: { [key: string]: T } = definition[1] ? safeObjectCreate(null) : {};
   for (const keyValue of definition[0]) {
-    Object.defineProperty(obj, keyValue[0], {
+    safeObjectDefineProperty(obj, keyValue[0], {
       enumerable: true,
       configurable: true,
       writable: true,
@@ -16,16 +26,16 @@ export function keyValuePairsToObjectMapper<T>(definition: ObjectDefinition<T>):
 }
 
 /** @internal */
-function buildInvalidPropertyNameFilter(obj: unknown): (key: string) => boolean {
-  return function invalidPropertyNameFilter(key: string): boolean {
-    const descriptor = Object.getOwnPropertyDescriptor(obj, key);
+function buildIsValidPropertyNameFilter(obj: unknown): (key: string) => boolean {
+  return function isValidPropertyNameFilter(key: string): boolean {
+    const descriptor = safeObjectGetOwnPropertyDescriptor(obj, key);
     return (
-      descriptor === undefined ||
-      !descriptor.configurable ||
-      !descriptor.enumerable ||
-      !descriptor.writable ||
-      descriptor.get !== undefined ||
-      descriptor.set !== undefined
+      descriptor !== undefined &&
+      !!descriptor.configurable &&
+      !!descriptor.enumerable &&
+      !!descriptor.writable &&
+      descriptor.get === undefined &&
+      descriptor.set === undefined
     );
   };
 }
@@ -36,16 +46,16 @@ export function keyValuePairsToObjectUnmapper<T>(value: unknown): ObjectDefiniti
   if (typeof value !== 'object' || value === null) {
     throw new Error('Incompatible instance received: should be a non-null object');
   }
-  const hasNullPrototype = Object.getPrototypeOf(value) === null;
+  const hasNullPrototype = safeObjectGetPrototypeOf(value) === null;
   const hasObjectPrototype = 'constructor' in value && value.constructor === Object;
   if (!hasNullPrototype && !hasObjectPrototype) {
     throw new Error('Incompatible instance received: should be of exact type Object');
   }
-  if (Object.getOwnPropertySymbols(value).length > 0) {
+  if (safeObjectGetOwnPropertySymbols(value).length > 0) {
     throw new Error('Incompatible instance received: should contain symbols');
   }
-  if (Object.getOwnPropertyNames(value).find(buildInvalidPropertyNameFilter(value)) !== undefined) {
+  if (!safeEvery(safeObjectGetOwnPropertyNames(value), buildIsValidPropertyNameFilter(value))) {
     throw new Error('Incompatible instance received: should contain only c/e/w properties without get/set');
   }
-  return [Object.entries(value), hasNullPrototype];
+  return [safeObjectEntries(value), hasNullPrototype];
 }
