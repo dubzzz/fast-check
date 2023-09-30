25b6981e65f34cefbdf9dcbf615668c094f68f30
Nicolas DUBIEN <github@dubien.org>
2023-10-01 00:04:27 +0200

    ✅(ava) Confirm basic typings work well (#4283)

--- packages/ava/test-types/main.ts
+++ packages/ava/test-types/main.ts
@@ -1,1 +1,5 @@
-import '@fast-check/ava';
+import { testProp, fc } from '@fast-check/ava';
+
+testProp('should detect the substring', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
+  t.true((a + b + c).includes(b));
+});
