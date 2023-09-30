f310793431e00826c071c351297978e9923bd493
Nicolas DUBIEN <github@dubien.org>
2023-09-30 21:45:09 +0000

    ✅(packaged) Confirm basic typings work well

--- packages/packaged/test-types/main.ts
+++ packages/packaged/test-types/main.ts
@@ -1,1 +1,8 @@
-import '@fast-check/packaged';
+import { computePublishedFiles, removeNonPublishedFiles } from '@fast-check/packaged';
+
+computePublishedFiles('.').then((_publishedFilesRoot) => {
+  // not implemented
+});
+removeNonPublishedFiles('.', { dryRun: false, keepNodeModules: false }).then(({ kept: _kept, removed: _removed }) => {
+  // not implemented
+});
