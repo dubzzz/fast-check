7b7693f708ecf28858abb970b0e9e4d4e58cb801
Nicolas DUBIEN <github@dubien.org>
2023-10-01 00:52:23 +0200

    ✅(worker) Confirm basic typings work well (#4287)

--- packages/worker/test-types/main.ts
+++ packages/worker/test-types/main.ts
@@ -1,1 +1,10 @@
-import '@fast-check/worker';
+import fc from 'fast-check';
+import { assert, propertyFor } from '@fast-check/worker';
+
+const property = propertyFor(new URL('{{import.meta.url}}'));
+const p1 = property(fc.string(), fc.nat(), (s, num) => {
+  return s.length > num; // implicitly checking .length exists on s and num is a number
+});
+assert(p1, { timeout: 1000 }).then(() => {
+  // not implemented
+});
