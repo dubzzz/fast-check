59a8ab9d0e2a80410b1ddef43f7d4af0156e7d9b
Nicolas DUBIEN <github@dubien.org>
2023-10-20 07:46:58 +0000

    examples

--- packages/fast-check/jest.config.cjs
+++ packages/fast-check/jest.config.cjs
@@ -2,7 +2,6 @@
 // Useful for Jest plugin of vscode
 
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   collectCoverageFrom: ['<rootDir>/src/**'],
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
   setupFiles: [],
--- packages/packaged/jest.config.cjs
+++ packages/packaged/jest.config.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   collectCoverageFrom: ['<rootDir>/src/**'],
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
   setupFiles: [],
