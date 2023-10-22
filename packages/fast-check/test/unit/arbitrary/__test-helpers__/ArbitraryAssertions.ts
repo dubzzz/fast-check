644204a5ad4c89e8b48cf53ee7646562419eb989
Nicolas DUBIEN <github@dubien.org>
2023-10-22 09:47:53 +0000

    ✅ Faster CI with one property for arbitraries
    Up-to-now, checks for characteristics of the arbitraries have been executed in seperated properties. While it worked well and allowed better error reporting, it tend to increase by a lot the time of the checks and thus our time to deploy new versions and fixes of fast-check.
    
    In our target to make our CI faster we tried to gather them into a single check. It's not exactly what we would recommend ideally but given the number of things the properties we manipulate here are dealing with (scanning a possibly infinite tree), we falled onto that option.
    
    Let's see what's the CI thinks of it :)

--- packages/fast-check/test/unit/arbitrary/__test-helpers__/ArbitraryAssertions.ts
+++ packages/fast-check/test/unit/arbitrary/__test-helpers__/ArbitraryAssertions.ts
@@ -18,70 +18,58 @@ function poisoningAfterEach(nestedAfterEach: () => void) {
   }
 }
 
-// Minimal requirements
-// > The following assertions are supposed to be fulfilled by any of the arbitraries
-// > provided by fast-check.
-
-export function assertProduceSameValueGivenSameSeed<T, U = never>(
-  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
-  options: {
+type Checks<T, U> = {
+  // Minimal requirements
+  // > The following assertions are supposed to be fulfilled by any of the arbitraries
+  // > provided by fast-check.
+  sameValueGivenSameSeed: {
     isEqual?: (v1: T, v2: T, extraParameters: U) => void | boolean;
-    noInitialContext?: boolean;
+  };
+  // Optional requirements
+  // > The following requirements are optional as they do not break the design of fast-check when they are not totally ensured
+  // > But some of them are really recommended to build valid arbitraries that can be used.
+  correctValues?: {
+    isCorrect: (v: T, extraParameters: U, arb: Arbitrary<T>) => void | boolean;
+  };
+  equivalentTo?: {
+    otherArbitrary: (extraParameters: U) => Arbitrary<T>;
+    isEqual?: (v1: T, v2: T, extraParameters: U) => void | boolean;
+    isEqualContext?: (c1: unknown, c2: unknown, extraParameters: U) => void | boolean;
+  };
+  // Extra requirements
+  // The assertions above can be configured to push generators even further. They ensure more complex invariants.
+  // Following assertions are mostly derived from the one above.
+  // > assertShrinkProducesSameValueGivenSameSeed with option {noInitialContext:true}
+  // > assertGenerateProducesCorrectValues with option isCorrect: (v, _, arb) => arb.canShrinkWithoutContext(v)
+  // > assertShrinkProducesCorrectValues with option (v, _, arb) => arb.canShrinkWithoutContext(v)
+  sameValueWithoutInitialContext?: {
+    isEqual?: (v1: T, v2: T, extraParameters: U) => void | boolean;
+  };
+  shrinkableWithoutContext?: Record<string, never>;
+  strictlySmallerValue?: {
+    isStrictlySmaller: (vNew: T, vOld: T, extraParameters: U) => void | boolean;
+  };
+};
+
+export function assertValidArbitrary<T, U = never>(
+  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
+  checks: Checks<T, U>,
+  options: {
     extraParameters?: fc.Arbitrary<U>;
     assertParameters?: fc.Parameters<unknown>;
   } = {},
 ): void {
   const {
-    isEqual,
-    noInitialContext,
-    extraParameters: extra = fc.constant(undefined as unknown as U) as fc.Arbitrary<U>,
-    assertParameters,
-  } = options;
-  fc.assert(
-    fc
-      .property(
-        fc.integer().noShrink(),
-        biasFactorArbitrary(),
-        fc.infiniteStream(fc.nat({ max: 20 })),
-        extra,
-        (seed, biasFactor, shrinkPath, extraParameters) => {
-          // Arrange
-          const arb = arbitraryBuilder(extraParameters);
+    sameValueGivenSameSeed,
+    correctValues,
+    equivalentTo,
+    sameValueWithoutInitialContext,
+    shrinkableWithoutContext,
+    strictlySmallerValue,
+  } = checks;
+  const requiresG3 = sameValueWithoutInitialContext !== undefined;
+  const requiresG4 = equivalentTo !== undefined;
 
-          // Act / Assert
-          let g1: Value<T> | null = arb.generate(randomFromSeed(seed), biasFactor);
-          let g2: Value<T> | null = arb.generate(randomFromSeed(seed), biasFactor);
-          if (noInitialContext) {
-            const originalG2 = g2!;
-            g2 = new Value(originalG2.value_, undefined, () => originalG2.value);
-          }
-          while (g1 !== null && g2 !== null) {
-            assertEquality(isEqual, g1.value, g2.value, extraParameters);
-            const pos = shrinkPath.next().value;
-            g1 = arb.shrink(g1.value_, g1.context).getNthOrLast(pos);
-            g2 = arb.shrink(g2.value_, g2.context).getNthOrLast(pos);
-          }
-          expect(g1).toBe(null);
-          expect(g2).toBe(null);
-        },
-      )
-      .afterEach(poisoningAfterEach),
-    assertParameters,
-  );
-}
-
-// Optional requirements
-// > The following requirements are optional as they do not break the design of fast-check when they are not totally ensured
-// > But some of them are really recommended to build valid arbitraries that can be used.
-
-export function assertProduceCorrectValues<T, U = never>(
-  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
-  isCorrect: (v: T, extraParameters: U, arb: Arbitrary<T>) => void | boolean,
-  options: {
-    extraParameters?: fc.Arbitrary<U>;
-    assertParameters?: fc.Parameters<unknown>;
-  } = {},
-): void {
   const { extraParameters: extra = fc.constant(undefined as unknown as U) as fc.Arbitrary<U>, assertParameters } =
     options;
   fc.assert(
@@ -93,56 +81,78 @@ export function assertProduceCorrectValues<T, U = never>(
         extra,
         (seed, biasFactor, shrinkPath, extraParameters) => {
           // Arrange
+          const previousValue: { value?: T } = {};
           const arb = arbitraryBuilder(extraParameters);
 
           // Act / Assert
-          let g: Value<T> | null = arb.generate(randomFromSeed(seed), biasFactor);
-          while (g !== null) {
-            assertCorrectness(isCorrect, g.value, extraParameters, arb);
+          let g1: Value<T> | null = arb.generate(randomFromSeed(seed), biasFactor);
+          let g2: Value<T> | null = arb.generate(randomFromSeed(seed), biasFactor);
+          const tempG3 = requiresG3 ? arb.generate(randomFromSeed(seed), biasFactor) : null!;
+          let g3: Value<T> | null = requiresG3 ? new Value(tempG3.value_, undefined, () => tempG3.value) : null;
+          let g4: Value<T> | null = requiresG4
+            ? equivalentTo!.otherArbitrary(extraParameters).generate(randomFromSeed(seed), biasFactor)
+            : null;
+          while (g1 !== null && g2 !== null) {
+            // Extract relevant values
+            const v1 = g1.value;
+            const v2 = g2.value;
+            const v3 = requiresG3 ? g3!.value : null!;
+            const v4 = requiresG4 ? g4!.value : null!;
+            // Perform each check
+            assertEquality(sameValueGivenSameSeed.isEqual, v1, v2, extraParameters);
+            if (correctValues !== undefined) {
+              assertCorrectness(correctValues.isCorrect, v1, extraParameters, arb);
+            }
+            if (equivalentTo !== undefined) {
+              assertEquality(equivalentTo.isEqual, v1, v4, extraParameters);
+              if (equivalentTo.isEqualContext !== undefined) {
+                assertEquality(equivalentTo.isEqualContext, v1, v4, extraParameters);
+              }
+            }
+            if (sameValueWithoutInitialContext !== undefined) {
+              assertEquality(sameValueWithoutInitialContext.isEqual, v1, v3, extraParameters);
+            }
+            if (shrinkableWithoutContext !== undefined) {
+              assertCorrectness((v, _, arb) => arb.canShrinkWithoutContext(v), v1, extraParameters, arb);
+            }
+            if (strictlySmallerValue) {
+              // eslint-disable-next-line no-inner-declarations
+              function isStrictlySmallerInternal(v: T, extraParameters: U) {
+                try {
+                  if (!('value' in previousValue)) {
+                    return true;
+                  }
+                  const vNew = v;
+                  const vOld = previousValue.value!;
+                  try {
+                    const out = strictlySmallerValue!.isStrictlySmaller(vNew, vOld, extraParameters);
+                    expect(out).not.toBe(false);
+                  } catch (err) {
+                    throw new Error(
+                      `Expect: ${fc.stringify(vNew)} to be strictly smaller than ${fc.stringify(
+                        vOld,
+                      )}\n\nGot error: ${err}`,
+                    );
+                  }
+                } finally {
+                  previousValue.value = v;
+                }
+              }
+              assertCorrectness(isStrictlySmallerInternal, v1, extraParameters, arb);
+            }
+
             const pos = shrinkPath.next().value;
-            g = arb.shrink(g.value, g.context).getNthOrLast(pos);
+            g1 = arb.shrink(g1.value_, g1.context).getNthOrLast(pos);
+            g2 = arb.shrink(g2.value_, g2.context).getNthOrLast(pos);
+            g3 = requiresG3 ? arb.shrink(g3!.value_, g3!.context).getNthOrLast(pos) : null;
+            g4 = requiresG4 ? arb.shrink(g4!.value_, g4!.context).getNthOrLast(pos) : null;
           }
-          expect(g).toBe(null);
+          expect(g1).toBe(null);
+          expect(g2).toBe(null);
+          expect(g3).toBe(null);
+          expect(g4).toBe(null);
         },
       )
-      .afterEach(poisoningAfterEach),
-    assertParameters,
-  );
-}
-
-export function assertGenerateEquivalentTo<T, U = never>(
-  arbitraryBuilderA: (extraParameters: U) => Arbitrary<T>,
-  arbitraryBuilderB: (extraParameters: U) => Arbitrary<T>,
-  options: {
-    isEqual?: (v1: T, v2: T, extraParameters: U) => void | boolean;
-    isEqualContext?: (c1: unknown, c2: unknown, extraParameters: U) => void | boolean;
-    extraParameters?: fc.Arbitrary<U>;
-    assertParameters?: fc.Parameters<unknown>;
-  } = {},
-): void {
-  const {
-    isEqual,
-    isEqualContext,
-    extraParameters: extra = fc.constant(undefined as unknown as U) as fc.Arbitrary<U>,
-    assertParameters,
-  } = options;
-  fc.assert(
-    fc
-      .property(fc.integer().noShrink(), biasFactorArbitrary(), extra, (seed, biasFactor, extraParameters) => {
-        // Arrange
-        const arbA = arbitraryBuilderA(extraParameters);
-        const arbB = arbitraryBuilderB(extraParameters);
-
-        // Act
-        const gA = arbA.generate(randomFromSeed(seed), biasFactor);
-        const gB = arbB.generate(randomFromSeed(seed), biasFactor);
-
-        // Assert
-        assertEquality(isEqual, gA.value, gB.value, extraParameters);
-        if (isEqualContext) {
-          assertEquality(isEqualContext, gA.context, gB.context, extraParameters);
-        }
-      })
       .afterEach(poisoningAfterEach),
     assertParameters,
   );
@@ -154,62 +164,6 @@ export function assertGenerateEquivalentTo<T, U = never>(
 // > assertShrinkProducesSameValueGivenSameSeed with option {noInitialContext:true}
 // > assertGenerateProducesCorrectValues with option isCorrect: (v, _, arb) => arb.canShrinkWithoutContext(v)
 // > assertShrinkProducesCorrectValues with option (v, _, arb) => arb.canShrinkWithoutContext(v)
-
-export function assertShrinkProducesSameValueWithoutInitialContext<T, U = never>(
-  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
-  options: {
-    isEqual?: (v1: T, v2: T, extraParameters: U) => void | boolean;
-    extraParameters?: fc.Arbitrary<U>;
-    assertParameters?: fc.Parameters<unknown>;
-  } = {},
-): void {
-  return assertProduceSameValueGivenSameSeed(arbitraryBuilder, { ...options, noInitialContext: true });
-}
-
-export function assertProduceValuesShrinkableWithoutContext<T, U = never>(
-  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
-  options: {
-    extraParameters?: fc.Arbitrary<U>;
-    assertParameters?: fc.Parameters<unknown>;
-  } = {},
-): void {
-  return assertProduceCorrectValues(arbitraryBuilder, (v, _, arb) => arb.canShrinkWithoutContext(v), options);
-}
-
-export function assertShrinkProducesStrictlySmallerValue<T, U = never>(
-  arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
-  isStrictlySmaller: (vNew: T, vOld: T, extraParameters: U) => void | boolean,
-  options: {
-    extraParameters?: fc.Arbitrary<U>;
-    assertParameters?: fc.Parameters<unknown>;
-  } = {},
-): void {
-  const previousValue: { value?: T } = {};
-  function arbitraryBuilderInternal(...args: Parameters<typeof arbitraryBuilder>) {
-    delete previousValue.value;
-    return arbitraryBuilder(...args);
-  }
-  function isStrictlySmallerInternal(v: T, extraParameters: U) {
-    try {
-      if (!('value' in previousValue)) {
-        return true;
-      }
-      const vNew = v;
-      const vOld = previousValue.value!;
-      try {
-        const out = isStrictlySmaller(vNew, vOld, extraParameters);
-        expect(out).not.toBe(false);
-      } catch (err) {
-        throw new Error(
-          `Expect: ${fc.stringify(vNew)} to be strictly smaller than ${fc.stringify(vOld)}\n\nGot error: ${err}`,
-        );
-      }
-    } finally {
-      previousValue.value = v;
-    }
-  }
-  return assertProduceCorrectValues(arbitraryBuilderInternal, isStrictlySmallerInternal, options);
-}
 
 export function assertProduceSomeSpecificValues<T, U = never>(
   arbitraryBuilder: (extraParameters: U) => Arbitrary<T>,
@@ -228,11 +182,18 @@ export function assertProduceSomeSpecificValues<T, U = never>(
     return true; // success of the property
   }
   try {
-    assertProduceCorrectValues(arbitraryBuilder, detectSpecificValue, {
-      ...options,
-      // We default numRuns to 1000, but let user override it whenever needed
-      assertParameters: { numRuns: 1000, ...options.assertParameters, endOnFailure: true },
-    });
+    assertValidArbitrary(
+      arbitraryBuilder,
+      {
+        sameValueGivenSameSeed: { isEqual: () => true }, // check ignored
+        correctValues: { isCorrect: detectSpecificValue },
+      },
+      {
+        ...options,
+        // We default numRuns to 1000, but let user override it whenever needed
+        assertParameters: { numRuns: 1000, ...options.assertParameters, endOnFailure: true },
+      },
+    );
   } catch (err) {
     // no-op
   }
@@ -254,16 +215,22 @@ export function assertGenerateIndependentOfSize<T, U = never>(
     isEqualContext,
     assertParameters,
   } = options;
-  assertGenerateEquivalentTo(
+  assertValidArbitrary(
     (extra) => arbitraryBuilder(extra.requested),
-    (extra) => withConfiguredGlobal(extra.global, () => arbitraryBuilder(extra.requested)),
+    {
+      sameValueGivenSameSeed: { isEqual: () => true }, // check ignored
+      equivalentTo: {
+        otherArbitrary: (extra) => withConfiguredGlobal(extra.global, () => arbitraryBuilder(extra.requested)),
+        isEqual: isEqual !== undefined ? (a, b, extra) => isEqual(a, b, extra.requested) : undefined,
+        isEqualContext:
+          isEqualContext !== undefined ? (a, b, extra) => isEqualContext(a, b, extra.requested) : undefined,
+      },
+    },
     {
       extraParameters: fc.record({
         requested: extraParameters,
         global: fc.record({ defaultSizeToMaxWhenMaxSpecified: fc.boolean(), baseSize: sizeArb }, { requiredKeys: [] }),
       }),
-      isEqual: isEqual !== undefined ? (a, b, extra) => isEqual(a, b, extra.requested) : undefined,
-      isEqualContext: isEqualContext !== undefined ? (a, b, extra) => isEqualContext(a, b, extra.requested) : undefined,
       assertParameters,
     },
   );
