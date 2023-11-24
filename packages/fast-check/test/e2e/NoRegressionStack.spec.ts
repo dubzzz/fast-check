598ebcde7ba8ab475f882d10400c407870e56125
Nicolas DUBIEN <github@dubien.org>
2023-11-24 21:16:08 +0000

    ✅ More stable snapshot tests on stack traces
    We now ignore lines in the snapshots captured in the E2E ensuring we can capture some stacks.

--- packages/fast-check/test/e2e/NoRegressionStack.spec.ts
+++ packages/fast-check/test/e2e/NoRegressionStack.spec.ts
@@ -42,8 +42,8 @@ function sanitize(run: () => void) {
       const initialMessage = (err as Error).message;
       const lines = initialMessage
         .replace(/\\/g, '/')
-        .replace(/at [^(]*fast-check\/(packages|node_modules)(.*)/g, 'at $1$2')
-        .replace(/at (.*) \(.*fast-check\/(packages|node_modules)(.*)\)/g, 'at $1 ($2$3)')
+        .replace(/at [^(]*fast-check\/(packages|node_modules)(.*):\d+:\d+/g, 'at $1$2:?:?') // line for the spec file itself
+        .replace(/at (.*) \(.*fast-check\/(packages|node_modules)(.*):\d+:\d+\)/g, 'at $1 ($2$3:?:?)') // any import linked to internals of fast-check
         .replace(/at (.*) \(.*\/(\.yarn|Yarn)\/.*\/(node_modules\/.*):\d+:\d+\)/g, 'at $1 ($3:?:?)') // reducing risks of changes on bumps: .yarn (Linux and Mac), Yarn (Windows)
         .split('\n');
       throw new Error(
