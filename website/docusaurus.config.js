130567184cf086f4e180b58c51ed82eccba01cb3
Nicolas DUBIEN <github@dubien.org>
2023-11-27 20:57:49 +0000

    format

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const {themes} = require('prism-react-renderer');
+const { themes } = require('prism-react-renderer');
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
 
