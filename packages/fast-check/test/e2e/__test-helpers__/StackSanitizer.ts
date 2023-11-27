48eebf5f80a420ac8af43a66f776f363a2f7e81d
Nicolas DUBIEN <github@dubien.org>
2023-11-27 19:26:41 +0000

    better

--- packages/fast-check/test/e2e/__test-helpers__/StackSanitizer.ts
+++ packages/fast-check/test/e2e/__test-helpers__/StackSanitizer.ts
@@ -14,7 +14,7 @@ function sanitizeStack(initialMessage: string) {
       lines.length - 1 - [...lines].reverse().findIndex((line) => line.includes('node_modules/jest-'));
     lines.splice(firstLineWithJest, lastLineWithJest - firstLineWithJest + 1);
   }
-  return lines.filter((line) => !line.includes('node_modules/jest-circus')).join('\n');
+  return lines.join('\n');
 }
 
 /** Wrap a potentially throwing code within a caller that would sanitize the returned Error */
