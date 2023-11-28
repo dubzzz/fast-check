110814d1e447259ae984c8ff0434fae90f3a6bad
Nicolas DUBIEN <github@dubien.org>
2023-11-28 22:19:41 +0100

    💥 Include invalid dates by default (#4490)
    **💥 Breaking change**
    
    In v3, `fc.date` was not producing "Invalid Date" values by default. One has to specify `noInvalidDate: false` to get some. With this PR we now default the flag to false when not specified: in other words, the new default will be to produce invalid dates except asked explicitly not to.
    
    After that change, if you want not to produce "Invalid Date", you'll have to use:
    
    ```js
    // in v3, it was only producing valid dates
    fc.date()
    
    // equivalent in v4
    fc.date({noInvalidDate: true})
    
    //---
    
    // in v3, including invalid dates required users to pass an extra constraint to the arbitrary
    fc.date({noInvalidDate: false})
    
    // equivalent in v4
    fc.date()
    ```
    
    You can also prepare yourself to v4 by already toggling: `noInvalidDate: false` on your `fc.date`.

--- packages/fast-check/test/unit/arbitrary/_internals/mappers/TimeToDate.spec.ts
+++ packages/fast-check/test/unit/arbitrary/_internals/mappers/TimeToDate.spec.ts
@@ -10,7 +10,7 @@ import {
 describe('timeToDateUnmapper', () => {
   it('should be able to revert any mapped date correctly even invalid ones', () => {
     fc.assert(
-      fc.property(fc.date({ noInvalidDate: false }), (d) => {
+      fc.property(fc.date(), (d) => {
         // Arrange / Act
         const rev = timeToDateUnmapper(d);
         const revRev = timeToDateMapper(rev);
@@ -22,26 +22,22 @@ describe('timeToDateUnmapper', () => {
   });
 });
 
-describe('timeToDateUnmapperWithNane', () => {
+describe('timeToDateUnmapperWithNaN', () => {
   it('should be able to revert any mapped date correctly even invalid once', () => {
     fc.assert(
-      fc.property(
-        fc.date({ noInvalidDate: false }),
-        fc.integer({ min: -8640000000000000, max: 8640000000000001 }),
-        (d, nanValue) => {
-          // Arrange / Act
-          const rev = timeToDateUnmapperWithNaN(nanValue)(d);
-          const revRev = timeToDateMapperWithNaN(nanValue)(rev);
+      fc.property(fc.date(), fc.integer({ min: -8640000000000000, max: 8640000000000001 }), (d, nanValue) => {
+        // Arrange / Act
+        const rev = timeToDateUnmapperWithNaN(nanValue)(d);
+        const revRev = timeToDateMapperWithNaN(nanValue)(rev);
 
-          // Assert
-          if (d.getTime() === nanValue) {
-            expect(rev).toBe(nanValue);
-            expect(revRev.getTime()).toEqual(Number.NaN);
-          } else {
-            expect(revRev.getTime()).toEqual(d.getTime());
-          }
-        },
-      ),
+        // Assert
+        if (d.getTime() === nanValue) {
+          expect(rev).toBe(nanValue);
+          expect(revRev.getTime()).toEqual(Number.NaN);
+        } else {
+          expect(revRev.getTime()).toEqual(d.getTime());
+        }
+      }),
     );
   });
 });
