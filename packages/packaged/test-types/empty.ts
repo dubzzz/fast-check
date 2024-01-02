668a8df30ee33e8121f9c2c9701fa7cbed643e09
Nicolas DUBIEN <github@dubien.org>
2024-01-02 15:21:39 +0000

    placeholder

---
+++ packages/packaged/test-types/empty.ts
@@ -0,0 +1,2 @@
+// Just to avoid empty mv from complaining in CI.
+// Alternatively we could have muted the complaint by passsing some flags to mv but it would have hidden a potentially legit error.
