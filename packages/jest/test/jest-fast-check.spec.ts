4977a7dc4819652099e6738ef768ac0fbf5b947d
Nicolas DUBIEN <github@dubien.org>
2024-01-06 17:55:05 +0100

    🔧(jest) Migrate jest to esm (#4605)

--- packages/jest/test/jest-fast-check.spec.ts
+++ packages/jest/test/jest-fast-check.spec.ts
@@ -1,8 +1,13 @@
 import * as path from 'path';
+import * as url from 'url';
 import { promises as fs } from 'fs';
 import { promisify } from 'util';
 import { execFile as _execFile } from 'child_process';
+import { jest } from '@jest/globals';
+
 const execFile = promisify(_execFile);
+// @ts-expect-error --module must be higher
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 import type _fc from 'fast-check';
 import type { test as _test, it as _it } from '@fast-check/jest';
