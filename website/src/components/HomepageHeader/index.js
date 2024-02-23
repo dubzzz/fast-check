5594206296d4b1222a497537bf3563e0b9517cd5
Nicolas DUBIEN <github@dubien.org>
2024-02-24 00:55:11 +0100

    Update index.js

--- website/src/components/HomepageHeader/index.js
+++ website/src/components/HomepageHeader/index.js
@@ -18,7 +18,7 @@ export default function HomepageHeader() {
           {siteConfig.tagline}
           <div className={styles.taglineBadges}>
             <a href="https://www.npmjs.com/package/fast-check" target="_blank" rel="noopener">
-              <img src="https://badge.fury.io/js/fast-check.svg" alt="npm version" height={20} />
+              <img src="https://badge.fury.io/js/fast-check.svg" alt="npm version" width={133} height={20} />
             </a>
             <a href="https://www.npmjs.com/package/fast-check" target="_blank" rel="noopener">
               <img src="https://img.shields.io/npm/dm/fast-check" alt="monthly downloads" height={20} />
