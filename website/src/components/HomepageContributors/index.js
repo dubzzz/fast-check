aa6e4de52f383f5a4fbfa9c7464868874b38ad38
Nicolas DUBIEN <github@dubien.org>
2024-02-23 09:44:49 +0100

    ⚡️ Optimize image assets on homepage (#4730)
    Our current version of https://fast-check.dev/ is badly performing on https://www.websitecarbon.com/. It got an F for the homagepage but a good grade on others.
    
    Most of the problem comes from our numerous images assets:
    
    - there are many of them to load
    - they are not optimized in size
    
    The current PR is making sure we optimize them beforehand to deliver an optimized version of our assets.

--- website/src/components/HomepageContributors/index.js
+++ website/src/components/HomepageContributors/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BannerSection from '@site/src/components/BannerSection';
 import allContributors from './all-contributors.json';
 import styles from './styles.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function HomepageContributors(props) {
   return (
@@ -19,7 +20,7 @@ export default function HomepageContributors(props) {
             rel="noopener noreferrer"
           >
             <img
-              src={contributor.avatar_url}
+              src={useBaseUrl(`/img/_/avatar_64_${contributor.login}.jpg`)}
               alt={`Avatar for ${contributor.name} (${contributor.login})`}
               width={64}
               height={64}
