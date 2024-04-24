f4bae2f583e56e797fb565beb82fa8d53228976f
Nicolas DUBIEN <github@dubien.org>
2024-04-24 21:08:15 +0000

    oups

--- packages/fast-check/src/arbitrary/_internals/helpers/FloatOnlyHelpers.ts
+++ packages/fast-check/src/arbitrary/_internals/helpers/FloatOnlyHelpers.ts
@@ -1,9 +1,10 @@
 import type { FloatConstraints } from '../../float';
+import { MAX_VALUE_32 } from './FloatHelpers';
 import { refineConstraintsForFloatingOnly } from './FloatingOnlyHelpers';
 
 const safeNegativeInfinity = Number.NEGATIVE_INFINITY;
 const safePositiveInfinity = Number.POSITIVE_INFINITY;
-const safeMaxValue = Number.MAX_VALUE;
+const safeMaxValue = MAX_VALUE_32;
 
 // The last floating point value available with 32 bits floating point numbers is: 8388607.5
 // The start of integers' world is: 8388608 = 2**23 = 2**(significand_size_with_sign-1)
