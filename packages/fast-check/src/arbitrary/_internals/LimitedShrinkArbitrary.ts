7f8fe393bb4f19b948cf9ee46986712102b218a5
Nicolas DUBIEN <github@dubien.org>
2024-06-29 10:07:22 +0000

    rework

--- packages/fast-check/src/arbitrary/_internals/LimitedShrinkArbitrary.ts
+++ packages/fast-check/src/arbitrary/_internals/LimitedShrinkArbitrary.ts
@@ -56,14 +56,43 @@ function* iotaFrom(startValue: number) {
   }
 }
 
+type ZippedIterableIteratorValues<ITs extends IterableIterator<unknown>[]> = {
+  [K in keyof ITs]: ITs[K] extends IterableIterator<infer IT> ? IT : unknown;
+};
+
+type ZippedIterableIterator<ITs extends IterableIterator<unknown>[]> = IterableIterator<
+  ZippedIterableIteratorValues<ITs>
+>;
+
+function initZippedValues<ITs extends IterableIterator<unknown>[]>(its: ITs) {
+  const vs: IteratorResult<unknown, any>[] = [];
+  for (let index = 0; index !== its.length; ++index) {
+    vs.push(its[index].next());
+  }
+  return vs;
+}
+
+function nextZippedValues<ITs extends IterableIterator<unknown>[]>(its: ITs, vs: IteratorResult<unknown, any>[]) {
+  for (let index = 0; index !== its.length; ++index) {
+    vs[index] = its[index].next();
+  }
+}
+
+function isDoneZippedValues(vs: IteratorResult<unknown, any>[]): boolean {
+  for (let index = 0; index !== vs.length; ++index) {
+    if (vs[index].done) {
+      return true;
+    }
+  }
+  return false;
+}
+
 /** @internal */
-function* zip<T, U>(i1: IterableIterator<T>, i2: IterableIterator<U>): IterableIterator<[T, U]> {
-  let v1 = i1.next();
-  let v2 = i2.next();
-  while (!v1.done && !v2.done) {
-    yield [v1.value, v2.value];
-    v1 = i1.next();
-    v2 = i2.next();
+function* zip<ITs extends IterableIterator<unknown>[]>(...its: ITs): ZippedIterableIterator<ITs> {
+  const vs = initZippedValues(its);
+  while (!isDoneZippedValues(vs)) {
+    yield vs.map((v) => v.value) as unknown as ZippedIterableIteratorValues<ITs>;
+    nextZippedValues(its, vs);
   }
 }
 
