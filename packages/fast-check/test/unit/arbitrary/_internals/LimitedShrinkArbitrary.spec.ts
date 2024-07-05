fe6e948665dfc93d2f79eb8975613040bcaa7b90
Nicolas DUBIEN <github@dubien.org>
2024-07-05 10:15:13 +0000

    Add some shrink tests

--- packages/fast-check/test/unit/arbitrary/_internals/LimitedShrinkArbitrary.spec.ts
+++ packages/fast-check/test/unit/arbitrary/_internals/LimitedShrinkArbitrary.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import * as fc from 'fast-check';
 import { Value } from '../../../../src/check/arbitrary/definition/Value';
+import { Stream } from '../../../../src/stream/Stream';
 import { fakeRandom } from '../__test-helpers__/RandomHelpers';
 
 import { declareCleaningHooksForSpies } from '../__test-helpers__/SpyCleaner';
@@ -18,7 +19,7 @@ describe('LimitedShrinkArbitrary', () => {
           fc.anything(),
           fc.option(fc.integer({ min: 2 }), { nil: undefined }),
           fc.nat(),
-          (generatedValue, context, biasFactor, maxDepth) => {
+          (generatedValue, context, biasFactor, maxShrinks) => {
             // Arrange
             const value = new Value(generatedValue, context);
             const { instance: mrng } = fakeRandom();
@@ -26,7 +27,7 @@ describe('LimitedShrinkArbitrary', () => {
             generate.mockReturnValueOnce(value);
 
             // Act
-            const arb = new LimitedShrinkArbitrary(arbitrary, maxDepth);
+            const arb = new LimitedShrinkArbitrary(arbitrary, maxShrinks);
             const out = arb.generate(mrng, biasFactor);
 
             // Assert
@@ -60,7 +61,116 @@ describe('LimitedShrinkArbitrary', () => {
     });
   });
 
-  describe('shrink', () => {});
+  describe('shrink', () => {
+    it('should call shrink of the underlying arbitrary but cut it to maxShrinks when >=1 and receiving its own values of first level', () => {
+      fc.assert(
+        fc.property(
+          fc.anything(),
+          fc.anything(),
+          fc.option(fc.integer({ min: 2 }), { nil: undefined }),
+          fc.integer({ min: 1 }),
+          fc.array(fc.tuple(fc.anything(), fc.anything())),
+          (generatedValue, context, biasFactor, maxShrinks, shrunkValues) => {
+            // Arrange
+            const value = new Value(generatedValue, context);
+            const { instance: mrng } = fakeRandom();
+            const { instance: arbitrary, generate, shrink } = fakeArbitrary();
+            generate.mockReturnValueOnce(value);
+            shrink.mockReturnValueOnce(Stream.of(...shrunkValues.map(([value, context]) => new Value(value, context))));
+
+            // Act
+            const arb = new LimitedShrinkArbitrary(arbitrary, maxShrinks);
+            const g = arb.generate(mrng, biasFactor);
+            const shrinks = arb.shrink(g.value_, g.context);
+
+            // Assert
+            expect(shrink).toHaveBeenCalledTimes(1);
+            expect(shrink).toHaveBeenCalledWith(generatedValue, context);
+            expect([...shrinks].map((s) => s.value_)).toEqual(shrunkValues.map((s) => s[0]).slice(0, maxShrinks)); // Remark: The instance of Value itself might be different
+          },
+        ),
+      );
+    });
+
+    it('should not call shrink of the underlying arbitrary when no shrinks even if receiving its own values', () => {
+      fc.assert(
+        fc.property(
+          fc.anything(),
+          fc.anything(),
+          fc.option(fc.integer({ min: 2 }), { nil: undefined }),
+          (generatedValue, context, biasFactor) => {
+            // Arrange
+            const value = new Value(generatedValue, context);
+            const { instance: mrng } = fakeRandom();
+            const { instance: arbitrary, generate, shrink } = fakeArbitrary();
+            generate.mockReturnValueOnce(value);
+
+            // Act
+            const arb = new LimitedShrinkArbitrary(arbitrary, 0);
+            const g = arb.generate(mrng, biasFactor);
+            const shrinks = arb.shrink(g.value_, g.context);
+
+            // Assert
+            expect(shrink).not.toHaveBeenCalled();
+            expect([...shrinks].map((s) => s.value_)).toHaveLength(0);
+          },
+        ),
+      );
+    });
+
+    it('should cut the shrinker to only produce at most maxShrinks shrinks whatever the shrinking path', () => {
+      fc.assert(
+        fc.property(
+          fc.anything(),
+          fc.anything(),
+          fc.option(fc.integer({ min: 2 }), { nil: undefined }),
+          fc.nat({ max: 50 }), // limiting shrink count to 50 to avoid taking hours
+          fc.infiniteStream(fc.infiniteStream(fc.tuple(fc.anything(), fc.anything()))),
+          fc.infiniteStream(fc.integer({ min: 1 })),
+          fc.context(),
+          (generatedValue, context, biasFactor, maxShrinks, shrunkValuesLevels, shrinkingPath, ctx) => {
+            // Arrange
+            const value = new Value(generatedValue, context);
+            const { instance: mrng } = fakeRandom();
+            const { instance: arbitrary, generate, shrink } = fakeArbitrary();
+            generate.mockReturnValueOnce(value);
+            shrink.mockImplementation((): Stream<Value<any>> => {
+              const itShrinks = shrunkValuesLevels.next();
+              if (itShrinks.done) {
+                throw new Error('Not possible, only added for typings');
+              }
+              return new Stream(itShrinks.value.map(([value, context]) => new Value(value, context)));
+            });
+
+            // Act / Assert
+            const arb = new LimitedShrinkArbitrary(arbitrary, maxShrinks);
+            const g = arb.generate(mrng, biasFactor);
+            let done = false;
+            let shrinkCount = 0;
+            let lastValue = g;
+            while (!done) {
+              // Shrinking the value until we reach an end
+              const shrinkLength: number = shrinkingPath.next().value;
+              ctx.log('shrinking for length=' + shrinkLength);
+              const shrinks = arb.shrink(lastValue.value_, lastValue.context);
+              for (let i = 0; i !== shrinkLength; ++i) {
+                ctx.log('at i=' + i);
+                const nextShrink = shrinks.next();
+                if (nextShrink.done) {
+                  ctx.log('stopped');
+                  done = true;
+                  break;
+                }
+                ++shrinkCount;
+                lastValue = nextShrink.value;
+              }
+            }
+            expect(shrinkCount).toBe(maxShrinks); // exact as our underlying arbitrary is shrinking forever
+          },
+        ),
+      );
+    });
+  });
 });
 
 // TODO: As this Arbitrary is playing with mapping passed values we have to confirm it behaves well for cloneable values
