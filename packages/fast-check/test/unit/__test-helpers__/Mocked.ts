b36143b19c9e86c31b51a3ac9f87d3ee5709048b
Nicolas DUBIEN <github@dubien.org>
2024-07-09 07:26:07 +0000

    Fix a few more

--- packages/fast-check/test/unit/__test-helpers__/Mocked.ts
+++ packages/fast-check/test/unit/__test-helpers__/Mocked.ts
@@ -5,7 +5,8 @@ type MockableFunction = (...args: any[]) => any;
 type ArgumentsOf<T> = T extends (...args: infer A) => any ? A : never;
 type ConstructorArgumentsOf<T> = T extends new (...args: infer A) => any ? A : never;
 
-export interface MockWithArgs<T extends MockableFunction> extends MockInstance<T> {
+export interface MockWithArgs<T extends MockableFunction>
+  extends MockInstance<(...args: ArgumentsOf<T>) => ReturnType<T>> {
   new (...args: ConstructorArgumentsOf<T>): T;
   (...args: ArgumentsOf<T>): ReturnType<T>;
 }
