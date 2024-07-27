9dc97da4338db45469ecaacd566b37be70e838f3
Nicolas DUBIEN <github@dubien.org>
2024-07-27 08:42:24 +0200

    ✨ Support UUID versions [1-15] on `uuidV`

--- packages/fast-check/src/arbitrary/uuidV.ts
+++ packages/fast-check/src/arbitrary/uuidV.ts
@@ -4,16 +4,16 @@ import { buildPaddedNumberArbitrary } from './_internals/builders/PaddedNumberAr
 import { paddedEightsToUuidMapper, paddedEightsToUuidUnmapper } from './_internals/mappers/PaddedEightsToUuid';
 
 /**
- * For UUID of a given version (in v1 to v5)
+ * For UUID of a given version (in v1 to v15)
  *
- * According to {@link https://tools.ietf.org/html/rfc4122 | RFC 4122}
+ * According to {@link https://tools.ietf.org/html/rfc4122 | RFC 4122} and {@link https://datatracker.ietf.org/doc/html/rfc9562#name-version-field | RFC 9562} any version between 1 and 15 is valid even if only the ones from 1 to 8 have really been leveraged for now.
  *
  * No mixed case, only lower case digits (0-9a-f)
  *
  * @remarks Since 1.17.0
  * @public
  */
-export function uuidV(versionNumber: 1 | 2 | 3 | 4 | 5): Arbitrary<string> {
+export function uuidV(versionNumber: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15): Arbitrary<string> {
   const padded = buildPaddedNumberArbitrary(0, 0xffffffff);
   const offsetSecond = versionNumber * 0x10000000;
   const secondPadded = buildPaddedNumberArbitrary(offsetSecond, offsetSecond + 0x0fffffff);
