9a3c46460276b6c9645f48d1cd4ce2d82ea3704c
Nicolas DUBIEN <github@dubien.org>
2024-08-06 23:15:29 +0000

    fix wrapper

--- packages/fast-check/src/check/model/commands/CommandWrapper.ts
+++ packages/fast-check/src/check/model/commands/CommandWrapper.ts
@@ -3,6 +3,8 @@ import {
   hasAsyncToStringMethod,
   hasToStringMethod,
   toStringMethod,
+  WithAsyncToStringMethod,
+  WithToStringMethod,
 } from '../../../utils/stringify';
 import { cloneMethod, hasCloneMethod } from '../../symbols';
 import type { ICommand } from '../command/ICommand';
@@ -14,22 +16,20 @@ import type { ICommand } from '../command/ICommand';
 export class CommandWrapper<Model extends object, Real, RunResult, CheckAsync extends boolean>
   implements ICommand<Model, Real, RunResult, CheckAsync>
 {
-  [toStringMethod]?: () => string;
-  [asyncToStringMethod]?: () => Promise<string>;
-
   hasRan = false;
   constructor(readonly cmd: ICommand<Model, Real, RunResult, CheckAsync>) {
     if (hasToStringMethod(cmd)) {
       const method = cmd[toStringMethod];
-      this[toStringMethod] = function toStringMethod(): string {
+      (this as unknown as WithToStringMethod)[toStringMethod] = function toStringMethod(): string {
         return method.call(cmd);
       };
     }
     if (hasAsyncToStringMethod(cmd)) {
       const method = cmd[asyncToStringMethod];
-      this[asyncToStringMethod] = function asyncToStringMethod(): Promise<string> {
-        return method.call(cmd);
-      };
+      (this as unknown as WithAsyncToStringMethod)[asyncToStringMethod] =
+        function asyncToStringMethod(): Promise<string> {
+          return method.call(cmd);
+        };
     }
   }
   check(m: Readonly<Model>): CheckAsync extends false ? boolean : Promise<boolean> {
