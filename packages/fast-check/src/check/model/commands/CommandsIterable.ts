c41a10de55b3a8a988025c8d01a5ef2edad2d50e
Nicolas DUBIEN <github@dubien.org>
2024-08-07 08:01:46 +0000

    trick to pass isolatedDeclarations on unpublished

--- packages/fast-check/src/check/model/commands/CommandsIterable.ts
+++ packages/fast-check/src/check/model/commands/CommandsIterable.ts
@@ -1,4 +1,4 @@
-import { cloneMethod } from '../../symbols';
+import { cloneMethod, WithCloneMethod } from '../../symbols';
 import type { CommandWrapper } from './CommandWrapper';
 
 /**
@@ -14,12 +14,6 @@ export class CommandsIterable<Model extends object, Real, RunResult, CheckAsync 
   [Symbol.iterator](): Iterator<CommandWrapper<Model, Real, RunResult, CheckAsync>> {
     return this.commands[Symbol.iterator]();
   }
-  [cloneMethod](): CommandsIterable<Model, Real, RunResult, CheckAsync> {
-    return new CommandsIterable(
-      this.commands.map((c) => c.clone()),
-      this.metadataForReplay,
-    );
-  }
   toString(): string {
     const serializedCommands = this.commands
       .filter((c) => c.hasRan)
@@ -29,3 +23,17 @@ export class CommandsIterable<Model extends object, Real, RunResult, CheckAsync 
     return metadata.length !== 0 ? `${serializedCommands} /*${metadata}*/` : serializedCommands;
   }
 }
+
+/** @internal */
+type CommandsIterableType = (typeof CommandsIterable)['prototype'];
+
+// Extending "CommandsIterable.prototype" from the outside to pass isolatedDeclarations checks
+(CommandsIterable.prototype as unknown as CommandsIterableType & WithCloneMethod<CommandsIterableType>)[cloneMethod] =
+  function <Model extends object, Real, RunResult, CheckAsync extends boolean>(
+    this: CommandsIterable<Model, Real, RunResult, CheckAsync>,
+  ): CommandsIterable<Model, Real, RunResult, CheckAsync> {
+    return new CommandsIterable(
+      this.commands.map((c) => c.clone()),
+      this.metadataForReplay,
+    );
+  };
