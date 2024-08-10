c2cf4326eb10638dbec70f5af71217ff7508b689
Nicolas DUBIEN <github@dubien.org>
2024-08-10 11:11:31 +0200

    ♻️(vitest) Add missing types on exported (#5200)
    **Description**
    
    <!-- Please provide a short description and potentially linked issues
    justifying the need for this PR -->
    
    In order to toggle ON the flag `isolatedDeclarations` and allow us to
    use something else than tsc to build our project (not sure we will do
    one day), we start to make some types more explicit on our exported
    symbols.
    
    <!-- * Your PR is fixing a bug or regression? Check for existing issues
    related to this bug and link them -->
    <!-- * Your PR is adding a new feature? Make sure there is a related
    issue or discussion attached to it -->
    
    <!-- You can provide any additional context to help into understanding
    what's this PR is attempting to solve: reproduction of a bug, code
    snippets... -->
    
    **Checklist** — _Don't delete this checklist and make sure you do the
    following before opening the PR_
    
    - [x] The name of my PR follows [gitmoji](https://gitmoji.dev/)
    specification
    - [x] My PR references one of several related issues (if any)
    - [x] New features or breaking changes must come with an associated
    Issue or Discussion
    - [x] My PR does not add any new dependency without an associated Issue
    or Discussion
    - [x] My PR includes bumps details, please run `yarn bump` and flag the
    impacts properly
    - [x] My PR adds relevant tests and they would have failed without my PR
    (when applicable)
    
    <!-- More about contributing at
    https://github.com/dubzzz/fast-check/blob/main/CONTRIBUTING.md -->
    
    **Advanced**
    
    <!-- How to fill the advanced section is detailed below! -->
    
    - [x] Category: ♻️ Refactoring
    - [x] Impacts: None
    
    <!-- [Category] Please use one of the categories below, it will help us
    into better understanding the urgency of the PR -->
    <!-- * ✨ Introduce new features -->
    <!-- * 📝 Add or update documentation -->
    <!-- * ✅ Add or update tests -->
    <!-- * 🐛 Fix a bug -->
    <!-- * 🏷️ Add or update types -->
    <!-- * ⚡️ Improve performance -->
    <!-- * _Other(s):_ ... -->
    
    <!-- [Impacts] Please provide a comma separated list of the potential
    impacts that might be introduced by this change -->
    <!-- * Generated values: Can your change impact any of the existing
    generators in terms of generated values, if so which ones? when? -->
    <!-- * Shrink values: Can your change impact any of the existing
    generators in terms of shrink values, if so which ones? when? -->
    <!-- * Performance: Can it require some typings changes on user side?
    Please give more details -->
    <!-- * Typings: Is there a potential performance impact? In which cases?
    -->

--- packages/vitest/src/internals/TestBuilder.ts
+++ packages/vitest/src/internals/TestBuilder.ts
@@ -137,7 +137,7 @@ export type FastCheckItBuilder<T> = T &
 /**
  * Build the enriched version of {it,test}, the one with added `.prop`
  */
-function enrichWithTestProp<T extends (...args: any[]) => any>(
+export function buildTest<T extends (...args: any[]) => any>(
   testFn: T,
   fc: FcExtra,
   ancestors: Set<string> = new Set(),
@@ -148,8 +148,7 @@ function enrichWithTestProp<T extends (...args: any[]) => any>(
     const key = unsafeKey as keyof typeof testFn & string;
     if (!ancestors.has(key) && typeof testFn[key] === 'function') {
       atLeastOneExtra = true;
-      extraKeys[key] =
-        key !== 'each' ? enrichWithTestProp(testFn[key] as any, fc, new Set([...ancestors, key])) : testFn[key];
+      extraKeys[key] = key !== 'each' ? buildTest(testFn[key] as any, fc, new Set([...ancestors, key])) : testFn[key];
     }
   }
   if (!atLeastOneExtra) {
@@ -161,5 +160,3 @@ function enrichWithTestProp<T extends (...args: any[]) => any>(
   }
   return Object.assign(enrichedTestFn, extraKeys) as FastCheckItBuilder<T>;
 }
-
-export const buildTest = enrichWithTestProp;
