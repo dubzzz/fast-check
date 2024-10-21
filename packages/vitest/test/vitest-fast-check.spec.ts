5d4f146f85e9330765c95f57c5ed07d1711203ec
Nicolas DUBIEN <github@dubien.org>
2024-10-22 00:43:55 +0200

    Update packages/vitest/test/vitest-fast-check.spec.ts

--- packages/vitest/test/vitest-fast-check.spec.ts
+++ packages/vitest/test/vitest-fast-check.spec.ts
@@ -3,7 +3,7 @@ import * as url from 'url';
 import { promises as fs } from 'fs';
 import { promisify } from 'util';
 import { execFile as _execFile } from 'child_process';
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 const execFile = promisify(_execFile);
 // @ts-expect-error --module must be higher
