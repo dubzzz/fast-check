53757bbc5d74a6a674a600432e73247123d213f6
Nicolas DUBIEN <github@dubien.org>
2024-10-29 08:22:28 +0000

    workaround v23 of node for jest tests

--- packages/jest/vitest.config.ts
+++ packages/jest/vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
+const major = Number(process.versions.node.split('.')[0]);
+
 export default defineConfig({
   test: {
-    testTimeout: 120000, // 120s
+    testTimeout: 60000, // 60s
     include: ['**/test/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    retry: major === 23 ? 3 : undefined,
   },
 });
