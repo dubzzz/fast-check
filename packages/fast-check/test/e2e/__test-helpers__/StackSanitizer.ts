b975e634e3fa41844d722895a8bcc24f15d8c7ef
Nicolas DUBIEN <github@dubien.org>
2024-11-06 20:56:36 +0000

    FIX ⚡️ Delay computation of Error stack when no cause (#4472)

--- packages/fast-check/test/e2e/__test-helpers__/StackSanitizer.ts
+++ packages/fast-check/test/e2e/__test-helpers__/StackSanitizer.ts
@@ -6,7 +6,7 @@ function sanitizeStack(initialMessage: string) {
     .replace(/at [^(]*fast-check\/(packages|node_modules)(.*):\d+:\d+/g, 'at $1$2:?:?') // line for the spec file itself
     .replace(/at (.*) \(.*fast-check\/(packages|node_modules)(.*):\d+:\d+\)/g, 'at $1 ($2$3:?:?)') // any import linked to internals of fast-check
     .replace(/at (.*) \(.*\/(\.yarn|Yarn)\/.*\/(node_modules\/.*):\d+:\d+\)/g, 'at $1 ($3:?:?)') // reducing risks of changes on bumps: .yarn (Linux and Mac), Yarn (Windows)
-    .replace(/at file:\/\/\/.*\/\.yarn\/.*\/(node_modules\/.*):\d+:\d+/g, 'at $1:?:?') // reducing risks of changes on bumps: .yarn (Linux and Mac), Yarn (Windows)
+    .replace(/at file:\/\/\/.*\/(\.yarn|Yarn)\/.*\/(node_modules\/.*):\d+:\d+/g, 'at $2:?:?') // reducing risks of changes on bumps: .yarn (Linux and Mac), Yarn (Windows)
     .split('\n');
   // Drop internals of Vitest from the stack: internals of vitest, subject to regular changes and OS dependent
   const firstLineWithVitest = lines.findIndex((line) => line.includes('node_modules/vitest'));
