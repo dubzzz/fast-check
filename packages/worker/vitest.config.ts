4cd481a1882f4cc055d42ffa8beab451b705d9d6
Nicolas DUBIEN <github@dubien.org>
2025-02-22 19:12:04 +0000

    drop name unit for worker

--- packages/worker/vitest.config.ts
+++ packages/worker/vitest.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 // @ts-expect-error - We will fix that one by bumping Vitest to v3
 const config: UserConfig = defineConfig({
-  test: {
-    name: 'unit',
-  },
+  test: {},
 });
 export default config;
