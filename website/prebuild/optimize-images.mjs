f5471a7a917139537ce6344e6469b20705dbcc7c
Nicolas DUBIEN <github@dubien.org>
2025-04-26 20:37:03 +0000

    sanitize

--- website/prebuild/optimize-images.mjs
+++ website/prebuild/optimize-images.mjs
@@ -4,7 +4,7 @@ import { existsSync, mkdirSync } from 'fs';
 import { readFile, writeFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
 import path, { join } from 'path';
-import { globSync } from 'glob';
+import { glob } from 'glob';
 import allContributors from '../src/components/HomepageContributors/all-contributors.json' with { type: 'json' };
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
@@ -15,57 +15,66 @@ async function collectAvatar(imageUrl, imageFinalPath, squaredSize) {
   const image = await Jimp.read(imageUrl);
   await image.resize({ h: squaredSize, w: squaredSize }).write(imageFinalPath, { quality: 80 });
 }
-
-const quotes = [
-  'https://github.com/TomerAberbach.png',
-  'https://github.com/ssalbdivad.png',
-  'https://github.com/abrgr.png',
-  'https://github.com/emilianbold.png',
-  'https://github.com/jakebailey.png',
-  'https://github.com/Andarist.png',
-];
-const authorsOfPosts = ['https://github.com/dubzzz.png'];
-const allAvatars = [
-  ...allContributors.contributors.map((contributor) => ({
-    url: contributor.avatar_url,
-    login: contributor.login,
-    size: 64,
-  })),
-  ...quotes.map((quote) => ({
-    url: quote,
-    login: quote.split('/').at(-1).split('.').slice(0, -1).join('.'),
-    size: 64,
-  })),
-  ...authorsOfPosts.map((quote) => ({
-    url: quote,
-    login: quote.split('/').at(-1).split('.').slice(0, -1).join('.'),
-    size: 48,
-  })),
-];
-
-const pathFinalAvatarDirectory = join(__dirname, '..', 'static', 'img', '_');
-for (const avatar of allAvatars) {
-  const { url, login, size } = avatar;
-  const pathFinalImage = join(pathFinalAvatarDirectory, `avatar_${size}_${login}.jpg`);
-  if (!existsSync(pathFinalImage)) {
-    console.log(`Importing avatar ${size}x${size} for ${url}`);
-    mkdirSync(pathFinalAvatarDirectory, { recursive: true });
-    collectAvatar(url, pathFinalImage, 64);
-  } else {
-    console.log(`Skipped import of avatar ${size}x${size} for ${url}`);
-  }
+async function syncAvatars() {
+  const quotes = [
+    'https://github.com/TomerAberbach.png',
+    'https://github.com/ssalbdivad.png',
+    'https://github.com/abrgr.png',
+    'https://github.com/emilianbold.png',
+    'https://github.com/jakebailey.png',
+    'https://github.com/Andarist.png',
+  ];
+  const authorsOfPosts = ['https://github.com/dubzzz.png'];
+  const allAvatars = [
+    ...allContributors.contributors.map((contributor) => ({
+      url: contributor.avatar_url,
+      login: contributor.login,
+      size: 64,
+    })),
+    ...quotes.map((quote) => ({
+      url: quote,
+      login: quote.split('/').at(-1).split('.').slice(0, -1).join('.'),
+      size: 64,
+    })),
+    ...authorsOfPosts.map((quote) => ({
+      url: quote,
+      login: quote.split('/').at(-1).split('.').slice(0, -1).join('.'),
+      size: 48,
+    })),
+  ];
+  const pathFinalAvatarDirectory = join(__dirname, '..', 'static', 'img', '_');
+  const pendingRequests = allAvatars.map(async (avatar) => {
+    const { url, login, size } = avatar;
+    const pathFinalImage = join(pathFinalAvatarDirectory, `avatar_${size}_${login}.jpg`);
+    if (!existsSync(pathFinalImage)) {
+      console.log(`Importing avatar ${size}x${size} for ${url}`);
+      mkdirSync(pathFinalAvatarDirectory, { recursive: true });
+      await collectAvatar(url, pathFinalImage, 64);
+    } else {
+      console.log(`Skipped import of avatar ${size}x${size} for ${url}`);
+    }
+  });
+  await Promise.all(pendingRequests);
 }
+syncAvatars();
 
 // Collecting out-sourced STATIC ASSETS
 
-async function syncPngImages() {
+async function collectAsset(assetName, resultingFilePath) {
+  const response = await fetch(`https://github.com/dubzzz/fast-check-assets/blob/main/${assetName}?raw=true`);
+  const bytes = await response.bytes();
+  await writeFile(resultingFilePath, bytes);
+}
+async function syncStaticAssets() {
   const imageMatcherRegex = /@site\/static\/img\/([A-Za-z0-9./-]+\.(png|gif))/g;
   const pathFinalImageDirectory = join(__dirname, '..', 'static', 'img');
   const pendingImages = new Map();
-  const pendingScans = globSync(`./{blog,docs,src}/**/*.{js,ts,jsx,tsx,md}`, {
-    withFileTypes: true,
-    nodir: true,
-  }).map(async (fileDescriptor) => {
+  const pendingScans = (
+    await glob(`./{blog,docs,src}/**/*.{js,ts,jsx,tsx,md}`, {
+      withFileTypes: true,
+      nodir: true,
+    })
+  ).map(async (fileDescriptor) => {
     const fileContentBuffer = await readFile(fileDescriptor.fullpath());
     const fileContent = fileContentBuffer.toString();
     for (const m of fileContent.matchAll(imageMatcherRegex)) {
@@ -78,12 +87,7 @@ async function syncPngImages() {
         } else {
           console.log(`Importing image for ${m[1]}`);
           mkdirSync(resultingFileDirectoryPath, { recursive: true });
-          pendingImages.set(
-            m[1],
-            fetch(`https://github.com/dubzzz/fast-check-assets/blob/main/${m[1]}?raw=true`)
-              .then((response) => response.bytes())
-              .then((bytes) => writeFile(resultingFilePath, bytes)),
-          );
+          pendingImages.set(m[1], collectAsset(m[1], resultingFilePath));
         }
       }
     }
@@ -91,4 +95,4 @@ async function syncPngImages() {
   await Promise.all(pendingScans);
   await Promise.all([...pendingImages.values()]);
 }
-syncPngImages();
+syncStaticAssets();
