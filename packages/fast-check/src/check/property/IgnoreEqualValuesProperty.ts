de67aa1437cc6db0240730abc06ce1e3e88bf3a7
Nicolas DUBIEN <github@dubien.org>
2026-01-21 18:36:45 +0100

    💥 Allow async properties to behave sync (#6473)
    **Description**
    
    **💥 Minor breaking change:** Most of our users will not be impacted.
    Only impacted users will be the ones playing deeply within fast-check
    and trying to implement their own instances of IRawProperty.
    
    Acting synchronously is way faster than jumping from await to await. As
    such we want our async properties to offer (when implementation wants to
    support it) a fast path with sync returned value.
    
    This PR allows async properties to behave synchronously for run and for
    before and afterEach hooks. In this PR we don't make the async
    properties natively offered within fast-check faster as we only touch
    type definitions but such performance uplift will come next.
    
    Related to experiments started on #6455.
    
    <!-- Please provide a short description and potentially linked issues
    justifying the need for this PR -->
    
    <!-- * Your PR is fixing a bug or regression? Check for existing issues
    related to this bug and link them -->
    <!-- * Your PR is adding a new feature? Make sure there is a related
    issue or discussion attached to it -->
    
    <!-- You can provide any additional context to help into understanding
    what's this PR is attempting to solve: reproduction of a bug, code
    snippets... -->
    
    **Checklist** — _Don't delete this checklist and make sure you do the
    following before opening the PR_
    
    - [x] The name of my PR follows [gitmoji](https://gitmoji.dev/)
    specification
    - [x] My PR references one of several related issues (if any)
    - [x] New features or breaking changes must come with an associated
    Issue or Discussion
    - [x] My PR does not add any new dependency without an associated Issue
    or Discussion
    - [x] My PR includes bumps details, please run `pnpm run bump` and flag
    the impacts properly
    - [x] My PR adds relevant tests and they would have failed without my PR
    (when applicable)
    
    <!-- More about contributing at
    https://github.com/dubzzz/fast-check/blob/main/CONTRIBUTING.md -->
    
    **Advanced**
    
    <!-- How to fill the advanced section is detailed below! -->
    
    - [x] Category: 💥 Breaking change
    - [x] Impacts: Limited! Most of our users will not be impacted. Only
    impacted users will be the ones playing deeply within fast-check and
    trying to implement their own instances of IRawProperty.
    
    <!-- [Category] Please use one of the categories below, it will help us
    into better understanding the urgency of the PR -->
    <!-- * ✨ Introduce new features -->
    <!-- * 📝 Add or update documentation -->
    <!-- * ✅ Add or update tests -->
    <!-- * 🐛 Fix a bug -->
    <!-- * 🏷️ Add or update types -->
    <!-- * ⚡️ Improve performance -->
    <!-- * _Other(s):_ ... -->
    
    <!-- [Impacts] Please provide a comma separated list of the potential
    impacts that might be introduced by this change -->
    <!-- * Generated values: Can your change impact any of the existing
    generators in terms of generated values, if so which ones? when? -->
    <!-- * Shrink values: Can your change impact any of the existing
    generators in terms of shrink values, if so which ones? when? -->
    <!-- * Performance: Can it require some typings changes on user side?
    Please give more details -->
    <!-- * Typings: Is there a potential performance impact? In which cases?
    -->

--- packages/fast-check/src/check/property/IgnoreEqualValuesProperty.ts
+++ packages/fast-check/src/check/property/IgnoreEqualValuesProperty.ts
@@ -25,7 +25,10 @@ function fromCached<Ts>(
 function fromCached<Ts>(
   ...data: [ReturnType<IRawProperty<Ts, true>['run']>, true] | [ReturnType<IRawProperty<Ts, false>['run']>, false]
 ) {
-  if (data[1]) return data[0].then(fromSyncCached);
+  // if not async
+  if (!data[1]) return fromSyncCached(data[0]);
+  // if potentially async, check if really async
+  if (data[0] !== null && typeof data[0] === 'object' && 'then' in data[0]) return data[0].then(fromSyncCached);
   return fromSyncCached(data[0]);
 }
 
