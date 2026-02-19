1b392d856e2af77edab59c36898dc62f7e97ea04
Nicolas DUBIEN <github@dubien.org>
2026-02-19 20:55:55 +0000

    💥(worker) Drop CommonJS support

--- packages/jest/test-bundle/jest.config.cjs
+++ packages/jest/test-bundle/jest.config.cjs
@@ -1,4 +1,8 @@
 module.exports = {
   testMatch: ['<rootDir>/*.spec.cjs'],
   testTimeout: 20_000,
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(?:@fast-check/worker)/)'],
 };
