3669e42f95eab209103b38df70ce58aad682bc54
Nicolas DUBIEN <github@dubien.org>
2026-07-18 05:53:43 +0000

    fix test bug

--- packages/fast-check/test/unit/check/property/__test-helpers__/PropertyHelpers.ts
+++ packages/fast-check/test/unit/check/property/__test-helpers__/PropertyHelpers.ts
@@ -1,16 +1,20 @@
 import { vi } from 'vitest';
-import type { MaybeMocked } from '../../../__test-helpers__/Mocked.js';
+import type { MockInstance } from 'vitest';
 import type { Property } from '../../../../../src/check/property/types/Property.js';
+import { Stream } from '../../../../../src/stream/Stream.js';
+import { Value } from '../../../../../src/check/arbitrary/definition/Value.js';
 
 /**
  * Generate a fake instance inheriting from IProperty with all methods being mocked
  */
-export function fakeProperty<T = unknown>(): { instance: Property<T> } & MaybeMocked<Required<Property<T>>> {
-  const generate = vi.fn();
-  const shrink = vi.fn();
-  const runBeforeEach = vi.fn();
-  const runAfterEach = vi.fn();
-  const run = vi.fn();
+export function fakeProperty<T = unknown>(): { instance: Property<T> } & {
+  [K in keyof Property<T>]: MockInstance<Property<T>[K]>;
+} {
+  const generate = vi.fn<Property<T>['generate']>(() => new Value(Symbol() as unknown as T, undefined));
+  const shrink = vi.fn<Property<T>['shrink']>(() => Stream.nil());
+  const runBeforeEach = vi.fn<Property<T>['runBeforeEach']>(() => undefined);
+  const runAfterEach = vi.fn<Property<T>['runAfterEach']>(() => undefined);
+  const run = vi.fn<Property<T>['run']>(() => null);
   class MyProperty implements Property<unknown> {
     generate = generate;
     shrink = shrink;
