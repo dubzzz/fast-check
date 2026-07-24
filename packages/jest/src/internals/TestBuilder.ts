7f1ea64a2a9176173ff69aff471c8ef9e882a6f9
Nicolas DUBIEN <github@dubien.org>
2026-07-24 16:28:57 +0000

    fix vitest/jest types

--- packages/jest/src/internals/TestBuilder.ts
+++ packages/jest/src/internals/TestBuilder.ts
@@ -24,12 +24,15 @@ function adaptParametersForRecord<Ts>(
   parameters: FcParameters<[Ts]>,
   originalParamaters: FcParameters<Ts>,
 ): FcParameters<Ts> {
-  return {
+  const enrichedParameters: FcParameters<Ts> & {
+    asyncReporter: ((runDetails: RunDetails<Ts>) => Promise<void>) | undefined;
+  } = {
     ...(parameters as Required<FcParameters<[Ts]>>),
     examples: parameters.examples !== undefined ? parameters.examples.map((example) => example[0]) : undefined,
     reporter: originalParamaters.reporter,
-    asyncReporter: originalParamaters.asyncReporter,
+    asyncReporter: (originalParamaters as any).asyncReporter,
   };
+  return enrichedParameters;
 }
 
 function adaptExecutionTreeForRecord<Ts>(executionSummary: ExecutionTree<[Ts]>[]): ExecutionTree<Ts>[] {
@@ -80,7 +83,9 @@ function buildTestProp<Ts extends [any] | any[], TsParameters extends Ts = Ts>(
     }
     return (testName: string, prop: Prop<Ts>, timeout?: number) => {
       const recordArb = record<Ts>(arbitraries);
-      const recordParams: FcParameters<[TsParameters]> | undefined =
+      const recordParams:
+        | (FcParameters<[TsParameters]> & { asyncReporter?: (runDetails: RunDetails<[TsParameters]>) => Promise<void> })
+        | undefined =
         params !== undefined
           ? {
               // Spreading a "Required" makes us sure that we don't miss any parameters
@@ -94,9 +99,9 @@ function buildTestProp<Ts extends [any] | any[], TsParameters extends Ts = Ts>(
                     (runDetails) => params.reporter!(adaptRunDetailsForRecord(runDetails, params))
                   : undefined,
               asyncReporter:
-                params.asyncReporter !== undefined
+                (params as any).asyncReporter !== undefined
                   ? // oxlint-disable-next-line typescript/no-non-null-assertion
-                    (runDetails) => params.asyncReporter!(adaptRunDetailsForRecord(runDetails, params))
+                    (runDetails) => (params as any).asyncReporter!(adaptRunDetailsForRecord(runDetails, params))
                   : undefined,
             }
           : undefined;
