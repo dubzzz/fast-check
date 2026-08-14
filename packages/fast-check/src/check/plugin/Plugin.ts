8e1c4106e312878a8b0f3a7224ce5e3a807e49fd
Nicolas DUBIEN <github@dubien.org>
2026-08-14 14:09:36 +0000

    rename context

--- packages/fast-check/src/check/plugin/Plugin.ts
+++ packages/fast-check/src/check/plugin/Plugin.ts
@@ -15,11 +15,11 @@ export type PluginInstance<Ts, IsAsync extends boolean> = {
 
 // The Plugin is a builder function called when the assert/check execution flows starts
 // it is responsible to instatiate the plugin at init time
-// The variable called sharedSessionContext will be passed to all Plugins being instantiated for this assert/chekc flow.
+// The variable called crossPluginContext will be passed to all Plugins being instantiated for this assert/chekc flow.
 // This variable is only read by plugins and share with all of them. It can be used to share details between two instances of plugins
 // evolving in the same assert/check. As such we can envision leveraging it to merge all instanbces of a given plugin into a single
 // wrapper. Eg.: beforeEach(beforeEach(beforeEach(...))) could be beforeEachs(...) withg the first (or last) instance of the plugin stacking all the others.
-export type Plugin<Ts, IsAsync extends boolean> = (sharedSessionContext: { [K in any]?: unknown }) => PluginInstance<
+export type Plugin<Ts, IsAsync extends boolean> = (crossPluginContext: { [K in any]?: unknown }) => PluginInstance<
   Ts,
   IsAsync
 >;
@@ -27,13 +27,13 @@ export type Plugin<Ts, IsAsync extends boolean> = (sharedSessionContext: { [K in
 // Eg for beforeEach
 const beforeEachPluginSymbol = Symbol();
 export function beforeEachPlugin<Ts>(fn: () => void): Plugin<Ts, false> {
-  return function createPluginInstance(sharedSessionContext: { [K in any]?: unknown }): PluginInstance<Ts, false> {
-    if (beforeEachPluginSymbol in sharedSessionContext) {
-      const otherInstancesOfPlugin = sharedSessionContext[beforeEachPluginSymbol] as (() => void)[];
+  return function createPluginInstance(crossPluginContext: { [K in any]?: unknown }): PluginInstance<Ts, false> {
+    if (beforeEachPluginSymbol in crossPluginContext) {
+      const otherInstancesOfPlugin = crossPluginContext[beforeEachPluginSymbol] as (() => void)[];
       otherInstancesOfPlugin.push(fn);
       return {}; // no runtime for this instance, delegated to the first instance
     }
-    sharedSessionContext[beforeEachPluginSymbol] = [fn];
+    crossPluginContext[beforeEachPluginSymbol] = [fn];
     return {
       decorateRun: (nestedRun) => (value) => {
         fn();
