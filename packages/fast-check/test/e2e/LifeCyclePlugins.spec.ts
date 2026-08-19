6984945a1111c59655232f4e0aa1567a49d6280d
Nicolas DUBIEN <github@dubien.org>
2026-08-19 13:22:03 +0000

    fix e2e

--- packages/fast-check/test/e2e/LifeCyclePlugins.spec.ts
+++ packages/fast-check/test/e2e/LifeCyclePlugins.spec.ts
@@ -53,4 +53,79 @@ describe(`LifeCyclePlugins (seed: ${seed})`, () => {
       'afterEach #3',
     ]);
   });
+
+  it('should integrate properly with plugins deeply changing the execution flow', async () => {
+    // Arrange
+    const probes: string[] = [];
+    const retryTwice = (): fc.Plugin<unknown> => {
+      return () => ({
+        decorateRun: (nestedRun) => (value) => {
+          const out = nestedRun(value);
+          if (out === null) {
+            return nestedRun(value);
+          }
+          if ('then' in out) {
+            return out.then((result) => (result === null ? nestedRun(value) : result));
+          }
+          return out;
+        },
+      });
+    };
+
+    // Act
+    await fc.assert(
+      fc.asyncProperty(fc.integer(), async (_x) => {
+        probes.push('predicate');
+        return true;
+      }),
+      {
+        plugins: [
+          fc.beforeEach(() => {
+            probes.push('beforeEach #1');
+          }),
+          fc.afterEach(() => {
+            probes.push('afterEach #2');
+          }),
+          retryTwice(),
+          fc.beforeEach(() => {
+            probes.push('beforeEach #3');
+          }),
+          fc.afterEach(() => {
+            probes.push('afterEach #4');
+          }),
+        ],
+        numRuns: 2,
+      },
+    );
+
+    // Assert
+    expect(probes).toEqual([
+      // 1st run
+      'beforeEach #1',
+      // --1st try started
+      'beforeEach #3',
+      'predicate',
+      'afterEach #4',
+      // --1st try done
+      // --2nd try started
+      'beforeEach #3',
+      'predicate',
+      'afterEach #4',
+      // --2nd try done
+      'afterEach #2',
+      // 2nd run
+      'beforeEach #1',
+      // --1st try started
+      'beforeEach #3',
+      'predicate',
+      'afterEach #4',
+      // --1st try done
+      // --2nd try started
+      'beforeEach #3',
+      'predicate',
+      'afterEach #4',
+      // --2nd try done
+      'afterEach #2',
+    ]);
+  });
 });
