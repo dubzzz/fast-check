478f20324a8cbd4b8273235e3be74cb9c31f0420
Nicolas DUBIEN <github@dubien.org>
2026-09-09 20:06:25 +0000

    fix vitest/jest

--- packages/vitest/src/internals/TestBuilder.ts
+++ packages/vitest/src/internals/TestBuilder.ts
@@ -1,4 +1,4 @@
-import { record } from 'fast-check';
+import { record, Value } from 'fast-check';
 import { buildTestWithPropRunner } from './TestWithPropRunnerBuilder.js';
 
 import type { Parameters as FcParameters, ExecutionTree, RunDetails, RunDetailsCommon, Plugin } from 'fast-check';
@@ -63,6 +63,26 @@ function adaptPluginForRecord<Ts>(plugin: Plugin<Ts>, originalParamaters: FcPara
     const instance = plugin(pluginIndex, sharedSessionContext);
     return {
       ...instance,
+      decorateGenerate:
+        instance.decorateGenerate !== undefined
+          ? (nestedGenerate) => {
+              // oxlint-disable-next-line typescript/no-non-null-assertion
+              const decorated = instance.decorateGenerate!((mrng, runId) => {
+                const out = nestedGenerate(mrng, runId);
+                if (out.hasToBeCloned) {
+                  return new Value(out.value[0], out.context, () => out.value[0]);
+                }
+                return new Value(out.value_[0], out.context);
+              });
+              return (mrng, runId) => {
+                const out = decorated(mrng, runId);
+                if (out.hasToBeCloned) {
+                  return new Value([out.value], out.context, () => [out.value]);
+                }
+                return new Value([out.value_], out.context);
+              };
+            }
+          : undefined,
       decorateRun:
         instance.decorateRun !== undefined
           ? (nestedRun) => {
