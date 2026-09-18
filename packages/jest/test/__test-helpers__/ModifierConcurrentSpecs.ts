f13b9c497220acb2cfccb22f8c68f057f803df3e
Nicolas DUBIEN <github@dubien.org>
2026-09-18 21:57:22 +0000

    drop concurrent nesting

--- packages/jest/test/__test-helpers__/ModifierConcurrentSpecs.ts
+++ packages/jest/test/__test-helpers__/ModifierConcurrentSpecs.ts
@@ -11,72 +11,67 @@ export function buildModifierConcurrentSpecsFor(runOptions: RunOptions): void {
   const { runnerName, testRunner, useWorkers } = runOptions;
   const options = { useWorkers, testRunner };
 
-  describe('.concurrent', () => {
-    it.concurrent('should pass on truthy property', async () => {
-      // Arrange
-      const specDirectory = await writeToFile(runnerName, options, () => {
-        runner.concurrent.prop([fc.constant(null)])('property pass on truthy property', (_unused) => true);
-      });
-
-      // Act
-      const out = await runSpec(specDirectory);
-
-      // Assert
-      expectPass(out);
-      expect(out).toMatch(/[√✓] property pass on truthy property \(with seed=-?\d+\)/);
+  it.concurrent('should pass on truthy property', async () => {
+    // Arrange
+    const specDirectory = await writeToFile(runnerName, options, () => {
+      runner.concurrent.prop([fc.constant(null)])('property pass on truthy property', (_unused) => true);
     });
 
-    it.concurrent('should fail on falsy property', async () => {
-      // Arrange
-      const specDirectory = await writeToFile(runnerName, options, () => {
-        runner.concurrent.prop([fc.constant(null)])('property fail on falsy property', (_unused) => false);
-      });
+    // Act
+    const out = await runSpec(specDirectory);
 
-      // Act
-      const out = await runSpec(specDirectory);
-
-      // Assert
-      expectFail(out);
-      expectAlignedSeeds(out);
-      expect(out).toMatch(/[×✕] property fail on falsy property \(with seed=-?\d+\)/);
-    });
-
-    if (testRunner === undefined) {
-      describe('.failing', () => {
-        it.concurrent('should pass because failing', async () => {
-          // Arrange
-          const specDirectory = await writeToFile(runnerName, options, () => {
-            runner.concurrent.failing.prop([fc.constant(null)])(
-              'property pass because failing',
-              async (_unused) => false,
-            );
-          });
-
-          // Act
-          const out = await runSpec(specDirectory);
-
-          // Assert
-          expectPass(out);
-          expect(out).toMatch(/[√✓] property pass because failing \(with seed=-?\d+\)/);
-        });
-
-        it.concurrent('should fail because passing', async () => {
-          // Arrange
-          const specDirectory = await writeToFile(runnerName, options, () => {
-            runner.concurrent.failing.prop([fc.constant(null)])(
-              'property fail because passing',
-              async (_unused) => true,
-            );
-          });
-
-          // Act
-          const out = await runSpec(specDirectory);
-
-          // Assert
-          expectFail(out);
-          expect(out).toMatch(/[×✕] property fail because passing \(with seed=-?\d+\)/);
-        });
-      });
-    }
+    // Assert
+    expectPass(out);
+    expect(out).toMatch(/[√✓] property pass on truthy property \(with seed=-?\d+\)/);
   });
+
+  it.concurrent('should fail on falsy property', async () => {
+    // Arrange
+    const specDirectory = await writeToFile(runnerName, options, () => {
+      runner.concurrent.prop([fc.constant(null)])('property fail on falsy property', (_unused) => false);
+    });
+
+    // Act
+    const out = await runSpec(specDirectory);
+
+    // Assert
+    expectFail(out);
+    expectAlignedSeeds(out);
+    expect(out).toMatch(/[×✕] property fail on falsy property \(with seed=-?\d+\)/);
+  });
+
+  if (testRunner === undefined) {
+    describe('.failing', () => {
+      it.concurrent('should pass because failing', async () => {
+        // Arrange
+        const specDirectory = await writeToFile(runnerName, options, () => {
+          runner.concurrent.failing.prop([fc.constant(null)])(
+            'property pass because failing',
+            async (_unused) => false,
+          );
+        });
+
+        // Act
+        const out = await runSpec(specDirectory);
+
+        // Assert
+        expectPass(out);
+        expect(out).toMatch(/[√✓] property pass because failing \(with seed=-?\d+\)/);
+      });
+
+      it.concurrent('should fail because passing', async () => {
+        // Arrange
+        const specDirectory = await writeToFile(runnerName, options, () => {
+          runner.concurrent.failing.prop([fc.constant(null)])('property fail because passing', async (_unused) => true);
+        });
+
+        // Act
+        const out = await runSpec(specDirectory);
+
+        // Assert
+        expectFail(out);
+        expect(out).toMatch(/[×✕] property fail because passing \(with seed=-?\d+\)/);
+      });
+    });
+  }
 }
